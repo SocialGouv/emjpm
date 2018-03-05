7d514bfb0b6aa54dc010c5866dc66fa2b6fbb971
Julien Bouquillon <julien@revolunet.com>
2018-03-05 23:58:32 +0100

    refacto(CodePostalMandataire): use stateless component and handle enter key

--- src/components/CodePostalMandataire.js
+++ src/components/CodePostalMandataire.js
@@ -1,46 +1,43 @@
-class CodePostalMandataire extends React.Component {
-  state = {
-    codePostal: ""
+const CodePostalMandataire = ({ findPostcode }) => {
+  let input;
+  const onKeyDown = e => {
+    if (e.keyCode === 13) {
+      submit();
+    }
   };
-  render() {
-    return (
-      <div
-        style={{
-          width: 350,
-          height: 80,
-          borderRadius: 5,
-          padding: 10,
-          backgroundColor: "#ffedad",
-          margin: "0 auto"
-        }}
-      >
-        <img
-          style={{ width: 60, float: "left" }}
-          src="/static/images/locate.svg"
-          alt="carte presentation"
+  const submit = () => findPostcode(input.value);
+  return (
+    <div
+      style={{
+        width: 350,
+        height: 80,
+        borderRadius: 5,
+        padding: 10,
+        backgroundColor: "#ffedad",
+        margin: "0 auto"
+      }}
+    >
+      <img
+        style={{ width: 60, float: "left" }}
+        src="/static/images/locate.svg"
+        alt="carte presentation"
+      />
+      <div style={{ fontSize: "1.3em", marginBottom: 5 }}>Au plus proche du majeur protégé</div>
+      <div>
+        <input
+          style={{ width: 100, textAlign: "center", display: "inline", clear: "both" }}
+          ref={node => (input = node)}
+          type="text"
+          className="form-control"
+          placeholder="Code Postal"
+          onKeyDown={onKeyDown}
         />
-        <div style={{ fontSize: "1.3em", marginBottom: 5 }}>Au plus proche du majeur protégé</div>
-        <div>
-          <input
-            style={{ width: 100, textAlign: "center", display: "inline", clear: "both" }}
-            type="text"
-            className="form-control"
-            placeholder="Code Postal"
-            value={this.state.codePostal}
-            onChange={e => this.setState({ codePostal: e.target.value })}
-          />
-          <button
-            style={{ width: 50 }}
-            type="button"
-            className="btn btn-secondary"
-            onClick={e => this.props.findPostcode(this.state.codePostal)}
-          >
-            Ok
-          </button>
-        </div>
+        <button style={{ width: 50 }} type="button" className="btn btn-secondary" onClick={submit}>
+          Ok
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default CodePostalMandataire;
