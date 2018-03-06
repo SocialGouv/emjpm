b6ec0ede8eadd51d29bf02918e622cddb95cd9e7
Adrien Gonzalez <gonzalezad1@users.noreply.github.com>
2018-03-06 10:19:46 +0100

    Merge pull request #3 from SocialGouv/dev-jb3
    Dev jb3

--- packages/eMJPM-app/src/components/TableMandataire.js
+++ packages/eMJPM-app/src/components/TableMandataire.js
@@ -3,63 +3,86 @@ import TableRowMandataire from "./TableRowMandataire";
 const TableMandataire = ({ rows, updateFilters, openModal }) => {
   return (
     <div className="row">
-      <table
-        className="table table-bordered table-striped responsive table-hover "
-        style={{ tableLayout: "fixed", backgroundColor: "#eaf4f9" }}
-      >
-        <thead>
-          <tr style={{ backgroundColor: "white", alignText: "center" }}>
-            <td style={{ border: 0, color: "white" }}>
-              <select
-                id="type"
-                className="custom-select mr-sm-2"
-                onChange={e => updateFilters({ searchType: e.target.value })}
-              >
-                <option value="">Type MJPM...</option>
-                <option value="preposes">Préposés</option>
-                <option value="prives">Individuels</option>
-                <option value="services">Services</option>
-              </select>
-            </td>
+      <div className="offset-1 col-lg-10">
+        <table
+          className="table table-bordered table-striped responsive table-hover"
+          style={{ tableLayout: "fixed" }}
+        >
+          <thead>
+            <style jsx>{`
+              input {
+                margin-bottom: 0 !important;
+              }
+              td {
+                border: 1px solid #ade8ff !important;
+              }
+            `}</style>
+            <tr style={{ backgroundColor: "#c4eeffa8" }}>
+              <td style={{ width: 180 }}>
+                <select
+                  id="type"
+                  className="custom-select mr-sm-2"
+                  style={{ marginLeft: 20 }}
+                  onChange={e => updateFilters({ searchType: e.target.value })}
+                >
+                  <option value="">Type MJPM</option>
+                  <option value="preposes">Préposés</option>
+                  <option value="prives">Individuels</option>
+                  <option value="services">Services</option>
+                </select>
+              </td>
 
-            <td>
-              <input
-                type={"text"}
-                className="form-control mb-2"
-                style={{ textAlign: "center" }}
-                placeholder={"Lieux"}
-                onChange={e => updateFilters({ searchVille: e.target.value })}
+              <td style={{ width: 200 }}>
+                <input
+                  type={"text"}
+                  className="form-control mb-2"
+                  style={{ textAlign: "center" }}
+                  placeholder={"Lieux"}
+                  onChange={e => updateFilters({ searchVille: e.target.value })}
+                />
+              </td>
+              <td>
+                <input
+                  type={"text"}
+                  style={{ textAlign: "center" }}
+                  className="form-control mb-2"
+                  placeholder={"Identité"}
+                  onChange={e => updateFilters({ searchNom: e.target.value })}
+                />
+              </td>
+              <td
+                style={{
+                  textAlign: "center",
+                  verticalAlign: "middle",
+                  width: 120
+                }}
+              >
+                Disponibilite
+              </td>
+              <td
+                style={{
+                  textAlign: "center",
+                  verticalAlign: "middle",
+                  width: 150
+                }}
+              >
+                Contact
+              </td>
+              <td style={{ width: 40 }} />
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map(mandataire => (
+              <TableRowMandataire
+                key={mandataire.properties.tel}
+                mandataire={mandataire}
+                updateFilters={this.updateFilters}
+                onClick={() => openModal(mandataire)}
               />
-            </td>
-            <td>
-              <input
-                type={"text"}
-                style={{ textAlign: "center" }}
-                className="form-control mb-2"
-                placeholder={"Identité"}
-                onChange={e => updateFilters({ searchNom: e.target.value })}
-              />
-            </td>
-            <td style={{ textAlign: "center", verticalAlign: "middle" }}>
-              Disponibilite
-            </td>
-            <td style={{ textAlign: "center", verticalAlign: "middle" }}>
-              Contact
-            </td>
-            <td />
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map(mandataire => (
-            <TableRowMandataire
-              key={mandataire.properties.tel}
-              mandataire={mandataire}
-              updateFilters={this.updateFilters}
-              onClick={() => openModal(mandataire)}
-            />
-          ))}
-        </tbody>
-      </table>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
