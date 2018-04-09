fe50d8e7a9e150c60aa9d002695ca261223c94a1
Adrien Gonzalez <adrienutbm@gmail.com>
2018-04-09 10:16:49 +0200

    seed

---
+++ packages/eMJPM-api/db/seeds/development/mandatairetis.js
@@ -0,0 +1,9 @@
+exports.seed = function(knex, Promise) {
+    return knex('mandatairetis').del() // Deletes ALL existing entries
+        .then(function() { // Inserts seed entries one by one in series
+            return knex('mandatairetis').insert({
+                ti_id: 1,
+                mandataire_id: 1,
+            });
+        });
+};
