6426adf8472114a3e732f568d498d8a80402fdb4
Julien Bouquillon <julien@revolunet.com>
2018-04-09 18:11:13 +0200

    add styled-components

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,13 @@
 import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
   render() {
     return (
       <html>
@@ -9,6 +16,7 @@ export default class MyDocument extends Document {
             font-size: 14px;
           }
         `}</style>
+        {this.props.styleTags}
         <Head>
           <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
