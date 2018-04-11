bca6829237e7e4cc75d809e61d2f35cc06466c1b
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-04-11 12:11:22 +0200

    Merge branch 'docker2'

--- packages/eMJPM-app/next.config.js
+++ packages/eMJPM-app/next.config.js
@@ -1,19 +1,22 @@
+const webpack = require("webpack");
 const withCSS = require("@zeit/next-css");
 const withImages = require("next-images");
 
-const isProduction = process.env.NODE_ENV === "production";
-const baseUrl = isProduction ? "/lebontuteur-app/" : "";
+require("dotenv").config({
+  path: process.env.NODE_ENV === "production" ? "./.env.production" : "./.env"
+});
+
+const exportPathMap = () => ({
+  "/index.html": { page: "/" },
+  "/login.html": { page: "/login" },
+  "/tis.html": { page: "/tis" }
+});
 
 module.exports = withCSS(
   withImages({
-    assetPrefix: baseUrl,
-    exportPathMap: function() {
-      return {
-        "/": { page: "/" }
-      };
-    },
+    exportPathMap,
     webpack: config => {
-      config.output.publicPath = baseUrl;
+      config.plugins.push(new webpack.EnvironmentPlugin(process.env));
       return config;
     }
   })
