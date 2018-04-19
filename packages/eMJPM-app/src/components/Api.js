1764be4c00dfa05e1bdcf94e6bc87c274d728ebb
Adrien Gonzalez <gonzalezad1@users.noreply.github.com>
2018-04-19 17:23:19 +0200

    Merge pull request #11 from SocialGouv/dev-jb2
    Dev jb2

--- packages/eMJPM-app/src/components/Api.js
+++ packages/eMJPM-app/src/components/Api.js
@@ -4,22 +4,22 @@ import fetch from "isomorphic-fetch";
 const API_URL = process.env.API_URL;
 
 const apiFetch = (route, params) =>
-    fetch(`${API_URL}/api/v1${route}`, {
-        method: "GET",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        ...params
+  fetch(`${API_URL}/api/v1${route}`, {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    ...params
+  })
+    .then(res => {
+      // intercept
+      if (res.status === 401) {
+        Router.push("/login");
+        return;
+      }
+      return res;
     })
-        .then(res => {
-            // intercept
-            if (res.status === 401) {
-                Router.push("/login");
-                return;
-            }
-            return res;
-        })
-        .then(res => res.json());
+    .then(res => res.json());
 
 export default apiFetch;
