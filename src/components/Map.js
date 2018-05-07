c3ce9ac1ceca6a23a5cc74f121346ffd4aec7a0d
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-05-07 11:52:39 +0200

    Map refactor

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,29 +1,3 @@
-// import { compose, withProps } from "recompose"
-// import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-//
-// import InfoBox from "react-google-maps/lib/components/addons/InfoBox";
-//
-//
-// const MyMapComponent = compose(
-//     withProps({
-//         googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
-//         loadingElement: <div style={{ height: `100%` }} />,
-//         containerElement: <div style={{ height: `400px` }} />,
-//         mapElement: <div style={{ height: `100%` }} />,
-//     }),
-//     withScriptjs,
-//     withGoogleMap
-// )((props) =>
-//     <GoogleMap
-//         defaultZoom={8}
-//         defaultCenter={{ lat: -34.397, lng: 150.644 }}
-//     >
-//         <Marker onClick={this.onMarkerClick}
-//                 name={'Current location'} />
-//
-//         {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-//     </GoogleMap>
-
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 export const MapsView = (mesures, zoom, center) => (
