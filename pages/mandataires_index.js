39c59bd0f80a391dc72fdc6d1a1a631ddf4b20b3
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-05-09 10:47:26 +0200

    mandataires changement on tabs

--- pages/mandataires_index.js
+++ pages/mandataires_index.js
@@ -1,16 +1,72 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import styled from "styled-components";
-
 import Navigation from "../src/components/Navigation";
 import TableMesure from "../src/components/TableMesure";
 import Footer from "../src/components/Footer";
 import FormulaireMandataire from "../src/components/formulaire_mandataire";
 import apiFetch from "../src/components/Api";
+import dynamic from "next/dynamic";
 
 const Title = styled.div`
   color: black;
   font-size: 1.5em;
 `;
+
+const OpenStreeMap = dynamic(import("../src/components/Map"), {
+  ssr: false,
+  loading: () => <div style={{ textAlign: "center", paddingTop: 20 }}>Chargement…</div>
+});
+
+const tabStyle = {
+  backgroundColor: "#ebeff2",
+  lineHeight: "50px",
+  paddingBottom: 5,
+  bottom: 0
+};
+
+const Pill = styled.p`
+  font-size: 18px;
+  width: 100px;
+  height: 28px;
+  line-height: 28px;
+  border-radius: 2px;
+  text-align: center;
+  color: white;
+  display: inline-block;
+  margin-right: 10px;
+`;
+
+const nbCol = 4;
+
+const antenne = require("../static/images/home.svg");
+const map = require("../static/images/map.svg");
+const person = require("../static/images/user.svg");
+
+const getColorFromDisponibilite = dispo => {
+  if (dispo <= 0) {
+    return "#f05659";
+  } else if (dispo <= 5) {
+    return "#eb9123";
+  }
+  return "#43b04a";
+};
+
+const getSize = nbCol => {
+  if (nbCol == 4) {
+    return "25%";
+  }
+  return "50%";
+};
+
+const PillDispo = ({ dispo, dispo_max }) => (
+  <Pill
+    style={{
+      background: getColorFromDisponibilite(dispo_max - dispo)
+    }}
+  >
+    {dispo} / {dispo_max}
+  </Pill>
+);
 
 class MandatairesIndex extends React.Component {
   state = {
@@ -71,15 +127,55 @@ class MandatairesIndex extends React.Component {
                   <Title>
                     {this.state.currentMandataire.nom} {this.state.currentMandataire.prenom}
                   </Title>
-                  <Tab style={tabStyle}>Mesures en cours</Tab>
-                  <Tab style={tabStyle}>Vos informations</Tab>
+                  {/*<Tab style={tabStyle}>Mesures en cours</Tab>*/}
+                  {/*<Tab style={tabStyle}>Vue Carte</Tab>*/}
+                  {/*<Tab style={tabStyle}>Mesure éteinte</Tab>*/}
+                  {/*<Tab style={tabStyle}>Vos informations</Tab>*/}
+                    <Tab style={{ tabStyle, verticalAlign: "middle", width: getSize(nbCol) }}>
+                        <PillDispo dispo={this.state.currentMandataire.disponibilite} dispo_max={30} />
+                        <b>Mesures en cours</b>
+                    </Tab>
+                    <Tab style={{ tabStyle, verticalAlign: "middle", width: getSize(nbCol) }}>
+                        <img src={map} style={{ padding: "5px", width: "35px ", height: "35px " }} />
+                        <b>Vue Carte</b>
+                    </Tab>
+                    <Tab style={{ tabStyle, verticalAlign: "middle", width: getSize(nbCol) }}>
+                        <PillDispo dispo={this.state.currentMandataire.disponibilite} dispo_max={30} />
+                        <b>Mesures éteintes</b>
+                    </Tab>
+                    <Tab style={{ tabStyle, verticalAlign: "middle", width: getSize(nbCol) }}>
+                        <img src={antenne} style={{ padding: "5px", width: "35px ", height: "35px " }} />
+                        <b>Vos informations</b>
+                    </Tab>
                 </div>
               </div>
             </div>
           </TabList>
-          <div className="container" style={{ backgroundColor: "white", minHeight: "70vh" }}>
+          <div
+            className="container"
+            style={{ backgroundColor: "white", minHeight: "70vh", padding: "10px" }}
+          >
             <TabPanel>
-              <TableMesure rows={filteredMesures} updateMesure={this.updateMesure} />
+              <TableMesure
+                display_ext={"none"}
+                rows={filteredMesures}
+                updateMesure={this.updateMesure}
+              />
+            </TabPanel>
+            <TabPanel>
+              <OpenStreeMap
+                width={"100%"}
+                height={"70vh"}
+                postcodeMandataire={[1, 1]}
+                mesures={filteredMesures}
+              />
+            </TabPanel>
+            <TabPanel>
+              <TableMesure
+                display={"none"}
+                rows={filteredMesures}
+                updateMesure={this.updateMesure}
+              />
             </TabPanel>
             <TabPanel>
               <div style={{ minHeight: "70vh", paddingTop: "10px" }}>
