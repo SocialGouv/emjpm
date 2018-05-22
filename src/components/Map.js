8c85559b285330b014bf5cb33c5d5db3f6b3ccf5
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-05-22 15:38:58 +0200

    refactor Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,32 +1,30 @@
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
-
-export const MapsView = ({mesures, zoom, center, width , height}) => (
-    <Map center={center} zoom={zoom} style={{width: width, height: `${height}`}}>
-      <TileLayer
-        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-      />
-      {mesures &&
-        mesures.map(manda => (
-          <Marker
-            key={manda.id}
-            position={[manda.latitude, manda.longitude]}
-            style={{ backgroundColor: "black" }}
-          >
-            <Popup>
-              <span>
-                {manda.nom} <br />
-                {manda.prenom} <br />
-                {manda.type} <br />
-                {/*{manda.date_ouverture} <br />*/}
-              </span>
-            </Popup>
-          </Marker>
-        ))};
-    </Map>
+export const MapsView = ({ mesures, zoom, center, width, height }) => (
+  <Map center={center} zoom={zoom} style={{ width, height }}>
+    <TileLayer
+      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+      url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+    />
+    {mesures &&
+      mesures.map(manda => (
+        <Marker
+          key={manda.id}
+          position={[manda.latitude, manda.longitude]}
+          style={{ backgroundColor: "black" }}
+        >
+          <Popup>
+            <span>
+              {manda.nom} <br />
+              {manda.prenom} <br />
+              {manda.type} <br />
+              {/*{manda.date_ouverture} <br />*/}
+            </span>
+          </Popup>
+        </Marker>
+      ))};
+  </Map>
 );
-
 
 class Mapstry extends React.Component {
   state = {
@@ -37,7 +35,15 @@ class Mapstry extends React.Component {
     const center = this.props.postcodeMandataire
       ? [this.props.postcodeMandataire[1], this.props.postcodeMandataire[0]]
       : [50.459441, 2.693963];
-    return <MapsView zoom={this.state.zoom} width={this.props.width} height={this.props.height} center={center} mesures={this.props.mesures} />;
+    return (
+      <MapsView
+        zoom={this.state.zoom}
+        width={this.props.width}
+        height={this.props.height}
+        center={center}
+        mesures={this.props.mesures}
+      />
+    );
   }
 }
 
