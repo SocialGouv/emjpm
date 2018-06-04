9d5a79ae279fe59cfeca408976bdff8aa9676273
Julien Bouquillon <julien@bouquillon.com>
2018-06-04 13:12:10 +0200

    tests: prettier

--- test/routes.mesures.test.js
+++ test/routes.mesures.test.js
@@ -14,46 +14,46 @@ chai.use(chaiHttp);
 passportStub.install(server);
 
 describe("routes : mesures", () => {
-    beforeEach(() => {
-        return knex.migrate
-            .rollback()
-            .then(() => {
-                return knex.migrate.latest();
+  beforeEach(() => {
+    return knex.migrate
+      .rollback()
+      .then(() => {
+        return knex.migrate.latest();
+      })
+      .then(() => {
+        return knex.seed.run();
+      });
+  });
+
+  afterEach(() => {
+    passportStub.logout();
+    return knex.migrate.rollback();
+  });
+
+  describe("GET /api/v1/mesures", () => {
+    it("should get list of mesures by mandataires", done => {
+      var agent = chai.request.agent(server);
+      agent
+        .post("/auth/login")
+        .send({
+          username: "jeremy",
+          password: "johnson123"
+        })
+        .then(function(res) {
+          return agent
+            .get("/api/v1/mesures")
+            .then(function(res) {
+              res.status.should.eql(200);
+              res.type.should.eql("application/json");
+              res.body.length.should.eql(1);
+              res.body[0].latitude.should.eql(1);
+              done();
+              // todo : check que les mandataires soient bien filtés
             })
-            .then(() => {
-                return knex.seed.run();
+            .catch(err => {
+              throw err;
             });
-    });
-
-    afterEach(() => {
-        passportStub.logout();
-        return knex.migrate.rollback();
-    });
-
-    describe("GET /api/v1/mesures", () => {
-        it("should get list of mesures by mandataires", done => {
-            var agent = chai.request.agent(server);
-            agent
-                .post("/auth/login")
-                .send({
-                    username: "jeremy",
-                    password: "johnson123"
-                })
-                .then(function(res) {
-                    return agent
-                        .get("/api/v1/mesures")
-                        .then(function(res) {
-                            res.status.should.eql(200);
-                            res.type.should.eql("application/json");
-                            res.body.length.should.eql(1);
-                            res.body[0].latitude.should.eql(1);
-                            done();
-                            // todo : check que les mandataires soient bien filtés
-                        })
-                        .catch(err => {
-                            throw err;
-                        });
-                });
         });
     });
+  });
 });
