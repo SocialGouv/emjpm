4d90e4616021b7624e2cf2e39a50a8d02e02bac4
Julien Bouquillon <contact@revolunet.com>
2018-06-12 16:38:49 +0200

    Merge pull request #30 from SocialGouv/dev-ag-mandataires-front
    Dev ag mandataires front

--- packages/eMJPM-app/stories/creationMesure.stories.js
+++ packages/eMJPM-app/stories/creationMesure.stories.js
@@ -4,7 +4,7 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 
-import { default as MesureInput, FormMesure } from "../src/components/CreationMesure";
+import { default as MesureInput, FormMesure } from "../src/components/mandataireComponents/CreationMesure";
 
 
 storiesOf("MesuresCreation", module).add("PanelCreation", () => (
