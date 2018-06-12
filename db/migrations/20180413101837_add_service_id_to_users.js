1901ecb6e7921bf814d7a2a3aec4e0e6c44c9c5e
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-06-12 15:57:02 +0200

    migratons: use knex.fn.now() + prettier

--- db/migrations/20180413101837_add_service_id_to_users.js
+++ db/migrations/20180413101837_add_service_id_to_users.js
@@ -1,9 +1,9 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.alterTable("users", function(table) {
-        table.boolean("service");
-    });
+  return knex.schema.alterTable("users", function(table) {
+    table.boolean("service");
+  });
 };
 
 exports.down = function(knex, Promise) {
-    return Promise.resolve();
+  return Promise.resolve();
 };
--- db/migrations/20180430100903_preposesEtablissements.js
+++ db/migrations/20180430100903_preposesEtablissements.js
@@ -1,17 +1,18 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('EtablissementPreposes', function(table){
-        table.increments();
-        table.string('etablissement');
-        table.string('code_postal');
-        table.string('ville');
-        table.string('telephone');
-        table.string('adresse');
-        table.integer('mandataire_id')
-            .references('id')
-            .inTable('mandataires');
-    });
+  return knex.schema.createTable("EtablissementPreposes", function(table) {
+    table.increments();
+    table.string("etablissement");
+    table.string("code_postal");
+    table.string("ville");
+    table.string("telephone");
+    table.string("adresse");
+    table
+      .integer("mandataire_id")
+      .references("id")
+      .inTable("mandataires");
+  });
 };
 
 exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('EtablissementPreposes');
+  return knex.schema.dropTable("EtablissementPreposes");
 };
