3609f9e220af059d2dfc2177ef6366f33d9b1315
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-06-14 11:26:40 +0200

    Create check date fonction

--- src/components/communComponents/checkDate.js
+++ src/components/communComponents/checkDate.js
@@ -1,8 +1,9 @@
-import { compareAsc,format, addMonths, formatDistance, formatRelative, subDays } from "date-fns";
+import { format, addMonths } from "date-fns";
 
-const checkDate = (date) => {
-  console.log("de", format(addMonths(new Date(date), 1), "MM/DD/YYYY") < format(new Date(Date.now()), "MM/DD/YYYY"));
-  if (format(addMonths(new Date(date), 1), "MM/DD/YYYY") < format(new Date(Date.now()), "MM/DD/YYYY")) {
+const checkDate = date => {
+  if (
+    format(addMonths(new Date(date), 1), "MM/DD/YYYY") < format(new Date(Date.now()), "MM/DD/YYYY")
+  ) {
     return false;
   } else {
     return true;
@@ -10,10 +11,3 @@ const checkDate = (date) => {
 };
 
 export default checkDate;
-
-// new Date( new Date(Date.now()).setMonth((new Date(Date.now()).getMonth())-2)).getMonth()
-
-// compareAsc(
-//     new Date(1987, 1, 11),
-//     new Date(1989, 6, 10)
-// )
