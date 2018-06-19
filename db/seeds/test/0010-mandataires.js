5ea87f2e1c70bb05242c638ea514e509ce3c9ee8
Swatnes64 <robin.moncoutie@orange.fr>
2018-06-19 17:59:43 +0200

    fix update seeds

--- db/seeds/test/0010-mandataires.js
+++ db/seeds/test/0010-mandataires.js
@@ -1,82 +1,58 @@
 exports.seed = function(knex, Promise) {
-  return knex("mandataires")
-    .del() // Deletes ALL existing entries
-    .then(function() {
-      // Inserts seed entries one by one in series
-      return knex("mandataires").insert({
-        etablissement: "UDAHF",
-        email: "ud@ud.com",
-        type: "preposes",
-        code_postal: "62000",
-        ville: "Arras",
-        telephone: "0237100000",
-        tribunal_instance: "arras",
-        latitude: 1,
-        longitude: 1,
-        adresse: "21 rue de houx",
-        capacite: 1,
-        disponibilite: 2,
-        dispo_max: 3,
-        curatelle: 4,
-        sauvegarde: 5,
-        curatelle_renforce: 6,
-        referent: "aucun",
-        commentaire: "specialite",
-        specilite: "personnes agees",
-        service_id: 1,
-        user_id: 1
-      });
-    })
-    .then(function() {
-      // Inserts seed entries one by one in series
-      return knex("mandataires").insert({
-        etablissement: "test2",
-        email: "panam@paris.com",
-        type: "preposes",
-        code_postal: "75000",
-        ville: "Paris",
-        telephone: "0237100000",
-        tribunal_instance: "arras",
-        latitude: 1,
-        longitude: 1,
-        adresse: "21 rue de houx",
-        capacite: 1,
-        disponibilite: 2,
-        dispo_max: 3,
-        curatelle: 4,
-        sauvegarde: 5,
-        curatelle_renforce: 6,
-        referent: "aucun",
-        commentaire: "specialite",
-        specilite: "personnes agees",
-        service_id: 1,
-        user_id: 2
-      });
-    })
-    .then(function() {
-      // Inserts seed entries one by one in series
-      return knex("mandataires").insert({
-        etablissement: "test42",
-        email: "panam@paris.com",
-        type: "preposes",
-        code_postal: "75000",
-        ville: "Paris",
-        telephone: "0237100000",
-        tribunal_instance: "paris",
-        latitude: 1,
-        longitude: 1,
-        adresse: "21 rue monge",
-        capacite: 1,
-        disponibilite: 2,
-        dispo_max: 3,
-        curatelle: 4,
-        sauvegarde: 5,
-        curatelle_renforce: 6,
-        referent: "aucun",
-        commentaire: "specialite",
-        specilite: "personnes agees",
-        service_id: 1,
-        user_id: 2
-      });
-    });
+    return knex("mandataires")
+        .del() // Deletes ALL existing entries
+        .then(function() {
+            // Inserts seed entries one by one in series
+            return knex("mandataires").insert({
+                etablissement: "UDAHF",
+                email: "ud@ud.com",
+                type: "preposes",
+                code_postal: "62000",
+                ville: "Arras",
+                telephone: "0237100000",
+                telephone_portable: "0101010101",
+                tribunal_instance: "arras",
+                latitude: 1,
+                longitude: 1,
+                adresse: "21 rue de houx",
+                capacite: 1,
+                disponibilite: 2,
+                dispo_max: 3,
+                curatelle: 4,
+                sauvegarde: 5,
+                curatelle_renforce: 6,
+                referent: "aucun",
+                commentaire: "specialite",
+                specilite: "personnes agees",
+                service_id: 1,
+                user_id: 1
+            });
+        })
+        .then(function() {
+            // Inserts seed entries one by one in series
+            return knex("mandataires").insert({
+                etablissement: "test42",
+                email: "panam@paris.com",
+                type: "preposes",
+                code_postal: "75000",
+                ville: "Paris",
+                telephone: "0237100000",
+                telephone_portable: "0101010101",
+                tribunal_instance: "paris",
+                latitude: 1,
+                longitude: 1,
+                adresse: "21 rue monge",
+                capacite: 1,
+                disponibilite: 2,
+                dispo_max: 3,
+                curatelle: 4,
+                sauvegarde: 5,
+                curatelle_renforce: 6,
+                referent: "aucun",
+                commentaire: "specialite",
+                specilite: "personnes agees",
+                service_id: 1,
+                user_id: 2
+            });
+        });
 };
