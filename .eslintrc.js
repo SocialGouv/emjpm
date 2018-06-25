a0a93a2a41c1a40e0f5d76cd9b3d998b8ef78b6b
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-06-25 18:11:23 +0200

    minimal cypress setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,31 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es6: true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  parser: "babel-eslint",
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 7,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
     },
-    parser: "babel-eslint",
-    extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
-    parserOptions: {
-        ecmaVersion: 7,
-        ecmaFeatures: {
-            experimentalObjectRestSpread: true,
-            jsx: true
-        },
-        sourceType: "module"
-    },
-    plugins: ["react", "prettier"],
-    globals: {
-        React: true,
-        process: true
-    },
-    rules: {
-        "prettier/prettier": "error",
-        "react/react-in-jsx-scope": "off",
-        "react/prop-types": "off",
-        "no-unused-vars": "warn"
-    }
+    sourceType: "module"
+  },
+  plugins: ["react", "prettier"],
+  globals: {
+    React: true,
+    process: true,
+    cy: true,
+    describe: true,
+    it: true
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "no-unused-vars": "warn"
+  }
 };
