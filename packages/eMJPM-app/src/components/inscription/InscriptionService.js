497eaa54e4d8e942915f3683c41109568c19a755
Julien Bouquillon <contact@revolunet.com>
2018-06-25 10:12:37 +0200

    Merge pull request #47 from SocialGouv/dev-inscription
    Dev inscription

--- packages/eMJPM-app/src/components/inscription/InscriptionService.js
+++ packages/eMJPM-app/src/components/inscription/InscriptionService.js
@@ -15,76 +15,82 @@ function validate(formData, errors) {
 const schema = {
   type: "object",
   required: [
-    "Susername",
-    "Spass1",
-    "Spass2",
-    "Snom",
-    "Sprenom",
-    "Stelephone",
-    "Semail",
-    "Sadresse",
-    "Sville",
-    "Scode_postal"
+    "username",
+    "pass1",
+    "pass2",
+    "etablissement",
+    "nom",
+    "prenom",
+    "telephone",
+    "email",
+    "adresse",
+    "ville",
+    "code_postal"
   ],
   properties: {
-    Susername: {
+    username: {
       type: "string",
       title: "Identifiant (vous servira pour vous connecter sur E-MJPM)",
       default: ""
     },
-    Spass1: { type: "string", title: "Mot de passe", minLength: 10 },
-    Spass2: { type: "string", title: "Répéter mot de passe", minLength: 10 },
-    Snom: { type: "string", title: "Nom du contact dans le service", default: "" },
-    Sprenom: { type: "string", title: "Prénom du contact dans le service", default: "" },
-    Stelephone: { type: "string", title: "Téléphone du contact dans le service", default: "" },
-    Semail: { type: "string", title: "Adresse email du contact dans le service", default: "" },
-    Sadresse: { type: "string", title: "Rue", default: "" },
-    Sville: { type: "string", title: "Commune", default: "" },
-    Scode_postal: { type: "string", title: "Code Postal", default: "" }
+    pass1: { type: "string", title: "Mot de passe", minLength: 10 },
+    pass2: { type: "string", title: "Répéter mot de passe", minLength: 10 },
+    etablissement: { type: "string", title: "Nom du service", default: "" },
+    nom: { type: "string", title: "Nom du contact dans le service", default: "" },
+    prenom: { type: "string", title: "Prénom du contact dans le service", default: "" },
+    telephone: { type: "string", title: "Téléphone du contact dans le service", default: "" },
+    email: { type: "string", title: "Adresse email du contact dans le service", default: "" },
+    adresse: { type: "string", title: "Rue", default: "" },
+    ville: { type: "string", title: "Commune", default: "" },
+    code_postal: { type: "string", title: "Code Postal", default: "" }
   }
 };
 
 const uiSchema = {
-  Spass1: {
-    classNames: "input_form_inscription",
+  pass1: {
+    classNames: "I_input_form_inscription",
     "ui:placeholder": "10 caratères minimum",
     "ui:widget": "password"
   },
-  Spass2: {
-    classNames: "input_form_inscription",
+  pass2: {
+    classNames: "I_input_form_inscription",
     "ui:placeholder": "10 caratères minimum",
     "ui:widget": "password"
   },
-  Susername: {
-    classNames: "input_form_inscription",
+  username: {
+    classNames: "I_input_form_inscription",
     "ui:placeholder": "Identifiant"
   },
-  Snom: {
-    classNames: "input_form_inscription",
+  nom: {
+    classNames: "I_input_form_inscription",
     "ui:placeholder": "Nom"
   },
-  Sprenom: {
-    classNames: "input_form_inscription",
+  prenom: {
+    classNames: "I_input_form_inscription",
     "ui:placeholder": "Prénom"
   },
-  Stelephone: {
-    classNames: "input_form_inscription",
+  telephone: {
+    classNames: "I_input_form_inscription",
     "ui:placeholder": "Téléphone"
   },
-  Semail: {
-    classNames: "input_form_inscription",
+  email: {
+    classNames: "I_input_form_inscription",
     "ui:placeholder": "Adresse email"
   },
-  Sadresse: {
-    classNames: "input_form_inscription",
+  adresse: {
+    classNames: "I_input_form_inscription",
     "ui:placeholder": "Rue"
   },
-  Scode_postal: {
-    classNames: "input_form_inscription",
+  code_postal: {
+    classNames: "I_input_form_inscription",
     "ui:placeholder": "Code Postal"
   },
-  Sville: {
-    classNames: "input_form_inscription",
+  etablissement: {
+    classNames: "I_input_form_inscription",
+    "ui:placeholder": "Nom du service"
+  },
+  ville: {
+    classNames: "I_input_form_inscription",
     "ui:placeholder": "Commune"
   }
 };
@@ -94,33 +100,33 @@ const formData = {};
 class InscriptionService extends React.Component {
   /*onSubmit = ({ formData }) => {
 
-		 apiFetch(`/mandataires/1`, {
+     apiFetch(`/mandataires/1`, {
       method: "PUT",
       body: JSON.stringify({
-    	SusernameS:
-    	Spass1:
-    	Spass2:
-       	SnomS:
-        SprenomS:
-        StelephoneS:
-        Stelephone_portableS:
-        SemailS:
-        SadresseS:
-        Scode_postalS:
-        SvilleS:
+      username:
+      pass1:
+      pass2:
+      etablissement:
+      nom:
+      prenom:
+      telephone:
+      telephone_portable:
+      email:
+      adresse:
+      code_postal:
+      ville:
 
       })
     }).then(json => {
      //piwik
      // this.props.updateMadataire(json);
     });
-	};*/
+  };*/
 
   render() {
     return (
       <div>
-        <br />
-        <h2 style={{ margin: 20 }}>
+        <h2 style={{ margin: "0px 20px 20px 20px" }}>
           Veuillez renseigner ci-dessous vos informations professionelles:
         </h2>
         <br />
