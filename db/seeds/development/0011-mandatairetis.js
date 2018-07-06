8b5af9f84f24c6ef676e6678e8447499d31a41f8
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-07-06 10:36:03 +0200

    Update seed for cypress

--- db/seeds/development/0011-mandatairetis.js
+++ db/seeds/development/0011-mandatairetis.js
@@ -4,14 +4,14 @@ exports.seed = function(knex, Promise) {
 		.then(function() {
 			// Inserts seed entries one by one in series
 			return knex("mandatairetis").insert({
-				ti_id: 1,
+				ti_id: 4,
 				mandataire_id: 1
 			});
 		})
 		.then(function() {
 			// Inserts seed entries one by one in series
 			return knex("mandatairetis").insert({
-				ti_id: 1,
+				ti_id: 4,
 				mandataire_id: 2
 			});
 		});
