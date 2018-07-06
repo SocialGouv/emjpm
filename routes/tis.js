07174313a1906f725ad1952f145cfcdd7c3db7b0
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-07-06 10:35:19 +0200

    Add routes for tis

---
+++ routes/tis.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var router = express.Router();
+var queries = require("../db/queries");
+
+const { loginRequired } = require("../auth/_helpers");
+
+router.get("/1/ti", loginRequired, async (req, res, next) => {
+    const mandataire = await queries.getMandataireByUserId(req.user.id);
+    queries
+        .getAllTisByMandataire(mandataire.id)
+        .then(etablissements => res.status(200).json(etablissements))
+        .catch(error => next(error));
+});
+
+
+// TODO:  Security for mandataire
+router.get("/:mandataireId/tis-by-mandataire", loginRequired, async (req, res, next) => {
+    queries
+        .getAllTisByMandataire(req.params.mandataireId)
+        .then(etablissements => res.status(200).json(etablissements))
+        .catch(error => next(error));
+});
+
+router.post("/1/tis", async (req, res, next) => {
+    // secu : ensure TI can write on this mandataire + add related test
+    const mandataire = await queries.getMandataireByUserId(req.user.id);
+    queries
+        .addMandataireTis({
+            ti_id: req.body.ti_id,
+            mandataire_id: mandataire.id
+        })
+        .then(commentaireID => queries.getAllTisByMandataire(mandataire.id))
+        .then(etablissements => res.status(200).json(etablissements))
+        .catch(error => next(error));
+});
+
+router.delete(
+    "/1/tis/:tiId",
+    loginRequired,
+    async (req, res, next) => {
+        const mandataire = await queries.getMandataireByUserId(req.user.id);
+        queries
+            .deleteMandataireTis(req.params.tiId)
+            .then(() => queries.getAllTisByMandataire(mandataire.id))
+            .then(etablissements => res.status(200).json(etablissements))
+            .catch(error => {
+                console.log(error);
+                next(error);
+            });
+    }
+);
+
+router.get("/1/tis", loginRequired, async (req, res, next) => {
+    queries
+        .getTis()
+        .then(etablissements => res.status(200).json(etablissements))
+        .catch(error => next(error));
+});
+
+
+
+
+
+
+
+module.exports = router;
