3044b9604a48cd5d5cc8f14d383c1650e51c62ed
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-07-11 11:22:17 +0200

    Fix delete Tis

--- routes/tis.js
+++ routes/tis.js
@@ -4,15 +4,6 @@ var queries = require("../db/queries");
 
 const { loginRequired } = require("../auth/_helpers");
 
-router.get("/1/ti", loginRequired, async (req, res, next) => {
-    const mandataire = await queries.getMandataireByUserId(req.user.id);
-    queries
-        .getAllTisByMandataire(mandataire.id)
-        .then(etablissements => res.status(200).json(etablissements))
-        .catch(error => next(error));
-});
-
-
 // TODO:  Security for mandataire
 router.get("/:mandataireId/tis-by-mandataire", loginRequired, async (req, res, next) => {
     queries
@@ -24,6 +15,7 @@ router.get("/:mandataireId/tis-by-mandataire", loginRequired, async (req, res, n
 router.post("/1/tis", async (req, res, next) => {
     // secu : ensure TI can write on this mandataire + add related test
     const mandataire = await queries.getMandataireByUserId(req.user.id);
+    console.log(req.body.ti_id)
     queries
         .addMandataireTis({
             ti_id: req.body.ti_id,
@@ -40,7 +32,7 @@ router.delete(
     async (req, res, next) => {
         const mandataire = await queries.getMandataireByUserId(req.user.id);
         queries
-            .deleteMandataireTis(req.params.tiId)
+            .deleteMandataireTis(req.params.tiId,mandataire.id)
             .then(() => queries.getAllTisByMandataire(mandataire.id))
             .then(etablissements => res.status(200).json(etablissements))
             .catch(error => {
@@ -50,7 +42,16 @@ router.delete(
     }
 );
 
+
 router.get("/1/tis", loginRequired, async (req, res, next) => {
+    const mandataire = await queries.getMandataireByUserId(req.user.id);
+    queries
+        .getAllTisByMandataire(mandataire.id)
+        .then(etablissements => res.status(200).json(etablissements))
+        .catch(error => next(error));
+});
+
+router.get("/tis", loginRequired, async (req, res, next) => {
     queries
         .getTis()
         .then(etablissements => res.status(200).json(etablissements))
