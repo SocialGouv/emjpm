5afc6f6e39b1d116e45eee0bcb00e0ab6a2ed2df
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-07-13 15:16:58 +0200

    Update email send after one month

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const queries = require("../db/queries");
 var csv = require("csvjson");
 // var fs = require("fs");
 const mailer = require("express-mailer");
+const { format, addMonths, addDays } = require("date-fns");
 const app = express();
 
 const nodemailer = require("nodemailer");
@@ -99,49 +100,65 @@ router.get("/", function(req, res, next) {
 
 // Generate test SMTP service account from ethereal.email
 // Only needed if you don't have a real mail account for testing
-  router.get("/email", function(req, res, next) {
-    let transporter = nodemailer.createTransport({
-      host: "in-v3.mailjet.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: "6632cabd2873712b0bafdf60d0693003",
-        pass: "5e65d90fbf59644d075ccf48e2b0ad30"
-      }
-    });
+router.get("/email", function(req, res, next) {
+  queries
+    .getMandataires()
+    .then(mandataires => res.status(200).json(mandataires))
+    .then(mandataires =>
+      mandataires.map(mandataire => {
+        if (
+          format(addMonths(new Date(mandataire.email_send), 1), "MM/DD/YYYY") <
+            format(new Date(Date.now()), "MM/DD/YYYY") &&
+          format(addDays(new Date(mandataire.updateMesure), 15), "MM/DD/YYYY") <
+            format(new Date(Date.now()), "MM/DD/YYYY")
+        ) {
+          let transporter = nodemailer.createTransport({
+            host: "in-v3.mailjet.com",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+              user: "6632cabd2873712b0bafdf60d0693003",
+              pass: "5e65d90fbf59644d075ccf48e2b0ad30"
+            }
+          });
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-      from: "gonzalez_ad@hotmail.fr", // sender address
-      to: "adrienutbm@gmail.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "Bonjour,\n" +
-      "\n" +
-      "L'équipe \"e-mjpm\" constate que vous n'avez pas actualisé vos informations durant ces 30 derniers jours. Afin que les magistrats puissent connaître en temps réel votre activité et bénéficier d'un outil d'aide à la décision performant, nous vous prions de mettre à jour vos données. Vous pouvez directement vous connecter via ce lien : https://emjpm.num.social.gouv.fr/ \n" +
-      "\n" +
-      "Pour toute difficulté, n'hésitez pas à nous contacter :\n" +
-      "\n" +
-      "contact@emjpm.beta.gouv.fr\n" +
-      "\n" +
-      "En vous remerciant de votre précieuse collaboration.\n" +
-      "\n" +
-      "Bien à vous" // html body
-    };
+          // setup email data with unicode symbols
+          let mailOptions = {
+            from: "gonzalez_ad@hotmail.fr", // sender address
+            to: "adrienutbm@gmail.com , gonzalez_ad@hotmail.fr", // list of receivers
+            subject: "e-MJPM : actualisez vos données", // Subject line
+            text: "e-MJPM : actualisez vos données", // plain text body
+            html:
+              "Bonjour,\n" +
+              "\n" +
+              "L'équipe \"e-mjpm\" constate que vous n'avez pas actualisé vos informations durant ces 30 derniers jours. Afin que les magistrats puissent connaître en temps réel votre activité et bénéficier d'un outil d'aide à la décision performant, nous vous prions de mettre à jour vos données. Vous pouvez directement vous connecter via ce lien : https://emjpm.num.social.gouv.fr/ \n" +
+              "\n" +
+              "Pour toute difficulté, n'hésitez pas à nous contacter :\n" +
+              "\n" +
+              "contact@emjpm.beta.gouv.fr\n" +
+              "\n" +
+              "En vous remerciant de votre précieuse collaboration.\n" +
+              "\n" +
+              "Bien à vous" // html body
+          };
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return console.log(error);
-      }
-      console.log("Message sent: %s", info.messageId);
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+          // send mail with defined transport object
+          transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+              return console.log(error);
+            }
+            console.log("Message sent: %s", info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
-  });
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+          });
+        }
+      })
+    )
+    .catch(error => next(error));
+});
 
 // router.get("/email", function(req, res, next) {
 //   res.mailer.send(
