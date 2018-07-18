4a64ec0138dbc02d1c517aedb3a23977d1c60f35
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-07-18 10:38:17 +0200

    Update no mesures eteintes

--- src/components/mandataireComponents/TableMesure.js
+++ src/components/mandataireComponents/TableMesure.js
@@ -79,7 +79,7 @@ export const TableMesureView = ({ rows, display, display_ext, updateMesureEteint
         </tbody>
       </table>
     ) : (
-      <div style={{ textAlign: "center", paddingTop: "35vh" }}> vous n'avez pas de mesures </div>
+      <div style={{ textAlign: "center", paddingTop: "35vh",display: display_ext }}> vous n'avez pas de mesures éteintes </div>
     )}
   </LineContainer>
 );
