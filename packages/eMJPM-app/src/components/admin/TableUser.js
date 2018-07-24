2dd9f39232a8b3879e1fc5f5b8b028743a038b89
Adrien Gonzalez <gonzalezad1@users.noreply.github.com>
2018-07-24 16:54:14 +0200

    Merge pull request #64 from SocialGouv/dev-ag-refactor-api-register-admin
    UI admin

---
+++ packages/eMJPM-app/src/components/admin/TableUser.js
@@ -0,0 +1,137 @@
+import ReactTable from "react-table";
+import format from "date-fns/format";
+import queryString from "query-string";
+
+import ToggleState from "./ToggleState";
+import SearchButton from "../communComponents/SearchButton";
+import { default as apiFetch, updateUser } from "../communComponents/Api";
+
+const RowActive = ({ active }) => (
+  <span>
+    <span
+      style={{
+        color: active ? "#57d500" : "#ff2e00"
+      }}
+    >
+      &#x25cf;
+    </span>
+  </span>
+);
+
+const RowAction = ({ row: { id, active } }) => (
+  <ToggleState
+    onToggle={active =>
+      updateUser({
+        id,
+        active
+      })}
+    active={active}
+    render={({ active }) => {
+      return (
+        <SearchButton
+          error={active}
+          style={{ textAlign: "center", fontSize: "0.8em" }}
+          type="submit"
+        >
+          {(active && "Désactiver") || "Activer"}
+        </SearchButton>
+      );
+    }}
+  />
+);
+
+const COLUMNS = [
+  {
+    Header: "ID",
+    accessor: "id",
+    width: 50,
+    show: false,
+    style: { textAlign: "center" }
+  },
+  {
+    Header: "Actif",
+    accessor: "active",
+    Cell: row => <RowActive active={row.value} />,
+    width: 70,
+    show: false, // the button show more accurate status
+    style: { textAlign: "center" }
+  },
+  {
+    Header: "Nom",
+    id: "nom",
+    accessor: d => d.nom + " " + d.prenom,
+    style: { alignSelf: "center" }
+  },
+  {
+    Header: "Code postal",
+    accessor: "code_postal",
+    width: 100,
+    style: { textAlign: "center", alignSelf: "center" }
+  },
+  {
+    Header: "Type",
+    id: "type",
+    width: 100,
+    accessor: d => d.type.toUpperCase(),
+    style: { textAlign: "center", alignSelf: "center" }
+  },
+  {
+    Header: "Création",
+    id: "created_at",
+    accessor: d => format(d.created_at, "DD/MM/YYYY"),
+    width: 120,
+    style: { textAlign: "center", alignSelf: "center" }
+  },
+  {
+    Header: "Connexion",
+    id: "last_login",
+    accessor: d => (d.last_login ? format(d.last_login, "DD/MM/YYYY") : "-"),
+    width: 120,
+    style: { textAlign: "center", alignSelf: "center" }
+  },
+  {
+    Header: "Activer",
+    Cell: row => <RowAction row={row.row} />,
+    width: 120,
+    style: { textAlign: "center", alignSelf: "center" }
+  }
+];
+
+// client side pagination
+class TableUser extends React.Component {
+  state = {
+    data: [],
+    loading: true
+  };
+  fetchData = (state, instance) => {
+    const url = `/admin/mandataires?${queryString.stringify(this.props.filters)}`;
+    this.setState({ loading: true }, () =>
+      apiFetch(url).then(res => {
+        this.setState({
+          data: res,
+          loading: false
+        });
+      })
+    );
+  };
+  render() {
+    const { data, loading } = this.state;
+    return (
+      <ReactTable
+        style={{ backgroundColor: "white" }}
+        columns={COLUMNS}
+        noDataText="Aucun mandataire ici..."
+        // manual
+        showPagination={false}
+        data={data}
+        loading={loading}
+        loadingText="Chargement des mandataires..."
+        //defaultPageSize={PAGE_SIZE}
+        onFetchData={this.fetchData}
+        className="-striped -highlight"
+      />
+    );
+  }
+}
+
+export default TableUser;
