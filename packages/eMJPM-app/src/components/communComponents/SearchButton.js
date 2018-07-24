b3793b656ebea827643520ef66a7bdec4ae8a524
Adrien Gonzalez <gonzalezad1@users.noreply.github.com>
2018-07-24 16:54:14 +0200

    Merge pull request #64 from SocialGouv/dev-ag-refactor-api-register-admin
    UI admin

--- packages/eMJPM-app/src/components/communComponents/SearchButton.js
+++ packages/eMJPM-app/src/components/communComponents/SearchButton.js
@@ -1,7 +1,21 @@
 import styled from "styled-components";
 
+const variants = {
+  default: {
+    background: "#43b04a",
+    backgroundHover: "#69a65d"
+  },
+  error: {
+    background: "#da4747",
+    backgroundHover: "#ff2e00"
+  }
+};
+
+const getColor = property => props =>
+  (variants[props.error && "error"] || variants.default)[property];
+
 export const SearchButton = styled.button`
-  background: #43b04a;
+  background: ${getColor("background")};
   height: 40px;
   width: auto;
   text-align: center;
@@ -9,7 +23,7 @@ export const SearchButton = styled.button`
   padding-left: 15px;
   padding-right: 15px;
   border: 1px solid;
-  border-color: #43b04a;
+  border-color: ${getColor("background")};
   border-radius: 3px;
   box-shadow: 2px 2px 1px #c0c0c0;
   font-size: 17px;
@@ -17,8 +31,8 @@ export const SearchButton = styled.button`
   margin: 5px;
   cursor: pointer;
   &:hover {
-    border-color: #69a65d;
-    background: #69a65d;
+    border-color: ${getColor("backgroundHover")};
+    background: ${getColor("backgroundHover")};
   }
 `;
 
