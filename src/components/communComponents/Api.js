6c173739d1c1af6df04a1e79c6d470f8ecbf918e
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-07-26 11:40:09 +0200

    feat(apiFetch): prevent useless OPTIONS requests

--- src/components/communComponents/Api.js
+++ src/components/communComponents/Api.js
@@ -3,18 +3,24 @@ import fetch from "isomorphic-fetch";
 
 const API_URL = process.env.API_URL || "http://127.0.0.1:4000";
 
-const apiFetch = (route, params, options = { forceLogin: true }) =>
-  fetch(`${API_URL}/api/v1${route}`, {
+// forceLogin: redirect user to /login when receiving a 401
+const apiFetch = (route, params, options = { forceLogin: true }) => {
+  const fetchParams = {
     method: "GET",
     credentials: "include",
-    headers: {
-      "Content-Type": "application/json"
-    },
     ...params
-  })
+  };
+  // force content-type application/json for non GET-requests
+  if (fetchParams.method && fetchParams.method !== "GET" && !fetchParams.headers) {
+    fetchParams.headers = {
+      "Content-Type": "application/json"
+    };
+  }
+  return fetch(`${API_URL}/api/v1${route}`, fetchParams)
     .then(res => {
       // intercept
       if (options.forceLogin && res.status === 401) {
+        console.log(`401 on ${route}`);
         Router.push("/login");
       }
       if (res.status === 404) {
@@ -24,14 +30,15 @@ const apiFetch = (route, params, options = { forceLogin: true }) =>
       return res;
     })
     .then(res => res && res.json());
+};
 
 // admin toggle
-// destructuration as a whitelist
+// destructuring as a whitelist
 export const updateUser = ({ id, active }) =>
   apiFetch(`/admin/user/${id}`, {
     method: "PUT",
     body: JSON.stringify({
-      active: active
+      active
     })
   }).catch(e => {
     console.log(e);
