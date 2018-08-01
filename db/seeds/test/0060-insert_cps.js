37565b6e351c189a23c814dbeea56f42c2ce73c0
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-08-01 13:55:46 +0200

    refacto: move some queries to queries/mandataires

--- db/seeds/test/0060-insert_cps.js
+++ db/seeds/test/0060-insert_cps.js
@@ -4,6 +4,7 @@ exports.seed = function(knex, Promise) {
     { id: 1, code_postal: "62000", latitude: 0.1, longitude: 0.1 },
     { id: 2, code_postal: "75000", latitude: 0.2, longitude: 0.2 },
     { id: 3, code_postal: "93000", latitude: 0.3, longitude: 0.3 },
-    { id: 4, code_postal: "62009", latitude: 0.4, longitude: 0.4 }
+    { id: 4, code_postal: "62009", latitude: 0.4, longitude: 0.4 },
+    { id: 5, code_postal: "10000", latitude: 0.5, longitude: 0.5 }
   ]);
 };
