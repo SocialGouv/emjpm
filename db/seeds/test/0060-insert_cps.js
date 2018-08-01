bc215c33f10c25b84f15b09fe84b604918840589
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-08-01 13:55:11 +0200

    tests: add cps seeds

--- db/seeds/test/0060-insert_cps.js
+++ db/seeds/test/0060-insert_cps.js
@@ -3,6 +3,7 @@ exports.seed = function(knex, Promise) {
   return knex("geolocalisation_code_postal").insert([
     { id: 1, code_postal: "62000", latitude: 0.1, longitude: 0.1 },
     { id: 2, code_postal: "75000", latitude: 0.2, longitude: 0.2 },
-    { id: 3, code_postal: "93000", latitude: 0.3, longitude: 0.3 }
+    { id: 3, code_postal: "93000", latitude: 0.3, longitude: 0.3 },
+    { id: 4, code_postal: "62009", latitude: 0.4, longitude: 0.4 }
   ]);
 };
