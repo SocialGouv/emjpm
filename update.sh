5369b4b5804ab6826ab944108a6367e7644e0439
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-08-02 11:45:40 +0200

    chore: update.sh: dont stop when no existing container

--- update.sh
+++ update.sh
@@ -10,6 +10,6 @@ npm run build
 
 sudo docker build . -t emjpm-app
 
-sudo docker rm -f emjpm-app
+sudo docker rm -f emjpm-app || true
 
 sudo docker run -d --restart always --name emjpm-app -p 80:80 -v $PWD/nginx-logs:/var/log/nginx emjpm-app
