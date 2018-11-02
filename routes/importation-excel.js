6319dab5311fc979ac30a604fd02fcacee57c8be
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-11-02 10:03:59 +0100

    Update importation

--- routes/importation-excel.js
+++ routes/importation-excel.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-const knex = require("../db/knex");
 const queries = require("../db/queries/mesures");
 
+const {
+  updateCountMesures,
+  getMandataireByUserId
+} = require("../db/queries/mandataires");
+
 router.post("/:mandataireId/files", async (req, res, next) => {
+  const mandataire = await getMandataireByUserId(req.user.id);
+
   req.body.map(datum => {
     datum["mandataire_id"] = req.user.id;
     datum["status"] = "Mesure en cours";
   });
   return queries
     .addMesure(req.body)
+    .then(() => queries.getMesuresEnCoursMandataire(mandataire.id))
+    .then(() => updateCountMesures(mandataire.id))
     .then(() => {
       return res.json({ success: "success" });
     })
