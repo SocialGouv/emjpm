e2e60c9f0ec4023b8222726980c56d0c15fcfaa4
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-11-05 09:55:56 +0100

    fix(api): fix import excel

--- routes/importation-excel.js
+++ routes/importation-excel.js
@@ -1,31 +1,42 @@
 const express = require("express");
-const router = express.Router();
+const { typeRequired } = require("../auth/_helpers");
 
 const queries = require("../db/queries/mesures");
+
+const router = express.Router();
 
 const {
   updateCountMesures,
   getMandataireByUserId
 } = require("../db/queries/mandataires");
 
-router.post("/:mandataireId/files", async (req, res, next) => {
-  const mandataire = await getMandataireByUserId(req.user.id);
-
-  req.body.map(datum => {
-    datum["mandataire_id"] = req.user.id;
-    datum["status"] = "Mesure en cours";
-  });
-  return queries
-    .addMesure(req.body)
-    .then(() => queries.getMesuresEnCoursMandataire(mandataire.id))
-    .then(() => updateCountMesures(mandataire.id))
-    .then(() => {
-      return res.json({ success: "success" });
-    })
-    .catch(e => {
-      console.log(e);
-      return res.status(500).json({ success: "noSuccess" });
-    });
-});
+router.post(
+  "/:mandataireId/bulk",
+  typeRequired("individuel", "prepose"),
+  async (req, res, next) => {
+    const mandataire = await getMandataireByUserId(req.user.id);
+    const mesures = req.body.map(datum => ({
+      ...datum,
+      mandataire_id: mandataire.id,
+      status: "Mesure en cours"
+    }));
+    return (
+      queries
+        .addMesure(mesures)
+        //.then(() => queries.getMesuresEnCoursMandataire(mandataire.id)) // ???
+        .then(() => updateCountMesures(mandataire.id))
+        .then(() =>
+          res.json({
+            success: "success",
+            message: `${req.body.length} Mesures importées`
+          })
+        )
+        .catch(e => {
+          console.log(e);
+          return res.status(500).json({ success: "noSuccess" });
+        })
+    );
+  }
+);
 
 module.exports = router;
