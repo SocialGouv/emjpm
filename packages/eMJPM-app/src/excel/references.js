acf7f2e84ba315289b904f1866076869011f59dc
Adrien Gonzalez <gonzalezad1@users.noreply.github.com>
2018-11-12 14:13:53 +0100

    Merge pull request #119 from SocialGouv/develop-jb3
    Develop jb3

---
+++ packages/eMJPM-app/src/excel/references.js
@@ -0,0 +1,34 @@
+const references = {
+  invalidCodePostal: ["75000"],
+  residence: ["A domicile", "En établissement"],
+  type: [
+    "Tutelle",
+    "Curatelle",
+    "Sauvegarde de justice",
+    "Mesure ad hoc",
+    "MAJ",
+    "tutelle aux biens",
+    "tutelle à la personne",
+    "tutelle aux biens et à la personne",
+    "curatelle simple aux biens",
+    "curatelle simple à la personne",
+    "curatelle simple aux biens et à la personne",
+    "curatelle renforcée aux biens",
+    "curatelle renforcée à la personne",
+    "curatelle renforcée aux biens et à la personne",
+    "sauvegarde de justice avec mandat spécial"
+  ],
+  civilite: ["F", "H", "Homme", "Femme"],
+  mandatoryColumns: [
+    "date_ouverture",
+    "type",
+    "code_postal",
+    "ville",
+    "civilite",
+    "annee",
+    "numero_dossier",
+    "residence"
+  ]
+};
+
+export default references;
