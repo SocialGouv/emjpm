59ae5d3f6cd730fa38dc66ae9e139c75fb5a91ac
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-12-07 14:28:04 +0100

    feat: change polyfills

--- pages/_document.js
+++ pages/_document.js
@@ -29,7 +29,7 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <meta name="description" content="" />
           <meta name="author" content="" />
-          <script src="https://cdn.polyfill.io/v2/polyfill.js?features=default-3.6,fetch,Array.prototype.find,Array.prototype.findIndex,Array.prototype.includes,set" />
+          <script src="https://cdn.polyfill.io/v2/polyfill.js?features=default,fetch,Array.prototype.find,Array.prototype.findIndex,Array.prototype.includes" />
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link rel="stylesheet" href="/static/css/react-table.css" />
           <link rel="stylesheet" href="/static/css/leaflet/leaflet.css" />
