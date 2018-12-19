60af00e25e19c8696de5113fc69b01fdecdd6d7a
gonzalezad1 <gonzalez_ad@hotmail.fr>
2018-12-19 17:33:01 +0100

    feat: update naming and add service mesure en attente onglet

--- src/components/mandataires/service.js
+++ src/components/mandataires/service.js
@@ -1,7 +1,9 @@
-import { Home } from "react-feather";
+import { Clock, Home } from "react-feather";
 import Profile from "./Profile";
 import Header from "./Header";
 import { DummyTabs } from "../index";
+import TableMesures from "./TableMesures";
+import apiFetch from "../communComponents/Api";
 
 class ServiceTabs extends React.Component {
   render() {
@@ -12,6 +14,27 @@ class ServiceTabs extends React.Component {
         url: "/mandataires/mes-infos",
         icon: <Home />,
         content: <Profile />
+      },
+      {
+        text: "Mesures en attente",
+        url: "/mandataires/mesures/en-attente",
+        icon: <Clock />,
+        content: (
+          <TableMesures
+            fetch={() => apiFetch(`/mandataires/1/mesures/attente`)}
+            hideColumns={[
+              "date_ouverture",
+              "modifier",
+              "reactiver",
+              "fin-mandat",
+              "extinction",
+              "residence",
+              "status",
+              "professionnel",
+              "valider"
+            ]}
+          />
+        )
       }
     ];
     return (
