acbbedb44c9a7238bd203018c08ed91f1c07af69
Adrien Gonzalez <gonzalezad1@users.noreply.github.com>
2019-01-10 17:05:01 +0100

    Merge pull request #154 from SocialGouv/dev-ag-cypress-correct-bug
    Dev ag cypress correct bug

--- packages/eMJPM-app/src/components/inscription/InscriptionTi.js
+++ packages/eMJPM-app/src/components/inscription/InscriptionTi.js
@@ -7,8 +7,8 @@ const schema = {
   required: ["username", "pass1", "pass2", "email"],
   properties: {
     username: { type: "string", title: "Identifiant", default: "" },
-    pass1: { type: "string", title: "Mot de passe" },
-    pass2: { type: "string", title: "Verifier le Mot de passe" },
+    pass1: { type: "string", title: "Mot de passe", minLength: 8 },
+    pass2: { type: "string", title: "Verifier le Mot de passe", minLength: 8 },
     nom: { type: "string", title: "Nom", default: "" },
     prenom: { type: "string", title: "Prenom", default: "" },
     email: { type: "string", title: "Adresse email", default: "" },
