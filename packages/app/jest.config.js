c4d6e8e1b503a1e105ec88857c2983dd19e7b85b
Douglas Duteil <douglasduteil@gmail.com>
2019-02-14 12:39:49 +0100

    refacto: run jest test again

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  collectCoverageFrom: ["src/**/*.js"],
   verbose: true,
   setupFiles: ["<rootDir>/jest.setup.js"],
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/", "<rootDir>/cypress"]
