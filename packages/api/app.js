eea27ccadf1f5964498eaee8b94f3e00c0b59cfd
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-02-14 10:46:15 +0100

    chore: cleanup app.js, remove cookie stuff

--- packages/api/app.js
+++ packages/api/app.js
@@ -1,35 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-//const logger = require("morgan");
-const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const session = require("express-session");
 const routes = require("./routes/index");
 const users = require("./routes/users");
 const authRoutes = require("./routes/auth");
 const inscriptionRoutes = require("./routes/inscription");
-const Knex = require("knex");
-const KnexSessionStore = require("connect-session-knex")(session);
 const Sentry = require("@sentry/node");
 
 const app = express();
 
-Sentry.init({
-  dsn:
-    process.env.SENTRY_PUBLIC_DSN ||
-    "https://d9ba9b75ff784cba87abd847b6162b02@sentry.num.social.gouv.fr/3"
-});
+if (process.env.SENTRY_PUBLIC_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_PUBLIC_DSN || "https://sentry.dev"
+  });
+}
 
 process.on("unhandledRejection", r => {
   // not sure this is ever reached
   Sentry.captureException(r);
   console.log("unhandledRejection", r);
 });
-
-const SECRET_KEY =
-  process.env.SECRET_KEY ||
-  "\xa9\x93v\x8b\x0cJ\xc1\x94l\x83MY\xa4\xb1\xb2\xb1\xe5\x0e\x98m\x9ee\x1a\x16";
 
 const corsOptions = {
   credentials: true,
@@ -38,44 +29,10 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(cookieParser());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// use persistents sessions in production
-if (process.env.NODE_ENV === "production") {
-  console.log("process.env.DATABASE_URL", process.env.DATABASE_URL);
-  const knex = Knex({
-    client: "pg",
-    connection: process.env.DATABASE_URL
-  });
-
-  const store = new KnexSessionStore({
-    knex
-  });
-
-  app.use(
-    session({
-      secret: SECRET_KEY,
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 4 // 4 hours
-      },
-      store: store
-    })
-  );
-} else {
-  app.use(
-    session({
-      secret: SECRET_KEY,
-      resave: false,
-      saveUninitialized: true
-    })
-  );
-}
-
 app.use(passport.initialize());
-app.use(passport.session());
 app.use("/api/v1/inscription", inscriptionRoutes);
 app.use("/api/v1", passport.authenticate("jwt", { session: false }), routes);
 app.use("/auth", authRoutes);
