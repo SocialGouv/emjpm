7cc57510c4f476cf4c51f3f95594c1e20d584ab8
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-02-18 10:34:56 +0100

    fix(tests): add GET_commentaires

---
+++ packages/api/__test__/e2e/comments/GET_commentaires.test.js
@@ -0,0 +1,51 @@
+//
+
+const request = require("supertest");
+const server = require("@emjpm/api/app");
+const knex = require("@emjpm/api/db/knex");
+
+const { getTokenByUserType } = require("../utils");
+
+afterAll(async () => {
+  await knex.destroy();
+});
+
+test("anonymous cannot read commentaires", async () => {
+  const response = await request(server).get(
+    "/api/v1/mandataires/1/commentaires"
+  );
+  expect(response.status).toBe(401);
+});
+
+const simplerComment = ({ created_at, ...props }) => props;
+
+test("ti can read own commentaires", async () => {
+  const token = await getTokenByUserType("ti");
+  const response = await request(server)
+    .get("/api/v1/mandataires/1/commentaires")
+    .set("Authorization", "Bearer " + token);
+
+  expect(response.body.map(simplerComment)).toMatchSnapshot();
+  expect(response.status).toBe(200);
+});
+
+test("ti cannot read other tis commentaires", async () => {
+  const token = await getTokenByUserType("ti");
+  const response = await request(server)
+    .get("/api/v1/mandataires/1/commentaires")
+    .set("Authorization", "Bearer " + token);
+
+  // there are 3 comments on mandataire_id=1 in the seeds, 2 from userTypes.ti
+  expect(response.body.length).toEqual(2);
+  expect(response.status).toBe(200);
+});
+
+test("user cannot read other mandataires commentaires", async () => {
+  const token = await getTokenByUserType("ti");
+  const response = await request(server)
+    .get("/api/v1/mandataires/3/commentaires")
+    .set("Authorization", "Bearer " + token);
+
+  expect(response.body.length).toEqual(0);
+  expect(response.status).toBe(200);
+});
