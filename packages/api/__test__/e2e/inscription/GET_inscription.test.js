256adee08fedf742f7d9dc8b94d371c3a112a227
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-02-18 10:34:59 +0100

    fix(tests): inscription

---
+++ packages/api/__test__/e2e/inscription/GET_inscription.test.js
@@ -0,0 +1,17 @@
+//
+
+const request = require("supertest");
+const server = require("@emjpm/api/app");
+const knex = require("@emjpm/api/db/knex");
+
+const { getTokenByUserType, shouldBeProtected } = require("../utils");
+
+afterAll(async () => {
+  await knex.destroy();
+});
+
+test("should return list of tis by region", async () => {
+  const response = await request(server).get("/api/v1/inscription/tis");
+  expect(response.body).toMatchSnapshot();
+  expect(response.status).toBe(200);
+});
