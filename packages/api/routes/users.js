65d6e572db4b3be015b94fe3cf84eface6fb65f3
gonzalezad1 <gonzalez_ad@hotmail.fr>
2019-02-25 11:01:58 +0100

    feat: add jwt limitation and ti profile

--- packages/api/routes/users.js
+++ packages/api/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 //const queries = require("../db/queries");
 
 const authHelpers = require("../auth/_helpers");
+const { getSpecificUser, updateUser } = require("../db/queries/users");
 
 // router.get("/index_users", function(req, res, next) {
 //   queries
@@ -26,5 +27,61 @@ router.get("/", authHelpers.loginRequired, (req, res, next) => {
 function handleResponse(res, code, statusMsg) {
   res.status(code).json({ status: statusMsg });
 }
+const WHITELIST = ["nom", "prenom", "email", "cabinet", "type"];
+
+const whiteList = obj =>
+  Object.keys(obj)
+    .filter(key => WHITELIST.indexOf(key) > -1)
+    .reduce((a, c) => ({ ...a, [c]: obj[c] }), {});
+
+router.put(
+  "/1",
+  authHelpers.typeRequired("ti"),
+  authHelpers.loginRequired,
+  async (req, res, next) => {
+    const { nom, prenom, email, cabinet, type } = req.body;
+
+    const user = await getSpecificUser({ id: req.user.id });
+    if (!user) {
+      return next(new Error(401));
+    }
+    const body = whiteList(req.body);
+
+    if (Object.keys(body).length === 0) {
+      res.status(200).json(user);
+      return next();
+    }
+
+    updateUser(req.user.id, {
+      nom,
+      prenom,
+      email,
+      type,
+      cabinet
+    })
+      .then(() => getSpecificUser({ id: req.user.id }))
+      .then(user => {
+        res.status(200).json(user);
+      })
+      .catch(error => {
+        console.log(error);
+        throw error;
+        next(error);
+      });
+  }
+);
+
+router.get(
+  "/1",
+  authHelpers.loginRequired,
+  authHelpers.typeRequired("ti"),
+  async (req, res, next) => {
+    const user = await getSpecificUser({ id: req.user.id });
+    if (!user) {
+      return next(new Error(401));
+    }
+    res.status(200).json(user);
+  }
+);
 
 module.exports = router;
