3d095a9730bd4dc05c57699f95024fbe46f6bc51
Douglas Duteil <douglasduteil@gmail.com>
2019-03-27 06:18:23 +0100

    test(e2e): correct tis e2e tests
    Note that to make them work, I added an hack on the viewport that we
    want to remove in the future !

--- optional/e2e/cypress/integration/tis.spec.js
+++ optional/e2e/cypress/integration/tis.spec.js
@@ -2,79 +2,92 @@ require("./utils");
 
 describe("Tis", function() {
   describe("Session Tis", () => {
+    let token;
+
     before(function() {
-      cy.loginByForm("ti1", "ti1");
+      cy.exec("npm run cypress:api-reset");
     });
 
-    beforeEach(() => {
-      cy.loginTi();
-    });
     beforeEach(function() {
-      Cypress.Cookies.preserveOnce("connect.sid");
+      if (token) {
+        localStorage.setItem("id_token", token);
+      } else {
+        cy.loginByForm("ti1", "ti1");
+      }
+    });
+
+    afterEach(function() {
+      token = token || localStorage.getItem("id_token");
+    });
+
+    beforeEach(function() {
+      cy.visit("/tis");
+
+      // ! HACK(douglasduteil): force resize to ensure to trigger new data request
+      // ! Seems like changing the screen size change the data in the list as
+      // ! it changes the size of the map...
+      cy.viewport("macbook-15");
+      // ! HACK(douglasduteil): wait for react to render...
+      // ! This is making the test less fuzzy
+      cy.wait(250);
     });
 
     context("session Tis", () => {
       describe("/tis", () => {
-        it("table should show 1 mandataires", () => {
-          cy.visit("/tis", { Authorization: "Bearer " + localStorage.getItem("id_token") });
-          cy.get(".react-tabs .rt-tr-group").should("have.length", 1);
+        it("table should show 2 mandataires", () => {
+          cy.get(".react-tabs .rt-tr-group").should("have.length", 2);
         });
-        it("counter should show 1 professionnel", () => {
-          cy.visit("/tis", { Authorization: "Bearer " + localStorage.getItem("id_token") });
+        it("counter should show 2 professionnels", () => {
           cy.get("[data-cy=Mandataires]").click();
-          cy.get(".react-tabs .rt-tr-group").should("have.length", 1);
+          cy.get(".react-tabs .rt-tr-group").should("have.length", 2);
         });
         it("can add a comments for a specific Mandataire", () => {
-          cy.visit("/tis", { Authorization: "Bearer " + localStorage.getItem("id_token") });
-          cy.get(".react-tabs .rt-tr-group:nth-child(1)").click();
-          cy.get(".form-group #root_comment").type("Hello i send you a comments");
+          cy.get(".react-tabs .rt-tr-group:nth-child(2)").click();
+          cy.get(".form-group #root_comment").type(
+            "Hello i send you a comments"
+          );
           cy.get("[data-cy=button-enregistrer-comment]").click();
           cy.get("[data-cy=tab-comment] div:nth-child(2) div").contains(
             "Hello i send you a comments"
           );
         });
+
         it("add reservation mesure", () => {
-          cy.visit("/tis", { Authorization: "Bearer " + localStorage.getItem("id_token") });
-          // hack to prevent weird initial scroll
-          //$('.ReactTable')[0].parentNode.scrollTop=0
           cy.get(
-            ".react-tabs .rt-tr-group:nth-child(1) [data-cy=button-reservation-mesure]"
+            ".react-tabs .rt-tr-group:nth-child(2) [data-cy=button-reservation-mesure]"
           ).click();
-          // cy.get('.ReactTable:first-child').parent().scrollTo('top')
           cy.get(".form-group #root_type").select("Tutelle");
           cy.get(".form-group #root_civilite").select("F");
           cy.get(".form-group #root_annee").type("1987");
           cy.get("[data-cy=button-submit-mesure]").click();
           cy.get("[data-cy=button-validation]").click();
-          cy.visit("/tis", { Authorization: "Bearer " + localStorage.getItem("id_token") });
-          cy.get(".react-tabs .rt-tr-group:nth-child(1) [data-cy=attente]").contains(1);
+          cy.get(
+            ".react-tabs .rt-tr-group:nth-child(2) [data-cy=attente]"
+          ).contains(1);
         });
 
         it("check display of the mandataire Identity", () => {
-          cy.visit("/tis", { Authorization: "Bearer " + localStorage.getItem("id_token") });
-          cy.get(".react-tabs .rt-tr-group:nth-child(1)").click();
-          cy.get("[data-cy=fiche-manda-telephone]").contains("0101010108");
+          cy.get(".react-tabs .rt-tr-group:nth-child(2)").click();
+          cy.get("[data-cy=fiche-manda-telephone]").contains(
+            "0237100000"
+          );
         });
 
         it("table should show 1 mandataires on individuel filter", () => {
-          cy.visit("/tis", { Authorization: "Bearer " + localStorage.getItem("id_token") });
           cy.get("[data-cy=tab-Individuel]").click();
           cy.get(".react-tabs .rt-tr-group").should("have.length", 1);
         });
 
-        it("table should show 0 mandataires on prepose filter", () => {
-          cy.visit("/tis", { Authorization: "Bearer " + localStorage.getItem("id_token") });
+        it("table should show 1 mandataires on prepose filter", () => {
           cy.get("[data-cy=tab-Prepose]").click();
-          cy.get(".react-tabs .rt-tr-group").should("have.length", 0);
+          cy.get(".react-tabs .rt-tr-group").should("have.length", 1);
         });
 
         it("table should show 0 mandataires on service filter", () => {
-          cy.visit("/tis", { Authorization: "Bearer " + localStorage.getItem("id_token") });
           cy.get("[data-cy=tab-Service]").click();
           cy.get(".react-tabs .rt-tr-group").should("have.length", 0);
         });
         it("table should show 1 mandataires on Lille Geolocalisation ", () => {
-          cy.visit("/tis", { Authorization: "Bearer " + localStorage.getItem("id_token") });
           cy.get("[data-cy=tab-code-postal]").type("59000");
           cy.get("[data-cy=tab-recherche]").click();
           cy.get(".react-tabs .rt-tr-group").should("have.length", 1);
