e419799e6fa55eca4a1447f39c7f8fc880a8aaa9
Douglas Duteil <douglasduteil@gmail.com>
2019-03-27 20:38:39 +0100

    fix(app): handle phone and postcode validation without alerts

--- packages/app/src/components/inscription/InscriptionIndividuel.js
+++ packages/app/src/components/inscription/InscriptionIndividuel.js
@@ -16,7 +16,7 @@ const schema = {
     "dispo_max"
   ],
   properties: {
-    email: { type: "string",format: "email",  default: "" },
+    email: { type: "string", format: "email", default: "" },
     pass1: { type: "string", minLength: 8 },
     pass2: { type: "string", minLength: 8 },
     nom: { type: "string", default: "" },
@@ -27,18 +27,46 @@ const schema = {
       enum: ["F", "H"],
       enumNames: ["Femme", "Homme"]
     },
-    telephone: { type: "string", default: "" },
-    telephone_portable: { type: "string", default: "" },
+    telephone: { type: "string", default: "", pattern: "^\\d{10}$" },
+    telephone_portable: { type: "string", default: "", pattern: "^\\d{10}$" },
 
     adresse: { type: "string", default: "" },
-    code_postal: { type: "string", default: "" },
+    code_postal: {
+      default: "",
+      pattern: "^(([0-8][0-9])|(9[0-5])|(2[AB]))[0-9]{3}$",
+      type: "string"
+    },
     ville: { type: "string", default: "" },
     dispo_max: { type: "integer", default: 0 }
   }
 };
 
+function transformErrors(errors) {
+  return errors.map(error => {
+    if (error.name === "pattern")
+      switch (error.property) {
+        case ".telephone":
+        case ".telephone_portable":
+          error.message = "Numéro de téléphone invalide";
+          break;
+
+        case ".code_postal":
+          error.message = "Code postal non valide";
+          break;
+        default:
+          break;
+      }
+    return error;
+  });
+}
+
 const InscriptionIndividuel = props => (
-  <FormMandataire schema={schema} uiSchema={uiSchema} {...props} />
+  <FormMandataire
+    schema={schema}
+    uiSchema={uiSchema}
+    transformErrors={transformErrors}
+    {...props}
+  />
 );
 
 export default InscriptionIndividuel;
