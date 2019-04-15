d6362b4f9cdca719373f4a766cadeb7b66d4ff87
Julien Bouquillon <contact@revolunet.com>
2019-04-15 14:47:05 +0200

    fix(import): add missing IE11 polyfill (#80)

--- packages/app/pages/_document.js
+++ packages/app/pages/_document.js
@@ -29,7 +29,7 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <meta name="description" content="" />
           <meta name="author" content="" />
-          <script src="https://cdn.polyfill.io/v2/polyfill.js?features=default,fetch,Array.prototype.find,Array.prototype.findIndex,Array.prototype.includes" />
+          <script src="https://cdn.polyfill.io/v2/polyfill.js?features=default,fetch,Array.prototype.find,Array.prototype.findIndex,Array.prototype.includes,Object.values" />
           <script
             type="text/javascript"
             dangerouslySetInnerHTML={{
