4ded75c3be825991d4fa8ccb41d5a386a3b069a3
kwiss-macbook <christophe@hooraylabs.com>
2019-05-22 17:42:49 +0200

    fix(lint): add lint and prettier cmd

--- packages/api/.eslintrc.js
+++ packages/api/.eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-    env: {
-        browser: false,
-        commonjs: true,
-        es6: true,
-        node: true,
-        mocha: true
+  env: {
+    browser: false,
+    commonjs: true,
+    es6: true,
+    node: true,
+    mocha: true
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 7,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
     },
-    extends: ["eslint:recommended", "plugin:prettier/recommended"],
-    parserOptions: {
-        ecmaVersion: 7,
-        ecmaFeatures: {
-            experimentalObjectRestSpread: true,
-            jsx: true
-        },
-        sourceType: "module"
-    },
-    plugins: ["prettier"],
-    globals: {},
-    rules: {
-        "prettier/prettier": "error",
-        "no-unused-vars": "warn"
-    }
+    sourceType: "module"
+  },
+  plugins: ["prettier"],
+  globals: {},
+  rules: {
+    "prettier/prettier": "error",
+    "no-unused-vars": "warn"
+  }
 };
