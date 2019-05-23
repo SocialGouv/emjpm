2c853f50040fc1ea4de3bc0d5718b5932e440cf1
Douglas Duteil <douglasduteil@gmail.com>
2019-05-23 23:50:01 +0200

    test(api): sort access chack map by url key
    Thanks to https://codeshack.io/json-sorter/

--- packages/api/__test__/e2e/routes.test.js
+++ packages/api/__test__/e2e/routes.test.js
@@ -1,3 +1,4 @@
+//
 
 const { knex } = global;
 jest.setMock("@emjpm/api/db/knex", knex);
@@ -12,49 +13,6 @@ beforeAll(async () => {
 });
 
 const accessChecks = [
-  {
-    method: "PUT",
-    url: "/api/v1/mandataires/1/mesures/1",
-    type: ["mandataire", "ti", "service"]
-  },
-  {
-    method: "POST",
-    url: "/api/v1/mandataires/1/mesure-reservation",
-    type: ["ti"]
-  },
-  {
-    method: "POST",
-    url: "/api/v1/mandataires/1/mesures",
-    type: ["mandataire", "ti", "service"]
-  },
-  { method: "DELETE", url: "/api/v1/mandataires/1/commentaires/1", type: "ti" },
-  { method: "GET", url: "/api/v1/mandataires/1/commentaires", type: "ti" },
-  { method: "POST", url: "/api/v1/mandataires/1/commentaires", type: "ti" },
-  {
-    method: "POST",
-    url: "/api/v1/mandataires/mesures/bulk",
-    type: ["mandataire", "service"]
-  },
-  {
-    method: "GET",
-    url: "/api/v1/mandataires/1/mesures",
-    type: ["mandataire", "service"]
-  },
-  {
-    method: "GET",
-    url: "/api/v1/mandataires/1/mesuresForMaps",
-    type: ["mandataire", "service"]
-  },
-  {
-    method: "GET",
-    url: "/api/v1/mandataires/1/mesures/attente",
-    type: ["mandataire", "service"]
-  },
-  {
-    method: "GET",
-    url: "/api/v1/mandataires/1/mesures/Eteinte",
-    type: ["mandataire", "service"]
-  },
   {
     method: "GET",
     url: "/api/v1/mandataires",
@@ -71,29 +29,89 @@ const accessChecks = [
     type: ["mandataire", "service"]
   },
   {
-    method: "POST",
-    url: "/api/v1/mandataires/filters",
-    type: "ti"
-  },
-  {
-    method: "GET",
-    url: "/api/v1/mandataires/services",
-    type: "ti"
-  },
-  {
-    method: "GET",
-    url: "/api/v1/mandataires/services",
-    type: "ti"
-  },
-  {
     method: "PUT",
     url: "/api/v1/mandataires/1/capacite",
+    type: ["mandataire", "service"]
+  },
+  {
+    method: "GET",
+    url: "/api/v1/mandataires/1/commentaires",
+    type: "ti"
+  },
+  {
+    method: "POST",
+    url: "/api/v1/mandataires/1/commentaires",
+    type: "ti"
+  },
+  {
+    method: "DELETE",
+    url: "/api/v1/mandataires/1/commentaires/1",
+    type: "ti"
+  },
+  {
+    method: "POST",
+    url: "/api/v1/mandataires/1/mesure-reservation",
+    type: ["ti"]
+  },
+  {
+    method: "POST",
+    url: "/api/v1/mandataires/1/mesures",
+    type: ["mandataire", "ti", "service"]
+  },
+  {
+    method: "GET",
+    url: "/api/v1/mandataires/1/mesures",
     type: ["mandataire", "service"]
   },
   {
     method: "PUT",
     url: "/api/v1/mandataires/1/mesures-en-attente",
     type: ["mandataire", "ti", "service"]
+  },
+  {
+    method: "PUT",
+    url: "/api/v1/mandataires/1/mesures/1",
+    type: ["mandataire", "ti", "service"]
+  },
+  {
+    method: "GET",
+    url: "/api/v1/mandataires/1/mesures/Eteinte",
+    type: ["mandataire", "service"]
+  },
+  {
+    method: "GET",
+    url: "/api/v1/mandataires/1/mesures/attente",
+    type: ["mandataire", "service"]
+  },
+  {
+    method: "GET",
+    url: "/api/v1/mandataires/1/mesuresForMaps",
+    type: ["mandataire", "service"]
+  },
+  {
+    method: "POST",
+    url: "/api/v1/mandataires/filters",
+    type: "ti"
+  },
+  {
+    method: "POST",
+    url: "/api/v1/mandataires/mesures/bulk",
+    type: ["mandataire", "service"]
+  },
+  {
+    method: "GET",
+    url: "/api/v1/mandataires/services",
+    type: "ti"
+  },
+  {
+    method: "GET",
+    url: "/api/v1/mandataires/services",
+    type: "ti"
+  },
+  {
+    method: "GET",
+    url: "/api/v1/mesures",
+    type: "ti"
   }
 ];
 
