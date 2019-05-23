49485b87a1d4ced349545acee4bc0442ae00b708
Douglas Duteil <douglasduteil@gmail.com>
2019-05-23 21:04:35 +0200

    ci(gitlab): try to fixing the coverage spamming

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   collectCoverageFrom: ["src/**/*.js"],
-  verbose: true,
-  collectCoverageFrom: ["src/**/*.js"],
+  coverageDirectory: process.env.JEST_COVERAGE_DIRECTORY,
   setupFiles: ["<rootDir>/jest.setup.js"],
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/", "<rootDir>/cypress"]
 };
