30475279d2ef893196b9f9e629ad81a0bae5643e
Douglas Duteil <douglasduteil@gmail.com>
2019-05-24 13:41:25 +0200

    test(api): add /api/v1/admin/* tests (#229)

---
+++ packages/api/__test__/e2e/admin/GET_tis.test.js
@@ -0,0 +1,31 @@
+//
+
+const request = require("supertest");
+
+const { knex } = global;
+jest.setMock("@emjpm/api/db/knex", knex);
+
+const server = require("@emjpm/api/app");
+
+const { getTokenByUserType } = require("../utils");
+
+//
+
+beforeAll(async () => {
+  await knex.migrate.latest();
+  await knex.seed.run();
+});
+
+// strip the created_at date in the response
+const simpler = ({ created_at, ...props }) => props;
+
+test("should GET tis", async () => {
+  const token = await getTokenByUserType("admin");
+  const response = await request(server)
+    .get("/api/v1/admin/tis")
+    .set("Authorization", "Bearer " + token);
+
+  expect(response.body.map(simpler)).toMatchSnapshot();
+
+  expect(response.status).toBe(200);
+});
