b68e108a447fc348fba054239b1d6e55646a5da1
kwiss <mr.dumont.christophe@gmail.com>
2019-05-27 18:22:26 +0200

    fix(next-config): remove color comment

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -16,13 +16,7 @@ module.exports = withTM(
         SENTRY_PUBLIC_DSN: process.env.SENTRY_PUBLIC_DSN || "https://sentry.dev",
         API_URL: process.env.API_URL || "http://127.0.0.1:4000"
       },
-      transpileModules: [
-        // "color",
-        "strict-uri-encode",
-        "decode-uri-component",
-        "query-string",
-        "fuse.js"
-      ]
+      transpileModules: ["strict-uri-encode", "decode-uri-component", "query-string", "fuse.js"]
     })
   )
 );
