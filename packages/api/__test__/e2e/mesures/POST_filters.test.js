deab0ee71881746900ca32eeec05cee6b3b3fd40
Douglas Duteil <douglasduteil@gmail.com>
2019-05-27 18:38:29 +0200

    test(api): add /api/v1/mesures/* tests (#240)

---
+++ packages/api/__test__/e2e/mesures/POST_filters.test.js
@@ -0,0 +1,37 @@
+//
+
+const request = require("supertest");
+
+const { knex } = global;
+jest.setMock("@emjpm/api/db/knex", knex);
+
+const server = require("@emjpm/api/app");
+
+const { getTokenByUserType } = require("../utils");
+
+//
+
+beforeAll(async () => {
+  await knex.migrate.latest();
+  await knex.seed.run();
+});
+
+// strip the created_at date in the response
+const simpler = ({ created_at, ...props }) => props;
+
+test("should get mesures filtered by localisation", async () => {
+  const token = await getTokenByUserType("ti");
+  const response = await request(server)
+    .post("/api/v1/mesures/filters")
+    .set("Authorization", "Bearer " + token)
+    .send({
+      latNorthEast: 1,
+      latSouthWest: 0,
+      longNorthEast: 1,
+      longSouthWest: 0
+    });
+
+  expect(response.body.map(simpler)).toMatchSnapshot();
+
+  expect(response.status).toBe(200);
+});
