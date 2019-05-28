f43b03bcaf3f796ecf1541432f3ebfec5417da9c
gonzalezad1 <gonzalez_ad@hotmail.fr>
2019-05-28 16:00:43 +0200

    fix: code review

--- packages/knex/migrations/20190527173248_migration_status_mesures.js
+++ packages/knex/migrations/20190527173248_migration_status_mesures.js
@@ -1,46 +1,31 @@
+const nomination = {
+  "tutelle aux biens": "Tutelle aux biens",
+  "tutelle à la personne": "Tutelle à la personne",
+  "tutelle aux biens et à la personne": "Tutelle aux biens et à la personne",
+  "curatelle simple aux biens": "Curatelle simple aux biens",
+  "curatelle simple à la personne": "Curatelle simple à la personne",
+  "curatelle simple aux biens et à la personne":
+    "Curatelle simple aux biens et à la personne",
+  "curatelle renforcée aux biens": "Curatelle renforcée aux biens",
+  "curatelle renforcée aux biens et à la personne":
+    "Curatelle renforcée aux biens et à la personne",
+  "sauvegarde de justice": "Sauvegarde de justice",
+  "curatelle renforcée à la personne": "Curatelle renforcée à la personne",
+  "sauvegarde de justice avec mandat spécial":
+    "Sauvegarde de justice avec mandat spécial"
+};
+
 exports.up = function(knex, Promise) {
   const mesures = knex("mesures");
-
   return mesures.then(mesures =>
     Promise.all(
       mesures.map(mesure => {
         let newNames = "";
-        switch (mesure.type) {
-          case "tutelle aux biens":
-            newNames = "Tutelle aux biens";
-            break;
-          case "tutelle à la personne":
-            newNames = "Tutelle à la personne";
-            break;
-          case "tutelle aux biens et à la personne":
-            newNames = "Tutelle aux biens et à la personne";
-            break;
-          case "curatelle simple aux biens":
-            newNames = "Curatelle simple aux biens";
-            break;
-          case "curatelle simple à la personne":
-            newNames = "Curatelle simple à la personne";
-            break;
-          case "curatelle simple aux biens et à la personne":
-            newNames = "Curatelle simple aux biens et à la personne";
-            break;
-          case "curatelle renforcée aux biens":
-            newNames = "Curatelle renforcée aux biens";
-            break;
-          case "curatelle renforcée aux biens et à la personne":
-            newNames = "Curatelle renforcée aux biens et à la personne";
-            break;
-          case "sauvegarde de justice":
-            newNames = "Sauvegarde de justice";
-            break;
-          case "curatelle renforcée à la personne":
-            newNames = "Curatelle renforcée à la personne";
-            break;
-          case "sauvegarde de justice avec mandat spécial":
-            newNames = "Sauvegarde de justice avec mandat spécial";
-            break;
-          default:
-            newNames = mesure.type;
+
+        if (mesure.type in nomination) {
+          newNames = nomination[mesure.type];
+        } else {
+          newNames = mesure.type;
         }
         return knex("mesures")
           .where({ "mesures.id": mesure.id })
@@ -53,54 +38,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  const mesures = knex("mesures");
-
-  return mesures.then(mesures => {
-    Promise.all(
-      mesures.map(mesure => {
-        let newNames = "";
-        switch (mesure.type) {
-          case "Tutelle aux biens":
-            newNames = "tutelle aux biens";
-            break;
-          case "Tutelle à la personne":
-            newNames = "tutelle à la personne";
-            break;
-          case "Tutelle aux biens et à la personne":
-            newNames = "tutelle aux biens et à la personne";
-            break;
-          case "Curatelle simple aux biens":
-            newNames = "curatelle simple aux biens";
-            break;
-          case "Curatelle simple à la personne":
-            newNames = "curatelle simple à la personne";
-            break;
-          case "Curatelle simple aux biens et à la personne":
-            newNames = "curatelle simple aux biens et à la personne";
-            break;
-          case "Curatelle renforcée aux biens":
-            newNames = "curatelle renforcée aux biens";
-            break;
-          case "Curatelle renforcée aux biens et à la personne":
-            newNames = "curatelle renforcée aux biens et à la personne";
-            break;
-          case "Sauvegarde de justice":
-            newNames = "sauvegarde de justice";
-            break;
-          case "Sauvegarde de justice avec mandat spécial":
-            newNames = "sauvegarde de justice avec mandat spécial";
-            break;
-          default:
-            console.log(
-              "we haven't found " + mesure.type + " with id " + mesure.id + "."
-            );
-        }
-        knex("mesures")
-          .where("mesures.id", mesure.id)
-          .update({
-            type: newNames
-          });
-      })
-    );
-  });
+  return Promise.resolve();
 };
