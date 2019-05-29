ed7c9b64d9fedfee027c7c91c3e7c13dcd8ae5da
kwiss <mr.dumont.christophe@gmail.com>
2019-05-29 09:37:17 +0200

    chore(lint): add tslint-config-prettier
    fix(lint-jest-environnement-knex): fix tslint errors
    
    fix(jest-environnement-knex): array typing
    
    fix(typescript): fix compilation errors

--- packages/jest-environment-knex/src/jest-environment-knex.ts
+++ packages/jest-environment-knex/src/jest-environment-knex.ts
@@ -1,31 +1,34 @@
 //
 
-import { Script, runInContext } from "vm";
-import NodeEnvironment from "jest-environment-node";
-import { Global, Config } from "@jest/types";
-import Knex from "knex";
-const parseConnection = require("knex/lib/util/parse-connection");
-const debug = require("debug")("jest-environment-knex");
-const { uid } = require("rand-token");
+import { Config, Global } from '@jest/types';
+import NodeEnvironment from 'jest-environment-node';
+import { runInContext, Script } from 'vm';
+
+import Debug from 'debug';
+const debug = Debug('jest-environment-knex');
+import Knex from 'knex';
+/* tslint:disable:no-var-requires */
+const parseConnection = require('knex/lib/util/parse-connection');
+const { uid } = require('rand-token');
+/* tslint:enable:no-var-requires */
 
 //
 
 class KnexEnvironment extends NodeEnvironment {
-  destroyPromises: Promise<void>[] = [];
-
-  global: Global.Global & { databaseName: string; knex: Knex };
-  options: Knex.Config;
+  public global: Global.Global & { databaseName: string; knex: Knex };
+  private destroyPromises: any = [];
+  private options: Knex.Config;
 
   constructor(config: Config.ProjectConfig) {
     super(config);
-    debug("super(config)");
+    debug('super(config)');
 
     //
 
-    const global = (this.global = runInContext(
-      "this",
-      Object.assign(this.context, config.testEnvironmentOptions)
-    ));
+    const global = (this.global = runInContext('this', {
+      ...this.context,
+      ...config.testEnvironmentOptions,
+    }));
     global.databaseName = `emjpm_test_${uid(16).toLowerCase()}`;
 
     //
@@ -33,56 +36,47 @@ class KnexEnvironment extends NodeEnvironment {
     this.options = config.testEnvironmentOptions;
   }
 
-  lazyDestroy(knex: Knex) {
-    this.destroyPromises.push(
-      ((knex.destroy() as any) as Promise<void>).then(
-        debug.bind(null, "knex.destroyed"),
-        console.error
-      )
-    );
-  }
-
-  async setup() {
-    debug("setup");
+  public async setup() {
+    debug('setup');
     await super.setup();
-    debug("super.setup()");
+    debug('super.setup()');
 
     //
 
-    debug("new Knex");
+    debug('new Knex');
     const knex = Knex(this.options);
     debug(`await knex.raw(CREATE DATABASE ${this.global.databaseName});`);
     await knex.raw(`CREATE DATABASE ${this.global.databaseName};`);
-    debug("knex.destroy()");
+    debug('knex.destroy()');
     this.lazyDestroy(knex);
 
     //
 
-    const connection: Object = typeof this.options.connection === 'string' ?
-      parseConnection(this.options.connection) :
-      this.options.connection;
+    const connection: {} =
+      typeof this.options.connection === 'string'
+        ? parseConnection(this.options.connection)
+        : this.options.connection;
 
     this.global.knex = Knex({
       ...this.options,
       connection: {
         ...connection,
-        database: this.global.databaseName
-      }
+        database: this.global.databaseName,
+      },
     });
-    debug("setup");
+    debug('setup');
   }
-
-  async teardown() {
-    debug("teardown");
+  public async teardown() {
+    debug('teardown');
     this.lazyDestroy(this.global.knex);
 
     //
 
-    debug("new Knex");
+    debug('new Knex');
     const knex = Knex(this.options);
     debug(`await knex.raw(DROP DATABASE ${this.global.databaseName});`);
     await knex.raw(`DROP DATABASE ${this.global.databaseName};`);
-    debug("knex.destroy()");
+    debug('knex.destroy()');
     this.lazyDestroy(knex);
 
     //
@@ -91,13 +85,23 @@ class KnexEnvironment extends NodeEnvironment {
 
     //
 
-    debug("super.teardown()");
+    debug('super.teardown()');
     await super.teardown();
-    debug("teardown");
+    debug('teardown');
   }
 
-  runScript(script: Script) {
+  public runScript(script: Script) {
     return super.runScript(script);
+  }
+
+  private lazyDestroy(knex: Knex) {
+    this.destroyPromises.push(
+      ((knex.destroy() as any) as Promise<void>).then(
+        debug.bind(null, 'knex.destroyed'),
+        /* tslint:disable:no-console */
+        console.error,
+      ),
+    );
   }
 }
 
