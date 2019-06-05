aef6ff1d60388fc1ad9aa2c196b46e4a5a460319
christophe <christophe@hooraylabs.com>
2019-06-05 14:35:15 +0200

    style(jest-environnement-knex): lint the source files (#253)

--- packages/jest-environment-knex/src/jest-environment-knex.ts
+++ packages/jest-environment-knex/src/jest-environment-knex.ts
@@ -1,20 +1,23 @@
 //
 
-import { Script, runInContext } from "vm";
+import { Config, Global } from "@jest/types";
+import Debug from "debug";
 import NodeEnvironment from "jest-environment-node";
-import { Global, Config } from "@jest/types";
 import Knex from "knex";
-const parseConnection = require("knex/lib/util/parse-connection");
-const debug = require("debug")("jest-environment-knex");
-const { uid } = require("rand-token");
+import parseConnection from "knex/lib/util/parse-connection";
+import { uid } from "rand-token";
+import { runInContext, Script } from "vm";
+
+//
+
+const debug = Debug("jest-environment-knex");
 
 //
 
 class KnexEnvironment extends NodeEnvironment {
-  destroyPromises: Promise<void>[] = [];
-
-  global: Global.Global & { databaseName: string; knex: Knex };
-  options: Knex.Config;
+  public global: Global.Global & { databaseName: string; knex: Knex };
+  private destroyPromises: any = [];
+  private options: Knex.Config;
 
   constructor(config: Config.ProjectConfig) {
     super(config);
@@ -24,6 +27,15 @@ class KnexEnvironment extends NodeEnvironment {
 
     const global = (this.global = runInContext(
       "this",
+
+      // NOTE(douglasduteil): don't replace `Object.assign` by object spreading
+      //
+      // It's important to not spread the context so it keeps its object type
+      // and doesn't become an plain object.
+      // vm.runInContext requires the context to be of type vm.Context
+      // see https://nodejs.org/dist/latest-v10.x/docs/api/vm.html#vm_vm_runincontext_code_contextifiedsandbox_options
+      //
+      // tslint:disable-next-line: prefer-object-spread
       Object.assign(this.context, config.testEnvironmentOptions)
     ));
     global.databaseName = `emjpm_test_${uid(16).toLowerCase()}`;
@@ -33,16 +45,7 @@ class KnexEnvironment extends NodeEnvironment {
     this.options = config.testEnvironmentOptions;
   }
 
-  lazyDestroy(knex: Knex) {
-    this.destroyPromises.push(
-      ((knex.destroy() as any) as Promise<void>).then(
-        debug.bind(null, "knex.destroyed"),
-        console.error
-      )
-    );
-  }
-
-  async setup() {
+  public async setup() {
     debug("setup");
     await super.setup();
     debug("super.setup()");
@@ -58,9 +61,10 @@ class KnexEnvironment extends NodeEnvironment {
 
     //
 
-    const connection: Object = typeof this.options.connection === 'string' ?
-      parseConnection(this.options.connection) :
-      this.options.connection;
+    const connection: {} =
+      typeof this.options.connection === "string"
+        ? parseConnection(this.options.connection)
+        : this.options.connection;
 
     this.global.knex = Knex({
       ...this.options,
@@ -71,8 +75,7 @@ class KnexEnvironment extends NodeEnvironment {
     });
     debug("setup");
   }
-
-  async teardown() {
+  public async teardown() {
     debug("teardown");
     this.lazyDestroy(this.global.knex);
 
@@ -96,8 +99,18 @@ class KnexEnvironment extends NodeEnvironment {
     debug("teardown");
   }
 
-  runScript(script: Script) {
+  public runScript(script: Script) {
     return super.runScript(script);
+  }
+
+  private lazyDestroy(knex: Knex) {
+    this.destroyPromises.push(
+      ((knex.destroy() as any) as Promise<void>).then(
+        debug.bind(null, "knex.destroyed"),
+        /* tslint:disable:no-console */
+        console.error
+      )
+    );
   }
 }
 
