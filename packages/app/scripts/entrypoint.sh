7d0c04c743f22328f91a063296e344cd23918b30
Douglas Duteil <douglasduteil@gmail.com>
2019-06-06 13:53:37 +0200

    build(docker): sed the frontend artifact to config the API_URL

---
+++ packages/app/scripts/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+ROOT_DIRECTORY="/www"
+
+##############################################################
+# Step 1 : replace default environment values in .html files #
+##############################################################
+
+# Save env variable in file
+printenv >> env-vars
+
+while IFS='=' read -r KEY VALUE
+
+do
+    match="%%$KEY%%"
+    repl="$VALUE"
+
+    # replace default environment variables value
+    sed -i -e "s|$match|$repl|" $ROOT_DIRECTORY/*.html
+    sed -i -e "s|$match|$repl|" $ROOT_DIRECTORY/**/index.html
+done <env-vars
+
+rm env-vars
+
+#########################
+# Step 2 : Start NGINX  #
+#########################
+
+exec nginx -g 'daemon off;'
