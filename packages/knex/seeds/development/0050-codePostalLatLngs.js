b1297e5c4191163c11a573856f89e21e294f8b33
kwiss-macbook <christophe@hooraylabs.com>
2019-06-11 15:07:09 +0200

    fix(postal-code): fix import file

--- packages/knex/seeds/development/0050-codePostalLatLngs.js
+++ packages/knex/seeds/development/0050-codePostalLatLngs.js
@@ -1,36 +1,25 @@
 var zipCodes = require("../postal.json");
 
-const getCitiesForZipCode = currentZipcode => {
-  const array = [];
-  zipCodes.filter(e => {
-    if (e.fields.code_postal === currentZipcode) {
-      array.push(e.fields.nom_commune);
-    }
-  });
-  return array;
-};
-
 const removeUnique = (arr, key) => {
   const unique = arr
     .map(e => e[key])
     .map((e, i, final) => final.indexOf(e) === i && i)
     .filter(e => arr[e])
     .map(e => arr[e]);
-
   return unique;
 };
 
 exports.seed = knex => {
   const newRows = zipCodes.map(row => {
-    const zipCodeCities = getCitiesForZipCode(row.fields.code_postal);
     return {
-      code_postal: row.fields.code_postal,
-      latitude: row.fields.coordonnees_gps[0],
-      longitude: row.fields.coordonnees_gps[1],
-      cities: zipCodeCities,
-      code_insee: row.fields.code_commune_insee
+      code_postal: row.code_postal,
+      latitude: parseFloat(row.latitude),
+      longitude: parseFloat(row.longitude),
+      cities: row.cities,
+      code_insee: row.code_commune_INSEE
     };
   });
+
   return knex.transaction(tr => {
     return knex
       .table("geolocalisation_code_postal")
