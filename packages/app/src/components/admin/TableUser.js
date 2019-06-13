363bcc01d0afccee31349dc97ec1f9dafdc3fee7
Douglas Duteil <douglasduteil@gmail.com>
2019-06-13 19:27:45 +0200

    Revert "fix(app): revert to TableTi lint to minimum changes"
    This reverts commit b5183fdff877c050731e5a7e11c13d1c8f76dca8.

--- packages/app/src/components/admin/TableUser.js
+++ packages/app/src/components/admin/TableUser.js
@@ -3,46 +3,52 @@ import ReactTable from "react-table";
 import format from "date-fns/format";
 import queryString from "query-string";
 
-import ToggleState from "../common/ToggleState";
-import SearchButton from "../communComponents/SearchButton";
-import { default as apiFetch, updateUser } from "../communComponents/Api";
+// TODO: Should we remove that ?
+// import ToggleState from "../common/ToggleState";
+// import SearchButton from "../communComponents/SearchButton";
+import { default as apiFetch } from "../communComponents/Api";
 
-const CellActive = ({ active }) => (
-  <span>
-    <span
-      style={{
-        color: active ? "#57d500" : "#ff2e00"
-      }}
-    >
-      &#x25cf;
+const CellActive = ({ active }) => {
+  return (
+    <span>
+      <span
+        style={{
+          color: active ? "#57d500" : "#ff2e00"
+        }}
+      >
+        &#x25cf;
+      </span>
     </span>
-  </span>
-);
+  );
+};
 
-const CellAction = ({ row: { id, active } }) => (
-  <ToggleState
-    getPromise={active =>
-      updateUser({
-        id,
-        active
-      })
-    }
-    active={active}
-    render={({ active, toggle }) => {
-      return (
-        <SearchButton
-          onClick={toggle}
-          error={active}
-          data-cy="UserCellAction"
-          style={{ textAlign: "center", fontSize: "0.8em" }}
-          type="submit"
-        >
-          {(active && "Désactiver") || "Activer"}
-        </SearchButton>
-      );
-    }}
-  />
-);
+CellActive.displayName = "CellActive";
+
+// TODO: Should we remove that ?
+// const CellAction = ({ row: { id, active } }) => (
+//   <ToggleState
+//     getPromise={active =>
+//       updateUser({
+//         id,
+//         active
+//       })
+//     }
+//     active={active}
+//     render={({ active, toggle }) => {
+//       return (
+//         <SearchButton
+//           onClick={toggle}
+//           error={active}
+//           data-cy="UserCellAction"
+//           style={{ textAlign: "center", fontSize: "0.8em" }}
+//           type="submit"
+//         >
+//           {(active && "Désactiver") || "Activer"}
+//         </SearchButton>
+//       );
+//     }}
+//   />
+// );
 
 const COLUMNS = [
   {
@@ -55,8 +61,9 @@ const COLUMNS = [
   {
     Header: "Actif",
     accessor: "active",
-    // eslint-disable-next-line react/display-name
-    Cell: row => <CellActive active={row.value} />,
+    Cell(row) {
+      <CellActive active={row.value} />;
+    },
     width: 70,
     show: false, // the button show more accurate status
     style: { textAlign: "center" }
@@ -105,8 +112,9 @@ const COLUMNS = [
   },
   {
     Header: "Activer",
-    // eslint-disable-next-line react/display-name
-    Cell: row => <CellAction row={row.row} />,
+    Cell(row) {
+      <CellActive active={row.row} />;
+    },
     width: 120,
     style: { textAlign: "center", alignSelf: "center" }
   }
@@ -151,7 +159,6 @@ class TableUser extends React.Component {
             desc: false
           }
         ]}
-        //defaultPageSize={PAGE_SIZE}
         onFetchData={this.fetchData}
         className="-striped -highlight"
       />
