e2e0dfa2d6507478d3e627d0fd93ce5d48878c07
kwiss-macbook <christophe@hooraylabs.com>
2019-06-28 12:18:22 +0200

    feat(authv2): add working hasura claim token with validation

---
+++ packages/api/routes/auth-v2.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const { check, body } = require("express-validator");
+const userController = require("../controllers/user");
+
+router.post(
+  "/login",
+  [
+    check("username").isLength({ min: 5 }),
+    check("password").isLength({ min: 5 })
+  ],
+  userController.postLogin
+);
+router.post(
+  "/signup",
+  [
+    body("username")
+      .not()
+      .isEmpty(),
+    check("password", "Password must be at least 4 characters long").isLength({
+      min: 4
+    })
+  ],
+  userController.postSignup
+);
+router.get("/webhook", userController.getWebhook);
+router.get("/jwks", userController.getJwks);
+
+module.exports = router;
