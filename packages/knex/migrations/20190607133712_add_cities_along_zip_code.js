9c152ea4dda71d4de6e2517e53c81ebc99b0b5e9
kwiss <christophe@hooraylabs.com>
2019-07-01 14:46:31 +0200

    fix(lint): remove useless promise param

--- packages/knex/migrations/20190607133712_add_cities_along_zip_code.js
+++ packages/knex/migrations/20190607133712_add_cities_along_zip_code.js
@@ -1,4 +1,4 @@
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return knex.schema.alterTable("geolocalisation_code_postal", function(table) {
     table.dropUnique("code_postal", "codepostallatlngs_code_postal_unique");
     table.specificType("cities", "text[]");
@@ -6,7 +6,7 @@ exports.up = function(knex, Promise) {
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   return knex.schema.alterTable("geolocalisation_code_postal", function(table) {
     table.dropColumn("cities_id");
   });
