5634a67b0a6d52f73cbedb42569a30d6dede8188
kwiss-macbook <christophe@hooraylabs.com>
2019-07-02 15:59:33 +0200

    fix(cities-migrations): fix cities migration along matching test

--- packages/knex/migrations/20190607133712_add_cities_along_zip_code.js
+++ packages/knex/migrations/20190607133712_add_cities_along_zip_code.js
@@ -1,13 +1,11 @@
 exports.up = function(knex) {
   return knex.schema.alterTable("geolocalisation_code_postal", function(table) {
-    table.dropUnique("code_postal", "codepostallatlngs_code_postal_unique");
     table.specificType("cities", "text[]");
-    table.string("code_insee");
   });
 };
 
 exports.down = function(knex) {
   return knex.schema.alterTable("geolocalisation_code_postal", function(table) {
-    table.dropColumn("cities_id");
+    table.dropColumn("cities");
   });
 };
