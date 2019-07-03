3bfca41348079cd923b95c53eb290e81ee66c175
Douglas Duteil <douglasduteil@gmail.com>
2019-07-03 15:05:43 +0200

    refactor(gitlab): use advance release process

---
+++ .gitlab-ci/env.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+#
+
+export BRANCH_NAME=${BRANCH_NAME:=$( printf "${CI_COMMIT_REF_NAME}" | LC_CTYPE=C sed -r "s/[^[:alpha:][:digit:].-_]/-/g" )}
+export COMMIT_TAG=${COMMIT_TAG:=$CI_COMMIT_TAG}
+export COMMIT=${COMMIT:=$CI_COMMIT_SHA}
+export ENVIRONMENT=${ENVIRONMENT:="dev.factory"};
+export HASH_SIZE=${HASH_SIZE:=7}
+export JOB_ID=${JOB_ID:=$CI_JOB_ID}
+
+BRANCH_NAME_HASHED=$( printf "${BRANCH_NAME}" | sha1sum | cut -c1-${HASH_SIZE} )
+export BRANCH_HASH=${BRANCH_HASH:=$BRANCH_NAME_HASHED}
+
+#
+
+if [[ "${BRANCH_NAME}" = "master" ]]; then
+  export BRANCH_HASH=master;
+fi
+
+if [[ -n "${COMMIT_TAG}" ]]; then
+  export ENVIRONMENT=incubateur;
+  export BRANCH_HASH=$( printf "${COMMIT_TAG}" | sed "s/\./-/g" );
+fi
+
+if [[ -n "${PRODUCTION+x}" ]]; then
+  export BRANCH_HASH=prod;
+  #
+  export FRONTEND_HOST="emjpm.${ENVIRONMENT}.social.gouv.fr";
+  export API_HOST="api.emjpm.${ENVIRONMENT}.social.gouv.fr";
+else
+  export FRONTEND_HOST="${BRANCH_HASH}.emjpm.${ENVIRONMENT}.social.gouv.fr";
+  export API_HOST="${BRANCH_HASH}.api.emjpm.${ENVIRONMENT}.social.gouv.fr";
+fi
+
+#
+
+printenv | grep -E \
+  "BRANCH_HASH|BRANCH_NAME|COMMIT|COMMIT_TAG|ENVIRONMENT|HASH_SIZE|JOB_ID" \
+  | sort
+printenv | grep -E \
+  "FRONTEND_HOST" \
+  "API_HOST" \
+  | sort
