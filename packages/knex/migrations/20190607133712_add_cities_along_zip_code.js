e8c447eef913ae27dbff3f430eb5fc44b5a38e7f
christophe <christophe@hooraylabs.com>
2019-07-09 17:20:01 +0200

    feat(api): add postal-code and city synchronisation (#424)
    * feat(postal-code): add new postal code json file
    
    * feat(postal-code): add cities migration and seed
    
    * feat(code-postal): edit migration and seed
    
    * fix(postal-code): fix import file
    
    * fix(postal-code): fix import
    
    * fix(tis-names): fix spacing problem in ti csv
    
    * feat(postal-code): add city coordinates
    
    * fix(lint): remove useless promise param
    
    * fix(cities-migrations): fix cities migration along matching test
    
    * test(api): update snapshots
    
    * fix(knex): comment debug
    
    * fix(seed): fix postal code seed
    
    * test(postal-code): fix postal code test seed
    
    * fix(mesures-cities): fix map and filter
    
    * feat(postal-code): add seeds to production
    
    * fix(mesures-map-api): fix mesures api city coordinates filter

---
+++ packages/knex/migrations/20190607133712_add_cities_along_zip_code.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.alterTable("geolocalisation_code_postal", function(table) {
+    table.specificType("cities", "text[]");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.alterTable("geolocalisation_code_postal", function(table) {
+    table.dropColumn("cities");
+  });
+};
