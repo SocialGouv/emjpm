350d6ec563d299a3dde0667cb3b748e77f071da4
Adrien Gonzalez <gonzalezad1@users.noreply.github.com>
2019-07-11 11:56:53 +0200

    feat(inscription): Inscription for mandataires, tis and services (#495)
    * feat(Model): Add Service,Mandataire and UserTI models
    
    * feat(inscription): add insert of data for inscription
    
    * refactor(signup): refactor controller and model
    
    * fix(signup): add working default signup
    
    * feat(inscription): inscription for mandataire,service,tis
    
    * fix(inscription): revert test from feat/hasura
    
    * fix(inscritpion): test for mesures
    
    * fix(inscription): change naming
    
    * fix(inscription): change createtService to createService

---
+++ packages/api/model/Tis.js
@@ -0,0 +1,16 @@
+const knexConnection = require("../db/knex");
+const { Model } = require("objection");
+
+Model.knex(knexConnection);
+
+class Tis extends Model {
+  static get tableName() {
+    return "tis";
+  }
+
+  static get idColumn() {
+    return "id";
+  }
+}
+
+module.exports = { Tis };
