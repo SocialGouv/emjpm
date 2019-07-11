baa3ca2f0bcb29910b4112c2144a41dda44a38d5
christophe <christophe@hooraylabs.com>
2019-07-11 15:43:43 +0200

    fix(test): fix auth tests (#503)

--- packages/api/controllers/user/login.js
+++ packages/api/controllers/user/login.js
@@ -10,12 +10,12 @@ const postLogin = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors });
+    return res.status(400).json({ errors });
   }
 
   passport.authenticate("local", (err, user) => {
     if (err) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(401).json({ err });
     }
     if (user) {
       updateLastLogin(user.id).then(() => {
