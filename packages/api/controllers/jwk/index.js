d95fac5e820cd4793884d8fb69b18d0867973434
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-07-11 18:06:33 +0200

    feat(hasura): introduce hasura, change auth (#504)

---
+++ packages/api/controllers/jwk/index.js
@@ -0,0 +1,21 @@
+const rasha = require("rasha");
+const jwtConfig = require("../../config/jwt");
+
+/**
+ * Sends the JWT key set
+ */
+
+exports.getJwks = async (req, res) => {
+  const jwk = {
+    ...rasha.importSync({ pem: jwtConfig.publicKey }),
+    alg: "RS256",
+    use: "sig",
+    kid: jwtConfig.publicKey
+  };
+  const jwks = {
+    keys: [jwk]
+  };
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify(jwks, null, 2) + "\n");
+  return res.status(200).json(jwks);
+};
