52c3d8478cec4d26dba2b1f6c878c256926a3acb
kwiss <christophe@hooraylabs.com>
2019-07-12 17:24:18 +0200

    feat(graphql-example): add test with type resolver

--- packages/graphql-server/src/resolvers/hello.ts
+++ packages/graphql-server/src/resolvers/hello.ts
@@ -1,7 +1,40 @@
-const resolvers = {
+import { Resolvers } from "../types/resolvers-types";
+
+// const client = new ApolloClient({
+//   cache: new InMemoryCache(),
+//   link: createHttpLink({
+//     fetch,
+//     uri: "http://localhost:5000/v1/graphql"
+//   })
+// });
+
+// const MESURES = gql`
+//   {
+//     mesures {
+//       annee
+//       cabinet
+//       civilite
+//     }
+//   }
+// `;
+
+// // a query with apollo-client
+// const test = client.query({
+//   context: {
+//     // example of setting the headers with context per operation
+//     headers: {
+//       "x-hasura-admin-secret": "secret"
+//     }
+//   },
+//   query: MESURES
+// });
+
+export const resolvers: Resolvers = {
   Query: {
-    hello: () => "Hello world",
+    hello: () => {
+      return "hello world";
+    }
   }
 };
 
-export default resolvers
+export default resolvers;
