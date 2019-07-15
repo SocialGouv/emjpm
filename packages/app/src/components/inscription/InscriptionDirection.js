9cac82e0b792ffe2202bfc654c2d56edf1a1a6f5
gonzalezad1 <gonzalez_ad@hotmail.fr>
2019-07-15 17:19:14 +0200

    feat(inscription): add type of direction inside form

--- packages/app/src/components/inscription/InscriptionDirection.js
+++ packages/app/src/components/inscription/InscriptionDirection.js
@@ -1,7 +1,6 @@
 import React from "react";
 import FormMandataire from "./FormMandataire";
 import uiSchema from "./uiSchema.json";
-import { cabinet } from "../common/nomination";
 
 const schema = {
   type: "object",
@@ -11,10 +10,32 @@ const schema = {
     pass1: { type: "string", title: "Mot de passe", minLength: 8 },
     pass2: { type: "string", title: "Verifier le Mot de passe", minLength: 8 },
     nom: { type: "string", title: "Nom", default: "" },
-    prenom: { type: "string", title: "Prenom", default: "" }
+    prenom: { type: "string", title: "Prenom", default: "" },
+    type: {
+      type: "string",
+      anyOf: [
+        {
+          type: "string",
+          title: "direction départementale",
+          enum: ["direction_departementale"]
+        },
+        {
+          type: "string",
+          title: "direction régionale",
+          enum: ["direction_regional"]
+        },
+        {
+          type: "string",
+          title: "direction nationale",
+          enum: ["direction_nationale"]
+        }
+      ]
+    }
   }
 };
 
-const InscriptionTis = props => <FormMandataire schema={schema} uiSchema={uiSchema} {...props} />;
+const InscriptionDirection = props => (
+  <FormMandataire schema={schema} uiSchema={uiSchema} {...props} />
+);
 
-export default InscriptionTis;
+export default InscriptionDirection;
