8974356381de26113fcc9019d1c10485458fb6dd
christophe <christophe@hooraylabs.com>
2019-07-15 10:28:21 +0200

    feat(graphql-example): add test with type resolver (#522)
    * feat(graphql-example): add test with type resolver
    
    * feat(graphql-example): add working mesure query in hello resolver

--- packages/graphql-server/src/resolvers/hello.ts
+++ packages/graphql-server/src/resolvers/hello.ts
@@ -1,7 +1,50 @@
-const resolvers = {
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from "apollo-link-http";
+import { gql } from 'apollo-server-koa';
+import fetch from "node-fetch"
+import { Resolvers } from "../types/resolvers-types";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: createHttpLink({
+    fetch,
+    uri: "http://localhost:5000/v1/graphql"
+  })
+});
+
+const MESURES = gql`
+  {
+    mesures {
+      annee
+      cabinet
+      civilite
+    }
+  }
+`;
+
+// a query with apollo-client
+const test = client.query({
+  context: {
+    // example of setting the headers with context per operation
+    headers: {
+      "x-hasura-admin-secret": "secret"
+    }
+  },
+  query: MESURES
+});
+
+export const resolvers: Resolvers = {
   Query: {
-    hello: () => "Hello world",
+    hello: () => {
+      test.then((result) => {
+        const mesures = result.data.mesures;
+        // tslint:disable-next-line no-console
+        console.log(mesures.length);
+      })
+      return "hello world";
+    }
   }
 };
 
-export default resolvers
+export default resolvers;
