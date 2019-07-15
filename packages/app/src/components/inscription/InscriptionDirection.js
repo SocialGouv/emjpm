7d29837148e425c6e952b2ccbb0b76d5a3de9084
gonzalezad1 <gonzalez_ad@hotmail.fr>
2019-07-15 16:32:57 +0200

    feat(inscription): form for direction

---
+++ packages/app/src/components/inscription/InscriptionDirection.js
@@ -0,0 +1,20 @@
+import React from "react";
+import FormMandataire from "./FormMandataire";
+import uiSchema from "./uiSchema.json";
+import { cabinet } from "../common/nomination";
+
+const schema = {
+  type: "object",
+  required: ["pass1", "pass2", "email"],
+  properties: {
+    email: { type: "string", format: "email", title: "Adresse email", default: "" },
+    pass1: { type: "string", title: "Mot de passe", minLength: 8 },
+    pass2: { type: "string", title: "Verifier le Mot de passe", minLength: 8 },
+    nom: { type: "string", title: "Nom", default: "" },
+    prenom: { type: "string", title: "Prenom", default: "" }
+  }
+};
+
+const InscriptionTis = props => <FormMandataire schema={schema} uiSchema={uiSchema} {...props} />;
+
+export default InscriptionTis;
