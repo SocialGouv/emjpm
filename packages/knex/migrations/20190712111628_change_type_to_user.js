a25a82808fd4edb085aef07c497a5d7763899437
Adrien Gonzalez <gonzalezad1@users.noreply.github.com>
2019-07-17 11:22:30 +0200

    feat(inscription): inscription for dr and dd (#518)
    * feat(inscription): inscription for dr and dd
    
    * fix(inscription): code review
    
    * fix(inscription): rm direction from switch

---
+++ packages/knex/migrations/20190712111628_change_type_to_user.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.alterTable("users", function(table) {
+    table.string("type").alter();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.alterTable("users", function(table) {
+    table.string("type", 20);
+  });
+};
