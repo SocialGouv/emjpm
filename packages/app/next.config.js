3fd53755515b0ecbf228df86d8fbdf80ca44be2d
Douglas Duteil <douglasduteil@gmail.com>
2019-07-23 13:06:39 +0200

    feat(app): update to next 9 and styled-components 4
    Following #83

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,30 +1,27 @@
 const webpack = require("webpack");
 const withCSS = require("@zeit/next-css");
 const withImages = require("next-images");
-const withTM = require("next-plugin-transpile-modules");
+const withTM = require("next-transpile-modules");
+const flow = require("lodash.flow");
 
-/* eslint-disable no-console */
-process.on("unhandledRejection", r => console.log(r));
-/* eslint-enable no-console */
-
-module.exports = withTM(
-  withCSS(
-    withImages({
-      transpileModules: [
-        "color",
-        "strict-uri-encode",
-        "decode-uri-component",
-        "query-string",
-        "fuse.js"
-      ],
-      webpack: config => {
-        config.plugins.push(new webpack.EnvironmentPlugin(process.env));
-        return config;
-      },
-      publicRuntimeConfig: {
-        SENTRY_PUBLIC_DSN: process.env.SENTRY_PUBLIC_DSN,
-        API_URL: process.env.API_URL || "http://127.0.0.1:4000"
-      }
-    })
-  )
-);
+module.exports = flow(
+  withTM,
+  withCSS,
+  withImages
+)({
+  webpack: config => {
+    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+    return config;
+  },
+  publicRuntimeConfig: {
+    SENTRY_PUBLIC_DSN: process.env.SENTRY_PUBLIC_DSN,
+    API_URL: process.env.API_URL || "http://127.0.0.1:4000"
+  },
+  transpileModules: [
+    "color",
+    "strict-uri-encode",
+    "decode-uri-component",
+    "query-string",
+    "fuse.js"
+  ]
+});
