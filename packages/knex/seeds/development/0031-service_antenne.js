92b5f5b3daaaa43bcc590ec413b9329a017a47d6
Thomas Glatt <tglatt@gmail.com>
2019-07-24 15:15:30 +0200

    refactor(seed): clean service-antenne seed

--- packages/knex/seeds/development/0031-service_antenne.js
+++ packages/knex/seeds/development/0031-service_antenne.js
@@ -1,11 +1,8 @@
 exports.seed = function(knex) {
   return knex("service_antenne")
     .del() // Deletes ALL existing entries
-    .then(function() {
-      // Inserts seed entries one by one in series
-      return knex("services");
-    })
-    .then(function(services) {
+    .then(async function() {
+      const services = await knex("services");
       return Promise.all(
         services.map(service =>
           knex("service_antenne").insert({
