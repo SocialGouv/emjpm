936b868d24f27d6f55fdcc44d37fdcf7069ebff6
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-07-25 10:12:37 +0200

    chore(qraphql-server): remove useless query (#593)

--- packages/graphql-server/src/resolvers/mesure-stat.resolver.ts
+++ packages/graphql-server/src/resolvers/mesure-stat.resolver.ts
@@ -1,23 +1,7 @@
-import { logger } from "../logger";
-import { QueryAvailableMesureNumberArgs } from "../types/resolvers-types";
-import {
-  closedMesureNumber,
-  mesureStateCategoryStatistics,
-  mesureTypeCategoryEvolution,
-  mesureTypeCategoryStatistics,
-  newMesureNumber
-} from "./query/mesure-stat";
+import { closedMesureNumber, mesureStateCategoryStatistics, mesureTypeCategoryEvolution, mesureTypeCategoryStatistics, newMesureNumber } from "./query/mesure-stat";
 
 export const resolvers = {
   Query: {
-    availableMesureNumber: async (
-      _: any,
-      args: QueryAvailableMesureNumberArgs,
-      context: any
-    ) => {
-      logger.info(args, context);
-      return null;
-    },
     closedMesureNumber,
     mesureStateCategoryStatistics,
     mesureTypeCategoryEvolution,
