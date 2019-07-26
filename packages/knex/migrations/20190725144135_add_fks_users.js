8b8c4e28a0bc2b86880a9eafe99dac41d2237fbb
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-07-26 12:26:57 +0200

    feat(db): add missing foreign key (#616)
    * feat(db): initialise departement for mesures and add FKwq
    
    * feat(db): add missing foreign key
    
    * fix(seed): change seed order to avoid FK violation

---
+++ packages/knex/migrations/20190725144135_add_fks_users.js
@@ -0,0 +1,16 @@
+exports.up = async function(knex) {
+  return knex.schema.alterTable("users", function(table) {
+    table.unique("service_id");
+    table
+      .foreign("service_id")
+      .references("id")
+      .inTable("services");
+  });
+};
+
+exports.down = async function(knex) {
+  return knex.schema.alterTable("users", function(table) {
+    table.dropForeign("service_id");
+    table.dropUnique("service_id");
+  });
+};
