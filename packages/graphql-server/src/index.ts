5693d540e6173c325b1907471d884298ccbf1318
Thomas Glatt <tglatt@gmail.com>
2019-08-13 09:30:17 +0200

    ci(gql): add health check

--- packages/graphql-server/src/index.ts
+++ packages/graphql-server/src/index.ts
@@ -2,6 +2,7 @@ import * as cors from "@koa/cors";
 import { ApolloServer } from "apollo-server-koa";
 import * as Koa from "koa";
 import * as bodyParser from "koa-bodyparser";
+import * as Router from "koa-router";
 import { logger } from "./logger";
 import resolvers from "./resolvers";
 import typeDefs from "./schemas";
@@ -22,6 +23,15 @@ const app = new Koa();
 app.use(bodyParser());
 app.use(cors());
 server.applyMiddleware({ app });
+
+const router = new Router();
+
+router.get(`/health-check`, (ctx: Koa.Context) => {
+  ctx.status = 200;
+  ctx.body = "health-check ok!!";
+});
+
+app.use(router.routes());
 
 app.listen({ port: 4001 }, () =>
   logger.info(`🚀 Server ready at http://localhost:4001${server.graphqlPath}`)
