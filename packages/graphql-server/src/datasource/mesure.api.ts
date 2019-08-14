389ddeb5c4eecaefd0a52f63e48651b02595b0f0
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-08-14 19:30:54 +0200

    feat(gql): use apollo-link-rest and pass auth token (#634)

---
+++ packages/graphql-server/src/datasource/mesure.api.ts
@@ -0,0 +1,123 @@
+import { Mesures } from "../model/mesures.model";
+import { NullableNumber } from "../utils/types.util";
+import { AuthDataSource } from "./auth-datasource";
+
+const convertDates = (mesure: SearchMesureResult) => {
+  mesure.date_ouverture = mesure.date_ouverture
+    ? new Date(mesure.date_ouverture)
+    : null;
+  mesure.extinction = mesure.extinction ? new Date(mesure.extinction) : null;
+};
+
+export interface SearchMesureResultWrapper {
+  mesures: SearchMesureResult[];
+}
+
+export interface SearchMesuresParam {
+  opening?: {
+    between?: {
+      start: string;
+      end: string;
+    };
+    lt?: string;
+  };
+  closed?: {
+    between?: {
+      start: string;
+      end: string;
+    };
+    gt_or_null?: string;
+  };
+  created?: {
+    between?: {
+      start: string;
+      end: string;
+    };
+    lt?: string;
+  };
+  region?: NullableNumber;
+  department?: NullableNumber;
+  court?: NullableNumber;
+}
+
+export type SearchMesureResult = Pick<
+  Mesures,
+  "date_ouverture" | "cabinet" | "civilite" | "status" | "type" | "extinction"
+>;
+
+export class MesureAPI extends AuthDataSource {
+  constructor() {
+    super();
+  }
+
+  public searchMesures(params: SearchMesuresParam) {
+    const query = `
+    {
+      mesures(where: { ${this.buildFilters(params).join(", ")} }) {
+        date_ouverture
+        cabinet
+        civilite
+        status
+        type
+        extinction
+      }
+    }
+  `;
+
+    return this.post("/", { query }).then(response => {
+      return response.data.mesures.map(convertDates);
+    });
+  }
+
+  private buildFilters(params: SearchMesuresParam): string[] {
+    const filters = [];
+    if (params.created) {
+      if (params.created.between) {
+        const between = params.created.between;
+        filters.push(
+          `created_at: { _gt: "${between.start}", _lt: "${between.end}" }`
+        );
+      }
+      if (params.created.lt) {
+        filters.push(`created_at: { _lt: "${params.created.lt}" }`);
+      }
+    }
+    if (params.opening) {
+      if (params.opening.between) {
+        const between = params.opening.between;
+        filters.push(
+          `date_ouverture: { _gt: "${between.start}", _lt: "${between.end}" }`
+        );
+      }
+      if (params.opening.lt) {
+        filters.push(`date_ouverture: { _lt: "${params.opening.lt}" }`);
+      }
+    }
+    if (params.closed) {
+      if (params.closed.between) {
+        const between = params.closed.between;
+        filters.push(
+          `extinction: { _gt: "${between.start}", _lt: "${between.end}" }`
+        );
+      }
+      if (params.closed.gt_or_null) {
+        filters.push(
+          `_or: [
+                  {extinction: { _gt: "${params.closed.gt_or_null}"}},
+                  {status: {_neq: "Eteindre mesure"}}
+              ]`
+        );
+      }
+    }
+    if (params.region) {
+      filters.push(`region_id: {_eq: ${params.region}}`);
+    }
+    if (params.department) {
+      filters.push(`code_postal: {_like: "${params.department}%"}`);
+    }
+    if (params.court) {
+      filters.push(`ti_id: {_eq: ${params.court}}`);
+    }
+    return filters;
+  }
+}
