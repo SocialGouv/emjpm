ffb10dc00412727e4201dff4e803747772090e5e
kwiss <christophe@hooraylabs.com>
2019-08-14 14:46:29 +0200

    feat(layout): add mesure

--- packages/app/pages/direction.js
+++ packages/app/pages/direction.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Layout } from "../src/components-v2/layout";
 import { Filters } from "../src/components-v2/filters";
+import { Card, Heading4, Heading1 } from "@socialgouv/emjpm-ui-core";
+import { Flex, Box } from "rebass";
 
 const links = [
   {
@@ -17,6 +19,26 @@ const direction = () => {
   return (
     <Layout links={links}>
       <Filters />
+      <Box mt={5}>
+        <Flex justifyContent="space-between" flex={"wrap"}>
+          <Card flexBasis="252px">
+            <Heading4>Mesures en cours</Heading4>
+            <Heading1 mt={"5"}>3949</Heading1>
+          </Card>
+          <Card flexBasis="252px">
+            <Heading4>Mesures disponibles</Heading4>
+            <Heading1 mt={"5"}>399</Heading1>
+          </Card>
+          <Card flexBasis="252px">
+            <Heading4>Nouvelles mesures</Heading4>
+            <Heading1 mt={"5"}>349</Heading1>
+          </Card>
+          <Card flexBasis="252px">
+            <Heading4>Mesure éteintes</Heading4>
+            <Heading1 mt={"5"}>30949</Heading1>
+          </Card>
+        </Flex>
+      </Box>
     </Layout>
   );
 };
