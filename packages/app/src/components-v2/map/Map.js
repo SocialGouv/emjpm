ef102f1b901d929b60167e9798b2360beaf69bac
christophe <christophe@hooraylabs.com>
2019-08-16 21:10:50 +0200

    feat(direction-map): add map component with query ref #544 (#641)

---
+++ packages/app/src/components-v2/map/Map.js
@@ -0,0 +1,36 @@
+import React, { Fragment, useState } from "react";
+import { SvgLoader, SvgProxy } from "react-svgmt";
+
+import Map from "./MapSvg";
+import Panel from "./Panel";
+
+const France = props => {
+  const { color, aboveColor, belowColor, departements } = props;
+  const [currentPanel, togglePanel] = useState(false);
+  return (
+    <Fragment>
+      {currentPanel.isActive && <Panel togglePanel={togglePanel} currentPanel={currentPanel} />}
+      <SvgLoader svgXML={Map}>
+        <SvgProxy selector="#carte" fill={color} />
+        {departements.map((dpt, index) => (
+          <SvgProxy
+            key={`${dpt}-${index}`}
+            onClick={() => togglePanel({ isActive: true, ...dpt })}
+            selector={`#dpt-${dpt.department.code}`}
+            fill={dpt.isAbove ? aboveColor : belowColor}
+          />
+        ))}
+      </SvgLoader>
+    </Fragment>
+  );
+};
+
+France.propTypes = {};
+
+France.defaultProps = {
+  color: "#f6f6f9",
+  aboveColor: "#FF6966",
+  belowColor: "#70D54F",
+  departements: []
+};
+export default France;
