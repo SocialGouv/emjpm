1485785929b240b1b80c42fc8d9c9a9ab00d8e8e
christophe <christophe@hooraylabs.com>
2019-08-16 16:12:59 +0200

    fix(select-options): change key args (#639)

--- packages/app/src/util/option/OptionUtil.js
+++ packages/app/src/util/option/OptionUtil.js
@@ -1,5 +1,13 @@
+export const regionsToOptions = datas => {
+  return toOptions(datas, "id", "nom");
+};
+
+export const departementToOptions = datas => {
+  return toOptions(datas, "code", "nom");
+};
+
 export const toOptions = (datas, idKey, labelKey) =>
   datas.map(data => ({
-    id: data[idKey],
+    value: data[idKey],
     label: data[labelKey]
   }));
