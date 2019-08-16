840e213bf30dac503a2d1838fd7ccc96882c8b6c
christophe <christophe@hooraylabs.com>
2019-08-16 15:57:29 +0200

    Feat/footer (#637)
    * feat(filters): add filters components
    
    * feat(layout): add mesure
    
    * feat(filters): add lib components
    
    * feat(footer): add footer components
    
    * fix(yarn): fix yarn lock conflict

---
+++ packages/app/src/components-v2/commons/Link.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link as RebassLink } from "rebass";
+import NextLink from "next/link";
+
+const LinkStyle = () => {
+  return {
+    color: "#007AD9",
+    cursor: "pointer",
+    fontWeight: 600,
+    fontSize: "13px",
+    "&:hover": {
+      textDecoration: "underline"
+    }
+  };
+};
+
+const Link = props => {
+  return (
+    <NextLink {...props}>
+      <RebassLink {...props} sx={LinkStyle()}>
+        {props.children}
+      </RebassLink>
+    </NextLink>
+  );
+};
+
+export { Link };
