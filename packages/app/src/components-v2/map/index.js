484917e471e4d396cc6b76241828d817b1722e40
kwiss <christophe@hooraylabs.com>
2019-08-16 19:31:36 +0200

    feat(direction-map): add map component with query ref #544

---
+++ packages/app/src/components-v2/map/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { Box } from "rebass";
+import { Card, Heading2 } from "@socialgouv/emjpm-ui-core";
+import MapComponent from "./Map";
+import { GET_DEPARTEMENTS_AVAILABILITY } from "../../graphql/Queries";
+
+export const Map = () => {
+  const { data, loading } = useQuery(GET_DEPARTEMENTS_AVAILABILITY);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  const departements = data.view_department_availability
+    .filter(departement => departement.mesures_in_progress > 0)
+    .map(departement => {
+      if (departement.mesures_in_progress > 0) {
+        return {
+          ...departement,
+          isAbove: departement.mesures_in_progress > departement.mesures_max
+        };
+      }
+      return null;
+    });
+
+  return (
+    <Card flexBasis="50%">
+      <Box p="1">
+        <Heading2>Cartes de la disponibilité des mesures</Heading2>
+      </Box>
+      <Box sx={{ position: "relative", p: "6" }}>
+        <MapComponent departements={departements} />
+      </Box>
+    </Card>
+  );
+};
--- packages/app/src/graphql/Queries.js
+++ packages/app/src/graphql/Queries.js
@@ -31,3 +31,18 @@ export const GET_REGIONS = gql`
     }
   }
 `;
+
+export const GET_DEPARTEMENTS_AVAILABILITY = gql`
+  {
+    view_department_availability {
+      department_id
+      mesures_awaiting
+      mesures_in_progress
+      mesures_max
+      department {
+        code
+        nom
+      }
+    }
+  }
+`;
