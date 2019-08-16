d66a1df0415c20f4e4ade71d8b10482bd098fe5e
kwiss <christophe@hooraylabs.com>
2019-08-17 00:59:39 +0200

    feat(mesure-evolution): add mesure evolution components

--- packages/app/src/graphql/Queries.js
+++ packages/app/src/graphql/Queries.js
@@ -55,3 +55,30 @@ export const GET_CATEGORY_STATS = gql`
     }
   }
 `;
+
+export const GET_CATEGORY_EVOLUTION = gql`
+  query mesureTypeCategoryEvolution(
+    $end: String!
+    $start: String!
+    $court: Int
+    $department: Int
+    $region: Int
+  ) {
+    mesureTypeCategoryEvolution(
+      end: $end
+      start: $start
+      court: $court
+      department: $department
+      region: $region
+    ) {
+      mesureTypeCategory
+      start
+      end
+      monthlyEvolutions {
+        month
+        number
+        year
+      }
+    }
+  }
+`;
