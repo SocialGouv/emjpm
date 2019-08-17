04f7bab398c4e98caee35d76ff343813c8c15900
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-08-17 14:33:32 +0200

    feat(direction): implement indicators (#640)
    * feat(direction): implement indicators
    
    * feat(indicators): add closed mesure and add indicator components

---
+++ packages/app/src/components-v2/direction-indicators/DirectionIndicator.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card, Heading0, Heading4 } from "@socialgouv/emjpm-ui-core";
+
+const DirectionIndicator = props => {
+  return (
+    <Card flexBasis="250px">
+      <Heading4>{props.title}</Heading4>
+      <Heading0 mt={"7"}>{props.indicator}</Heading0>
+    </Card>
+  );
+};
+
+export { DirectionIndicator };
---
+++ packages/app/src/components-v2/direction-indicators/NewMesureIndicator.js
@@ -0,0 +1,29 @@
+import { useQuery } from "@apollo/react-hooks";
+import React, { useContext } from "react";
+import { GET_NEW_MESURE_NUMBER } from "../../graphql/Queries";
+import { FiltersContext } from "../filters/context";
+import { DirectionIndicator } from "./DirectionIndicator";
+
+const NewMesureIndicator = () => {
+  // eslint-disable-next-line no-unused-vars
+  const { selectedRegionalValue, selectedDepartementValue, selectedTribunalValue } = useContext(
+    FiltersContext
+  );
+
+  const { data, loading } = useQuery(GET_NEW_MESURE_NUMBER, {
+    variables: {
+      start: "2017-01-01",
+      end: "2019-12-31",
+      department: selectedDepartementValue ? parseInt(selectedDepartementValue.value) : undefined,
+      region: selectedRegionalValue ? parseInt(selectedRegionalValue.value) : undefined
+    }
+  });
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  return <DirectionIndicator title="Nouvelles mesures" indicator={data.newMesureNumber} />;
+};
+
+export { NewMesureIndicator };
