85f37a9ed8bca9e32c22419f541b7a8ec42cf956
Thomas Glatt <tglatt@gmail.com>
2019-08-17 15:11:08 +0200

    feat(direction): add open mesure indicator

--- packages/app/src/components-v2/direction-indicators/index.js
+++ packages/app/src/components-v2/direction-indicators/index.js
@@ -1,5 +1,6 @@
 import { DirectionIndicator } from "./DirectionIndicator";
 import { NewMesureIndicator } from "./NewMesureIndicator";
 import { ClosedMesureIndicator } from "./ClosedMesureIndicator";
+import { OpenMesureIndicator } from "./OpenMesureIndicator";
 
-export { DirectionIndicator, NewMesureIndicator, ClosedMesureIndicator };
+export { DirectionIndicator, NewMesureIndicator, ClosedMesureIndicator, OpenMesureIndicator };
--- packages/graphql-server/src/resolvers/query/mesure-stat/index.ts
+++ packages/graphql-server/src/resolvers/query/mesure-stat/index.ts
@@ -3,3 +3,4 @@ export * from "./department-availabilities";
 export * from "./mesure-type-category-evolution";
 export * from "./mesure-type-category-statistics";
 export * from "./new-mesure-number";
+export * from "./open-mesure-number";
