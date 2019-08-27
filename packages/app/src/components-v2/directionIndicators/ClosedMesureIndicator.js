ca9b3f3121acbd2e5be9af91fdb5a63a9bb0dac0
christophe <christophe@hooraylabs.com>
2019-08-27 12:51:57 +0200

    feat(components-package): add new component package (#656)
    * feat(components-package): add new component package
    
    * feat(components-package): update deps and change package
    
    * feat(header): use header component form socialgouv package
    
    * chore(deps): update emjpm-ui-components deps
    
    * chore(deps): update packages and update new data page accordingly
    
    * fix(metadata): remove local url
    
    * fix(deps): remove useless eslint-plugin-jest

--- packages/app/src/components-v2/directionIndicators/ClosedMesureIndicator.js
+++ packages/app/src/components-v2/directionIndicators/ClosedMesureIndicator.js
@@ -2,7 +2,7 @@ import { useQuery } from "@apollo/react-hooks";
 import React, { useContext } from "react";
 import { GET_CLOSED_MESURE_NUMBER } from "../../graphql/Queries";
 import { FiltersContext } from "../filters/context";
-import { Indicator } from "./Indicator";
+import { Indicator } from "@socialgouv/emjpm-ui-components";
 
 const ClosedMesureIndicator = () => {
   const {
