b4fca4e2ab6c203007fc404f1325da844318056c
christophe <christophe@hooraylabs.com>
2019-08-28 12:14:06 +0200

    Feat/hygen (#669)
    * feat(hygen): add component generator
    
    * feat(hygen): add basic jest test in generated files

---
+++ packages/app/_templates/component/new/prompt.js
@@ -0,0 +1,13 @@
+const promptArgs = require("./promptArgUtil");
+
+const questions = [
+  {
+    message: `what is the name of your component:`,
+    name: "name",
+    type: "input"
+  }
+];
+
+module.exports = {
+  prompt: promptArgs(questions)
+};
---
+++ packages/app/_templates/component/new/style.ejs.t
@@ -0,0 +1,8 @@
+---
+to: src/components-v2/<%= h.capitalize(name) %>/style.js
+---
+const <%= h.capitalize(name)%>Style = {
+  bg: "blue"
+};
+
+export { <%= h.capitalize(name)%>Style };
