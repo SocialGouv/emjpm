b0ff9b38a169b9f89b8718ba031542f90e948ae8
christophe <christophe@hooraylabs.com>
2019-08-28 12:14:22 +0200

    feat(services-pages): add services basic pages and layout (#667)

---
+++ packages/app/pages/services/Informations.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { Box, Flex } from "rebass";
+
+import { BoxWrapper } from "../../src/components-v2/commons";
+import { Heading2 } from "@socialgouv/emjpm-ui-core";
+import { LayoutServices } from "../../src/components-v2/layout";
+
+const Informations = () => {
+  return (
+    <LayoutServices>
+      <BoxWrapper mt={6} px="1">
+        <Flex
+          sx={{
+            flexWrap: "wrap"
+          }}
+        >
+          <Box
+            sx={{
+              flexGrow: 99999,
+              flexBasis: 0,
+              minWidth: 320
+            }}
+          >
+            <Heading2>Vos informations</Heading2>
+            Mesures list
+          </Box>
+          <Box
+            sx={{
+              flexGrow: 1,
+              flexBasis: 256
+            }}
+          >
+            <Heading2>Mesures souhaitées</Heading2>
+            map and other stuff
+          </Box>
+        </Flex>
+      </BoxWrapper>
+    </LayoutServices>
+  );
+};
+
+export default Informations;
