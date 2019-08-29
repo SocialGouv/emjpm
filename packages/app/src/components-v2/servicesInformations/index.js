4ec0dc92b86ab15676ed33f94b695d250944a8b1
christophe <christophe@hooraylabs.com>
2019-08-29 11:20:05 +0200

    feat(services): add informations components and query (#670)
    * feat(inscription): fix bad tis api url
    
    * feat(informations): add informations components and query

---
+++ packages/app/src/components-v2/servicesInformations/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { GET_SERVICES_ANTENNE } from "./queries";
+import { Informations } from "../informations";
+
+const ServicesInformations = () => {
+  const { data, error, loading } = useQuery(GET_SERVICES_ANTENNE, {
+    variables: {
+      serviceId: 1
+    }
+  });
+
+  if (loading) {
+    return <div>loading</div>;
+  }
+  if (error) {
+    return <div>error</div>;
+  }
+
+  console.log(data);
+
+  const { service_antenne } = data;
+
+  return (
+    <div>
+      <Informations loading={loading} error={loading} service_antenne={service_antenne} />
+    </div>
+  );
+};
+
+export { ServicesInformations };
