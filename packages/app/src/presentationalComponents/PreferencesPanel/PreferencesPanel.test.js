9a937d903bf68635e7251c0495db90ce00ca4c20
kwiss <christophe@hooraylabs.com>
2019-08-30 18:25:16 +0200

    feat(service-information): add preference panel

--- packages/app/src/presentationalComponents/PreferencesPanel/PreferencesPanel.test.js
+++ packages/app/src/presentationalComponents/PreferencesPanel/PreferencesPanel.test.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Informations } from "./Informations";
+import { PreferencesPanel } from "./PreferencesPanel";
 import renderer from "react-test-renderer";
 
 it("renders correctly", () => {
-  const tree = renderer.create(<Informations />).toJSON();
+  const tree = renderer.create(<PreferencesPanel />).toJSON();
   expect(tree).toMatchSnapshot();
 });
