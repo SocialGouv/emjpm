3b8927e5b54994adc7c0e8822a8ddebeef53f245
Thomas Glatt <tglatt@gmail.com>
2019-09-03 08:36:24 +0200

    feat(direction): add gql query for mandataire activity

--- packages/app/src/components-v2/MandatairesActivity/MandatairesActivity.js
+++ packages/app/src/components-v2/MandatairesActivity/MandatairesActivity.js
@@ -1,24 +1,16 @@
-import React, { useContext } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { Card, Heading2, Heading4, Spinner } from "@socialgouv/emjpm-ui-core";
+import React, { useContext } from "react";
 import { Box } from "rebass";
-
-import { MandatairesActivityChart } from "./MandatairesActivityChart";
 import { FiltersContext } from "../Filters/context";
+import { MandatairesActivityChart } from "./MandatairesActivityChart";
 import { MANDATAIRE_ACTIVITY } from "./queries";
 
 const MandatairesActivity = props => {
-  const {
-    selectedRegionalValue,
-    startDateValue,
-    selectedDepartementValue,
-    endDateValue
-  } = useContext(FiltersContext);
+  const { selectedRegionalValue, selectedDepartementValue } = useContext(FiltersContext);
 
   const { data, error, loading } = useQuery(MANDATAIRE_ACTIVITY, {
     variables: {
-      start: startDateValue,
-      end: endDateValue,
       department: selectedDepartementValue ? parseInt(selectedDepartementValue.value) : undefined,
       region: selectedRegionalValue ? parseInt(selectedRegionalValue.value) : undefined
     }
@@ -47,9 +39,42 @@ const MandatairesActivity = props => {
   return (
     <Card p="4" {...props}>
       <Heading2>Répartition de l’activité par type de mandataires</Heading2>
-      <MandatairesActivityChart data={data} />
+      <MandatairesActivityChart data={adaptData(data)} />
     </Card>
   );
 };
 
 export { MandatairesActivity };
+
+function adaptData(data) {
+  const service = data.service.aggregate.sum.mesures_in_progress;
+  const mandataireIndividuel = data.mandataireIndividuel.aggregate.sum.mesures_in_progress;
+  const mandatairePrepose = data.mandatairePrepose.aggregate.sum.mesures_in_progress;
+  const total = service + mandataireIndividuel + mandatairePrepose;
+  return {
+    total,
+    service: {
+      sum: service,
+      percentage: percentage(service, total)
+    },
+    mandataireIndividuel: {
+      sum: mandataireIndividuel,
+      percentage: percentage(mandataireIndividuel, total)
+    },
+    mandatairePrepose: {
+      sum: mandatairePrepose,
+      percentage: percentage(mandatairePrepose, total)
+    }
+  };
+}
+
+function percentage(value, total) {
+  if (!total || total == 0) {
+    return;
+  }
+  return round((value / total) * 100);
+}
+
+function round(value) {
+  return Math.round(value * 100) / 100;
+}
