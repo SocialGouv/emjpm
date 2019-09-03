75493aef6082ba018e35ea3b82a4b78df6923411
christophe <christophe@hooraylabs.com>
2019-09-03 17:06:17 +0200

    fix(apollo-state): fix only browser apollo cache state (#685)

--- packages/app/src/lib/apollo/with-apollo-client.js
+++ packages/app/src/lib/apollo/with-apollo-client.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import React from "react";
 import initApollo from "./init-apollo";
 import jwtDecode from "jwt-decode";
+import { isBrowser } from "../../util";
 function parseCookies(req, options = {}) {
   return cookie.parse(req ? req.headers.cookie || "" : document.cookie, options);
 }
@@ -16,10 +17,10 @@ export default App => {
     static async getInitialProps(context) {
       const { ctx } = context;
       const { req, res } = ctx;
-      const { id } = jwtDecode(parseCookies(req).token);
+      const currentId = !isBrowser() ? null : jwtDecode(parseCookies(req).token);
       const apollo = initApollo(
         {
-          currentId: id
+          currentId
         },
         {
           getToken: () => parseCookies(req).token
