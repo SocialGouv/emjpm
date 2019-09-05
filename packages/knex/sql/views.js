62d65f845ef7179d88479a7f2bd7b984617e1c1d
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-09-05 14:13:08 +0200

    feat(direction): relies statistic on service (#697)

--- packages/knex/sql/views.js
+++ packages/knex/sql/views.js
@@ -1,20 +1,22 @@
 module.exports.VIEW_MESURE_GESTIONNAIRE = `
 CREATE VIEW view_mesure_gestionnaire AS
- SELECT dep.id department_id, dep.nom dep_nom, 'ANTENNE' discriminator, ser.id source_id, ser.mesures_awaiting, ser.mesures_in_progress, ser.mesures_max
-  FROM service_antenne ser, departements dep
-   WHERE dep.code = substring(ser.address_zip_code, 0, 3)
-union
- SELECT dep.id department_id, dep.nom dep_nom, 'MANDATAIRE_IND' discriminator, man.id source_id, man.mesures_en_attente mesures_awaiting, man.mesures_en_cours mesures_in_progress, man.dispo_max mesures_max
-  FROM mandataires man, departements dep, users u
-   WHERE dep.code = substring(man.code_postal, 0, 3)
-   AND man.user_id = u.id
-   AND u.type = 'individuel'
-union
- SELECT dep.id department_id, dep.nom dep_nom, 'MANDATAIRE_PRE' discriminator, man.id source_id, man.mesures_en_attente mesures_awaiting, man.mesures_en_cours mesures_in_progress, man.dispo_max mesures_max
-  FROM mandataires man, departements dep, users u
-   WHERE dep.code = substring(man.code_postal, 0, 3)
-   AND man.user_id = u.id
-   AND u.type = 'prepose'
+ SELECT ser.id source_id, 'SERVICE' discriminator, dep.id department_id, dep.nom dep_nom, ser.dispo_max mesures_max, sum(ant.mesures_awaiting) mesures_awaiting, sum(ant.mesures_in_progress) mesures_in_progress
+   FROM services ser, service_antenne ant, departements dep
+    WHERE ser.id = ant.service_id
+    AND dep.code = substring(ser.code_postal, 0, 3)
+    GROUP BY ser.id, dep.id, dep.nom, ser.dispo_max
+ union
+ SELECT man.id source_id, 'MANDATAIRE_IND' discriminator, dep.id department_id, dep.nom dep_nom, man.dispo_max mesures_max, man.mesures_en_attente mesures_awaiting, man.mesures_en_cours mesures_in_progress
+   FROM mandataires man, departements dep, users u
+    WHERE dep.code = substring(man.code_postal, 0, 3)
+    AND man.user_id = u.id
+    AND u.type = 'individuel'
+ union
+  SELECT man.id source_id, 'MANDATAIRE_PRE' discriminator, dep.id department_id, dep.nom dep_nom, man.dispo_max mesures_max, man.mesures_en_attente mesures_awaiting, man.mesures_en_cours mesures_in_progress
+   FROM mandataires man, departements dep, users u
+    WHERE dep.code = substring(man.code_postal, 0, 3)
+    AND man.user_id = u.id
+    AND u.type = 'prepose'
   `;
 
 module.exports.VIEW_DEPARTMENT_AVAILIBILITY = `
