e34820e8037b22dd9f7d5d2c9b07ccdc7f77bcfd
Thomas Glatt <tglatt@gmail.com>
2019-09-09 10:45:25 +0200

    feat(departement-id): update seeds

--- packages/knex/seeds/development/0016-services.js
+++ packages/knex/seeds/development/0016-services.js
@@ -6,6 +6,7 @@ exports.seed = function(knex) {
       return knex("services").insert({
         etablissement: "UDAHF Siege",
         code_postal: "62000",
+        department_id: 6,
         ville: "Arras",
         telephone: "0237100000",
         adresse: "21 rue de houx",
