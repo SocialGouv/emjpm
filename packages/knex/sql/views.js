4c7a5908149b0e63e3ce4e00badd0e7c17947145
christophe <christophe@hooraylabs.com>
2019-09-10 10:25:32 +0200

    feat(mandataire-list): add real data fetch and paginate (#708)
    * feat(mandataire-list): add real data fetch and paginate
    
    * feat(knex): update sql view
    
    * feat(knex): update snapshot
    
    * feat(hasura): update metadata
    
    * feat(direction): update mandataire list query
    
    * chore(emjpm): bump version
    
    * feat(mandataire-list-load-more): add load more button
    
    * feat(mandataire-list): update genre and fix props issues
    
    * chore(direction): remove console.log

--- packages/knex/sql/views.js
+++ packages/knex/sql/views.js
@@ -1,20 +1,20 @@
 module.exports.VIEW_MESURE_GESTIONNAIRE = `
 CREATE VIEW view_mesure_gestionnaire AS
- SELECT ser.id source_id, 'SERVICE' discriminator, dep.id department_id, dep.nom dep_nom, ser.dispo_max mesures_max, sum(ant.mesures_awaiting) mesures_awaiting, sum(ant.mesures_in_progress) mesures_in_progress
+ SELECT ser.id service_id, NULL mandataire_id,  'SERVICE' discriminator, dep.id department_id, dep.nom dep_nom, ser.dispo_max mesures_max, sum(ant.mesures_awaiting) mesures_awaiting, sum(ant.mesures_in_progress) mesures_in_progress
    FROM services ser, service_antenne ant, departements dep
     WHERE ser.id = ant.service_id
-    AND dep.code = substring(ser.code_postal, 0, 3)
+    AND dep.id = ser.department_id
     GROUP BY ser.id, dep.id, dep.nom, ser.dispo_max
  union
- SELECT man.id source_id, 'MANDATAIRE_IND' discriminator, dep.id department_id, dep.nom dep_nom, man.dispo_max mesures_max, man.mesures_en_attente mesures_awaiting, man.mesures_en_cours mesures_in_progress
+ SELECT NULL service_id, man.id mandataire_id, 'MANDATAIRE_IND' discriminator, dep.id department_id, dep.nom dep_nom, man.dispo_max mesures_max, man.mesures_en_attente mesures_awaiting, man.mesures_en_cours mesures_in_progress
    FROM mandataires man, departements dep, users u
-    WHERE dep.code = substring(man.code_postal, 0, 3)
+    WHERE dep.id = man.department_id
     AND man.user_id = u.id
     AND u.type = 'individuel'
  union
-  SELECT man.id source_id, 'MANDATAIRE_PRE' discriminator, dep.id department_id, dep.nom dep_nom, man.dispo_max mesures_max, man.mesures_en_attente mesures_awaiting, man.mesures_en_cours mesures_in_progress
+  SELECT NULL service_id, man.id mandataire_id, 'MANDATAIRE_PRE' discriminator, dep.id department_id, dep.nom dep_nom, man.dispo_max mesures_max, man.mesures_en_attente mesures_awaiting, man.mesures_en_cours mesures_in_progress
    FROM mandataires man, departements dep, users u
-    WHERE dep.code = substring(man.code_postal, 0, 3)
+    WHERE dep.id = man.department_id
     AND man.user_id = u.id
     AND u.type = 'prepose'
   `;
