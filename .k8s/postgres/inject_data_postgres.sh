f9269b409a60615e9e0ecbe3789724fad44daa50
Yohan Lascombe <yohan.lascombe@gmail.com>
2019-09-10 15:56:19 +0200

    ci(gitlab): new deployment pipeline (#706)

--- .k8s/postgres/inject_data_postgres.sh
+++ .k8s/postgres/inject_data_postgres.sh
@@ -8,9 +8,9 @@ printenv | grep -E "DATABASE_URL" | sort
 # Check if emjpm-dataset pod exists
 if [ ! "$INJECT_DATA_POSTGRES_POD_STATUS" ]
 then
-    kubectl apply -f ./pod-inject-dataset.yml
+    kubectl apply -f ./pod-inject-dataset.yml -n emjpm-feature
 else
     kubectl delete pod "${K8S_NAMESPACE}"-dataset-"$1"
-    kubectl apply -f ./pod-inject-dataset.yml
+    kubectl apply -f ./pod-inject-dataset.yml -n emjpm-feature
 fi
 
