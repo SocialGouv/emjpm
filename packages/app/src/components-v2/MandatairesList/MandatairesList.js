a8c8fb8451717374d42c8cf1cf43d8e93bf777d1
kwiss-macbook <christophe@hooraylabs.com>
2019-09-10 15:46:59 +0200

    feat(mandataires-list): add filters for mandataires list

--- packages/app/src/components-v2/MandatairesList/MandatairesList.js
+++ packages/app/src/components-v2/MandatairesList/MandatairesList.js
@@ -1,19 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { useQuery } from "@apollo/react-hooks";
 
-import { Button, Card, Heading2, Heading4, Spinner } from "@socialgouv/emjpm-ui-core";
+import { Button, Card, Heading2, Heading4, Spinner, Select } from "@socialgouv/emjpm-ui-core";
 import { Box, Flex } from "rebass";
 import { Mandatairelist } from "@socialgouv/emjpm-ui-components";
 
+import { FiltersContext } from "../Filters/context";
 import { formatMandatairesList } from "./utils";
 import { GET_MANDATAIRES } from "./queries";
 import { MandatairesListStyle } from "./style";
 
+const optionsType = [
+  { label: "Tous les types", value: null },
+  { label: "Préposé", value: "MANDATAIRE_PRE" },
+  { label: "Individuel", value: "MANDATAIRE_IND" },
+  { label: "Service", value: "SERVICE" }
+];
+
+const optionsCapacity = [
+  { label: "Ascendant", value: "MANDATAIRE_IND" },
+  { label: "Descendant", value: "SERVICE" }
+];
+
 const MandatairesList = props => {
+  const { selectedDepartementValue, selectedRegionalValue } = useContext(FiltersContext);
+  const [selectedType, setType] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
   const { data, error, loading, fetchMore } = useQuery(GET_MANDATAIRES, {
     variables: {
-      offset: 0
+      offset: 0,
+      discriminator: selectedType ? selectedType.value : null,
+      departement: selectedDepartementValue ? selectedDepartementValue.value : null,
+      region: selectedRegionalValue ? selectedRegionalValue.value : null
     }
   });
 
@@ -36,10 +54,30 @@ const MandatairesList = props => {
       </Card>
     );
   }
-
+  console.log(data.mandatairesList);
   const list = formatMandatairesList(data.mandatairesList);
   return (
     <Box sx={MandatairesListStyle} {...props}>
+      <Flex mb="3">
+        <Box width="200px" mr="2">
+          <Select
+            size="small"
+            placeholder="Type de mandataire"
+            onChange={selectedOption => setType(selectedOption)}
+            value={selectedType}
+            options={optionsType}
+          />
+        </Box>
+        <Box width="200px">
+          <Select
+            size="small"
+            placeholder="trier par capacité"
+            onChange={selectedOption => setType(selectedOption)}
+            value={selectedType}
+            options={optionsCapacity}
+          />
+        </Box>
+      </Flex>
       <Mandatairelist mandataires={list} />
       <Flex mt="5" alignItem="center">
         <Button
--- packages/app/src/components-v2/MandatairesList/queries.js
+++ packages/app/src/components-v2/MandatairesList/queries.js
@@ -1,13 +1,26 @@
 import gql from "graphql-tag";
 
 export const GET_MANDATAIRES = gql`
-  query view_mesure_gestionnaire($offset: Int!) {
+  query view_mesure_gestionnaire(
+    $offset: Int!
+    $departement: Int
+    $region: Int
+    $discriminator: String
+  ) {
     count: view_mesure_gestionnaire_aggregate {
       aggregate {
         count
       }
     }
-    mandatairesList: view_mesure_gestionnaire(limit: 10, offset: $offset) {
+    mandatairesList: view_mesure_gestionnaire(
+      limit: 10
+      offset: $offset
+      where: {
+        discriminator: { _eq: $discriminator }
+        departement: { id_region: { _eq: $region }, id: { _eq: $departement } }
+      }
+      order_by: {}
+    ) {
       discriminator
       mesures_awaiting
       mesures_in_progress
