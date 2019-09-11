cdaea09dc61fe2ec5b63ee841cd4f94ba3d1ee09
christophe <christophe@hooraylabs.com>
2019-09-11 13:39:54 +0200

    Feat/mandataires filters (#714)
    * chore(social-gouv): update package version and data accordingly
    
    * feat(mandataires-list): add filters for mandataires list
    
    * feat(mandataires-list-filters): add capacity ordering
    
    * feat(mandataires-list-filters): update show button rule

--- packages/app/src/components-v2/MandatairesList/queries.js
+++ packages/app/src/components-v2/MandatairesList/queries.js
@@ -1,18 +1,38 @@
 import gql from "graphql-tag";
 
 export const GET_MANDATAIRES = gql`
-  query view_mesure_gestionnaire($offset: Int!) {
-    count: view_mesure_gestionnaire_aggregate {
+  query view_mesure_gestionnaire(
+    $offset: Int!
+    $departement: Int
+    $region: Int
+    $discriminator: String
+    $order: order_by
+  ) {
+    count: view_mesure_gestionnaire_aggregate(
+      where: {
+        discriminator: { _eq: $discriminator }
+        departement: { id_region: { _eq: $region }, id: { _eq: $departement } }
+      }
+    ) {
       aggregate {
         count
       }
     }
-    mandatairesList: view_mesure_gestionnaire(limit: 10, offset: $offset) {
+    mandatairesList: view_mesure_gestionnaire(
+      limit: 10
+      offset: $offset
+      where: {
+        discriminator: { _eq: $discriminator }
+        departement: { id_region: { _eq: $region }, id: { _eq: $departement } }
+      }
+      order_by: { remaining_capacity: $order }
+    ) {
       discriminator
       mesures_awaiting
       mesures_in_progress
       mesures_max
       mandataire_id
+      remaining_capacity
       service_id
       mandataire {
         telephone
