2dc074d2b9adbd9a39c0644f036a4c007c358862
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-09-11 13:40:33 +0200

    feat(direction): add columns to mandataire export (#715)

--- packages/app/src/components-v2/MandatairesExport/MandatairesExcel.js
+++ packages/app/src/components-v2/MandatairesExport/MandatairesExcel.js
@@ -1,113 +1,116 @@
 import * as XLSX from "xlsx";
 
+const NON_RENSEIGNE = "Non renseigné";
+
+const cellValue = value => {
+  if (value == undefined) {
+    return NON_RENSEIGNE;
+  }
+  return value;
+};
+
+const remainingCapacity = (max, awaiting, inProgress) => {
+  if (max != undefined && awaiting != undefined && inProgress != undefined) {
+    return max - awaiting - inProgress;
+  }
+  return NON_RENSEIGNE;
+};
+
 const getMandataireRows = (datas, type) => {
   return datas
     .filter(user => user.type === type)
     .map(data => {
+      const inProgress = data.mandataire ? data.mandataire.mesures_en_cours : undefined;
+      const awaiting = data.mandataire ? data.mandataire.mesures_en_attente : undefined;
+      const max = data.mandataire ? data.mandataire.dispo_max : undefined;
       return {
-        Genre: data.mandataire ? data.mandataire.genre : "",
-        Prénom: data.prenom,
-        Nom: data.nom,
-        Adresse: data.mandataire ? data.mandataire.adresse : "",
-        "Code postal": data.mandataire ? data.mandataire.code_postal : "",
-        Ville: data.mandataire ? data.mandataire.ville : "",
-        Télephone: data.mandataire ? data.mandataire.telephone : "",
-        Portable: data.mandataire ? data.mandataire.portable : "",
-        "Mesures en cours": data.mandataire ? data.mandataire.mesures_en_cours : "",
-        "Mesures en attente": data.mandataire ? data.mandataire.mesures_en_attente : "",
-        "Disponibilité max": data.mandataire ? data.mandataire.dispo_max : ""
+        Genre: cellValue(data.mandataire ? data.mandataire.genre : undefined),
+        Prénom: cellValue(data.prenom),
+        Nom: cellValue(data.nom),
+        Adresse: cellValue(data.mandataire ? data.mandataire.adresse : undefined),
+        "Code postal": cellValue(data.mandataire ? data.mandataire.code_postal : undefined),
+        Ville: cellValue(data.mandataire ? data.mandataire.ville : undefined),
+        Télephone: cellValue(data.mandataire ? data.mandataire.telephone : undefined),
+        Portable: cellValue(data.mandataire ? data.mandataire.portable : undefined),
+        "Disponibilité actuelle": remainingCapacity(max, awaiting, inProgress),
+        "Mesures en cours": cellValue(inProgress),
+        "Mesures en attente": cellValue(awaiting),
+        "Disponibilité max": cellValue(max)
       };
     });
 };
 
 const getServiceRows = datas => {
   return datas.map(data => {
+    const inProgress = data.service_antennes.reduce(
+      (acc, current) => acc + current.mesures_in_progress,
+      0
+    );
+    const awaiting = data.service_antennes.reduce(
+      (acc, current) => acc + current.mesures_awaiting,
+      0
+    );
+    const max = data.dispo_max;
     return {
-      Nom: data.etablissement,
-      Adresse: data.adresse,
-      "Code postal": data.code_postal,
-      Ville: data.ville,
-      Télephone: data.telephone,
-      "Mesures en cours": data.service_antennes.reduce(
-        (acc, current) => acc + current.mesures_in_progress,
-        0
-      ),
-      "Mesures en attente": data.service_antennes.reduce(
-        (acc, current) => acc + current.mesures_awaiting,
-        0
-      ),
-      "Disponibilité max": data.dispo_max
+      Nom: cellValue(data.etablissement),
+      Adresse: cellValue(data.adresse),
+      "Code postal": cellValue(data.code_postal),
+      Ville: cellValue(data.ville),
+      Email: cellValue(data.email),
+      Télephone: cellValue(data.telephone),
+      "Nom du contact": cellValue(data.nom),
+      "Prénom du contact": cellValue(data.prenom),
+      "Disponibilité actuelle": remainingCapacity(max, awaiting, inProgress),
+      "Mesures en cours": cellValue(inProgress),
+      "Mesures en attente": cellValue(awaiting),
+      "Disponibilité max": cellValue(max),
+      "Nombre d'antenne": data.service_antennes.length
     };
   });
 };
 
-export const exportMandataires = (users, services, regionalValue, departementalValue) => {
+const writeSheet = (wb, region, department, title, datas) => {
+  const filterData = [
+    {
+      Région: region ? region.label : "Aucun filtre",
+      Département: department ? department.label : "Aucun filtre"
+    }
+  ];
+
+  wb.SheetNames.push(title);
+  wb.Sheets[title] = XLSX.utils.sheet_add_json(wb.Sheets[title], [{ title: title }], {
+    skipHeader: true,
+    origin: "B2"
+  });
+  wb.Sheets[title] = XLSX.utils.sheet_add_json(wb.Sheets[title], filterData, {
+    skipHeader: false,
+    origin: "B4"
+  });
+  wb.Sheets[title] = XLSX.utils.sheet_add_json(wb.Sheets[title], datas, {
+    skipHeader: false,
+    origin: "B7"
+  });
+};
+
+export const exportMandataires = (users, services, region, department) => {
   const wb = XLSX.utils.book_new();
 
   wb.Props = {
     Title: "e-EMJPM - export des mandataires"
   };
 
-  const indTitle = "Mandataires individuels";
-  const preTitle = "Mandataires préposés";
-  const serviceTitle = "Services";
-
-  const exportDataOptions = {
-    skipHeader: false,
-    origin: "B2"
-  };
-  const exportFilterOptions = {
-    skipHeader: false,
-    origin: "N2"
-  };
-
-  const filterData = [
-    {
-      Région: regionalValue ? regionalValue.label : "Aucun filtre",
-      Département: departementalValue ? departementalValue.label : "Aucun filtre"
-    }
-  ];
-
-  const individuelsData = getMandataireRows(users, "individuel");
-  wb.SheetNames.push(indTitle);
-  wb.Sheets[indTitle] = XLSX.utils.sheet_add_json(
-    wb.Sheets[indTitle],
-    filterData,
-    exportFilterOptions
+  writeSheet(
+    wb,
+    region,
+    department,
+    "Mandataires individuels",
+    getMandataireRows(users, "individuel")
   );
-  wb.Sheets[indTitle] = XLSX.utils.sheet_add_json(
-    wb.Sheets[indTitle],
-    individuelsData,
-    exportDataOptions
-  );
+  writeSheet(wb, region, department, "Mandataires préposés", getMandataireRows(users, "prepose"));
+  writeSheet(wb, region, department, "Services", getServiceRows(services));
 
-  const preposesData = getMandataireRows(users, "prepose");
-  wb.SheetNames.push(preTitle);
-  wb.Sheets[preTitle] = XLSX.utils.sheet_add_json(
-    wb.Sheets[preTitle],
-    filterData,
-    exportFilterOptions
-  );
-  wb.Sheets[preTitle] = XLSX.utils.sheet_add_json(
-    wb.Sheets[preTitle],
-    preposesData,
-    exportDataOptions
-  );
-
-  const sercicesData = getServiceRows(services);
-  wb.SheetNames.push(serviceTitle);
-  wb.Sheets[serviceTitle] = XLSX.utils.sheet_add_json(
-    wb.Sheets[serviceTitle],
-    filterData,
-    exportFilterOptions
-  );
-  wb.Sheets[serviceTitle] = XLSX.utils.sheet_add_json(
-    wb.Sheets[serviceTitle],
-    sercicesData,
-    exportDataOptions
-  );
-
-  const region = regionalValue ? regionalValue.label : "France";
-  const departement = departementalValue ? departementalValue.label : undefined;
-  XLSX.writeFile(wb, `eEMJPM - Mandataire_${departement ? departement : region}.xlsx`);
+  const regionLabel = region ? region.label : "France";
+  const departementLabel = department ? department.label : undefined;
+  const filterLabel = departementLabel ? departementLabel : regionLabel;
+  XLSX.writeFile(wb, `eEMJPM - Mandataire_${filterLabel}.xlsx`);
 };
