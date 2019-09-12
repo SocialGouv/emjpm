fb27e731897179f534b2f871d0097f7a544de892
Douglas Duteil <douglasduteil@gmail.com>
2019-09-12 14:24:37 +0200

    refactor(k8s): multi namespaces (#727)

--- .k8s/postgres/check_init_postgres.sh
+++ .k8s/postgres/check_init_postgres.sh
@@ -5,9 +5,9 @@ INIT_POSTGRES_POD_STATUS=$(kubectl get pod emjpm-init-postgres"$1")
 # Check if emjpm-init-postgres pod exists
 if [ ! "$INIT_POSTGRES_POD_STATUS" ]
 then
-    kubectl apply -f k8s/postgres/pod-init-postgres-emjpm.yml -n emjpm-feature;
+    kubectl apply -f k8s/postgres/pod-init-postgres-emjpm.yml -n ${K8S_NAMESPACE};
 else
-    kubectl delete pod emjpm-init-postgres"$1" -n emjpm-feature;
-    kubectl apply -f k8s/postgres/pod-init-postgres-emjpm.yml -n emjpm-feature;
+    kubectl delete pod emjpm-init-postgres"$1" -n ${K8S_NAMESPACE};
+    kubectl apply -f k8s/postgres/pod-init-postgres-emjpm.yml -n ${K8S_NAMESPACE};
 fi
 
