b86b8e354888c69536af87efe604e2e1889d4fd2
kwiss <christophe@hooraylabs.com>
2019-09-13 12:13:23 +0200

    fix(services-mesures-metadata): update services mesures permissions

--- packages/app/src/components-v2/ServiceMesures/queries.js
+++ packages/app/src/components-v2/ServiceMesures/queries.js
@@ -3,7 +3,7 @@ import gql from "graphql-tag";
 export const MESURES = gql`
   query mesures($antenne: Int, $type: String, $status: String) {
     mesures(
-      limit: 10
+      limit: 20
       where: { status: { _eq: $status }, type: { _eq: $type }, antenne_id: { _eq: $antenne } }
     ) {
       antenne_id
