0cb1837909ed88978688cf78834bea650bbae118
christophe <christophe@hooraylabs.com>
2019-09-13 21:14:51 +0200

    Feat/service mesure list (#734)
    * feat(service-mesure-list): add mesure list pagination
    
    * feat(service-mesure-list): add empty mesure list
    
    * feat(service-mesure-list): fix style
    
    * feat(service-mesures-list): add pagination contrain and reset

--- packages/app/src/components-v2/ServiceMesures/ServiceMesures.js
+++ packages/app/src/components-v2/ServiceMesures/ServiceMesures.js
@@ -1,6 +1,11 @@
 import { useQuery } from "@apollo/react-hooks";
 import { MesureList } from "@socialgouv/emjpm-ui-components";
-import React, { useContext } from "react";
+import React, { Fragment, useContext, useState, useEffect } from "react";
+import { Flex, Box } from "rebass";
+import { Button } from "@socialgouv/emjpm-ui-core";
+
+import { MesureListStyle } from "./style";
+import { formatMesureList } from "./utils";
 import { FiltersContext } from "../ServicesFilters/context";
 import { MESURES } from "./queries";
 
@@ -9,11 +14,17 @@ const Link = props => {
   return <a href={href}>{children}</a>;
 };
 
+const RESULT_PER_PAGE = 20;
+
 const ServiceMesures = () => {
+  const [currentPage, setCurrentPage] = useState(0);
   const { mesureType, mesureStatus, antenne } = useContext(FiltersContext);
 
-  const { data, error, loading } = useQuery(MESURES, {
+  useEffect(() => setCurrentPage(RESULT_PER_PAGE), [mesureType, mesureStatus, antenne]);
+  const { data, error, loading, fetchMore } = useQuery(MESURES, {
     variables: {
+      offset: 0,
+      limit: RESULT_PER_PAGE,
       antenne: antenne ? antenne.value : undefined,
       type: mesureType ? mesureType.value : undefined,
       status: mesureStatus ? mesureStatus.value : undefined
@@ -28,27 +39,46 @@ const ServiceMesures = () => {
     return <div>error</div>;
   }
 
-  const mesures = data.mesures.map(mesure => {
-    const { type, ville, status, id, annee, civilite, date_ouverture, numero_rg } = mesure;
-    return {
-      age: annee ? annee : "nc",
-      civilite: civilite ? civilite : "H",
-      dateOuverture: date_ouverture ? date_ouverture : "non reseigné",
-      href: `/services/mesure/${id}/`,
-      id: id,
-      numeroRg: numero_rg ? numero_rg : "RG-00000000",
-      status: status ? status : "non reseigné",
-      type: type ? type : "type de mesure non reseigné",
-      ville: ville ? ville : "ville non reseigné"
-    };
-  });
-
+  const { count } = data.mesures_aggregate.aggregate;
+  const mesures = formatMesureList(data.mesures);
   return (
-    <MesureList
-      LinkComponent={props => <Link {...props} />}
-      validate={id => console.log(id)}
-      mesures={mesures}
-    />
+    <Box sx={MesureListStyle}>
+      <Fragment>
+        {mesures.length > 0 ? (
+          <Fragment>
+            <MesureList
+              LinkComponent={props => <Link {...props} />}
+              validate={id => console.log(id)}
+              mesures={mesures}
+            />
+            {count > RESULT_PER_PAGE && count > currentPage && (
+              <Flex mt="5" alignItem="center">
+                <Button
+                  onClick={() => {
+                    fetchMore({
+                      variables: {
+                        offset: currentPage + RESULT_PER_PAGE
+                      },
+                      updateQuery: (prev, { fetchMoreResult }) => {
+                        setCurrentPage(currentPage + RESULT_PER_PAGE);
+                        return {
+                          count: fetchMoreResult.count,
+                          mesures: [...prev.mesures, ...fetchMoreResult.mesures]
+                        };
+                      }
+                    });
+                  }}
+                >
+                  Afficher les mandataires suivants
+                </Button>
+              </Flex>
+            )}
+          </Fragment>
+        ) : (
+          <div>Pas de donnée à afficher</div>
+        )}
+      </Fragment>
+    </Box>
   );
 };
 
