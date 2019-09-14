36f8fe5d64195a7972c847ec2eac04b859f4484c
christophe <christophe@hooraylabs.com>
2019-09-14 03:20:53 +0200

    feat(service-reactivate-mesure): add reactivate mutation (#745)

--- packages/app/src/components-v2/ServiceMesures/ServiceReactivateMesure.js
+++ packages/app/src/components-v2/ServiceMesures/ServiceReactivateMesure.js
@@ -5,21 +5,20 @@ import { Formik } from "formik";
 import { useMutation } from "@apollo/react-hooks";
 import * as Yup from "yup";
 
-import { CLOSE_MESURE } from "./mutations";
+import { REACTIVATE_MESURE } from "./mutations";
 import { Button, Input, Heading3, Heading5 } from "@socialgouv/emjpm-ui-core";
 import { MesureContext, PANEL_TYPE } from "@socialgouv/emjpm-ui-components";
 
-export const ServiceCloseMesure = props => {
+export const ServiceReactivateMesure = props => {
   const { currentMesure } = props;
-  const [UpdateMesure] = useMutation(CLOSE_MESURE);
+  const [UpdateMesure] = useMutation(REACTIVATE_MESURE);
   const { setCurrentMesure, setPanelType } = useContext(MesureContext);
   return (
     <Flex flexWrap="wrap">
       <Box bg="cardSecondary" p="5" width={[1, 2 / 5]}>
-        <Heading5 mb="1">Mettre fin au mandat</Heading5>
+        <Heading5 mb="1">Réactiver la mesure</Heading5>
         <Text lineHeight="1.5">
-          {`On parle de fermeture d'une mesure, d'extinction d'une mesure ou de "fin de mandat pour dire que la mesure de protection a pris fin. Suite à ça, on parle de "mesure éteinte". Pour les raisons de traçabilité
-          évoquée ci-dessus, une mesure éteinte n'est pas supprimée.`}
+          {`En cliquant sur le bouton "réactiver la mesure", celle-ci redevient active, c'est-à-dire qu'elle reprend sa place dans les "mesures en cours" de votre service, elle est comptabilisée comme une "mesure en cours" pour l'ensemble des statistiques, apparaît sur les cartes et est décomptée dans la disponibilité du services.`}
         </Text>
       </Box>
       <Box p="5" width={[1, 3 / 5]}>
@@ -32,8 +31,7 @@ export const ServiceCloseMesure = props => {
               UpdateMesure({
                 variables: {
                   id: currentMesure,
-                  reason_extinction: values.reason_extinction,
-                  extinction: values.extinction
+                  reason_extinction: values.reason_extinction
                 }
               });
               setSubmitting(false);
@@ -42,10 +40,9 @@ export const ServiceCloseMesure = props => {
             }, 500);
           }}
           validationSchema={Yup.object().shape({
-            extinction: Yup.date().required("Required"),
             reason_extinction: Yup.string().required("Required")
           })}
-          initialValues={{ reason_extinction: "", extinction: "" }}
+          initialValues={{ reason_extinction: "" }}
         >
           {props => {
             const { values, touched, errors, isSubmitting, handleChange, handleSubmit } = props;
@@ -53,23 +50,12 @@ export const ServiceCloseMesure = props => {
               <form onSubmit={handleSubmit}>
                 <Box mb="2">
                   <Input
-                    value={values.extinction}
-                    id="extinction"
-                    name="extinction"
-                    hasError={errors.extinction && touched.extinction}
-                    type="date"
-                    onChange={handleChange}
-                    placeholder="Date d'extinction"
-                  />
-                </Box>
-                <Box mb="2">
-                  <Input
                     value={values.reason_extinction}
                     hasError={errors.reason_extinction && touched.reason_extinction}
                     id="reason_extinction"
                     name="reason_extinction"
                     onChange={handleChange}
-                    placeholder="Raison de l'extinction"
+                    placeholder="Raison de la réactivation"
                   />
                 </Box>
                 <Flex justifyContent="flex-end">
@@ -87,7 +73,7 @@ export const ServiceCloseMesure = props => {
                   </Box>
                   <Box>
                     <Button type="submit" disabled={isSubmitting} isLoading={isSubmitting}>
-                      Confirmer la fin du mandat
+                      Réactiver la mesure
                     </Button>
                   </Box>
                 </Flex>
@@ -100,6 +86,6 @@ export const ServiceCloseMesure = props => {
   );
 };
 
-ServiceCloseMesure.propTypes = {
+ServiceReactivateMesure.propTypes = {
   currentMesure: PropTypes.number.isRequired
 };
