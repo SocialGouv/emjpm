dedc2284899b7bc7e8ef42a700b3a85d45900330
christophe <christophe@hooraylabs.com>
2019-09-14 03:20:53 +0200

    feat(service-reactivate-mesure): add reactivate mutation (#745)

--- packages/app/src/components-v2/ServiceMesures/mutations.js
+++ packages/app/src/components-v2/ServiceMesures/mutations.js
@@ -1,7 +1,7 @@
 import gql from "graphql-tag";
 
-export const UPDATE_MESURE = gql`
-  mutation UpdateMesure($id: Int!, $reason_extinction: String!, $extinction: date!) {
+export const CLOSE_MESURE = gql`
+  mutation closeMesure($id: Int!, $reason_extinction: String!, $extinction: date!) {
     update_mesures(
       where: { id: { _eq: $id } }
       _set: {
@@ -9,6 +9,38 @@ export const UPDATE_MESURE = gql`
         reason_extinction: $reason_extinction
         status: "Eteindre mesure"
       }
+    ) {
+      returning {
+        antenne_id
+        id
+        cabinet
+        civilite
+        code_postal
+        departement {
+          nom
+          region {
+            nom
+          }
+        }
+        status
+        type
+        ville
+        residence
+        numero_rg
+        numero_dossier
+        etablissement
+        annee
+        date_ouverture
+      }
+    }
+  }
+`;
+
+export const REACTIVATE_MESURE = gql`
+  mutation reactivateMesure($id: Int!, $reason_extinction: String!) {
+    update_mesures(
+      where: { id: { _eq: $id } }
+      _set: { reason_extinction: $reason_extinction, status: "Mesure en cours" }
     ) {
       returning {
         antenne_id
