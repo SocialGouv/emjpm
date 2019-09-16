b417410e6cf72aa42772e7eaf3bdda466e7393db
christophe <christophe@hooraylabs.com>
2019-09-16 13:07:21 +0200

    feat(service-accept-mesure): add base data component (#752)

--- packages/app/src/components-v2/ServiceMesures/mutations.js
+++ packages/app/src/components-v2/ServiceMesures/mutations.js
@@ -68,6 +68,50 @@ export const REACTIVATE_MESURE = gql`
   }
 `;
 
+export const ACCEPT_MESURE = gql`
+  mutation editMesure(
+    $id: Int!
+    $date_ouverture: date
+    $residence: String
+    $code_postal: String
+    $ville: String
+  ) {
+    update_mesures(
+      where: { id: { _eq: $id } }
+      _set: {
+        status: "Mesure en cours"
+        date_ouverture: $date_ouverture
+        residence: $residence
+        code_postal: $code_postal
+        ville: $ville
+      }
+    ) {
+      returning {
+        antenne_id
+        id
+        cabinet
+        civilite
+        code_postal
+        departement {
+          nom
+          region {
+            nom
+          }
+        }
+        status
+        type
+        ville
+        residence
+        numero_rg
+        numero_dossier
+        etablissement
+        annee
+        date_ouverture
+      }
+    }
+  }
+`;
+
 export const DELETE_MESURE = gql`
   mutation deleteMesure($id: Int!) {
     delete_mesures(where: { id: { _eq: $id } }) {
