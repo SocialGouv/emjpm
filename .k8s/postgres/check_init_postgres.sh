3a14583bc16822206de5a1edffdbff2a776e0ae6
Douglas Duteil <douglasduteil@gmail.com>
2019-09-16 11:07:41 +0200

    build(k8s): rollback to v23.x config

--- .k8s/postgres/check_init_postgres.sh
+++ .k8s/postgres/check_init_postgres.sh
@@ -5,9 +5,9 @@ INIT_POSTGRES_POD_STATUS=$(kubectl get pod emjpm-init-postgres"$1")
 # Check if emjpm-init-postgres pod exists
 if [ ! "$INIT_POSTGRES_POD_STATUS" ]
 then
-    kubectl apply -f k8s/postgres/pod-init-postgres-emjpm.yml -n ${K8S_NAMESPACE};
+    kubectl apply -f k8s/postgres/pod-init-postgres-emjpm.yml
 else
-    kubectl delete pod emjpm-init-postgres"$1" -n ${K8S_NAMESPACE};
-    kubectl apply -f k8s/postgres/pod-init-postgres-emjpm.yml -n ${K8S_NAMESPACE};
+    kubectl delete pod emjpm-init-postgres"$1"
+    kubectl apply -f k8s/postgres/pod-init-postgres-emjpm.yml
 fi
 
--- .k8s/postgres/inject_data_postgres.sh
+++ .k8s/postgres/inject_data_postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-INJECT_DATA_POSTGRES_POD_STATUS=$(kubectl get pod "${K8S_PROJECT}"-dataset-"$1" -n ${K8S_NAMESPACE})
+INJECT_DATA_POSTGRES_POD_STATUS=$(kubectl get pod "${K8S_NAMESPACE}"-dataset-"$1")
 
 printenv | grep -E "DATABASE_URL" | sort
 
@@ -8,9 +8,9 @@ printenv | grep -E "DATABASE_URL" | sort
 # Check if emjpm-dataset pod exists
 if [ ! "$INJECT_DATA_POSTGRES_POD_STATUS" ]
 then
-    kubectl apply -f ./pod-inject-dataset.yml -n ${K8S_NAMESPACE};
+    kubectl apply -f ./pod-inject-dataset.yml
 else
-    kubectl delete pod "${K8S_PROJECT}"-dataset-"$1" -n ${K8S_NAMESPACE};
-    kubectl apply -f ./pod-inject-dataset.yml -n ${K8S_NAMESPACE};
+    kubectl delete pod "${K8S_NAMESPACE}"-dataset-"$1"
+    kubectl apply -f ./pod-inject-dataset.yml
 fi
 
