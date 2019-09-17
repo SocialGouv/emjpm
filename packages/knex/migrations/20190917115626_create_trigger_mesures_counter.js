c300b135563b2dac1b5501e8b05f69e556390193
Thomas Glatt <tglatt@gmail.com>
2019-09-17 14:49:58 +0200

    feat(mesure-counter): create trigger to update mesure counters

--- packages/knex/migrations/20190917115626_create_trigger_mesures_counter.js
+++ packages/knex/migrations/20190917115626_create_trigger_mesures_counter.js
@@ -1,38 +1,51 @@
 exports.up = function(knex) {
   return knex.raw(`
-CREATE TRIGGER on_mesures_change
-	AFTER update or insert or delete ON mesures
-	FOR EACH ROW
-	EXECUTE PROCEDURE update_mesure_counter();
-
-CREATE OR REPLACE FUNCTION update_mesure_counter()
-  RETURNS trigger AS $$
+  CREATE OR REPLACE FUNCTION update_mandataire_counters(mandataire_id integer) RETURNS void AS $$
   BEGIN
-	IF OLD.mandataire_id is not null THEN
-		UPDATE mANDataires SET mesures_en_cours = (
-			SELECT count(mesures.id) FROM mesures WHERE mesures.mandataire_id = mANDataires.id AND mesures.status = 'Mesure en cours'
-		) WHERE id = OLD.mandataire_id;
-		UPDATE mANDataires SET mesures_en_attente = (
-			SELECT count(mesures.id) FROM mesures WHERE mesures.mandataire_id = mANDataires.id AND mesures.status = 'Mesure en attente'
-		) WHERE id = OLD.mandataire_id;
-	END IF;
-	IF OLD.antenne_id is not null THEN
-		UPDATE service_antenne SET mesures_in_progress = (
-			SELECT count(mesures.id) FROM mesures WHERE mesures.antenne_id = service_antenne.id AND mesures.status = 'Mesure en cours'
-		) WHERE id = OLD.antenne_id;
-		UPDATE service_antenne SET mesures_awaiting = (
-			SELECT count(mesures.id) FROM mesures WHERE mesures.antenne_id = service_antenne.id AND mesures.status = 'Mesure en attente'
-		) WHERE id = OLD.antenne_id;
-	END IF;
-	return OLD;
+   UPDATE mandataires SET mesures_en_cours = (
+     SELECT count(mesures.id) FROM mesures WHERE mesures.mandataire_id = mandataires.id AND mesures.status = 'Mesure en cours'
+   ) WHERE id = mandataire_id;
+   UPDATE mandataires SET mesures_en_attente = (
+     SELECT count(mesures.id) FROM mesures WHERE mesures.mandataire_id = mandataires.id AND mesures.status = 'Mesure en attente'
+   ) WHERE id = mandataire_id;
   END;
-$$ language 'plpgsql';
+ $$ language 'plpgsql';
+
+ CREATE OR REPLACE FUNCTION update_antenne_counters(antenne_id integer) RETURNS void AS $$
+  BEGIN
+   UPDATE service_antenne SET mesures_in_progress = (
+    SELECT count(mesures.id) FROM mesures WHERE mesures.antenne_id = service_antenne.id AND mesures.status = 'Mesure en cours'
+   ) WHERE id = antenne_id;
+   UPDATE service_antenne SET mesures_awaiting = (
+    SELECT count(mesures.id) FROM mesures WHERE mesures.antenne_id = service_antenne.id AND mesures.status = 'Mesure en attente'
+   ) WHERE id = antenne_id;
+  END;
+ $$ language 'plpgsql';
+
+ CREATE OR REPLACE FUNCTION update_gestionnaire_counters() RETURNS trigger AS $$
+  BEGIN
+   IF (TG_OP = 'UPDATE' and new.antenne_id is not null) THEN perform update_antenne_counters(new.antenne_id); return new;
+   elseif (TG_OP = 'UPDATE' and new.mandataire_id is not null) then perform  update_mandataire_counters(new.mandataire_id); return new;
+   elseif (TG_OP = 'INSERT' and new.antenne_id is not null) then perform  update_antenne_counters(new.antenne_id); return new;
+   elseif (TG_OP = 'INSERT' and new.mandataire_id is not null) then perform  update_mandataire_counters(new.mandataire_id); return new;
+   elseif (TG_OP = 'DELETE' and old.antenne_id is not null) then perform  update_antenne_counters(old.antenne_id); return old;
+   elseif (TG_OP = 'DELETE' and old.mandataire_id is not null) then perform  update_mandataire_counters(old.mandataire_id); return old;
+   end if;
+  END;
+ $$ language 'plpgsql';
+
+CREATE TRIGGER on_update_mesures
+ AFTER update or delete or insert ON mesures
+ FOR EACH row
+ EXECUTE PROCEDURE update_gestionnaire_counters();
   `);
 };
 
 exports.down = function(knex) {
   return knex.raw(`
-DROP TRIGGER on_mesures_change ON mesures;
-DROP FUNCTION update_mesure_counter;
+DROP TRIGGER on_update_mesures ON mesures;
+DROP FUNCTION update_gestionnaire_counters;
+DROP FUNCTION update_antenne_counters;
+DROP FUNCTION update_mandataire_counters;
   `);
 };
