31b32afb8530c1607f33af922db038cb82c6fff2
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-09-18 16:34:24 +0200

    deat(direction): update after review (#771)
    * feat(direction): display all departments and add sort
    
    * feat(active-link): active link for nested url

--- packages/app/src/util/option/OptionUtil.js
+++ packages/app/src/util/option/OptionUtil.js
@@ -1,9 +1,26 @@
 export const regionsToOptions = datas => {
-  return toOptions(datas, "id", "nom");
+  const all = [
+    {
+      label: "Toutes les régions",
+      value: null
+    }
+  ];
+  Array.prototype.push.apply(all, toOptions(datas, "id", "nom"));
+  return all;
 };
 
 export const departementToOptions = datas => {
-  return toOptions(datas, "id", "nom");
+  const all = [
+    {
+      label: "Tous les départements",
+      value: null
+    }
+  ];
+  Array.prototype.push.apply(all, toOptions(datas, "id", "nom"));
+  all.sort(function(a, b) {
+    return a.label - b.label;
+  });
+  return all;
 };
 
 export const toOptions = (datas, idKey, labelKey) =>
