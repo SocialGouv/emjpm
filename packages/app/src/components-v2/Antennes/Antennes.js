42858874ab73aba0611e507e5796c771ffbd9fe7
kwiss-macbook <christophe@hooraylabs.com>
2019-09-19 17:19:25 +0200

    feat(service-antenne): add component and query

--- packages/app/src/components-v2/Antennes/Antennes.js
+++ packages/app/src/components-v2/Antennes/Antennes.js
@@ -1,16 +1,47 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
 import { Box } from "rebass";
-import { Card, BoxWrapper } from "@socialgouv/emjpm-ui-core";
+import { Card, Text, BoxWrapper, Spinner, Heading4 } from "@socialgouv/emjpm-ui-core";
+
+import { ANTENNE } from "./queries";
 
 import { AntennesStyle } from "./style";
 
 const Antennes = props => {
+  const { data, loading, error } = useQuery(ANTENNE);
+
+  if (loading) {
+    return (
+      <Card p="1" minHeight="450px">
+        <Box sx={{ position: "relative", p: "6" }}>
+          <Spinner />
+        </Box>
+      </Card>
+    );
+  }
+
+  if (error) {
+    return (
+      <Card p="1">
+        <Box sx={{ position: "relative", p: "6" }}>error</Box>
+      </Card>
+    );
+  }
+
+  const { service_antenne } = data;
+  console.log(service_antenne);
   return (
     <BoxWrapper>
       <Box sx={AntennesStyle} {...props}>
-        <Card>Tile</Card>
-        <Card>Tile</Card>
-        <Card>Tile</Card>
+        {service_antenne.map(antenne => {
+          return (
+            <Card key={antenne.id}>
+              <Heading4 sx={{ textAlign: "center" }}>{antenne.name}</Heading4>
+              <Text>{antenne.mesures_max} mesures souhaitées</Text>
+              <Text>{antenne.mesures_in_progress} mesures en cours</Text>
+            </Card>
+          );
+        })}
       </Box>
     </BoxWrapper>
   );
---
+++ packages/app/src/components-v2/Antennes/queries.js
@@ -0,0 +1,25 @@
+import gql from "graphql-tag";
+
+export const ANTENNE = gql`
+  {
+    service_antenne {
+      name
+      mesures_aggregate {
+        aggregate {
+          count
+        }
+      }
+      mesures_max
+      mesures_in_progress
+      mesures_awaiting
+      headquarters
+      address_city
+      address_street
+      address_zip_code
+      contact_email
+      contact_firstname
+      contact_lastname
+      contact_phone
+    }
+  }
+`;
