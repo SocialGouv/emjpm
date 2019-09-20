c695271859b548db78863f623f6bf3919aec1b0d
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-09-20 11:58:58 +0200

    chore(menu-link): comment non implemented menu links (#797)

--- packages/app/src/components-v2/Layout/dropDownLink.js
+++ packages/app/src/components-v2/Layout/dropDownLink.js
@@ -1,5 +1,5 @@
 export const dropDownLinks = [
-  { title: "Centre d'assistance", url: "https://emjpm-blog.azurewebsites.net" },
-  { title: "Profil", url: "test" },
-  { title: "Paramètres", url: "test" }
+  { title: "Centre d'assistance", url: "https://emjpm-blog.azurewebsites.net" }
+  // { title: "Profil", url: "test" },
+  // { title: "Paramètres", url: "test" }
 ];
