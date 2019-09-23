7e00370d49aa87ca52c8978624cdc57277c4ae1d
christophe <christophe@hooraylabs.com>
2019-09-23 18:07:13 +0200

    feat(informations-panel): add panel and fixes (#821)

--- packages/app/pages/magistrats/index.js
+++ packages/app/pages/magistrats/index.js
@@ -1,16 +1,46 @@
-import { BoxWrapper, Heading1 } from "@socialgouv/emjpm-ui-core";
-import React from "react";
-import { Flex } from "rebass";
+import { BoxWrapper, Heading1, Card, Text, Heading3, Button } from "@socialgouv/emjpm-ui-core";
+import React, { useState } from "react";
+import { Flex, Box } from "rebass";
 
 import { withAuthSync } from "../../src/util/auth";
 import { LayoutMagistrat } from "../../src/components-v2/Layout";
 import { MagistratMandatairesList } from "../../src/components-v2/MagistratMandatairesList";
 import { UserInformations } from "../../src/components-v2/UserInformations";
 
+const cardStyle = isHidden => {
+  return {
+    display: isHidden ? "none" : "block"
+  };
+};
+
 const Mandataires = () => {
+  const [isHidden, togglePanel] = useState(false);
   return (
     <LayoutMagistrat>
       <BoxWrapper mt={6} px="1">
+        <Card sx={cardStyle(isHidden)} mb="5">
+          <Heading3 mb="2">{`Emjpm s'améliore`}</Heading3>
+          <Text mb="1" lineHeight="1.5">
+            {`Nous vous proposons une nouvelle version de notre plateforme e-MJPM. Cette nouvelle
+            formule vous propose une nouvelle ergonomie ainsi que plusieurs nouvelles
+            fonctionnalités issues directement de vos retours.`}
+          </Text>
+          <Text mb="1" lineHeight="1.5">
+            {`La version du profil Magistrats
+            actuelle vous permet de réserver des mesures auprès des mandataires agréés sur votre
+            tribunal d'instance.`}
+          </Text>
+          <Text mb="1" lineHeight="1.5">
+            {`Les fonctionnalités de géolocalisation sont en cours d'amélioration et vous seront
+            proposées dans quelques jours, début octobre. Vos retours sur ces améliorations sont les
+            bienvenus`}
+          </Text>
+          <Flex flexDirection="row" justifyContent="flex-end">
+            <Box>
+              <Button variant="outline" onClick={() => togglePanel(true)}>{`J'ai compris`}</Button>
+            </Box>
+          </Flex>
+        </Card>
         <Heading1>Tous les mandataires</Heading1>
         <Flex
           sx={{
