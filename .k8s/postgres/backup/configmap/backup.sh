be2b72d0a4a41820329a85589e29d3c8e9941726
Douglas Duteil <douglasduteil@gmail.com>
2019-09-23 10:29:23 +0200

    fix(k8s): change mounting points

--- .k8s/postgres/backup/configmap/backup.sh
+++ .k8s/postgres/backup/configmap/backup.sh
@@ -4,4 +4,4 @@ export TIMESTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
 
 pg_dump --version
 
-pg_dump --verbose -Fc emjpm > /tmp/backup_emjpm_${TIMESTAMP}.dump
+pg_dump --verbose -Fc emjpm > /mnt/data/backup_emjpm_${TIMESTAMP}.dump
