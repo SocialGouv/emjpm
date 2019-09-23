e1c442660e94147d0bcfb1ab9e7f169b6c38df8d
christophe <christophe@hooraylabs.com>
2019-09-23 18:07:13 +0200

    feat(informations-panel): add panel and fixes (#821)

--- packages/app/pages/services/index.js
+++ packages/app/pages/services/index.js
@@ -1,6 +1,6 @@
-import { BoxWrapper, Heading1 } from "@socialgouv/emjpm-ui-core";
-import React from "react";
-import { Flex } from "rebass";
+import { BoxWrapper, Heading1, Card, Text, Heading3, Button } from "@socialgouv/emjpm-ui-core";
+import React, { useState } from "react";
+import { Flex, Box } from "rebass";
 
 import { withAuthSync } from "../../src/util/auth";
 import { LayoutServices } from "../../src/components-v2/Layout";
@@ -9,11 +9,40 @@ import { ServicesFilters } from "../../src/components-v2/ServicesFilters";
 import { FiltersContextProvider } from "../../src/components-v2/ServicesFilters/context";
 import { UserInformations } from "../../src/components-v2/UserInformations";
 
+const cardStyle = isHidden => {
+  return {
+    display: isHidden ? "none" : "block"
+  };
+};
+
 const Mesures = () => {
+  const [isHidden, togglePanel] = useState(false);
   return (
     <FiltersContextProvider>
       <LayoutServices>
         <BoxWrapper mt={6} px="1">
+          <Card sx={cardStyle(isHidden)} mb="5">
+            <Heading3 mb="2">{`Emjpm s'améliore`}</Heading3>
+            <Text mb="1" lineHeight="1.5">
+              {`Nous vous proposons une nouvelle version de notre plateforme e-MJPM. Cette nouvelle
+            formule vous propose une nouvelle ergonomie ainsi que plusieurs nouvelles
+            fonctionnalités issues directement de vos retours.`}
+            </Text>
+            <Text mb="1" lineHeight="1.5">
+              {`La version du profil Service à venir dans les prochains jours vous permettra de créer des subdivisions (par exemple pour vos antennes) et d'y administrer des mesures.`}
+            </Text>
+            <Text mb="1" lineHeight="1.5">
+              {`Courant octobre nous vous proposerons une fonctionnalité permettant d'attribuer des administrateurs pour chaque subdivision, l'import excel revue ainsi qu'une visualisation des mesures géolocalisées`}
+            </Text>
+            <Flex flexDirection="row" justifyContent="flex-end">
+              <Box>
+                <Button
+                  variant="outline"
+                  onClick={() => togglePanel(true)}
+                >{`J'ai compris`}</Button>
+              </Box>
+            </Flex>
+          </Card>
           <Heading1>Toutes vos mesures</Heading1>
           <UserInformations Component={props => <ServicesFilters {...props} />} />
           <Flex
