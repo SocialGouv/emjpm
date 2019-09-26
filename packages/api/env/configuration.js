2a3bb2650228704d74e3de7f933d6d0ea2e035de
Thomas Glatt <tglatt@gmail.com>
2019-09-26 16:41:29 +0200

    feat(api): introduce env configuration

---
+++ packages/api/env/configuration.js
@@ -0,0 +1,11 @@
+
+
+exports.getConfiguration = env => ({
+  sentryPublicDSN: env.SENTRY_PUBLIC_DSN,
+  sentryEnvironment: process.env.SENTRY_ENV,
+  smtpHost: process.env.SMTP_HOST || "127.0.0.1",
+  smtpPort: process.env.SMTP_PORT || "25",
+  smtpUser: process.env.SMTP_USER,
+  smtpPass: process.env.SMTP_PASS,
+  smtpFrom: process.env.SMTP_FROM
+});
