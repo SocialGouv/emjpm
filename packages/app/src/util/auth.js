2624f330e22e53e1a6d5271c0f23a164318fd694
christophe <christophe@hooraylabs.com>
2019-09-26 10:32:33 +0200

    Fix/navigation (#844)
    * fix(navigation): add missing layout fix disconnect next js bug
    
    * fix: remove console.log

--- packages/app/src/util/auth.js
+++ packages/app/src/util/auth.js
@@ -4,12 +4,12 @@ import nextCookie from "next-cookies";
 import cookie from "js-cookie";
 
 export const logout = () => {
+  Router.push("/error", "/login");
   cookie.remove("token");
   // to support logging out from all windows
   window.localStorage.setItem("logout", Date.now());
   window.localStorage.removeItem("id_token");
   window.localStorage.removeItem("login");
-  Router.push("/login");
 };
 
 // Gets the display name of a JSX component for dev tools
