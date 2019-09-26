ba52babaae73e449c9d7f49f99d9719321a874cb
Thomas Glatt <tglatt@gmail.com>
2019-09-26 15:46:12 +0200

    chore(app): remove old ti components

--- packages/app/src/components/tiComponents/modals/index.js
+++
@@ -1,6 +0,0 @@
-export { default as FicheMandataireModal } from "./FicheMandataireModal";
-export { default as ModalMesureValidation } from "./ModalMesureValidation";
-export { default as ModalMesureReservation } from "./ModalMesureReservation";
-export { default as EditMesure } from "./EditMesure";
-export { default as EditUser } from "./EditUser";
-export { default as CloseMesureAttente } from "./CloseMesureAttente";
