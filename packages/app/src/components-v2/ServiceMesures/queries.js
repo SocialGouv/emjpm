fd29d83895f654a3b86c6fcded76aecf81a80fe4
Thomas Glatt <tglatt@gmail.com>
2019-10-02 08:48:58 +0200

    feat(service): add search input in mesures list

--- packages/app/src/components-v2/ServiceMesures/queries.js
+++ packages/app/src/components-v2/ServiceMesures/queries.js
@@ -1,9 +1,21 @@
 import gql from "graphql-tag";
 
 export const MESURES = gql`
-  query mesures($limit: Int, $antenne: Int, $type: String, $status: String, $offset: Int) {
+  query mesures(
+    $limit: Int
+    $antenne: Int
+    $type: String
+    $status: String
+    $searchText: String
+    $offset: Int
+  ) {
     mesures_aggregate(
-      where: { status: { _eq: $status }, type: { _eq: $type }, antenne_id: { _eq: $antenne } }
+      where: {
+        _or: { numero_dossier: { _ilike: $searchText }, numero_rg: { _ilike: $searchText } }
+        status: { _eq: $status }
+        type: { _eq: $type }
+        antenne_id: { _eq: $antenne }
+      }
     ) {
       aggregate {
         count
@@ -13,7 +25,12 @@ export const MESURES = gql`
       limit: $limit
       order_by: { date_ouverture: desc }
       offset: $offset
-      where: { status: { _eq: $status }, type: { _eq: $type }, antenne_id: { _eq: $antenne } }
+      where: {
+        _or: { numero_dossier: { _ilike: $searchText }, numero_rg: { _ilike: $searchText } }
+        status: { _eq: $status }
+        type: { _eq: $type }
+        antenne_id: { _eq: $antenne }
+      }
     ) {
       antenne_id
       id
