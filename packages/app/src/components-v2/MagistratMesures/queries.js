14e5fbce3fb1bd5c51656ab8acaf3369e82acc71
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-10-03 10:58:55 +0200

    feat(magistrat): add search input in mesure list (#876)
    * refactor(hook): move useDebounce
    
    * feat(magistrat): add search input in mesure list

--- packages/app/src/components-v2/MagistratMesures/queries.js
+++ packages/app/src/components-v2/MagistratMesures/queries.js
@@ -1,8 +1,14 @@
 import gql from "graphql-tag";
 
 export const MESURES = gql`
-  query mesures($antenne: Int, $type: String, $status: String, $offset: Int) {
-    mesures_aggregate(where: { status: { _eq: "Mesure en attente" }, type: { _eq: $type } }) {
+  query mesures($antenne: Int, $type: String, $status: String, $searchText: String, $offset: Int) {
+    mesures_aggregate(
+      where: {
+        numero_rg: { _ilike: $searchText }
+        status: { _eq: "Mesure en attente" }
+        type: { _eq: $type }
+      }
+    ) {
       aggregate {
         count
       }
@@ -10,7 +16,11 @@ export const MESURES = gql`
     mesures(
       offset: $offset
       limit: 20
-      where: { status: { _eq: "Mesure en attente" }, type: { _eq: $type } }
+      where: {
+        numero_rg: { _ilike: $searchText }
+        status: { _eq: "Mesure en attente" }
+        type: { _eq: $type }
+      }
       order_by: { created_at: desc }
     ) {
       id
