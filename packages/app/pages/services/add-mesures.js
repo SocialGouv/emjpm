7e813991eee9f6758bc12a3404ea95e866a32166
christophe <mr.dumont.christophe@gmail.com>
2019-10-03 17:12:05 +0200

    feat(service-add-mesure): add page style (#878)

--- packages/app/pages/services/add-mesures.js
+++ packages/app/pages/services/add-mesures.js
@@ -1,6 +1,5 @@
 import { BoxWrapper, Heading1 } from "@socialgouv/emjpm-ui-core";
 import React from "react";
-import { Box, Flex } from "rebass";
 import { LayoutServices } from "../../src/components-v2/Layout";
 import { ServiceAddMesure } from "../../src/components-v2/ServiceMesures";
 import { UserInformations } from "../../src/components-v2/UserInformations";
@@ -10,27 +9,12 @@ const AddMesures = () => {
   return (
     <LayoutServices hasNavigation={false}>
       <BoxWrapper mt={6} px="1">
-        <Heading1>{"Création d'une mesure"}</Heading1>
-        <Flex
-          sx={{
-            flexWrap: "wrap"
+        <Heading1 mx="1">{"Création d'une mesure"}</Heading1>
+        <UserInformations
+          Component={props => {
+            return <ServiceAddMesure {...props} />;
           }}
-        >
-          <Box
-            sx={{
-              p: 3,
-              flexGrow: 99999,
-              flexBasis: 0,
-              minWidth: 320
-            }}
-          >
-            <UserInformations
-              Component={props => {
-                return <ServiceAddMesure {...props} />;
-              }}
-            />
-          </Box>
-        </Flex>
+        />
       </BoxWrapper>
     </LayoutServices>
   );
