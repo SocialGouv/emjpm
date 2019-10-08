8c6a0d4ab20753f87cba036d11ffdfbef7e51da2
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-10-08 16:42:20 +0200

    fix(magistrat): change updatable fields (#885)

--- packages/app/src/components-v2/MagistratMesures/mutations.js
+++ packages/app/src/components-v2/MagistratMesures/mutations.js
@@ -3,30 +3,24 @@ import gql from "graphql-tag";
 export const EDIT_MESURE = gql`
   mutation editMesure(
     $id: Int!
-    $date_ouverture: date
     $type: String
     $residence: String
     $code_postal: String
     $ville: String
     $civilite: String
     $annee: String
-    $numero_dossier: String
     $numero_rg: String
-    $status: String
   ) {
     update_mesures(
       where: { id: { _eq: $id } }
       _set: {
-        date_ouverture: $date_ouverture
         type: $type
         residence: $residence
         code_postal: $code_postal
         ville: $ville
         civilite: $civilite
         annee: $annee
-        numero_dossier: $numero_dossier
         numero_rg: $numero_rg
-        status: $status
       }
     ) {
       returning {
