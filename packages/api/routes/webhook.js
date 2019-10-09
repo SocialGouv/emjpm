92a7e1f38dfd515a5aa0e6ae3dabbac53481c645
kwiss <christophe@hooraylabs.com>
2019-10-09 12:15:15 +0200

    feat(send-email): add mail with data and query

--- packages/api/routes/webhook.js
+++ packages/api/routes/webhook.js
@@ -1,17 +1,39 @@
 const express = require("express");
 
-const { sendEmail } = require("../email");
 const router = express.Router();
+const { ServiceAntenne } = require("../model/ServiceAntenne");
+const { Mandataire } = require("../model/Mandataire");
+const { Tis } = require("../model/Tis");
+const { reservationEmail } = require("../email/reservation-email");
 
-router.post("/mesure-reservation", function(req, res) {
-  // eslint-disable-next-line no-console
-  console.log(req.body.event.data);
-  sendEmail(
-    "support.emjpm@fabrique.social.gouv.fr",
-    "e-MJPM : test",
-    "Bonjour ! Adrien",
-    "Bonjour ! Adrien"
+const getUser = (headquarter, mandataire) => {
+  if (mandataire) {
+    return {
+      mesures_en_cours: mandataire.mesures_en_cours,
+      dispo_max: mandataire.dispo_max,
+      email: "test@gmail.com"
+    };
+  } else if (headquarter) {
+    return {
+      mesures_en_cours: headquarter.mesures_in_progress,
+      dispo_max: headquarter.mesures_max,
+      email: "test@gmail.com"
+    };
+  }
+  return null;
+};
+
+router.post("/mesure-reservation", async function(req, res) {
+  const newMesure = req.body.event.data.new;
+  const { ti_id, antenne_id, mandataire_id } = newMesure;
+  const antennes = await ServiceAntenne.query().where("id", antenne_id);
+  const [headquarter] = antennes.filter(
+    antenne => antenne.headquarters === true
   );
+  const [currentTi] = await Tis.query().where("id", ti_id);
+  const [mandataire] = await Mandataire.query().where("id", mandataire_id);
+  const user = getUser(headquarter, mandataire);
+  reservationEmail(currentTi, newMesure, user);
   res.json({ success: true });
 });
 
