0866022faa915c3359a7234a739b03d69a5d1c74
christophe <mr.dumont.christophe@gmail.com>
2019-10-11 15:59:30 +0200

    fix(email): fix email missing addresses (#892)

--- packages/api/routes/webhook.js
+++ packages/api/routes/webhook.js
@@ -4,20 +4,21 @@ const router = express.Router();
 const { ServiceAntenne } = require("../model/ServiceAntenne");
 const { Mandataire } = require("../model/Mandataire");
 const { Tis } = require("../model/Tis");
+const { User } = require("../model/User");
 const { reservationEmail } = require("../email/reservation-email");
 
-const getUser = (headquarter, mandataire) => {
+const getUser = (headquarter, mandataire, currentUser) => {
   if (mandataire) {
     return {
       mesures_en_cours: mandataire.mesures_en_cours,
       dispo_max: mandataire.dispo_max,
-      email: "test@gmail.com"
+      email: currentUser.email
     };
   } else if (headquarter) {
     return {
       mesures_en_cours: headquarter.mesures_in_progress,
       dispo_max: headquarter.mesures_max,
-      email: "test@gmail.com"
+      email: headquarter.contact_email
     };
   }
   return null;
@@ -26,19 +27,19 @@ const getUser = (headquarter, mandataire) => {
 router.post("/mesure-reservation", async function(req, res) {
   const newMesure = req.body.event.data.new;
   const { ti_id, antenne_id, mandataire_id } = newMesure;
-  const antennes = await ServiceAntenne.query().where("id", antenne_id);
-  const [headquarter] = antennes.filter(
-    antenne => antenne.headquarters === true
-  );
   const [currentTi] = await Tis.query().where("id", ti_id);
-  const [mandataire] = await Mandataire.query().where("id", mandataire_id);
-  const user = getUser(headquarter, mandataire);
   if (currentTi) {
+    const [headquarter] = antennes.filter(
+      antenne => antenne.headquarters === true
+    );
+    const antennes = await ServiceAntenne.query().where("id", antenne_id);
+    const [mandataire] = await Mandataire.query().where("id", mandataire_id);
+    const [currentUser] = await User.query().where("id", mandataire.user_id);
+    const user = getUser(headquarter, mandataire, currentUser);
     reservationEmail(currentTi, newMesure, user);
   } else {
     res.json({ success: true });
   }
-  res.json({ success: true });
 });
 
 module.exports = router;
