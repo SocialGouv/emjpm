6f18f25d73b1af924429efde718f56e8d0e63d82
christophe <mr.dumont.christophe@gmail.com>
2019-10-28 09:55:32 +0100

    Fix/signup style (#908)
    * fix(signup): fix style
    
    * fix(signup): fix congratulation style

--- packages/app/pages/signup/congratulation.js
+++ packages/app/pages/signup/congratulation.js
@@ -1,4 +1,4 @@
-import { BoxWrapper, Button, Heading2, Text } from "@socialgouv/emjpm-ui-core";
+import { BoxWrapper, Button, Card, Heading2, Text } from "@socialgouv/emjpm-ui-core";
 import Router from "next/router";
 import React from "react";
 import { Box, Flex, Link } from "rebass";
@@ -8,24 +8,28 @@ const backLogin = () => {
   Router.push("/login");
 };
 
+const cardStyle = { p: "5", m: "1", mt: "5" };
+
 const CongratulationPage = () => {
   return (
     <LayoutSignup>
       <BoxWrapper mt={6} px="1" p={"150px"}>
-        <Heading2>{"Votre demande d'inscription est terminée"}</Heading2>
-        <Text mb="1" lineHeight="2" pt={6}>
-          {"Merci ! Votre demande a bien été prise en compte."}
-        </Text>
-        <Text mb="1" lineHeight="2">
-          {"Notre équipe a été informée et vous recevrez une confirmation très prochainement."}
-        </Text>
-        <Flex mt={5}>
-          <Box>
-            <Button mr="2" variant="outline" onClick={() => backLogin()}>
-              <Link>Se connecter</Link>
-            </Button>
-          </Box>
-        </Flex>
+        <Card sx={cardStyle}>
+          <Heading2>{"Votre demande d'inscription est terminée"}</Heading2>
+          <Text mb="1" lineHeight="2" pt={6}>
+            {"Merci ! Votre demande a bien été prise en compte."}
+          </Text>
+          <Text mb="1" lineHeight="2">
+            {"Notre équipe a été informée et vous recevrez une confirmation très prochainement."}
+          </Text>
+          <Flex mt={5}>
+            <Box>
+              <Button mr="2" variant="outline" onClick={() => backLogin()}>
+                <Link>Se connecter</Link>
+              </Button>
+            </Box>
+          </Flex>
+        </Card>
       </BoxWrapper>
     </LayoutSignup>
   );
