21d782031904b2ae2485d88ed66546ed6d8c0ace
Thomas Glatt <tglatt@gmail.com>
2019-10-29 11:17:28 +0100

    feat(import): update import pdf

--- packages/app/src/components-v2/MandataireMesureImport/MandataireMesureImport.js
+++ packages/app/src/components-v2/MandataireMesureImport/MandataireMesureImport.js
@@ -34,9 +34,12 @@ const MandataireMesureImport = () => {
       <Card mb="5">
         <Flex flexDirection="column">
           <Box mb={2}>
-            <Heading3 mb="2">{`Import de vos mesures avec un fichier au format csv`}</Heading3>
+            <Heading3 mb="2">{`Format du fichier d'import`}</Heading3>
             <Text mb="1" lineHeight="2">
-              {`Pour simplifier la première mise à jour de toutes vos mesures en cours, vous pouvez importer vos mesures avec un fichier csv. Ce fichier csv doit être conforme au modèle décrit dans la note explicative ci-dessous:`}
+              {`Pour simplifier la première mise à jour de toutes vos mesures en cours, vous pouvez importer vos mesures avec un fichier csv. Ce fichier csv doit être conforme au modèle décrit dans la note explicative ci-dessous.`}
+            </Text>
+            <Text mb="1" lineHeight="2">
+              {`Rapprochez-vous de votre éditeur de logiciel pour vous assurez qu'un export est compatible avec ce format.`}
             </Text>
             <Flex flexDirection="row" justifyContent="center" mt={2} mb={2}>
               <DocumentLink document="/static/docs/Notice_d_utilisation_de_l_import.pdf">
@@ -47,13 +50,13 @@ const MandataireMesureImport = () => {
               </DocumentLink>
             </Flex>
             <Text mb="1" lineHeight="2">
-              {`Nous vous conseillons de réaliser l’importation de votre tableau Excel de mesures qu’une seule fois au début de votre utilisation de e-MJPM. Une fois le tableau importé une première fois, vous serez prêt.e à utiliser parfaitement e-MJPM. Vous pourrez alors faire toutes les modifications, les mises à jours et les ajoûts vous même.`}
+              {`Nous vous conseillons de réaliser l’import de vos mesures qu’une seule fois au début de votre utilisation de e-MJPM. Une fois le tableau importé une première fois, vous serez prêt.e à utiliser parfaitement e-MJPM. Vous pourrez alors faire toutes les modifications, les mises à jours et les ajoûts vous même.`}
             </Text>
           </Box>
           <Box mt={2}>
-            <Heading3 mb="2">{`Import de vos mesures avec un fichier excel`}</Heading3>
+            <Heading3 mb="2">{`Importez vos mesures`}</Heading3>
             <Text mb="1" lineHeight="2">
-              {`Utilisez le cadre ci-dessous pour nous transmettre votre tableau excel de mesures. Si vous eprouvez des difficultés, vous pouvez nous envoyer un mail avec votre tableau en pièce-jointe à contact@emjpm.beta.gouv.fr. Nous le vérifierons, le mettrons en page et nous vous le renverrons pour que vous puissiez l’importer.`}
+              {`Utilisez le cadre ci-dessous pour nous transmettre votre tableau csv de mesures. Si vous eprouvez des difficultés, vous pouvez nous envoyer un mail avec votre tableau en pièce-jointe à contact@emjpm.beta.gouv.fr. Nous le vérifierons, le mettrons en page et nous vous le renverrons pour que vous puissiez l’importer.`}
             </Text>
             <MandataireAddMesureImport />
           </Box>
