c44b8c46ab110014dc2d734b0739ecb313ddde5b
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-10-29 18:50:29 +0100

    fix(choose-mandataire): decremeent mesure states (#917)

--- packages/app/src/components-v2/MagistratMandatairesList/mutations.js
+++ packages/app/src/components-v2/MagistratMandatairesList/mutations.js
@@ -2,18 +2,69 @@ import gql from "graphql-tag";
 
 export const CHOOSE_MANDATAIRE = gql`
   mutation chooseMandataire(
-    $ti: Int
-    $mandataire_id: Int
-    $antenne_id: Int
-    $type: String
-    $civilite: String
-    $annee: String
-    $numero_rg: String
+    $ti: Int!
+    $mandataire_id: Int!
+    $type: String!
+    $civilite: String!
+    $annee: String!
+    $numero_rg: String!
   ) {
     insert_mesures(
       objects: {
         ti_id: $ti
         mandataire_id: $mandataire_id
+        type: $type
+        civilite: $civilite
+        annee: $annee
+        numero_rg: $numero_rg
+        status: "Mesure en attente"
+      }
+    ) {
+      returning {
+        id
+        cabinet
+        civilite
+        code_postal
+        departement {
+          nom
+          region {
+            nom
+          }
+        }
+        status
+        type
+        ville
+        ti_id
+        residence
+        numero_rg
+        numero_dossier
+        etablissement
+        annee
+        date_ouverture
+      }
+    }
+    update_mandataires(where: { id: { _eq: $mandataire_id } }, _inc: { mesures_en_attente: 1 }) {
+      affected_rows
+      returning {
+        id
+        mesures_en_attente
+      }
+    }
+  }
+`;
+
+export const CHOOSE_SERVICE = gql`
+  mutation chooseService(
+    $ti: Int!
+    $antenne_id: Int!
+    $type: String!
+    $civilite: String!
+    $annee: String!
+    $numero_rg: String!
+  ) {
+    insert_mesures(
+      objects: {
+        ti_id: $ti
         antenne_id: $antenne_id
         type: $type
         civilite: $civilite
@@ -46,11 +97,11 @@ export const CHOOSE_MANDATAIRE = gql`
         date_ouverture
       }
     }
-    update_mandataires(where: { id: { _eq: $mandataire_id } }, _inc: { mesures_en_cours: 1 }) {
+    update_service_antenne(where: { id: { _eq: $antenne_id } }, _inc: { mesures_awaiting: 1 }) {
       affected_rows
       returning {
         id
-        mesures_en_cours
+        mesures_awaiting
       }
     }
   }
