8f154976eebbe9d5ed45a8cb65332c141daf54ec
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-11-06 19:23:37 +0100

    Feat/knex reset migration (#930)
    * chore(test): update snapshot
    
    * feat(knex): add create schema script
    
    * fix(knex-reset-migration): fix init script

--- packages/api/test/routes.tis.test.js
+++ packages/api/test/routes.tis.test.js
@@ -16,7 +16,7 @@ passportStub.install(server);
 
 describe("routes : tis", () => {
   beforeEach(() => {
-    knex.raw("DELETE FROM 'knex_migrations_lock';");
+    knex.raw("DELETE FROM 'knex_migrations_v2_lock';");
     return knex.migrate
       .rollback()
       .then(() => knex.migrate.latest())
