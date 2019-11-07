14f6f465c542acfc996fb0ed1d158dbe0d0f2a14
christophe <mr.dumont.christophe@gmail.com>
2019-11-07 16:42:13 +0100

    feat(magistrat settings) (#938)
    * feat(magistrat-edit-informations): add edit informations components
    
    * feat(magistrat-edit-informations): remove useless query and fetch policy
    
    * feat(magistrat-edit-informations): update informations layout
    
    * feat(magistrat-edit-informations): add password reset
    
    * feat(magistrat-edit-informations): fix api path
    
    * feat(magistrat-edit-informations): remove username fix email lowercase
    
    * feat(magistrat-setting): change error message

--- packages/app/src/components-v2/MagistratInformations/MagistratInformations.js
+++ packages/app/src/components-v2/MagistratInformations/MagistratInformations.js
@@ -1,24 +1,62 @@
-import React from "react";
-import { useQuery } from "@apollo/react-hooks";
+import React, { useState } from "react";
+import { Box, Text, Flex } from "rebass";
+import { Card, Button, Heading3, Heading5 } from "@socialgouv/emjpm-ui-core";
+import { MailOutline } from "styled-icons/material";
 
-import { Informations } from "./Informations";
-import { MAGISTRAT } from "./queries";
+import { LinkButton } from "../Commons";
+import { innerTextStyle, iconTextStyle, boxStyle, flexStyle } from "./style";
+import { doForgotPassword } from "../../components/loginComponents/ForgotPasswordForm";
 
 const MagistratInformations = props => {
-  const { id } = props;
-  const { data, error, loading } = useQuery(MAGISTRAT, {
-    variables: { userId: id }
-  });
+  const [isOpen, toggleModal] = useState(false);
+  const { email, cabinet, nom, prenom } = props;
+  const changePassword = email => {
+    doForgotPassword({ email: email }).then(() => {
+      toggleModal(true);
+    });
+  };
 
-  if (loading) {
-    return <div>Chargement</div>;
-  }
-
-  if (error) {
-    return <div>Erreur</div>;
-  }
-
-  return <Informations {...props} userInformations={data} />;
+  return (
+    <Box {...props}>
+      <Card p="5">
+        <Heading3>
+          {nom ? nom : "Nom non renseigné"} {prenom ? prenom : "Prénom non renseigné"}
+        </Heading3>
+        <Flex sx={flexStyle}>
+          <Box sx={boxStyle}>
+            <Heading5>Vos informations</Heading5>
+            <Flex mt="2">
+              <MailOutline size="16" />
+              <Text sx={iconTextStyle}>{email}</Text>
+            </Flex>
+            <Text sx={innerTextStyle}>{nom ? nom : "Nom non renseigné"} </Text>
+            <Text sx={innerTextStyle}>{prenom ? prenom : "Prénom non renseigné"}</Text>
+            <Text sx={innerTextStyle}>
+              {cabinet ? `Cabinet: ${cabinet}` : "Cabinet non renseigné"}{" "}
+            </Text>
+          </Box>
+        </Flex>
+        <Flex mt="5">
+          <Box>
+            <LinkButton href="/magistrats/edit-informations">Modifier mes informations</LinkButton>
+          </Box>
+        </Flex>
+        <Flex mt="1">
+          {isOpen ? (
+            <Box>
+              <Card backgroundColor="success" color="white">
+                Un mail vient de vous être envoyer pour réinitialiser votre mot de passe
+              </Card>
+            </Box>
+          ) : (
+            <Box>
+              <Button onClick={() => changePassword(email)}>Réinitialiser mon mot de passe</Button>
+            </Box>
+          )}
+        </Flex>
+      </Card>
+    </Box>
+  );
 };
 
 export { MagistratInformations };
