b5e8bbed82594b0d5d045e30dbe87b70d6808889
christophe <mr.dumont.christophe@gmail.com>
2019-11-12 21:28:15 +0100

    Feat/magistrat comments (#943)
    * feat(magistrat-comment): add comment in mandataires informations
    
    * feat(magistrat-comment): add comment form
    
    * feat(magistrat-comment): add comment remove
    
    * feat(magistrat-comment): close form when submit
    
    * feat(magistrat-comments): add edit comments
    
    * feat(magistrat-comments): move comment on top

---
+++ packages/app/src/components-v2/MagistratMandatairesList/MagistratMandataireComment.js
@@ -0,0 +1,67 @@
+import React, { Fragment } from "react";
+import { useMutation } from "@apollo/react-hooks";
+import { Text, Flex, Box } from "rebass";
+import { CloseCircle, EditBox } from "styled-icons/remix-line";
+
+import { topTextStyle } from "./style";
+import { MagistratMandataireCommentForm } from "./MagistratMandataireCommentForm";
+import { REMOVE_COMMENT } from "./mutations";
+
+const MagistratMandataireComment = props => {
+  const {
+    setCurrentComment,
+    currentComment,
+    id,
+    comment,
+    isEditOpen,
+    toggleEditCommentForm,
+    toggleCommentForm
+  } = props;
+  const [RemoveComment] = useMutation(REMOVE_COMMENT);
+
+  return (
+    <Fragment>
+      {isEditOpen && currentComment === id ? (
+        <MagistratMandataireCommentForm
+          toggleEditCommentForm={toggleEditCommentForm}
+          id={id}
+          isEditing
+          comment={comment}
+        />
+      ) : (
+        <Text sx={topTextStyle} fontSize="1" lineHeight="1.5" key={id}>
+          <Flex>
+            <Box
+              sx={{
+                flexGrow: 1,
+                flexBasis: 256
+              }}
+            >
+              - {comment}
+            </Box>
+            <Box
+              sx={{ cursor: "pointer", width: "16px", mr: "1" }}
+              onClick={() => {
+                toggleCommentForm(false);
+                toggleEditCommentForm(true);
+                setCurrentComment(id);
+              }}
+            >
+              <EditBox size="16" />
+            </Box>
+            <Box
+              sx={{ cursor: "pointer", width: "16px" }}
+              onClick={() => {
+                RemoveComment({ variables: { id: id }, refetchQueries: ["MandataireComments"] });
+              }}
+            >
+              <CloseCircle size="16" />
+            </Box>
+          </Flex>
+        </Text>
+      )}
+    </Fragment>
+  );
+};
+
+export { MagistratMandataireComment };
