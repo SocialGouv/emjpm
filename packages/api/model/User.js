27c1dbbe0abc8e88ebcc246634e3f00803056512
christophe <mr.dumont.christophe@gmail.com>
2019-11-17 13:23:12 +0100

    feat(auth-redirect): redirect when on login signup with token (#969)
    * feat(auth-redirect): redirect when on login signup with token
    
    * fix(pathname): fix auth pathname redirection
    
    * chore(hasura): restore matadata.yml from master

--- packages/api/model/User.js
+++ packages/api/model/User.js
@@ -130,6 +130,8 @@ class User extends Model {
     const claim = {
       name: this.username,
       id: this.id,
+      url: redirs[this.type] || redirs.default,
+      role: this.getDefaultRole(),
       "https://hasura.io/jwt/claims": this.getHasuraClaims()
     };
     return jwt.sign(claim, jwtConfig.key, signOptions);
