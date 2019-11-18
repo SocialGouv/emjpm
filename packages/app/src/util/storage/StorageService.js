4f5432ada61c9c1212b3d72a1914cb4c64e32856
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-11-18 17:23:14 +0100

    feat(impersonate): allow starting impersonate (#926)
    * feat(impersonate): allow starting impersonate
    
    * feat(impersonate): in progress
    
    * feat(impersonate): in progress
    
    * feat(impersonate): in progress
    
    * fix(redir): add missing fall back url
    
    * chore(clean): remove removeLogin

--- packages/app/src/util/storage/StorageService.js
+++ packages/app/src/util/storage/StorageService.js
@@ -2,7 +2,6 @@ import cookie from "cookie";
 
 const TOKEN_KEY = "token";
 const ID_TOKEN_KEY = "id_token";
-const LOGIN_KEY = "login";
 
 const THIRTY_DAYS_TIME = 30 * 24 * 60 * 60;
 
@@ -17,17 +16,11 @@ export const storageService = {
     const token = getToken();
     return !!token;
   },
-  removeLogin() {
-    localStorage.removeItem(LOGIN_KEY);
-  },
   removeToken() {
     localStorage.removeItem(ID_TOKEN_KEY);
     document.cookie = cookie.serialize(TOKEN_KEY, "", {
       maxAge: -1 // Expire the cookie immediately
     });
-  },
-  setLogin(login) {
-    localStorage.setItem(LOGIN_KEY, login);
   },
   setToken(token) {
     localStorage.setItem(ID_TOKEN_KEY, token);
