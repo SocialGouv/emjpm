588b42a4aeb8b8cc14f4e87fe681c60a4e681df5
Paul Launay <launay.p@gmail.com>
2019-11-29 18:30:08 +0100

    feat(choose-mandataire): add lastlogin to choosemandataire (#1018)
    * feat(choose-mandataire): add lastlogin to choosemandataire
    
    * feat(choose-mandataire): add service_admins lastLogin
    
    * fix(choose-mandataire): handle invalid date with empty services_admins
    
    * fix(choose-mandataire): update hasura metadata with tis permissions
    
    * feat(choose-mandataire): colorize lastLogin when one month old or more

--- packages/app/src/util/mandataires/MandataireUtils.js
+++ packages/app/src/util/mandataires/MandataireUtils.js
@@ -1,11 +1,29 @@
+import { compareDesc, differenceInMonths, format } from "date-fns";
+
 const TYPES = {
   MANDATAIRE_IND: "individuel",
   MANDATAIRE_PRE: "préposé",
   SERVICE: "service"
 };
+const TODAY = new Date();
 
 const capitalize = string => {
   return string.charAt(0).toUpperCase() + string.toLowerCase().slice(1);
+};
+
+const formatLastLogin = date => {
+  return format(date, ["DD/MM/YYYY"]);
+};
+
+const newestLastLogin = admins => {
+  const dates = admins.map(({ user: { last_login } }) => last_login).filter(Boolean);
+  const [newest] = dates.sort(compareDesc);
+
+  return newest;
+};
+
+const isCriticalDate = date => {
+  return differenceInMonths(TODAY, date) >= 1;
 };
 
 export const formatMandataire = (
@@ -19,6 +37,7 @@ export const formatMandataire = (
   gestionnaire_tis
 ) => {
   let currentDiscriminator = {};
+
   const common = {
     currentAvailability: remaining_capacity ? remaining_capacity : 0,
     cvLink: "test",
@@ -31,6 +50,11 @@ export const formatMandataire = (
   };
 
   if (discriminator === "SERVICE") {
+    const lastLogin =
+      service.service_admins && service.service_admins.length
+        ? newestLastLogin(service.service_admins)
+        : null;
+
     currentDiscriminator = {
       adresse: service.adresse ? capitalize(service.adresse) : "non renseigné",
       codePostal: service.code_postal ? capitalize(service.code_postal) : "non renseigné",
@@ -38,6 +62,8 @@ export const formatMandataire = (
       etablissement: service.etablissement ? service.etablissement : "non renseigné",
       genre: "F",
       id: `${discriminator}-${service.id}`,
+      lastLogin: lastLogin ? formatLastLogin(lastLogin) : "non renseigné",
+      lastLoginIsCritical: lastLogin && isCriticalDate(lastLogin),
       nom: service.nom ? service.nom : null,
       prenom: service.prenom ? service.prenom : null,
       serviceId: service.id,
@@ -51,6 +77,12 @@ export const formatMandataire = (
       email: mandataire.user && mandataire.user.email ? mandataire.user.email : "non renseigné",
       genre: mandataire.genre ? mandataire.genre : "F",
       id: `${discriminator}-${mandataire.id}`,
+      lastLogin:
+        mandataire.user && mandataire.user.last_login
+          ? formatLastLogin(mandataire.user.last_login)
+          : "non renseigné",
+      lastLoginIsCritical:
+        mandataire.user && mandataire.user.last_login && isCriticalDate(mandataire.user.last_login),
       mandataireId: mandataire.id,
       nom:
         mandataire.user && mandataire.user.nom ? capitalize(mandataire.user.nom) : "non renseigné",
