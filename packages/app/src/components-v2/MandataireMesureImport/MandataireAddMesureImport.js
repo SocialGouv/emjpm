d01691cf4646ec2fe28fc98d29fe19bf073c1d29
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-12-03 12:42:03 +0100

    feat(import): handle excel (#1020)
    * feat(import): handle excel
    
    * refactor(import): rename variables
    
    * feat(import): handle code postal as number
    
    * feat(import): start handling excel
    
    * feat(import): clean import components / utils with xls implementation
    
    * fix(import): fix style and validation leak
    
    * fix(import): fix wrong xls template link
    
    * fix(import): use same code for xls / csv with valid date
    
    * feat(import): validate headers too
    
    * refactor(import): use constants for import template and manual
    
    * style: use named exports everywhere
    
    * style(import): use fragment everywhere
    
    * style(import): use more destructuring

--- packages/app/src/components-v2/MandataireMesureImport/MandataireAddMesureImport.js
+++ packages/app/src/components-v2/MandataireMesureImport/MandataireAddMesureImport.js
@@ -1,119 +1,27 @@
 import { useMutation } from "@apollo/react-hooks";
-import { Button, Card, Heading2, Input } from "@socialgouv/emjpm-ui-core";
-import { Text } from "@socialgouv/emjpm-ui-core/dist/Type";
 import React, { useState } from "react";
-import { Box, Flex } from "rebass";
 
-import checkDatas from "./checkDatas";
+import { csvToJson, validateImportData, xlsToJson } from "../../util/import/index";
+import { MandataireMesureImportFilepicker } from "./MandataireMesureImportFilepicker";
+import { MandataireMesureImportResult } from "./MandataireMesureImportResult";
 import { ADD_IMPORT } from "./mutations";
-import { ServiceMesureImportResultStyle } from "./style";
-
-const grayCard = {
-  bg: "cardSecondary",
-  borderRadius: "5px 0 0 5px",
-  m: "2",
-  p: "5"
-};
-
-const FilePicker = props => {
-  return (
-    <Box mb="2">
-      <Input
-        id="file"
-        type="file"
-        name="file"
-        value=""
-        onChange={event => {
-          props.handleFileChosen(event.currentTarget.files[0]);
-        }}
-        placeholder="Sélectionner votre fichier excel"
-      />
-    </Box>
-  );
-};
-
-const RestartButton = ({ restart }) => (
-  <Button variant="outline" onClick={() => restart()}>
-    Sélectionner un autre fichier
-  </Button>
-);
-
-const ResultMessage = ({ result, restart }) => {
-  const errors = result.errors;
-  const mesures = result.mesures;
-  return (
-    <>
-      <Flex alignItems="center" p={7} sx={ServiceMesureImportResultStyle}>
-        <Flex flexDirection="column">
-          <Heading2>{`Résultat de l'import`}</Heading2>
-          <Text
-            m={2}
-            fontSize={2}
-          >{`${mesures.length} mesures vont être importées ou mises à jour. Vous allez recevoir un email dans quelques instants.`}</Text>
-          <Text
-            m={2}
-            fontSize={2}
-          >{`${errors.length} mesures ne seront pas importées ou mises à jour. Les erreurs sont indiquées ci-dessous.`}</Text>
-        </Flex>
-        <RestartButton restart={restart} />
-      </Flex>
-      {errors && (
-        <Card sx={grayCard} overflow="hidden">
-          <pre>{JSON.stringify(errors, null, 2)}</pre>
-        </Card>
-      )}
-    </>
-  );
-};
-
-const findSeparator = line => {
-  const tabulation = "\t";
-  const semilicon = ";";
-
-  let separatorChar = tabulation;
-  if (line.split(semilicon).length > 1) {
-    separatorChar = semilicon;
-  }
-  return separatorChar;
-};
-
-function csvToArray(csv) {
-  var lines = csv.split("\n");
-  const separatorChar = findSeparator(lines[0]);
-
-  var result = [];
-  const headerCells = lines[0].split(separatorChar);
-  var headers = headerCells.map(header => header.trim());
-
-  for (var i = 1; i < lines.length; i++) {
-    var obj = {};
-    var currentline = lines[i].split(separatorChar);
-
-    for (var j = 0; j < headers.length; j++) {
-      const val = currentline[j];
-      obj[headers[j]] = val ? val.trim() : undefined;
-    }
-
-    result.push(obj);
-  }
-
-  return result;
-}
 
 export const MandataireAddMesureImport = () => {
-  const [result, setResult] = useState(false);
-  const [AddImport] = useMutation(ADD_IMPORT);
+  const [result, setResult] = useState(null);
+  const [addImport] = useMutation(ADD_IMPORT);
 
   const handleFileChosen = file => {
     const reader = new FileReader();
+    const filename = file.name;
+    const isExcel = filename.endsWith(".xls") || filename.endsWith(".xlsx");
 
     reader.onload = function() {
-      const result = reader.result;
-      const datas = csvToArray(result);
-      const { errors, mesures } = checkDatas(datas);
+      const { result } = reader;
+      const data = isExcel ? xlsToJson(result) : csvToJson(result);
+      const { errors, mesures } = validateImportData(data);
 
-      if (mesures.length > 0) {
-        AddImport({
+      if (mesures.length) {
+        addImport({
           variables: {
             content: mesures,
             file_name: file.name,
@@ -122,18 +30,20 @@ export const MandataireAddMesureImport = () => {
           }
         });
       }
-      setResult({
-        errors,
-        mesures
-      });
+
+      setResult({ errors, mesures });
     };
-    reader.readAsText(file);
+
+    if (isExcel) {
+      reader.readAsBinaryString(file);
+    } else {
+      reader.readAsText(file);
+    }
   };
 
-  return (
-    <>
-      {!result && <FilePicker handleFileChosen={handleFileChosen} />}
-      {result && <ResultMessage restart={() => setResult(false)} result={result} />}
-    </>
-  );
+  if (result) {
+    return <MandataireMesureImportResult reset={() => setResult(null)} result={result} />;
+  }
+
+  return <MandataireMesureImportFilepicker handleFileChosen={handleFileChosen} />;
 };
---
+++ packages/app/src/util/import/index.js
@@ -0,0 +1,5 @@
+import csvToJson from "./csvToJson";
+import validateImportData from "./validateImportData";
+import xlsToJson from "./xlsToJson";
+
+export { csvToJson, validateImportData, xlsToJson };
