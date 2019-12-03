3ccf3d853f78a45c4a205b9311376074061b7ac7
Paul Launay <launay.p@gmail.com>
2019-12-03 18:19:46 +0100

    feat(admin-user-page): replace active by view button and move it to user

--- packages/app/pages/admin/users/[user_id].js
+++ packages/app/pages/admin/users/[user_id].js
@@ -1,16 +1,45 @@
-import { BoxWrapper, Heading1 } from "@socialgouv/emjpm-ui-core";
-import React from "react";
+import { useMutation } from "@apollo/react-hooks";
+import { BoxWrapper, Button, Heading1 } from "@socialgouv/emjpm-ui-core";
+import React, { useState } from "react";
 
+import { ACTIVATE_USER } from "../../../src/components-v2/AdminUsers/mutations";
+import { activateButtonStyle } from "../../../src/components-v2/AdminUsers/style";
 import { LayoutAdmin } from "../../../src/components-v2/Layout";
 import { withAuthSync } from "../../../src/util/auth";
 
 const User = props => {
   const { user_id } = props;
+  const active = false;
+  const [isActive, setActive] = useState(active);
+  const [activateUser] = useMutation(ACTIVATE_USER, {
+    onCompleted: data => {
+      setActive(data.update_users.returning[0].active);
+    }
+  });
+
+  const toogleActivation = () => {
+    const newState = !isActive;
+
+    activateUser({
+      variables: {
+        active: newState,
+        id: user_id
+      }
+    });
+  };
 
   return (
     <LayoutAdmin>
       <BoxWrapper mt={6} px="1">
-        <Heading1>Liste des utilisateurs / #{user_id}</Heading1>
+        <Heading1>Utilisateur #{user_id}</Heading1>
+        <Button
+          sx={activateButtonStyle(isActive)}
+          width="120px"
+          onClick={toogleActivation}
+          variant="outline"
+        >
+          {isActive ? "Bloquer" : "Activer"}
+        </Button>
       </BoxWrapper>
     </LayoutAdmin>
   );
