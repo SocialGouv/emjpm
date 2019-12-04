a1c3f952042c64739abc891280d7111589fba7f6
christophe <mr.dumont.christophe@gmail.com>
2019-12-04 16:12:49 +0100

    fix(login-redirect): remove useless style remove error push (#1054)

--- packages/app/pages/_error.js
+++ packages/app/pages/_error.js
@@ -1,19 +1,9 @@
-import { BoxWrapper, Heading1 } from "@socialgouv/emjpm-ui-core";
 import React from "react";
 
 import { LayoutPublic } from "../src/components-v2/Layout";
-import { withAuthSync } from "../src/util/auth";
 
-const LoginPage = () => {
-  return (
-    <LayoutPublic>
-      <BoxWrapper>
-        <Heading1 mt={"80px"} textAlign="center">
-          {`Il semble qu'une erreur se soit produite.`}
-        </Heading1>
-      </BoxWrapper>
-    </LayoutPublic>
-  );
+const ErrorPage = () => {
+  return <LayoutPublic>{`Il semble qu'une erreur se soit produite.`}</LayoutPublic>;
 };
 
-export default withAuthSync(LoginPage);
+export default ErrorPage;
