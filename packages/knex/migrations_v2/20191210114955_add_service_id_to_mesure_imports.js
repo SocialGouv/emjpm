0ae7266b68c6eda60be4bb3cde69009a32186a72
Paul Launay <launay.p@gmail.com>
2019-12-10 14:31:01 +0100

    fix(admin-service-import): add service id foreign key to mesure imports

--- packages/knex/migrations_v2/20191210114955_add_service_id_to_mesure_imports.js
+++ packages/knex/migrations_v2/20191210114955_add_service_id_to_mesure_imports.js
@@ -1,11 +1,16 @@
 exports.up = async function(knex) {
   await knex.schema.alterTable("mesures_import", function(table) {
     table.integer("service_id").defaultTo(null);
+    table
+      .foreign("service_id")
+      .references("id")
+      .inTable("services");
   });
 };
 
 exports.down = async function(knex) {
   await knex.schema.alterTable("mesures_import", function(table) {
+    table.dropForeign("service_id");
     table.dropColumn("service_id");
   });
 };
