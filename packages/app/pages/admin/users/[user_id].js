997d1b0d7fa1873103ea4d9ba44f56810ffea86a
Paul Launay <launay.p@gmail.com>
2019-12-11 10:53:40 +0100

    feat(admin-service-import): use emjpm-ui tabs

--- packages/app/pages/admin/users/[user_id].js
+++ packages/app/pages/admin/users/[user_id].js
@@ -1,50 +1,12 @@
-import styled from "@emotion/styled";
-import { BoxWrapper, Card, Heading4 } from "@socialgouv/emjpm-ui-core";
+import { BoxWrapper, Heading4, Tab, TabList, TabPanel, Tabs } from "@socialgouv/emjpm-ui-core";
 import Link from "next/link";
 import React from "react";
-import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import { Link as StyledLink } from "rebass";
 
 import { AdminUserInformations } from "../../../src/components-v2/AdminUsers/AdminUserInformations";
 import { LayoutAdmin } from "../../../src/components-v2/Layout";
 import { MesureImportMandataire } from "../../../src/components-v2/MesureImport";
 import { withAuthSync } from "../../../src/util/auth";
-
-const StyledTabs = styled(Tabs)({
-  display: "flex"
-});
-
-StyledTabs.tabsRole = "Tabs";
-
-const StyledTabList = styled(TabList, { shouldForwardProp: prop => prop === "children" })();
-
-StyledTabList.tabsRole = "TabList";
-
-const StyledTab = styled(Tab)({
-  "&.react-tabs__tab--selected": {
-    borderLeft: "4px solid rgb(0, 103, 234)",
-    color: "rgb(0, 103, 234)"
-  },
-  borderLeft: "4px solid white",
-  cursor: "pointer",
-  fontWeight: "bold",
-  outline: "none",
-  paddingBottom: 16,
-  paddingLeft: 32,
-  paddingRight: 64,
-  paddingTop: 16
-});
-
-StyledTab.tabsRole = "Tab";
-
-const StyledTabPanel = styled(TabPanel)({
-  "&.react-tabs__tab-panel--selected": {
-    width: "100%"
-  },
-  minHeight: 600
-});
-
-StyledTabPanel.tabsRole = "TabPanel";
 
 const User = props => {
   const { userId } = props;
@@ -57,23 +19,19 @@ const User = props => {
             &larr; Retour
           </StyledLink>
         </Link>
-        <StyledTabs>
-          <StyledTabList>
-            <Card p={0} py={1} mr={4}>
-              <StyledTab>Informations</StyledTab>
-              <StyledTab>Import</StyledTab>
-            </Card>
-          </StyledTabList>
-          <StyledTabPanel>
+        <Tabs>
+          <TabList>
+            <Tab>Informations</Tab>
+            <Tab>Import</Tab>
+          </TabList>
+          <TabPanel>
             <AdminUserInformations userId={userId} />
-          </StyledTabPanel>
-          <StyledTabPanel>
-            <Card>
-              <Heading4 mb={2}>Selectionnez le fichier</Heading4>
-              <MesureImportMandataire id={userId} />
-            </Card>
-          </StyledTabPanel>
-        </StyledTabs>
+          </TabPanel>
+          <TabPanel>
+            <Heading4 mb={2}>Selectionnez le fichier</Heading4>
+            <MesureImportMandataire id={userId} />
+          </TabPanel>
+        </Tabs>
       </BoxWrapper>
     </LayoutAdmin>
   );
