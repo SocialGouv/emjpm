ae8f45e7942432aa86ee363a7606b333ce47a303
kwiss <christophe@hooraylabs.com>
2019-12-13 10:50:55 +0100

    fix(seeds): remove useless seeds

--- packages/knex/seeds/development/0012-codePostalLatLngs.js
+++ packages/knex/seeds/development/0012-codePostalLatLngs.js
@@ -1,41 +1,8 @@
-const zipCodes = require("../postal.json");
-
 exports.seed = function(knex) {
   if (process.env.NODE_ENV === "test") {
     return knex("geolocalisation_code_postal").insert(getTestDatas());
   }
-
-  const newRows = zipCodes.map(row => {
-    return {
-      code_postal: row.code_postal,
-      latitude: row.latitude,
-      longitude: row.longitude,
-      cities: row.cities
-    };
-  });
-  return knex.transaction(tr => {
-    return knex
-      .table("geolocalisation_code_postal")
-      .del()
-      .then(() => {
-        return knex
-          .batchInsert(
-            "geolocalisation_code_postal",
-            removeUnique(newRows, "code_postal")
-          )
-          .transacting(tr);
-      });
-  });
 };
-
-function removeUnique(arr, key) {
-  const unique = arr
-    .map(e => e[key])
-    .map((e, i, final) => final.indexOf(e) === i && i)
-    .filter(e => arr[e])
-    .map(e => arr[e]);
-  return unique;
-}
 
 function getTestDatas() {
   return [
