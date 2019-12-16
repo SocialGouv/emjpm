8ef0169914bbc28ddb9165ee1301295e75ac4bb6
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-12-16 15:41:14 +0100

    feat(mesure-import): handle longitude/latitude (#1120)
    * feat(mesure-import): handle longitude/latitude
    
    * chore(import-geoloc): remove console.log

---
+++ packages/api/model/GeolocalisationCodePostal.js
@@ -0,0 +1,16 @@
+const knexConnection = require("../db/knex");
+const { Model } = require("objection");
+
+Model.knex(knexConnection);
+
+class GeolocalisationCodePostal extends Model {
+  static get tableName() {
+    return "geolocalisation_code_postal";
+  }
+
+  static get idColumn() {
+    return "id";
+  }
+}
+
+module.exports = { GeolocalisationCodePostal };
