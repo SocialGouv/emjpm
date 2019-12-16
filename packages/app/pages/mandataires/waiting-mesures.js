df6d6ba5e2d58465396f0e352f9f2c52aca56821
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-12-17 00:57:00 +0100

    feat(manataire): add waiting mesures page (#1121)

---
+++ packages/app/pages/mandataires/waiting-mesures.js
@@ -0,0 +1,32 @@
+import { BoxWrapper, Heading1 } from "@socialgouv/emjpm-ui-core";
+import React from "react";
+import { Flex } from "rebass";
+
+import { LayoutMandataire } from "../../src/components/Layout";
+import { MandataireFilters } from "../../src/components/MandataireFilters";
+import { FiltersContextProvider } from "../../src/components/MandataireFilters/context";
+import { MandatairesMesures } from "../../src/components/MandatairesMesures";
+import { withAuthSync } from "../../src/util/auth";
+
+const Mandataires = () => {
+  return (
+    <FiltersContextProvider>
+      <LayoutMandataire>
+        <BoxWrapper mt={6} px="1">
+          <Heading1>Vos mesures en attente</Heading1>
+          <MandataireFilters />
+          <Flex
+            sx={{
+              flexWrap: "wrap",
+              mt: "2"
+            }}
+          >
+            <MandatairesMesures isOnlyWaiting />
+          </Flex>
+        </BoxWrapper>
+      </LayoutMandataire>
+    </FiltersContextProvider>
+  );
+};
+
+export default withAuthSync(Mandataires);
