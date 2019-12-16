8575bdb8b70333ac506cd61b7ba29f95b41d05e6
Thomas Glatt <tglatt@gmail.com>
2019-12-16 12:25:10 +0100

    feat(admin-import): handle import page visibility

--- packages/app/src/components/AdminUsers/AdminUsers.js
+++ packages/app/src/components/AdminUsers/AdminUsers.js
@@ -8,84 +8,36 @@ import { AdminFilterContext } from "../AdminFilterBar/context";
 import { PaginatedList } from "../PaginatedList";
 import { USERS } from "./queries";
 import { cardStyle, descriptionStyle, labelStyle } from "./style";
-const ServiceDetail = ({ service_admins }) => (
-  <>
-    <Text sx={labelStyle}>service</Text>
-    {service_admins.map((val, index) => (
-      <Text sx={descriptionStyle} key={index}>
-        {val.service.etablissement}
-      </Text>
-    ))}
-  </>
-);
-
-const MandataireDetail = ({ user_tis }) => (
-  <>
-    <Text sx={labelStyle}>tribunaux</Text>
-    {user_tis.map((val, index) => (
-      <Text sx={descriptionStyle} key={index}>
-        {val.ti.ville}
-      </Text>
-    ))}
-  </>
-);
-
-const MagistratDetail = ({ magistrat }) => (
-  <>
-    <Text sx={labelStyle}>tribunal</Text>
-    <Text sx={descriptionStyle}>
-      {magistrat && magistrat.ti ? magistrat.ti.ville : "Non renseigné"}
-    </Text>
-  </>
-);
-
-const DirectionDetail = () => <></>;
-
-const getDetail = type => {
-  if (type === "service") {
-    return ServiceDetail;
-  } else if (type === "individuel") {
-    return MandataireDetail;
-  } else if (type === "prepose") {
-    return MandataireDetail;
-  } else if (type === "ti") {
-    return MagistratDetail;
-  } else {
-    return DirectionDetail;
-  }
-};
 
 const RowItem = ({ item }) => {
   const { id, nom, prenom, email, type, active } = item;
-  const DetailComponent = getDetail(type);
 
   return (
     <>
       <Card sx={cardStyle(active)} width="100%">
-        <Flex justifyContent="flex-start">
-          <Flex width="100px" flexDirection="column">
-            <Text sx={labelStyle}>id</Text>
-            <Text sx={descriptionStyle}>{id}</Text>
-            <Text sx={descriptionStyle}>{type}</Text>
-          </Flex>
-          <Box width="300px">
-            <Text sx={labelStyle}>email</Text>
-            <Text sx={descriptionStyle}>{email}</Text>
-          </Box>
-          <Flex width="200px" flexDirection="column">
-            <Text sx={labelStyle}>prénom / nom</Text>
-            <Text sx={descriptionStyle}>{prenom}</Text>
-            <Text sx={descriptionStyle}>{nom}</Text>
-          </Flex>
-          <Flex width="100px" flexDirection="column">
-            <Text sx={labelStyle}>état</Text>
-            <Text sx={descriptionStyle}>{active ? "activé" : "non activé"}</Text>
-          </Flex>
-          <Flex width="300px" flexDirection="column">
-            <DetailComponent {...item} />
+        <Flex justifyContent="space-between">
+          <Flex justifyContent="flex-start">
+            <Flex width="100px" flexDirection="column">
+              <Text sx={labelStyle}>id</Text>
+              <Text sx={descriptionStyle}>{id}</Text>
+              <Text sx={descriptionStyle}>{type}</Text>
+            </Flex>
+            <Box width="300px">
+              <Text sx={labelStyle}>email</Text>
+              <Text sx={descriptionStyle}>{email}</Text>
+            </Box>
+            <Flex width="200px" flexDirection="column">
+              <Text sx={labelStyle}>prénom / nom</Text>
+              <Text sx={descriptionStyle}>{prenom}</Text>
+              <Text sx={descriptionStyle}>{nom}</Text>
+            </Flex>
+            <Flex width="100px" flexDirection="column">
+              <Text sx={labelStyle}>état</Text>
+              <Text sx={descriptionStyle}>{active ? "activé" : "non activé"}</Text>
+            </Flex>
           </Flex>
           <Box mr="1" width="120px">
-            <Link href={`/admin/users/${id}`}>
+            <Link href={`/admin/users/${id}?type=${type}&active=${active}`}>
               <a>
                 <Button>Voir</Button>
               </a>
