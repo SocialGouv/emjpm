8f09f98e73f462c958330edcc24aeb1ac13dcdae
Thomas Glatt <tglatt@gmail.com>
2019-12-16 14:06:12 +0100

    fix(admin-import): handle import for service

--- packages/api/email/mesures-import-email.js
+++ packages/api/email/mesures-import-email.js
@@ -1,6 +1,12 @@
 const { sendEmail } = require(".");
 
-const EMAIL_TEXT = (creationNumber, updateNumber, errors) => `
+const EMAIL_TEXT = (
+  creationNumber,
+  updateNumber,
+  errors,
+  mesures_en_cours,
+  dispo_max
+) => `
 Bonjour,
 
 Résultat de l'import de vos mesures:
@@ -9,19 +15,27 @@ Résultat de l'import de vos mesures:
 - ${errors.length} mesure(s) non traitée(s):
         ${errors.join("\n\t")}
 
+Pour rappel, à ce jour, vous avez déclaré "${mesures_en_cours}" mesures pour une capacité souhaitée de "${dispo_max}" mesures .
+
 Bien à vous.
 
 eMJPM Team
 `;
 
 const mesuresImportEmail = (
-  email,
+  { email, mesures_en_cours, dispo_max },
   { creationNumber, updateNumber, errors }
 ) => {
   sendEmail(
     email,
     "eMJPM > Résultat de l'import de vos mesures",
-    EMAIL_TEXT(creationNumber, updateNumber, errors),
+    EMAIL_TEXT(
+      creationNumber,
+      updateNumber,
+      errors,
+      mesures_en_cours,
+      dispo_max
+    ),
     null,
     "support.emjpm@fabrique.social.gouv.fr"
   ).catch(e => {
