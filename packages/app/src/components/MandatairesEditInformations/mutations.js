7da642d02812256d45a7305b1360964e1bb728a0
christophe <mr.dumont.christophe@gmail.com>
2019-12-17 08:40:00 +0100

    fix(mandataire-edit): add mutation for missing form fields (#1134)

--- packages/app/src/components/MandatairesEditInformations/mutations.js
+++ packages/app/src/components/MandatairesEditInformations/mutations.js
@@ -1,8 +1,41 @@
 import gql from "graphql-tag";
 
 export const EDIT_USER = gql`
-  mutation EditUser($cabinet: String, $prenom: String!, $nom: String!, $email: String!, $id: Int!) {
+  mutation EditUser(
+    $dispoMax: String
+    $telephone: String
+    $telephonePortable: String
+    $ville: String
+    $zipcode: String
+    $longitude: String
+    $latitude: String
+    $adresse: String
+    $codePostal: String
+    $genre: String
+    $cabinet: String
+    $prenom: String!
+    $nom: String!
+    $email: String!
+    $id: Int!
+  ) {
     __typename
+    update_mandataires(
+      _set: {
+        dispo_max: $dispoMax
+        telephone: $telephone
+        telephone_portable: $telephonePortable
+        ville: $ville
+        zip: $zipcode
+        longitude: $longitude
+        latitude: $latitude
+        adresse: $adresse
+        code_postal: $codePostal
+        genre: $genre
+      }
+      where: { user_id: { _eq: 10 } }
+    ) {
+      affected_rows
+    }
     update_users(
       _set: { cabinet: $cabinet, prenom: $prenom, nom: $nom, email: $email }
       where: { id: { _eq: $id } }
