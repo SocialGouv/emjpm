129350dda9e515f0a0d912aed09bbfef2f7c78bb
Paul Launay <launay.p@gmail.com>
2019-12-18 12:31:28 +0100

    feat(geocode-mandataire): handle geocode for mandataire signup (#1160)
    * feat(add-geocode-to-mandataire-signup): add geocode to mandataire signup
    
    * feat(aad-geocode-to-mandataire-signup): add lat/lng to mandataire signup

--- packages/app/src/lib/validationSchemas/index.js
+++ packages/app/src/lib/validationSchemas/index.js
@@ -1,5 +1,11 @@
 import { mandataireEditSchema } from "./mandataireEditSchema";
 import { mandataireMesureSchema } from "./mandataireMesureSchema";
+import { mandataireSignupSchema } from "./mandataireSignupSchema";
 import { serviceMesureSchema } from "./serviceMesureSchema";
 
-export { mandataireEditSchema, mandataireMesureSchema, serviceMesureSchema };
+export {
+  mandataireEditSchema,
+  mandataireMesureSchema,
+  serviceMesureSchema,
+  mandataireSignupSchema
+};
