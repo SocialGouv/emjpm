2953ba9e2dec644bdc4970db03e72eaf8a9c55ae
christophe <mr.dumont.christophe@gmail.com>
2019-12-19 13:58:41 +0100

    fix(metadata-mandataires): add antenne to mandataires permissions (#1243)

--- packages/app/src/components/MandataireMesureEdit/MandataireMesureEdit.js
+++ packages/app/src/components/MandataireMesureEdit/MandataireMesureEdit.js
@@ -2,24 +2,16 @@ import React, { useContext } from "react";
 import { Box } from "rebass";
 
 import { MesureContext } from "../MesureContext";
-import { UserContext } from "../UserContext";
 import { MandataireMesureEditForm } from "./MandataireMesureEditForm";
 import { ServiceMesureEditStyle } from "./style";
 
 const MandataireMesureEdit = props => {
   const { mesureId } = props;
   const mesure = useContext(MesureContext);
-  const { user_antennes } = useContext(UserContext);
 
   return (
     <Box sx={ServiceMesureEditStyle} {...props}>
-      <MandataireMesureEditForm
-        mt="3"
-        currentMesure={mesureId}
-        isPage
-        user_antennes={user_antennes}
-        {...mesure}
-      />
+      <MandataireMesureEditForm mt="3" currentMesure={mesureId} isPage {...mesure} />
     </Box>
   );
 };
