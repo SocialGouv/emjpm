40ba0bad7e7bd37b73c66d93a02aff64cda14b7a
Paul Launay <launay.p@gmail.com>
2019-12-19 20:57:51 +0100

    (feat): add geocode to antenne edit (#1251)
    * feat(add-geocode-to-antenne-create): readd address/lat/lng perms
    
    * feat(geocode): add lat/lng/address perms
    
    * feat(geocode): add geocode to service edit antenne
    
    * feat(geocode): readd service antenne permissions
    
    * fix(hasura-permission): add service_antenne permission

--- packages/app/src/components/ServiceAntenneEdit/mutations.js
+++ packages/app/src/components/ServiceAntenneEdit/mutations.js
@@ -12,8 +12,10 @@ export const EDIT_ANTENNE = gql`
     $contact_firstname: String
     $contact_email: String
     $address_zip_code: String
-    $address_street: String
+    $address: String
     $address_city: String
+    $latitude: Float
+    $longitude: Float
   ) {
     update_service_antenne(
       where: { id: { _eq: $antenne_id } }
@@ -25,14 +27,18 @@ export const EDIT_ANTENNE = gql`
         contact_firstname: $contact_firstname
         contact_email: $contact_email
         address_zip_code: $address_zip_code
-        address_street: $address_street
+        address: $address
         address_city: $address_city
+        latitude: $latitude
+        longitude: $longitude
       }
     ) {
       returning {
         address_city
-        address_street
+        address
         address_zip_code
+        latitude
+        longitude
         contact_email
         contact_firstname
         contact_lastname
