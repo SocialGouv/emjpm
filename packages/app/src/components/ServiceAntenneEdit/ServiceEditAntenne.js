d7dc0f4bdd07c077167ca11880c60779cf641c46
Paul Launay <launay.p@gmail.com>
2019-12-19 20:57:51 +0100

    (feat): add geocode to antenne edit (#1251)
    * feat(add-geocode-to-antenne-create): readd address/lat/lng perms
    
    * feat(geocode): add lat/lng/address perms
    
    * feat(geocode): add geocode to service edit antenne
    
    * feat(geocode): readd service antenne permissions
    
    * fix(hasura-permission): add service_antenne permission

--- packages/app/src/components/ServiceAntenneEdit/ServiceEditAntenne.js
+++ packages/app/src/components/ServiceAntenneEdit/ServiceEditAntenne.js
@@ -1,56 +1,59 @@
 import { useMutation } from "@apollo/react-hooks";
-import { Button, Card, Heading4, Input, Text } from "@socialgouv/emjpm-ui-core";
-import { Formik } from "formik";
+import { Card, Heading4, Text } from "@socialgouv/emjpm-ui-core";
 import Router from "next/router";
 import React from "react";
 import { Box, Flex } from "rebass";
-import * as Yup from "yup";
 
+import { ServiceAntenneForm } from "../ServiceAntenneForms";
 import { EDIT_ANTENNE } from "./mutations";
-import { ServiceEditAntenneStyle } from "./style";
-
-const grayBox = {
-  bg: "cardSecondary",
-  borderRadius: "5px 0 0 5px",
-  p: "5"
-};
-
-const cardStyle = { m: "1", mt: "5", p: "0" };
-
-export const getCurrentAntenne = (user_antennes, currentAntenne) => {
-  return user_antennes.filter(
-    user_antenne => user_antenne.antenne_id === parseInt(currentAntenne, 10)
-  );
-};
 
 const ServiceEditAntenne = props => {
-  const { service_admins, id, currentAntenne, user_antennes } = props;
-  const [currentUserService] = service_admins;
-  const { service_id } = currentUserService;
-  const [antenne] = getCurrentAntenne(user_antennes, currentAntenne);
-  const {
-    address_city,
-    address_street,
-    address_zip_code,
-    contact_email,
-    contact_firstname,
-    contact_lastname,
-    contact_phone,
-    mesures_max,
-    name
-  } = antenne.service_antenne;
-  const [createAntenne] = useMutation(EDIT_ANTENNE, {
+  const { service_admins, id, antenneId, user_antennes } = props;
+  const [{ service_id }] = service_admins;
+
+  const [{ service_antenne: antenne }] = user_antennes.filter(
+    ({ antenne_id }) => antenneId === antenne_id
+  );
+
+  const [editAntenne] = useMutation(EDIT_ANTENNE, {
     update() {
-      Router.push("/services/antennes/[antenne_id]", `/services/antennes/${currentAntenne}`, {
+      Router.push("/services/antennes/[antenne_id]", `/services/antennes/${antenneId}`, {
         shallow: true
       });
     }
   });
 
+  const handleSubmit = async (values, { setSubmitting }) => {
+    try {
+      await editAntenne({
+        refetchQueries: ["service_antenne"],
+        variables: {
+          address_city: values.geocode.city,
+          address: values.geocode.label,
+          address_zip_code: values.geocode.postcode,
+          latitude: values.geocode.lat,
+          longitude: values.geocode.lng,
+          contact_email: values.contact_email,
+          contact_firstname: values.contact_firstname,
+          contact_lastname: values.contact_lastname,
+          contact_phone: values.contact_phone,
+          mesures_max: values.mesures_max,
+          name: values.name,
+          service_id: service_id,
+          user_id: id
+        }
+      });
+    } catch (error) {
+      // TODO(paullaunay): log in sentry and handle in form
+    }
+
+    setSubmitting(false);
+  };
+
   return (
-    <Card sx={cardStyle}>
-      <Flex sx={ServiceEditAntenneStyle} {...props}>
-        <Box width={[1, 2 / 5]} sx={grayBox}>
+    <Card m="1" mt="5" p="5">
+      <Flex flexWrap="wrap">
+        <Box width={[1, 2 / 5]} bg="cardSecondary" p="5">
           <Box height="80px">
             <Heading4>{`Information de cette  antenne`}</Heading4>
             <Text lineHeight="1.5" color="textSecondary">
@@ -72,153 +75,7 @@ const ServiceEditAntenne = props => {
         </Box>
         <Box p="5" width={[1, 3 / 5]}>
           <Box sx={{ position: "relative", zIndex: "1" }} mb="2">
-            <Formik
-              onSubmit={(values, { setSubmitting }) => {
-                createAntenne({
-                  refetchQueries: ["service_antenne"],
-                  variables: {
-                    address_city: values.address_city,
-                    address_street: values.address_street,
-                    address_zip_code: values.address_zip_code,
-                    antenne_id: currentAntenne,
-                    contact_email: values.contact_email,
-                    contact_firstname: values.contact_firstname,
-                    contact_lastname: values.contact_lastname,
-                    contact_phone: values.contact_phone,
-                    mesures_max: values.mesures_max,
-                    name: values.name,
-                    service_id: service_id,
-                    user_id: id
-                  }
-                });
-                setSubmitting(false);
-              }}
-              validationSchema={Yup.object().shape({
-                address_city: Yup.string(),
-                address_street: Yup.string(),
-                address_zip_code: Yup.string(),
-                contact_email: Yup.string(),
-                contact_firstname: Yup.string(),
-                contact_lastname: Yup.string(),
-                contact_phone: Yup.string(),
-                mesures_max: Yup.number(),
-                name: Yup.string()
-              })}
-              initialValues={{
-                address_city: address_city || "",
-                address_street: address_street || "",
-                address_zip_code: address_zip_code || "",
-                contact_email: contact_email || "",
-                contact_firstname: contact_firstname || "",
-                contact_lastname: contact_lastname || "",
-                contact_phone: contact_phone || "",
-                mesures_max: mesures_max || "",
-                name: name || ""
-              }}
-            >
-              {props => {
-                const { values, touched, errors, isSubmitting, handleChange, handleSubmit } = props;
-                return (
-                  <form onSubmit={handleSubmit}>
-                    <Box sx={{ position: "relative", zIndex: "1" }} mb="2">
-                      <Input
-                        value={values.name}
-                        id="name"
-                        name="name"
-                        hasError={errors.name && touched.name}
-                        onChange={handleChange}
-                        placeholder="Nom de l'antenne"
-                      />
-                    </Box>
-                    <Box sx={{ position: "relative", zIndex: "1" }} mb="2" mt="5">
-                      <Input
-                        value={values.contact_lastname}
-                        id="contact_lastname"
-                        name="contact_lastname"
-                        hasError={errors.contact_lastname && touched.contact_lastname}
-                        onChange={handleChange}
-                        placeholder="Nom du responsable"
-                      />
-                    </Box>
-                    <Box sx={{ position: "relative", zIndex: "1" }} mb="2">
-                      <Input
-                        value={values.contact_firstname}
-                        id="contact_firstname"
-                        name="contact_firstname"
-                        hasError={errors.contact_firstname && touched.contact_firstname}
-                        onChange={handleChange}
-                        placeholder="Prénom du responsable"
-                      />
-                    </Box>
-                    <Box sx={{ position: "relative", zIndex: "1" }} mb="2">
-                      <Input
-                        value={values.mesures_max.toString()}
-                        id="mesures_max"
-                        name="mesures_max"
-                        hasError={errors.mesures_max && touched.mesures_max}
-                        onChange={handleChange}
-                        placeholder="Mesures maximum"
-                      />
-                    </Box>
-                    <Box sx={{ position: "relative", zIndex: "1" }} mb="2" mt="5">
-                      <Input
-                        value={values.contact_phone}
-                        id="contact_phone"
-                        name="contact_phone"
-                        hasError={errors.contact_phone && touched.contact_phone}
-                        onChange={handleChange}
-                        placeholder="Numéro de téléphone"
-                      />
-                    </Box>
-                    <Box sx={{ position: "relative", zIndex: "1" }} mb="2">
-                      <Input
-                        value={values.contact_email}
-                        id="contact_email"
-                        name="contact_email"
-                        hasError={errors.contact_email && touched.contact_email}
-                        onChange={handleChange}
-                        placeholder="Adresse email"
-                      />
-                    </Box>
-                    <Box sx={{ position: "relative", zIndex: "1" }} mb="2">
-                      <Input
-                        value={values.address_street}
-                        id="address_street"
-                        name="address_street"
-                        hasError={errors.address_street && touched.address_street}
-                        onChange={handleChange}
-                        placeholder="Adresse"
-                      />
-                    </Box>
-                    <Box sx={{ position: "relative", zIndex: "1" }} mb="2">
-                      <Input
-                        value={values.address_zip_code}
-                        id="address_zip_code"
-                        name="address_zip_code"
-                        hasError={errors.address_zip_code && touched.address_zip_code}
-                        onChange={handleChange}
-                        placeholder="Code postal"
-                      />
-                    </Box>
-                    <Box sx={{ position: "relative", zIndex: "1" }} mb="2">
-                      <Input
-                        value={values.address_city}
-                        id="address_city"
-                        name="address_city"
-                        hasError={errors.address_city && touched.address_city}
-                        onChange={handleChange}
-                        placeholder="Ville"
-                      />
-                    </Box>
-                    <Box>
-                      <Button type="submit" disabled={isSubmitting} isLoading={isSubmitting}>
-                        Enregistrer
-                      </Button>
-                    </Box>
-                  </form>
-                );
-              }}
-            </Formik>
+            <ServiceAntenneForm antenne={antenne} handleSubmit={handleSubmit} />
           </Box>
         </Box>
       </Flex>
