ec276a691a2ea5ec5f78e86a1dc82b8feec0a2b7
Paul Launay <launay.p@gmail.com>
2019-12-19 14:54:04 +0100

    feat(geocode): add geocode to antenne create (#1242)
    * feat(add-geocode-to-antenne-create): add address/lat/lng fields and perm
    
    * feat(add-geocode-to-antenne-create): add service antenne form to create
    
    * feat(add-geocode-to-antenne-create): readd address/lat/lng perms
    
    * test(geocode): update knex test snapshot

--- packages/app/src/lib/validationSchemas/index.js
+++ packages/app/src/lib/validationSchemas/index.js
@@ -1,11 +1,13 @@
 import { mandataireEditSchema } from "./mandataireEditSchema";
 import { mandataireMesureSchema } from "./mandataireMesureSchema";
 import { mandataireSignupSchema } from "./mandataireSignupSchema";
+import { serviceAntenneSchema } from "./serviceAntenneSchema";
 import { serviceMesureSchema } from "./serviceMesureSchema";
 
 export {
   mandataireEditSchema,
   mandataireMesureSchema,
+  serviceAntenneSchema,
   serviceMesureSchema,
   mandataireSignupSchema
 };
