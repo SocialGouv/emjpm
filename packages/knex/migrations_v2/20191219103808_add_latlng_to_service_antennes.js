a5d0b8a4bf30005e7f94b2f8207e711addd67d89
Paul Launay <launay.p@gmail.com>
2019-12-19 12:34:35 +0100

    feat(add-geocode-to-antenne-create): add address/lat/lng fields and perm

---
+++ packages/knex/migrations_v2/20191219103808_add_latlng_to_service_antennes.js
@@ -0,0 +1,13 @@
+exports.up = async function(knex) {
+  await knex.schema.alterTable("service_antenne", function(table) {
+    table.float("latitude");
+    table.float("longitude");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.alterTable("service_antenne", function(table) {
+    table.dropColumn("latitude");
+    table.dropColumn("longitude");
+  });
+};
