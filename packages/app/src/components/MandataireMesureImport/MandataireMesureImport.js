6fa0685ea7b9734c21a33143f3d1084febd98c1b
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2019-12-19 21:09:30 +0100

    feat(mandataire-import): add xls template (#1270)

--- packages/app/src/components/MandataireMesureImport/MandataireMesureImport.js
+++ packages/app/src/components/MandataireMesureImport/MandataireMesureImport.js
@@ -1,12 +1,12 @@
 import { Card, Heading3, Text } from "@socialgouv/emjpm-ui-core";
 import React, { Fragment } from "react";
 import { Box, Flex } from "rebass";
-// import { FileExcel } from "styled-icons/fa-regular/FileExcel";
+import { FileExcel } from "styled-icons/fa-regular/FileExcel";
 import { FilePdf } from "styled-icons/fa-regular/FilePdf";
 
 import {
-  MANDATAIRE_MESURE_IMPORT_MANUAL
-  //, MANDATAIRE_MESURE_IMPORT_TEMPLATE
+  MANDATAIRE_MESURE_IMPORT_MANUAL,
+  MANDATAIRE_MESURE_IMPORT_TEMPLATE
 } from "../../constants/import";
 import { MandataireAddMesureImport } from "./MandataireAddMesureImport";
 
@@ -54,12 +54,12 @@ const MandataireMesureImport = () => {
                   Télécharger la note explicative (.pdf)
                 </Box>
               </DocumentLink>
-              {/* <DocumentLink document={MANDATAIRE_MESURE_IMPORT_TEMPLATE}>
+              <DocumentLink document={MANDATAIRE_MESURE_IMPORT_TEMPLATE}>
                 <FileExcel size="86" width="100%" color="#008000" />
                 <Box textAlign="center" mt={2} color="#0067EA">
                   Télécharger la modèle Excel
                 </Box>
-              </DocumentLink> */}
+              </DocumentLink>
             </Flex>
             <Text mb="1" lineHeight="2">
               {`Nous vous conseillons de réaliser l’import de vos mesures qu’une seule fois au début de votre utilisation de e-MJPM. Une fois le tableau importé une première fois, vous serez prêt.e à utiliser parfaitement e-MJPM. Vous pourrez alors faire toutes les modifications, les mises à jours et les ajoûts vous même.`}
