9c338338367ef7876394290f42d12c8dfddd3a3d
kwiss <christophe@hooraylabs.com>
2019-12-23 11:11:36 +0100

    fix(maps): fix map queries

---
+++ packages/app/src/components/ServiceMap/index.js
@@ -0,0 +1,3 @@
+import { MapContext, MapContextConsumer, MapContextProvider } from "./context";
+import { ServiceMap } from "./ServiceMap";
+export { ServiceMap, MapContext, MapContextConsumer, MapContextProvider };
