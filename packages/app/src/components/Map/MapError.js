3c987ad144ff423802632a01f21c04beef34d9e3
Paul Launay <launay.p@gmail.com>
2019-12-27 11:30:04 +0100

    fix(map): add map error when lat/lng not present (#1348)

---
+++ packages/app/src/components/Map/MapError.js
@@ -0,0 +1,15 @@
+import { Card } from "@socialgouv/emjpm-ui-core";
+import Link from "next/link";
+import React from "react";
+
+const MapError = () => (
+  <Card mt={2} mr={2}>
+    Votre adresse est invalide, veuillez la renseigner
+    <Link href="/mandataires/edit-informations">
+      <a> ici</a>
+    </Link>
+    .
+  </Card>
+);
+
+export { MapError };
