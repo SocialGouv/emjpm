f5278c3ea1b51b6f84150c6c9d428fb6d2f79fe2
Paul Launay <launay.p@gmail.com>
2020-01-06 15:45:45 +0100

    feat(geocode): add address, lat, lng tis migration

---
+++ packages/knex/migrations_v2/20200106151518_add_latitude_longitude_to_tis.js
@@ -0,0 +1,15 @@
+exports.up = async function(knex) {
+  await knex.schema.alterTable("tis", function(table) {
+    table.string("address");
+    table.float("latitude");
+    table.float("longitude");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.alterTable("tis", function(table) {
+    table.dropColumn("address");
+    table.dropColumn("latitude");
+    table.dropColumn("longitude");
+  });
+};
