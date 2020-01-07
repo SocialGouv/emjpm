08dfdb06cec3434b613000375f9f5cbeb73bf8ff
Paul Launay <launay.p@gmail.com>
2020-01-07 21:46:25 +0100

    fix(forms): revert to fetch query in magistrat mesure delete

--- packages/app/pages/magistrats/mesures/[mesure_id]/delete.js
+++ packages/app/pages/magistrats/mesures/[mesure_id]/delete.js
@@ -4,18 +4,16 @@ import { Flex } from "rebass";
 
 import { LayoutMagistrat } from "../../../../src/components/Layout";
 import { MagistratMesureDelete } from "../../../../src/components/MagistratMesureDelete";
-import { MesureProvider } from "../../../../src/components/MesureContext";
 import { withAuthSync } from "../../../../src/util/auth";
 
 const MagistratMesureDeletePage = props => {
   const { mesureId } = props;
+
   return (
     <LayoutMagistrat>
       <BoxWrapper mt="6" px="1">
         <Flex flexWrap="wrap" mt="2">
-          <MesureProvider mesureId={mesureId}>
-            <MagistratMesureDelete />
-          </MesureProvider>
+          <MagistratMesureDelete mesureId={mesureId} />
         </Flex>
       </BoxWrapper>
     </LayoutMagistrat>
--- packages/app/src/components/MagistratMesureDelete/MagistratMesureDelete.js
+++ packages/app/src/components/MagistratMesureDelete/MagistratMesureDelete.js
@@ -1,112 +1,33 @@
-import { useMutation } from "@apollo/react-hooks";
-import { Button, Heading3, Heading5, Input } from "@socialgouv/emjpm-ui-core";
-import { useFormik } from "formik";
-import Router from "next/router";
-import React, { useContext } from "react";
-import { Box, Flex, Text } from "rebass";
+import { useQuery } from "@apollo/react-hooks";
+import React from "react";
+import { Flex } from "rebass";
 
-import { magistratMesureDeleteSchema } from "../../lib/validationSchemas";
-import { MESURES } from "../MagistratMesures/queries";
-import { MesureContext } from "../MesureContext";
-import { DELETE_MANDATAIRE_MESURE, DELETE_SERVICE_MESURE } from "./mutations";
+import { MagistratMesureDeleteForm } from "./MagistratMesureDeleteForm";
+import { MESURE } from "./queries";
 import { MagistratMesureRemoveStyle } from "./style";
 
-export const MagistratMesureDelete = () => {
-  const mesure = useContext(MesureContext);
+export const MagistratMesureDelete = props => {
+  const { mesureId } = props;
 
-  const [deleteMandataireMesure] = useMutation(DELETE_MANDATAIRE_MESURE, {
-    onCompleted() {
-      Router.push(`/magistrats/mesures`);
+  const { data, loading, error } = useQuery(MESURE, {
+    variables: {
+      id: mesureId
     }
   });
 
-  const [deleteServiceMesure] = useMutation(DELETE_SERVICE_MESURE, {
-    onCompleted() {
-      Router.push(`/magistrats/mesures`);
-    }
-  });
+  if (error) {
+    return <div>error</div>;
+  }
 
-  const queryVariables = {
-    offset: 0,
-    searchText: null,
-    status: null,
-    type: null,
-    __deleted: true
-  };
+  if (loading) {
+    return <div>loading...</div>;
+  }
 
-  const formik = useFormik({
-    onSubmit: async (values, { setSubmitting }) => {
-      if (mesure.mandataireId) {
-        await deleteMandataireMesure({
-          refetchQueries: [{ query: MESURES, variables: queryVariables }],
-          variables: {
-            id: mesure.id,
-            mandataire_id: mesure.mandataireId
-          }
-        });
-      } else if (mesure.serviceId) {
-        await deleteServiceMesure({
-          refetchQueries: ["mesures"],
-          variables: {
-            id: mesure.id,
-            service_id: mesure.serviceId
-          }
-        });
-      }
-
-      setSubmitting(false);
-    },
-    validationSchema: magistratMesureDeleteSchema,
-    initialValues: {
-      reason_delete: ""
-    }
-  });
+  const [mesure] = data.mesures;
 
   return (
     <Flex sx={MagistratMesureRemoveStyle}>
-      <Box bg="cardSecondary" p="5" width={[1, 3 / 5]}>
-        <Heading5 mb="1">Supprimer la mesure</Heading5>
-        <Text mb="2" lineHeight="1.5">
-          {`Vous êtes sur le point de supprimer définitivement une mesure réservée du système eMJPM. Toute suppression est irréversible.`}
-        </Text>
-        <Text lineHeight="1.5">{`Si vous souhaitez supprimer cette mesure réservée, cliquez sur "Supprimer la mesure".`}</Text>
-        <Text lineHeight="1.5">{`Dans le cas contraire, cliquez sur "Annuler".`}</Text>
-      </Box>
-      <Box p="5" width={[1, 2 / 5]}>
-        <Box mb="3">
-          <Heading3>Supprimer la mesure</Heading3>
-        </Box>
-        <form onSubmit={formik.handleSubmit}>
-          <Box mb="2">
-            <Input
-              value={formik.values.reason_delete}
-              hasError={formik.errors.reason_delete && formik.touched.reason_delete}
-              id="reason_delete"
-              name="reason_delete"
-              onChange={formik.handleChange}
-              placeholder="Raison de la suppression"
-            />
-          </Box>
-          <Flex justifyContent="flex-end">
-            <Box>
-              <Button
-                mr="2"
-                variant="outline"
-                onClick={() => {
-                  // Router.push(`/magistrats/mesures/${mesure.id}`);
-                }}
-              >
-                Annuler
-              </Button>
-            </Box>
-            <Box>
-              <Button type="submit" disabled={formik.isSubmitting} isLoading={formik.isSubmitting}>
-                Supprimer la mesure
-              </Button>
-            </Box>
-          </Flex>
-        </form>
-      </Box>
+      <MagistratMesureDeleteForm mesure={mesure} />
     </Flex>
   );
 };
