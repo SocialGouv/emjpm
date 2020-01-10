a3caf590ddfafeb96e593d86bfc915e2d6725f33
kwiss <christophe@hooraylabs.com>
2020-01-10 18:15:23 +0100

    feat(magistrat-mesure): update page style

--- packages/app/src/components/MagistratMesureMandataire/MagistratMesureMandataire.js
+++ packages/app/src/components/MagistratMesureMandataire/MagistratMesureMandataire.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/react-hooks";
-import { Card, Heading4, Spinner } from "@socialgouv/emjpm-ui-core";
+import { Card, Heading3, Heading4, Spinner } from "@socialgouv/emjpm-ui-core";
 import dynamic from "next/dynamic";
 import React, { useContext } from "react";
 import { Box, Flex, Text } from "rebass";
@@ -9,8 +9,9 @@ import { MesureContext } from "../MesureContext";
 import { GESTIONNAIRES } from "./queries";
 import {
   MagistratContentMandataireStyle,
+  MagistratMainStyle,
   MagistratMandataireStyle,
-  MagistratSideMandataireStyle,
+  MagistratSideStyle,
   MagistratTitleMandataireStyle,
   MagistratTribunal
 } from "./style";
@@ -75,78 +76,85 @@ const MagistratMesureMandataire = props => {
   const lastLoginColor = lastLoginIsCritical ? "error" : "";
   return (
     <Box {...props} width="100%" mb={6}>
+      <Heading3 mt="4" mb="3">
+        {`Le ${serviceId ? "service" : "mandataire"} attribué à la mesure`}
+      </Heading3>
       <Flex sx={MagistratMandataireStyle}>
-        <Box width="50%">
-          {mandataireId && (
-            <Box>
-              <Text sx={MagistratTitleMandataireStyle}>Nom du mandataire</Text>
-              <Text sx={MagistratContentMandataireStyle}>{`${prenom} ${nom}`}</Text>
-            </Box>
-          )}
-          {serviceId && (
-            <Box>
-              <Text sx={MagistratTitleMandataireStyle}>{`Nom de l'association`}</Text>
-              <Text sx={MagistratContentMandataireStyle}>{etablissement}</Text>
-            </Box>
-          )}
-          <Box>
-            <Text sx={MagistratTitleMandataireStyle}>{`Adresse d’activité`}</Text>
-            <Text sx={MagistratContentMandataireStyle}>{`${adresse} ${codePostal} ${ville}`}</Text>
-          </Box>
-          <Box>
-            <Text sx={MagistratTitleMandataireStyle}>Dernière connexion</Text>
-            <Text color={lastLoginColor} sx={MagistratContentMandataireStyle}>
-              {lastLogin}
-            </Text>
-          </Box>
-          <Box>
-            <Text sx={MagistratTitleMandataireStyle}>Tribunaux d’instance</Text>
-            {tis.map(ti => {
-              return (
-                <Text key={ti.tis.id} sx={MagistratTribunal}>
-                  {ti.tis.etablissement}
-                </Text>
-              );
-            })}
-          </Box>
+        <Box sx={MagistratSideStyle} height="400px">
+          <MagistratMandataireMap
+            longitude={longitude}
+            discriminator={discriminator}
+            latitude={latitude}
+            id={id}
+          />
         </Box>
-        <Box width="50%">
-          <Box>
-            <Text sx={MagistratTitleMandataireStyle}>
-              Informations (Préférences géographiques, compétences, ...)
-            </Text>
-            <Text sx={MagistratContentMandataireStyle}>{competences}</Text>
+        <Flex sx={MagistratMainStyle}>
+          <Box width="50%">
+            {mandataireId && (
+              <Box>
+                <Text sx={MagistratTitleMandataireStyle}>Nom du mandataire</Text>
+                <Text sx={MagistratContentMandataireStyle}>{`${prenom} ${nom}`}</Text>
+              </Box>
+            )}
+            {serviceId && (
+              <Box>
+                <Text sx={MagistratTitleMandataireStyle}>{`Nom de l'association`}</Text>
+                <Text sx={MagistratContentMandataireStyle}>{etablissement}</Text>
+              </Box>
+            )}
+            <Box>
+              <Text sx={MagistratTitleMandataireStyle}>{`Adresse d’activité`}</Text>
+              <Text
+                sx={MagistratContentMandataireStyle}
+              >{`${adresse} ${codePostal} ${ville}`}</Text>
+            </Box>
+            <Box>
+              <Text sx={MagistratTitleMandataireStyle}>Dernière connexion</Text>
+              <Text color={lastLoginColor} sx={MagistratContentMandataireStyle}>
+                {lastLogin}
+              </Text>
+            </Box>
+            <Box>
+              <Text sx={MagistratTitleMandataireStyle}>Tribunaux d’instance</Text>
+              {tis.map(ti => {
+                return (
+                  <Text key={ti.tis.id} sx={MagistratTribunal}>
+                    {ti.tis.etablissement}
+                  </Text>
+                );
+              })}
+            </Box>
           </Box>
-          <Box>
-            <Text sx={MagistratTitleMandataireStyle}>Email</Text>
-            <Text sx={MagistratContentMandataireStyle}>{email}</Text>
+          <Box width="50%">
+            <Box>
+              <Text sx={MagistratTitleMandataireStyle}>
+                Informations (Préférences géographiques, compétences, ...)
+              </Text>
+              <Text sx={MagistratContentMandataireStyle}>{competences}</Text>
+            </Box>
+            <Box>
+              <Text sx={MagistratTitleMandataireStyle}>Email</Text>
+              <Text sx={MagistratContentMandataireStyle}>{email}</Text>
+            </Box>
+            <Box>
+              <Text sx={MagistratTitleMandataireStyle}>Téléphone</Text>
+              <Text sx={MagistratContentMandataireStyle}>{telephone}</Text>
+            </Box>
+            <Box>
+              <Text sx={MagistratTitleMandataireStyle}>Disponibilité</Text>
+              <Text sx={MagistratContentMandataireStyle}>{dispoMax}</Text>
+            </Box>
+            <Box>
+              <Text sx={MagistratTitleMandataireStyle}>Mesure en attente</Text>
+              <Text sx={MagistratContentMandataireStyle}>{mesuresAwaiting}</Text>
+            </Box>
+            <Box>
+              <Text sx={MagistratTitleMandataireStyle}>Mesure en cours</Text>
+              <Text sx={MagistratContentMandataireStyle}>{mesuresInProgress}</Text>
+            </Box>
           </Box>
-          <Box>
-            <Text sx={MagistratTitleMandataireStyle}>Téléphone</Text>
-            <Text sx={MagistratContentMandataireStyle}>{telephone}</Text>
-          </Box>
-          <Box>
-            <Text sx={MagistratTitleMandataireStyle}>Disponibilité</Text>
-            <Text sx={MagistratContentMandataireStyle}>{dispoMax}</Text>
-          </Box>
-          <Box>
-            <Text sx={MagistratTitleMandataireStyle}>Mesure en attente</Text>
-            <Text sx={MagistratContentMandataireStyle}>{mesuresAwaiting}</Text>
-          </Box>
-          <Box>
-            <Text sx={MagistratTitleMandataireStyle}>Mesure en cours</Text>
-            <Text sx={MagistratContentMandataireStyle}>{mesuresInProgress}</Text>
-          </Box>
-        </Box>
+        </Flex>
       </Flex>
-      <Box height="400px" mt="5" sx={MagistratSideMandataireStyle}>
-        <MagistratMandataireMap
-          longitude={longitude}
-          discriminator={discriminator}
-          latitude={latitude}
-          id={id}
-        />
-      </Box>
       <Box>{serviceId && <MagistratServiceAntennes serviceId={serviceId} />}</Box>
     </Box>
   );
--- packages/app/src/components/MagistratMesureMandataire/style.js
+++ packages/app/src/components/MagistratMesureMandataire/style.js
@@ -4,25 +4,21 @@ const MagistratMandataireStyle = {
   overFlow: "hidden",
   flexWrap: "wrap",
   width: "100%",
-  position: "relative",
-  mt: "6",
-  p: "3"
+  position: "relative"
 };
 
-const MagistratMainMandataireStyle = {
+const MagistratSideStyle = {
+  flexGrow: 1,
+  flexBasis: 380,
+  minHeight: "320px"
+};
+
+const MagistratMainStyle = {
   px: 4,
   pt: 5,
   flexGrow: 99999,
   flexBasis: 0,
   minWidth: 320
-};
-
-const MagistratSideMandataireStyle = {
-  flexGrow: 1,
-  flexBasis: 380,
-  minHeight: "320px",
-  bg: "white",
-  borderRadius: "6px"
 };
 
 const MagistratTitleMandataireStyle = {
@@ -48,35 +44,11 @@ const MagistratTribunal = {
   mb: "5px"
 };
 
-const boxStyle = {
-  flexGrow: 1
-};
-
-const topTextStyle = {
-  color: "mediumGray",
-  fontWeight: "600",
-  mt: "2"
-};
-
-const MagistratAntennesStyle = {
-  px: 4,
-  mt: "6",
-  pt: 5,
-  flexGrow: 99999,
-  flexBasis: 0,
-  minWidth: 320,
-  bg: "white",
-  borderRadius: "6px"
-};
-
 export {
   MagistratMandataireStyle,
-  MagistratMainMandataireStyle,
-  MagistratSideMandataireStyle,
   MagistratTitleMandataireStyle,
   MagistratContentMandataireStyle,
-  topTextStyle,
-  boxStyle,
-  MagistratAntennesStyle,
-  MagistratTribunal
+  MagistratTribunal,
+  MagistratSideStyle,
+  MagistratMainStyle
 };
