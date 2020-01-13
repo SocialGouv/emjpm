7f1d14952553ca70cb2326763d8a9da9eb08bb70
kwiss <christophe@hooraylabs.com>
2020-01-13 11:59:39 +0100

    fix(mandataire-reservation): add mesures on mandataire map

--- packages/app/src/components/MagistratMandataireMap/MagistratMandataireMap.js
+++ packages/app/src/components/MagistratMandataireMap/MagistratMandataireMap.js
@@ -1,19 +1,36 @@
+import { useQuery } from "@apollo/react-hooks";
 import React from "react";
 
-import { MapContainer, MapLayer } from "../Map";
-import { mapImages } from "../Map/utils";
+import { formatGestionnaireId } from "../../util/mandataires";
+import { MagistratMandataireMapContent } from "./MagistratMandataireMapContent";
+import { MESURES } from "./queries";
 
 const MagistratMandataireMap = props => {
   const { latitude, longitude, id, discriminator } = props;
+  const { mandataireId, serviceId } = formatGestionnaireId(id);
+  const { data, loading, error } = useQuery(MESURES, {
+    variables: {
+      mandataireId: mandataireId,
+      serviceId: serviceId
+    }
+  });
+
+  if (loading) {
+    return <div>loading</div>;
+  }
+
+  if (error) {
+    return <div>error</div>;
+  }
+  const { mesures } = data;
   return (
-    <MapContainer latitude={latitude} longitude={longitude}>
-      <MapLayer image={mapImages["MESURE"]} items={[{ id, latitude, longitude }]} type={"MESURE"} />
-      <MapLayer
-        image={mapImages[discriminator]}
-        items={[{ id, latitude, longitude }]}
-        type={discriminator}
-      />
-    </MapContainer>
+    <MagistratMandataireMapContent
+      mesures={mesures}
+      latitude={latitude}
+      longitude={longitude}
+      id={id}
+      discriminator={discriminator}
+    />
   );
 };
 
