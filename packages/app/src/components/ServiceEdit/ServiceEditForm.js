dacc12239c7e22e3a2ed7349b813b63a4eef0ddd
Paul Launay <launay.p@gmail.com>
2020-01-13 19:56:28 +0100

    feat(form-errors): use inlinerror/localized yup in service edit form

--- packages/app/src/components/ServiceEdit/ServiceEditForm.js
+++ packages/app/src/components/ServiceEdit/ServiceEditForm.js
@@ -1,4 +1,4 @@
-import { Button, Field, Input, Text, Textarea } from "@socialgouv/emjpm-ui-core";
+import { Button, Field, InlineError, Input, Textarea } from "@socialgouv/emjpm-ui-core";
 import { useFormik } from "formik";
 import React from "react";
 
@@ -35,6 +35,7 @@ const ServiceEditForm = props => {
           onChange={formik.handleChange}
           placeholder="Nom du service"
         />
+        <InlineError message={formik.errors.etablissement} fieldId="etablissement" />
       </Field>
       <Field>
         <Input
@@ -45,6 +46,7 @@ const ServiceEditForm = props => {
           onChange={formik.handleChange}
           placeholder="Nom du responsable"
         />
+        <InlineError message={formik.errors.nom} fieldId="nom" />
       </Field>
       <Field>
         <Input
@@ -55,6 +57,7 @@ const ServiceEditForm = props => {
           onChange={formik.handleChange}
           placeholder="Prénom du responsable"
         />
+        <InlineError message={formik.errors.prenom} fieldId="prenom" />
       </Field>
       <Field>
         <Input
@@ -65,6 +68,7 @@ const ServiceEditForm = props => {
           onChange={formik.handleChange}
           placeholder="Mesures maximum"
         />
+        <InlineError message={formik.errors.dispo_max} fieldId="dispo_max" />
       </Field>
       <Field>
         <Input
@@ -75,6 +79,7 @@ const ServiceEditForm = props => {
           onChange={formik.handleChange}
           placeholder="Numéro de téléphone"
         />
+        <InlineError message={formik.errors.telephone} fieldId="telephone" />
       </Field>
       <Field>
         <Input
@@ -85,13 +90,14 @@ const ServiceEditForm = props => {
           onChange={formik.handleChange}
           placeholder="Adresse email"
         />
+        <InlineError message={formik.errors.etablissement} fieldId="email" />
       </Field>
       <Field>
         <Geocode
           resource={service}
           onChange={geocode => formik.setFieldValue("geocode", geocode)}
         />
-        {formik.errors.geocode && formik.touched.geocode && <Text>{formik.errors.geocode}</Text>}
+        <InlineError message={formik.errors.geocode} fieldId="geocode" />
       </Field>
       <Field>
         <Textarea
@@ -103,6 +109,7 @@ const ServiceEditForm = props => {
           label="Informations pour le magistrat"
           placeholder="Préférences géographiques, compétences, ..."
         />
+        <InlineError message={formik.errors.competences} fieldId="competences" />
       </Field>
       <Field>
         <Button type="submit" disabled={formik.isSubmitting} isLoading={formik.isSubmitting}>
