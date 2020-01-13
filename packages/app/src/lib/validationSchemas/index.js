c912ac4b63b316dc35059333fbd87d4df5a41860
Paul Launay <launay.p@gmail.com>
2020-01-13 20:37:05 +0100

    feat(form-errors): use inlineerror/localized yup in direction signup

--- packages/app/src/lib/validationSchemas/index.js
+++ packages/app/src/lib/validationSchemas/index.js
@@ -11,6 +11,7 @@ import { passwordSchema } from "./passwordSchema";
 import { serviceAcceptMesureSchema } from "./serviceAcceptMesureSchema";
 import { serviceAntenneSchema } from "./serviceAntenneSchema";
 import { serviceMesureSchema } from "./serviceMesureSchema";
+import { signupDirectionSchema } from "./signupDirectionSchema";
 import { signupMagistratSchema } from "./signupMagistratSchema";
 import { signupSchema } from "./signupSchema";
 import { signupServiceSchema } from "./signupServiceSchema";
@@ -29,6 +30,7 @@ export {
   mandataireSignupSchema,
   serviceAcceptMesureSchema,
   mandataireAcceptMesureSchema,
+  signupDirectionSchema,
   signupMagistratSchema,
   signupSchema,
   signupServiceSchema
