69471bb6d2167450aa3bee66d722a1f08b7cf228
Paul Launay <launay.p@gmail.com>
2020-01-14 18:55:58 +0100

    refactor(user-antennes): remove user antennes in service comps

--- packages/app/pages/services/antennes/[antenne_id]/edit.js
+++ packages/app/pages/services/antennes/[antenne_id]/edit.js
@@ -1,26 +1,25 @@
 import { BoxWrapper, Heading1 } from "@socialgouv/emjpm-ui-core";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useContext } from "react";
 
 import { LayoutServices } from "../../../../src/components/Layout";
-import { ServiceEditAntenne } from "../../../../src/components/ServiceAntenneEdit";
-import { UserInformations } from "../../../../src/components/UserInformations";
+import { ServiceAntenneEdit } from "../../../../src/components/ServiceAntenneEdit";
+import { UserContext } from "../../../../src/components/UserContext";
 import { withAuthSync } from "../../../../src/util/auth";
 
-const AddAntennes = () => {
+const ServiceAntenneEditPage = () => {
   const router = useRouter();
   const antenneId = parseInt(router.query.antenne_id);
+  const user = useContext(UserContext);
 
   return (
     <LayoutServices>
       <BoxWrapper mt={6}>
         <Heading1 mx="1">{`Modification de l'antenne`}</Heading1>
-        <UserInformations
-          Component={props => <ServiceEditAntenne {...props} antenneId={antenneId} mt="3" />}
-        />
+        <ServiceAntenneEdit user={user} antenneId={antenneId} mt="3" />
       </BoxWrapper>
     </LayoutServices>
   );
 };
 
-export default withAuthSync(AddAntennes);
+export default withAuthSync(ServiceAntenneEditPage);
