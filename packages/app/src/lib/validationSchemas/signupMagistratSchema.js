baaeadb98ae3a54c40af55efc9eebb478f699878
Paul Launay <launay.p@gmail.com>
2020-01-14 09:59:31 +0100

    fix(form-errors): remove useless unlocalized yup

--- packages/app/src/lib/validationSchemas/signupMagistratSchema.js
+++ packages/app/src/lib/validationSchemas/signupMagistratSchema.js
@@ -2,7 +2,7 @@ import yup from "./yup";
 
 const signupMagistratSchema = yup.object().shape({
   cabinet: yup.string(),
-  ti: yup.string().required("Champ obligatoire")
+  ti: yup.string().required()
 });
 
 export { signupMagistratSchema };
