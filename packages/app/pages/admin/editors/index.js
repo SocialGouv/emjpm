e35ba46ca4be18e6ac8f16877ffda477968983c4
Paul Launay <launay.p@gmail.com>
2020-01-28 09:40:29 +0100

    feat(editor): add editor api tokens (#1445)
    * feat(editors): add editors table
    
    * feat(editors): add editors hasura metadata
    
    * feat(editors): add admin editors index / create pages
    
    * test(editors): update knex test snapshot
    
    * test(editors): update knex snapshot
    
    * feat(editors): add admin editor create comps
    
    * feat(editors): add admin editor view page and comps

---
+++ packages/app/pages/admin/editors/index.js
@@ -0,0 +1,33 @@
+import { BoxWrapper, Heading1 } from "@socialgouv/emjpm-ui-core";
+import Router from "next/router";
+import React from "react";
+import { Flex } from "rebass";
+
+import { AdminEditors } from "../../../src/components/AdminEditors";
+import { AdminFilterBar } from "../../../src/components/AdminFilterBar";
+import { AdminFilterProvider } from "../../../src/components/AdminFilterBar/context";
+import { LayoutAdmin } from "../../../src/components/Layout";
+import { withAuthSync } from "../../../src/util/auth";
+
+const AdminEditorsPage = () => {
+  return (
+    <AdminFilterProvider>
+      <LayoutAdmin>
+        <BoxWrapper mt={6} px="1">
+          <Heading1>Liste des éditeurs</Heading1>
+          <Flex
+            sx={{
+              flexWrap: "wrap",
+              mt: "2"
+            }}
+          >
+            <AdminFilterBar onAddButtonClick={() => Router.push("/admin/editors/create ")} />
+            <AdminEditors />
+          </Flex>
+        </BoxWrapper>
+      </LayoutAdmin>
+    </AdminFilterProvider>
+  );
+};
+
+export default withAuthSync(AdminEditorsPage);
