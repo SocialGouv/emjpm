5dddc802d9b9eb30e651d7a0791630781e2d9dc0
christophe <mr.dumont.christophe@gmail.com>
2020-02-03 16:48:52 +0100

    fix(service-add-mesure): move files in the proper folder split form (#1464)

---
+++ packages/app/src/components/ServiceMesureCreate/ServiceMesureCreate.js
@@ -0,0 +1,142 @@
+import { useMutation, useQuery } from "@apollo/react-hooks";
+import { Card, Heading4, Text } from "@socialgouv/emjpm-ui-core";
+import Router from "next/router";
+import React from "react";
+import { Box, Flex } from "rebass";
+
+import { getRegionCode } from "../../util/departements";
+import { formatServiceTribunalList } from "../../util/services";
+import { ADD_MESURE, UPDATE_ANTENNE_COUTERS, UPDATE_SERVICES_COUTERS } from "./mutations";
+import { DEPARTEMENTS, SERVICE_TRIBUNAL } from "./queries";
+import { ServiceMesureCreateForm } from "./ServiceMesureCreateForm";
+
+export const ServiceMesureCreate = props => {
+  const { service } = props;
+  const { service_antennes } = service;
+
+  const { loading, error, data } = useQuery(SERVICE_TRIBUNAL, {
+    variables: { serviceId: service.id }
+  });
+
+  const {
+    data: departementsData,
+    loading: departementsLoading,
+    error: departementsError
+  } = useQuery(DEPARTEMENTS);
+
+  const [updateAntenneCounters] = useMutation(UPDATE_ANTENNE_COUTERS);
+  const [updateServicesCounter] = useMutation(UPDATE_SERVICES_COUTERS);
+
+  const [addMesure] = useMutation(ADD_MESURE, {
+    options: { refetchQueries: ["mesures", "mesures_aggregate"] },
+    onCompleted({ insert_mesures }) {
+      const [mesure] = insert_mesures.returning;
+      Router.push(`/services/mesures/${mesure.id}`);
+    },
+    update(
+      cache,
+      {
+        data: {
+          insert_mesures: { returning }
+        }
+      }
+    ) {
+      const [mesure] = returning;
+      updateServicesCounter({
+        variables: {
+          mesures_awaiting: 0,
+          mesures_in_progress: 1,
+          service_id: mesure.service_id
+        }
+      });
+      if (mesure.antenne_id) {
+        updateAntenneCounters({
+          variables: {
+            antenne_id: mesure.antenne_id,
+            inc_mesures_awaiting: 0,
+            inc_mesures_in_progress: 1
+          }
+        });
+      }
+    }
+  });
+
+  const handleSubmit = (values, { setSubmitting, setErrors }) => {
+    const regionCode = getRegionCode(values.geocode.postcode);
+    const departements = departementsData.departements;
+    const departement = departements.find(dep => dep.code === regionCode);
+
+    if (!departement) {
+      setErrors({
+        code_postal: `Aucun département trouvé pour le code postal ${values.code_postal}`
+      });
+    } else {
+      addMesure({
+        awaitRefetchQueries: true,
+        refetchQueries: ["mesures", "mesures_aggregate"],
+        variables: {
+          annee: values.annee.toString(),
+          antenne_id: values.antenne ? Number.parseInt(values.antenne.value) : null,
+          civilite: values.civilite.value,
+          code_postal: values.geocode.postcode,
+          ville: values.geocode.city,
+          latitude: values.geocode.latitude,
+          longitude: values.geocode.longitude,
+          date_ouverture: values.date_ouverture,
+          department_id: departement.id,
+          numero_dossier: values.numero_dossier,
+          numero_rg: values.numero_rg,
+          residence: values.residence.value,
+          ti_id: values.tribunal.value,
+          type: values.type.value
+        }
+      });
+    }
+
+    setSubmitting(false);
+  };
+
+  if (loading || departementsLoading) {
+    return <div>Chargement...</div>;
+  }
+
+  if (error || departementsError) {
+    return <div>Erreur...</div>;
+  }
+
+  const antenneOptions = service_antennes.map(antenne => ({
+    label: antenne.name,
+    value: antenne.id
+  }));
+
+  const tribunalList = formatServiceTribunalList(data.service_tis);
+
+  return (
+    <Card m="1" mt="5" p="0">
+      <Flex flexWrap="wrap" {...props}>
+        <Box width={[1, 2 / 5]} bg="cardSecondary" p="5">
+          <Box height="280px">
+            <Heading4>{`Informations générales`}</Heading4>
+            <Text lineHeight="1.5" color="textSecondary">
+              {`Informations relatives à votre mesure`}
+            </Text>
+          </Box>
+          <Box height="280px">
+            <Heading4>{`Caractéristique de la mesure`}</Heading4>
+            <Text lineHeight="1.5" color="textSecondary">
+              Ces informations nous permettent de vous présenter les mesures de mandataires les plus
+              adaptés.
+            </Text>
+          </Box>
+        </Box>
+        <Box p="5" width={[1, 3 / 5]}>
+          <ServiceMesureCreateForm
+            handleSubmit={handleSubmit}
+            tribunalList={tribunalList}
+            antenneOptions={antenneOptions}
+          />
+        </Box>
+      </Flex>
+    </Card>
+  );
+};
---
+++ packages/app/src/components/ServiceMesureCreate/mutations.js
@@ -0,0 +1,110 @@
+import gql from "graphql-tag";
+
+export const UPDATE_ANTENNE_COUTERS = gql`
+  mutation updateAntenneCounters(
+    $antenne_id: Int!
+    $inc_mesures_in_progress: Int!
+    $inc_mesures_awaiting: Int!
+  ) {
+    update_service_antenne(
+      where: { id: { _eq: $antenne_id } }
+      _inc: {
+        mesures_in_progress: $inc_mesures_in_progress
+        mesures_awaiting: $inc_mesures_awaiting
+      }
+    ) {
+      affected_rows
+      returning {
+        id
+        mesures_in_progress
+      }
+    }
+  }
+`;
+
+export const UPDATE_SERVICES_COUTERS = gql`
+  mutation UpdateServicesCounter(
+    $service_id: Int!
+    $mesures_in_progress: Int!
+    $mesures_awaiting: Int!
+  ) {
+    update_services(
+      where: { id: { _eq: $service_id } }
+      _inc: { mesures_in_progress: $mesures_in_progress, mesures_awaiting: $mesures_awaiting }
+    ) {
+      affected_rows
+      returning {
+        id
+        mesures_in_progress
+        mesures_awaiting
+      }
+    }
+  }
+`;
+
+export const ADD_MESURE = gql`
+  mutation addMesure(
+    $date_ouverture: date!
+    $department_id: Int!
+    $type: String!
+    $residence: String!
+    $code_postal: String!
+    $ville: String!
+    $civilite: String!
+    $annee: String!
+    $numero_dossier: String!
+    $numero_rg: String!
+    $antenne_id: Int
+    $ti_id: Int!
+    $latitude: Float!
+    $longitude: Float!
+  ) {
+    insert_mesures(
+      objects: {
+        department_id: $department_id
+        date_ouverture: $date_ouverture
+        type: $type
+        ti_id: $ti_id
+        residence: $residence
+        code_postal: $code_postal
+        ville: $ville
+        civilite: $civilite
+        annee: $annee
+        numero_dossier: $numero_dossier
+        numero_rg: $numero_rg
+        status: "Mesure en cours"
+        antenne_id: $antenne_id
+        latitude: $latitude
+        longitude: $longitude
+      }
+    ) {
+      returning {
+        antenne_id
+        service_id
+        id
+        cabinet
+        civilite
+        code_postal
+        departement {
+          id
+          nom
+          region {
+            id
+            nom
+          }
+        }
+        status
+        type
+        ville
+        latitude
+        longitude
+        residence
+        numero_rg
+        numero_dossier
+        etablissement
+        annee
+        date_ouverture
+      }
+    }
+  }
+`;
