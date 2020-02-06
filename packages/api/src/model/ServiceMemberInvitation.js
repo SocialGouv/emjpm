742f1aeb3d0d6a1a631ba4f51badd66cd3f2303a
Paul Launay <launay.p@gmail.com>
2020-02-06 01:12:03 +0100

    feat(service-member-invitations): add serv. member invitation api model

---
+++ packages/api/src/model/ServiceMemberInvitation.js
@@ -0,0 +1,29 @@
+const knexConnection = require("../db/knex");
+const { Model } = require("objection");
+
+Model.knex(knexConnection);
+
+class ServiceMemberInvitation extends Model {
+  static get tableName() {
+    return "service_member_invitations";
+  }
+
+  static get idColumn() {
+    return "id";
+  }
+
+  static get jsonSchema() {
+    return {
+      type: "object",
+      properties: {
+        id: { type: "integer" },
+        email: { type: "string" },
+        sent_at: { type: "string" },
+        service_id: { type: "integer" },
+        token: { type: "string" }
+      }
+    };
+  }
+}
+
+module.exports = { ServiceMemberInvitation };
