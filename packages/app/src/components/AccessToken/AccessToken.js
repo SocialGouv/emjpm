22b1d904b0a81ed9ef83c1bd7cb4ff3df899633a
kwiss <christophe@hooraylabs.com>
2020-02-07 12:46:30 +0100

    feat(token-authorization): add working token remove for services

--- packages/app/src/components/AccessToken/AccessToken.js
+++ packages/app/src/components/AccessToken/AccessToken.js
@@ -1,6 +1,6 @@
 import { useQuery } from "@apollo/react-hooks";
-import { Heading4 } from "@socialgouv/emjpm-ui-core";
-import React from "react";
+import { Heading5 } from "@socialgouv/emjpm-ui-core";
+import React, { Fragment } from "react";
 import { Box, Flex, Text } from "rebass";
 
 import { AccessTokenDelete } from "../AccessTokenDelete";
@@ -28,21 +28,25 @@ const AccessToken = () => {
 
   const { access_tokens } = data;
   return (
-    <Box sx={boxStyle}>
-      <Heading4 mt={2} mb="2">
-        Logitiels métier authorisé à accéder à votre compte
-      </Heading4>
-      <Box mr={4} mb="3">
-        {access_tokens.map(token => {
-          return (
-            <Flex sx={innerTextStyle} alignItem="center" key={token.id}>
-              <Text>{token.editors.name}</Text>
-              <AccessTokenDelete id={token.id} />
-            </Flex>
-          );
-        })}
-      </Box>
-    </Box>
+    <Fragment>
+      {access_tokens.length > 0 && (
+        <Box sx={boxStyle}>
+          <Heading5 mt={3} mb="2">
+            Logitiels métier authorisé à accéder à votre compte
+          </Heading5>
+          <Box mr={4} mb="3">
+            {access_tokens.map(token => {
+              return (
+                <Flex sx={innerTextStyle} alignItem="center" key={token.id}>
+                  <Text>{token.editors.name}</Text>
+                  <AccessTokenDelete id={token.id} />
+                </Flex>
+              );
+            })}
+          </Box>
+        </Box>
+      )}
+    </Fragment>
   );
 };
 
