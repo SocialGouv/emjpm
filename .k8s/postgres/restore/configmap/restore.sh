9df7cbd586261bf58e974a2717200c76a205aa60
Thomas Glatt <tglatt@gmail.com>
2020-02-11 18:13:27 +0100

    fix: restore db

--- .k8s/postgres/restore/configmap/restore.sh
+++ .k8s/postgres/restore/configmap/restore.sh
@@ -7,11 +7,13 @@ pg_isready
 echo "Restore with ${1}"
 
 pg_restore \
+  --no-owner \
+  --clean \
+  --if-exists \
   --exit-on-error \
   --format=c \
   --verbose \
   --dbname ${EMJPM_DB} \
-  --schema=public \
   ${1}
 
 # psql -d emjpm -c "ALTER SCHEMA public OWNER TO emjpm"
