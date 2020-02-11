6c83d694e4ff6e1fb3f57386c37c10b8026b6715
Thomas Glatt <tglatt@gmail.com>
2020-02-11 17:21:30 +0100

    fix: restore db

--- .k8s/postgres/restore/configmap/restore.sh
+++ .k8s/postgres/restore/configmap/restore.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+EMJPM_DB=emjpm_${BRANCH_HASH}
+
 pg_isready
 
 echo "Restore with ${1}"
@@ -10,11 +12,11 @@ pg_restore \
   --exit-on-error \
   --format=c \
   --verbose \
-  --dbname emjpm \
+  --dbname ${EMJPM_DB} \
   ${1}
 
 # psql -d emjpm -c "ALTER SCHEMA public OWNER TO emjpm"
-psql -d emjpm -c "DROP SCHEMA hdb_catalog CASCADE"
-psql -d emjpm -c "DROP SCHEMA hdb_views CASCADE"
+psql -d ${EMJPM_DB} -c "DROP SCHEMA hdb_catalog CASCADE"
+psql -d ${EMJPM_DB} -c "DROP SCHEMA hdb_views CASCADE"
 
 sleep 10s
