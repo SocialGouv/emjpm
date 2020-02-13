6dc252cfd89ab5e6d7963d20c56d8f4a154352d0
kwiss <christophe@hooraylabs.com>
2020-02-13 14:24:34 +0100

    fix(migration): update migration

--- packages/knex/migrations_v2/20200213122502_update_service_member_admin.js
+++ packages/knex/migrations_v2/20200213122502_update_service_member_admin.js
@@ -4,8 +4,6 @@ update service_members set is_admin = true where is_admin = false;
     `);
 };
 
-exports.down = async function(knex) {
-  return knex.raw(`
-  update service_members is_admin = false where is_admin = true;
-  `);
+exports.down = async function() {
+  return Promise.resolve();
 };
