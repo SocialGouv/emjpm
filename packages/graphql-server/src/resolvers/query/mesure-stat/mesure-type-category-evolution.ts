cd6a66ee70c9416971052a1e9b41fc204bd51d95
Thomas Glatt <tglatt@gmail.com>
2020-02-21 13:45:36 +0100

    fix(mesure-evolution-stat): optimize resolver

--- packages/graphql-server/src/resolvers/query/mesure-stat/mesure-type-category-evolution.ts
+++ packages/graphql-server/src/resolvers/query/mesure-stat/mesure-type-category-evolution.ts
@@ -1,6 +1,5 @@
 import { addMonths } from "date-fns";
 import { DataSource } from "../../../datasource";
-import { SearchMesureResult } from "../../../datasource/mesure.api";
 import { logger } from "../../../logger";
 import {
   MesureTypeCategoryEvolution,
@@ -24,12 +23,20 @@ export const mesureTypeCategoryEvolution = async (
     year: elm.year
   }));
 
-  for (const month of months) {
-    logger.info(`${month.year} ${month.month} - START`);
-    const startMonthDate = new Date(month.year, month.month, 1);
-    const endMonthDate = addMonths(startMonthDate, 1);
-    const mesures: SearchMesureResult[] = await dataSources.mesureAPI.searchMesures(
-      {
+  for (const mesureTypeEvolution of res) {
+    const currentTypeCategory = mesureTypeEvolution.mesureTypeCategory;
+
+    for (const month of months) {
+      logger.info(
+        `${month.year} ${month.month} - ${currentTypeCategory} - START`
+      );
+
+      const startMonthDate = new Date(month.year, month.month, 1);
+      const endMonthDate = addMonths(startMonthDate, 1);
+
+      const mesureTypes = mesureStatAdapter.adaptCategory(currentTypeCategory);
+
+      const mesureNumberResult = await dataSources.mesureAPI.countMesures({
         closed: {
           gt_or_null: startMonthDate.toISOString()
         },
@@ -38,34 +45,15 @@ export const mesureTypeCategoryEvolution = async (
         opening: {
           lt: endMonthDate.toISOString()
         },
-        region: args.region
-      }
-    );
-    logger.info(`${month.year} ${month.month} - ${mesures.length} mesures`);
+        region: args.region,
+        type: { _in: mesureTypes }
+      });
+      const mesureNumber =
+        mesureNumberResult.data.mesures_aggregate.aggregate.count;
 
-    const mesuresByCategoryMap = res.map(elm => ({
-      number: 0,
-      type: elm.mesureTypeCategory
-    }));
-
-    for (const mesure of mesures) {
-      const typeCategory = mesureStatAdapter.adaptType(mesure.type);
-      const mesuresByCategory = mesuresByCategoryMap.find(
-        elm => elm.type === typeCategory
+      logger.info(
+        `${month.year} ${month.month} - ${mesureNumber} ${currentTypeCategory}`
       );
-      if (mesuresByCategory) {
-        mesuresByCategory.number++;
-      }
-    }
-
-    for (const mesuresByCategory of mesuresByCategoryMap) {
-      const mesureTypeEvolution = res.find(
-        elm => elm.mesureTypeCategory === mesuresByCategory.type
-      );
-      if (!mesureTypeEvolution) {
-        logger.error(`No category type found for ${mesuresByCategory.type}`);
-        throw new Error(`No category type found for ${mesuresByCategory.type}`);
-      }
 
       const monthlyEvolution = mesureTypeEvolution.monthlyEvolutions.find(
         elm => elm.month === month.month && elm.year === month.year
@@ -78,10 +66,14 @@ export const mesureTypeCategoryEvolution = async (
           `No monthly evolution found for year ${month.year}, month ${month.month}`
         );
       }
-      monthlyEvolution.number = mesuresByCategory.number;
-      logger.info(`${month.year} ${month.month} - END`);
+      monthlyEvolution.number = mesureNumber;
+
+      logger.info(
+        `${month.year} ${month.month} - ${currentTypeCategory} - END`
+      );
     }
   }
+
   logger.info(res);
   return res;
 };
--- packages/graphql-server/src/resolvers/query/mesure-stat/utils/mesure-stat.adapter.ts
+++ packages/graphql-server/src/resolvers/query/mesure-stat/utils/mesure-stat.adapter.ts
@@ -2,6 +2,46 @@ import { MesureType } from "../../../../model/mesures.model";
 import { MesureTypeCategory } from "../../../../types/resolvers-types";
 
 export const mesureStatAdapter = {
+  adaptCategory: (category: MesureTypeCategory) => {
+    switch (category) {
+      case MesureTypeCategory.CuratelleRenforcee:
+        return [
+          "curatelle renforcée",
+          "Curatelle renforcée",
+          "Curatelle renforcée aux biens",
+          "Curatelle renforcée aux biens et à la personne",
+          "Curatelle renforcée à la personne"
+        ];
+      case MesureTypeCategory.CuratelleSimple:
+        return [
+          "Curatelle",
+          "Curatelle simple",
+          "Curatelle simple aux biens",
+          "Curatelle simple à la personne",
+          "Curatelle simple aux biens et à la personne"
+        ];
+      case MesureTypeCategory.Other:
+        return [
+          "MAJ",
+          "Mandat de protection future",
+          "Mesure ad hoc",
+          "Subrogé curateur",
+          "Subrogé tuteur"
+        ];
+      case MesureTypeCategory.SauvegardeJustice:
+        return [
+          "Sauvegarde de justice",
+          "Sauvegarde de justice avec mandat spécial"
+        ];
+      case MesureTypeCategory.Tutelle:
+        return [
+          "Tutelle",
+          "Tutelle aux biens",
+          "Tutelle aux biens et à la personne",
+          "Tutelle à la personne"
+        ];
+    }
+  },
   adaptType: (type: MesureType) => {
     if (!type) {
       return null;
