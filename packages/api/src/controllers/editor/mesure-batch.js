2a03f7e2dc8ef3820b1bccdff4b62cccc00cd094
Paul Launay <launay.p@gmail.com>
2020-02-24 19:59:10 +0100

    Feat/api mesures (#1504)
    * feat(api-mesures): add and use mesures controller
    
    * fix(api-mesures): handle missing token in apilog middleware
    
    * fix(api-mesures): fix mesure controller param typo
    
    * feat(api-mesures): add mesures delete api
    
    * feat(api-mesures): add api mesure batch insert
    
    * feat(api-mesures): use 201 and 204 for api mesures create and delete
    
    * test(api-mesures): add some api mesures tests
    
    * test(api-mesures): add api mesure index/create test with oauth
    
    * feat(api-mesures): add route validations
    
    * fix(api-mesures): fix user type issue
    
    * refactor(api-mesures): move mesure controller to editor
    
    * test(api-mesures): add api mesures batch and delete test
    
    * feat(api-mesures): add api mesure update controller and test

---
+++ packages/api/src/controllers/editor/mesure-batch.js
@@ -0,0 +1,41 @@
+const { User } = require("../../model/User");
+const { Mesures } = require("../../model/Mesures");
+
+const mesureBatch = async (req, res) => {
+  const {
+    body,
+    user: { user_id }
+  } = req;
+  let user;
+  let serviceOrMandataire;
+  let mesures;
+
+  try {
+    user = await User.query().findById(user_id);
+  } catch (error) {
+    return res.status(422).json({ error: "User not found" });
+  }
+
+  const type = user.type === "service" ? "service" : "mandataire";
+
+  try {
+    serviceOrMandataire = await user.$relatedQuery(type);
+  } catch (error) {
+    return res.status(422).json({ error: `${type} not found` });
+  }
+
+  const payload = body.mesures.map(mesure => ({
+    ...mesure,
+    [`${type}_id`]: serviceOrMandataire.id
+  }));
+
+  try {
+    mesures = await Mesures.query().insert(payload);
+  } catch (error) {
+    return res.status(422).json({ error: error.message });
+  }
+
+  return res.status(201).json({ mesures });
+};
+
+module.exports = mesureBatch;
