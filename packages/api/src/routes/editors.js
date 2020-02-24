e04e14ed05181c719353292e82b457c019b073cd
Paul Launay <launay.p@gmail.com>
2020-02-24 15:27:16 +0100

    feat(api-mesures): add route validations

--- packages/api/src/routes/editors.js
+++ packages/api/src/routes/editors.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
 
 const mesures = require("../../src/controllers/mesures");
 
 router.get("/mesures", mesures.index);
-router.post("/mesures", mesures.create);
+router.post(
+  "/mesures",
+  [
+    body("annee")
+      .not()
+      .isEmpty(),
+    body("civilite")
+      .not()
+      .isEmpty(),
+    body("date_ouverture")
+      .not()
+      .isEmpty(),
+    body("numero_dossier")
+      .not()
+      .isEmpty(),
+    body("numero_rg")
+      .not()
+      .isEmpty(),
+    body("residence")
+      .not()
+      .isEmpty(),
+    body("ti_id")
+      .not()
+      .isEmpty()
+  ],
+  mesures.create
+);
 router.post("/mesures/batch", mesures.batch);
 router.delete("/mesures/:id", mesures.destroy);
 
