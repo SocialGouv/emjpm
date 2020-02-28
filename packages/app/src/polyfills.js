8a2f9eac7567626e2ecfefe6ecf4d5b6c2c1eb40
christophe <mr.dumont.christophe@gmail.com>
2020-02-28 13:43:50 +0100

    Fix/ff38 issues (#1510)
    * fix(ff38): use imported polyfills and add yup required polyfills
    
    * feat(polyfills): add bundle analyzer
    
    Co-authored-by: Paul Launay <launay.p@gmail.com>

---
+++ packages/app/src/polyfills.js
@@ -0,0 +1,6 @@
+import "core-js/features/array/includes";
+import "core-js/features/array/find";
+import "core-js/features/object/values";
+import "core-js/features/set";
+import "core-js/features/map";
+import "core-js/features/promise";
