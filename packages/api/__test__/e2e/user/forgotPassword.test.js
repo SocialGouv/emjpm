028a70cedbab42c36b6ead30da149784b661e17a
Paul Launay <launay.p@gmail.com>
2020-02-28 19:11:00 +0100

    fix(forgot-password): handle forgot password user notfound and add tests

---
+++ packages/api/__test__/e2e/user/forgotPassword.test.js
@@ -0,0 +1,69 @@
+const request = require("supertest");
+
+const nodemailerMock = require("nodemailer-mock");
+jest.setMock("nodemailer", nodemailerMock);
+
+// Fake env
+process.env.SMTP_FROM = "ne-pas-repondre@emjpm.gouv.fr";
+process.env.APP_URL = "https://emjpm.gouv.fr";
+
+const { knex } = global;
+jest.setMock("@emjpm/api/src/db/knex", knex);
+
+const server = require("@emjpm/api/src/app");
+
+beforeAll(async () => {
+  await knex.migrate.latest();
+  await knex.seed.run();
+
+  const [user] = await knex("users");
+
+  global.user = user;
+});
+
+afterEach(async () => {
+  nodemailerMock.mock.reset();
+});
+
+describe("POST /api/auth/forgot-password", () => {
+  describe("when email is invalid or not found", () => {
+    test("it returns 422", async () => {
+      const response = await request(server)
+        .post("/api/auth/forgot-password")
+        .set("Accept", "application/json")
+        .send({ email: null });
+
+      expect(response.status).toBe(422);
+    });
+  });
+
+  test("it updates reset_password_token and reset_password_expires", async () => {
+    await request(server)
+      .post("/api/auth/forgot-password")
+      .set("Accept", "application/json")
+      .send({ email: global.user.email });
+
+    const [user] = await knex("users").where({ id: global.user.id });
+
+    expect(user.reset_password_token).toBeTruthy();
+    expect(user.reset_password_expires).toBeTruthy();
+  });
+
+  test("it sends reset password email", async () => {
+    await request(server)
+      .post("/api/auth/forgot-password")
+      .set("Accept", "application/json")
+      .send({ email: global.user.email });
+
+    expect(nodemailerMock.mock.sentMail().length).toBe(1);
+  });
+
+  test("it returns 200", async () => {
+    const response = await request(server)
+      .post("/api/auth/forgot-password")
+      .set("Accept", "application/json")
+      .send({ email: global.user.email });
+
+    expect(response.status).toBe(200);
+  });
+});
