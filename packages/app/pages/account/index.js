4b8cfbfb9cd317b94f8d3587cc8198ca20a643d1
kwiss <christophe@hooraylabs.com>
2020-03-05 17:37:48 +0100

    fix(direction-informations): remove usercontext

--- packages/app/pages/account/index.js
+++ packages/app/pages/account/index.js
@@ -5,7 +5,6 @@ import { Box, Flex } from "rebass";
 import { LayoutMandataire } from "../../src/components/Layout";
 import { ServiceAntennes } from "../../src/components/ServiceAntennes";
 import { ServiceSidebar } from "../../src/components/ServiceSidebar";
-import { UserInformations } from "../../src/components/UserInformations";
 import { withAuthSync } from "../../src/util/auth";
 
 const Informations = () => {
@@ -26,9 +25,7 @@ const Informations = () => {
             }}
           >
             <Heading2>Mesures souhaitées par votre service</Heading2>
-            <UserInformations
-              Component={props => <ServiceSidebar isDescriptionHidden={true} {...props} mt="3" />}
-            />
+            <ServiceSidebar isDescriptionHidden={true} mt="3" />
           </Box>
         </Flex>
         <Box
