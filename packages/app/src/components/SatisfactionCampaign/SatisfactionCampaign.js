4893482b9772b0ab976dcc7f7e6010cf8ddb0e3a
Paul Launay <launay.p@gmail.com>
2020-03-09 14:45:58 +0100

    feat(satisfaction-campaigns): add satisfaction campaign answers

--- packages/app/src/components/SatisfactionCampaign/SatisfactionCampaign.js
+++ packages/app/src/components/SatisfactionCampaign/SatisfactionCampaign.js
@@ -1,13 +1,43 @@
-import { useQuery } from "@apollo/react-hooks";
-import React from "react";
-import { Box } from "rebass";
+import { useMutation, useQuery } from "@apollo/react-hooks";
+import React, { useContext, useState } from "react";
+import { Box, Flex, Text } from "rebass";
 
+import { UserContext } from "../UserContext";
+import { ADD_SATISFACTION_CAMPAIGN_ANSWER } from "./mutations";
 import { CURRENT_SATISFACTION_CAMPAIGN } from "./queries";
 
+const containerStyle = {
+  bg: "gray",
+  position: "fixed",
+  bottom: 0,
+  right: 0,
+  m: 4,
+  p: 10,
+  width: 270,
+  textAlign: "center"
+};
+
+const buttonStyle = {
+  cursor: "pointer",
+  flexGrow: 1
+};
+
 const SatisfactionCampaign = () => {
+  const { id } = useContext(UserContext);
+  const [isDone, setDone] = useState(false);
+
+  const [addSatisfactionCampaignAnswer] = useMutation(ADD_SATISFACTION_CAMPAIGN_ANSWER);
+
   const { data, loading, error } = useQuery(CURRENT_SATISFACTION_CAMPAIGN, {
-    variables: { now: new Date().toUTCString() }
+    variables: {
+      now: new Date().toUTCString(),
+      user_id: id
+    }
   });
+
+  if (isDone) {
+    return null;
+  }
 
   if (loading) {
     return null;
@@ -23,19 +53,38 @@ const SatisfactionCampaign = () => {
     return null;
   }
 
+  const { answers } = satisfactionCampaign;
+
+  if (answers.length) {
+    return null;
+  }
+
+  const handleClick = async value => {
+    await addSatisfactionCampaignAnswer({
+      variables: {
+        satisfaction_campaign_id: satisfactionCampaign.id,
+        user_id: id,
+        value
+      }
+    });
+
+    setDone(true);
+  };
+
   return (
-    <Box
-      sx={{
-        bg: "orange",
-        position: "absolute",
-        bottom: 0,
-        right: 0,
-        m: 4,
-        p: 4
-      }}
-    >
-      Campagne de satisfaction
-      {satisfactionCampaign.name}
+    <Box sx={containerStyle}>
+      <Text mb={4}>Campagne de satisfaction</Text>
+      <Flex alignItems="center">
+        <Box sx={buttonStyle} onClick={() => handleClick(1)}>
+          1
+        </Box>
+        <Box sx={buttonStyle} onClick={() => handleClick(2)}>
+          2
+        </Box>
+        <Box sx={buttonStyle} onClick={() => handleClick(3)}>
+          3
+        </Box>
+      </Flex>
     </Box>
   );
 };
--- packages/app/src/components/SatisfactionCampaign/queries.js
+++ packages/app/src/components/SatisfactionCampaign/queries.js
@@ -1,7 +1,7 @@
 import gql from "graphql-tag";
 
 export const CURRENT_SATISFACTION_CAMPAIGN = gql`
-  query CurrentSatisfactionCampaign($now: timestamptz!) {
+  query CurrentSatisfactionCampaign($now: timestamptz!, $user_id: Int!) {
     satisfaction_campaigns(
       where: { _and: [{ started_at: { _lt: $now } }, { ended_at: { _gt: $now } }] }
     ) {
@@ -9,6 +9,9 @@ export const CURRENT_SATISFACTION_CAMPAIGN = gql`
       name
       started_at
       ended_at
+      answers: satisfaction_campaign_answers(where: { user_id: { _eq: $user_id } }) {
+        id
+      }
     }
   }
 `;
