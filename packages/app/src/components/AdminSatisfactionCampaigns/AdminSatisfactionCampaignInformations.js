c0c1b9417bed5d372328807286c9da3086c3f95c
Paul Launay <launay.p@gmail.com>
2020-03-09 21:08:46 +0100

    feat(satisfaction-campaigns): add satisfaction campaigns (#1549)
    * feat(campaigns): add admin campaigns page
    
    * feat(satisfaction-campaigns): add satisfaction campaign table / metadata
    
    * feat(satisfaction_campaigns): add admin satisfaction camp. pages / comps
    
    * feat(satisfaction-campaigns): add admin satisfaction campaign schema
    
    * feat(satisfaction-campaigns): add admin satisfaction campaign comps
    
    * test(satisfaction-campaigns): update knex test snapshot
    
    * feat(satisfaction-campaigns): add statisfaction campagain for services
    
    * feat(satisfaction-campaigns): add satisfaction campaign answers
    
    * feat(satisfaction-campaigns): add satisfaction campaign thanks message
    
    * test(satisfaction-campaigns): update knex test snapshot
    
    * feat(satisfaction-campaigns): add satisfaction campaign for magistrats
    
    * feat(satisfaction-campaigns): add satisfaction campaign in mandataires
    
    * feat(satisfaction-campaigns): add satisfaction campaigns in direction
    
    * feat(satisfaction-campaigns): use card ui comp in satisfaction campaign
    
    * chore(add-satisfaction-campaign): change text
    
    Co-authored-by: Thomas Glatt <tglatt@gmail.com>

---
+++ packages/app/src/components/AdminSatisfactionCampaigns/AdminSatisfactionCampaignInformations.js
@@ -0,0 +1,61 @@
+import { useQuery } from "@apollo/react-hooks";
+import { Text } from "@socialgouv/emjpm-ui-core";
+import { format } from "date-fns";
+import React from "react";
+import { Box, Flex } from "rebass";
+
+import { SATISFACTION_CAMPAIGN } from "./queries";
+
+const AdminSatisfactionCampaignInformations = props => {
+  const { id } = props;
+  const { data, loading, error } = useQuery(SATISFACTION_CAMPAIGN, { variables: { id } });
+
+  if (loading) {
+    return <div>Chargement</div>;
+  }
+
+  if (error) {
+    return <div>Erreur</div>;
+  }
+
+  const [satisfactionCampaign] = data.satisfaction_campaigns;
+
+  return (
+    <Box>
+      <Flex mb={4}>
+        <Box width={1 / 3} p={2} bg="cardSecondary">
+          ID
+        </Box>
+        <Box width={2 / 3} px={4} py={2}>
+          <Text>{satisfactionCampaign.id}</Text>
+        </Box>
+      </Flex>
+      <Flex mb={4}>
+        <Box width={1 / 3} p={2} bg="cardSecondary">
+          Nom
+        </Box>
+        <Box width={2 / 3} px={4} py={2}>
+          <Text>{satisfactionCampaign.name}</Text>
+        </Box>
+      </Flex>
+      <Flex mb={4}>
+        <Box width={1 / 3} p={2} bg="cardSecondary">
+          Date de début
+        </Box>
+        <Box width={2 / 3} px={4} py={2}>
+          <Text>{format(new Date(satisfactionCampaign.started_at), "dd/MM/yyyy")}</Text>
+        </Box>
+      </Flex>
+      <Flex mb={4}>
+        <Box width={1 / 3} p={2} bg="cardSecondary">
+          Date de fin
+        </Box>
+        <Box width={2 / 3} px={4} py={2}>
+          <Text>{format(new Date(satisfactionCampaign.ended_at), "dd/MM/yyyy")}</Text>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export { AdminSatisfactionCampaignInformations };
--- packages/app/src/components/Layout/LayoutMagistrat.js
+++ packages/app/src/components/Layout/LayoutMagistrat.js
@@ -7,6 +7,7 @@ import { Box } from "rebass";
 import { Footer } from "../Footer";
 import { Header } from "../Header";
 import { Navigation } from "../Navigation";
+import { SatisfactionCampaign } from "../SatisfactionCampaign";
 
 const navigationLinks = [
   {
@@ -58,6 +59,7 @@ const LayoutMagistrat = props => {
           <Footer />
         </BoxWrapper>
       </Box>
+      <SatisfactionCampaign />
     </Fragment>
   );
 };
