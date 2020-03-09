453653fd380fe7227cb2db1af9cc15e365f76c2e
kwiss <christophe@hooraylabs.com>
2020-03-09 11:10:03 +0100

    refactor(mandataire): use userContext in mandataire components

--- packages/app/pages/mandataires/edit-informations.js
+++ packages/app/pages/mandataires/edit-informations.js
@@ -3,7 +3,6 @@ import React from "react";
 
 import { LayoutMandataire } from "../../src/components/Layout";
 import { MandataireEditInformations } from "../../src/components/MandataireEditInformations";
-import { UserInformations } from "../../src/components/UserInformations";
 import { withAuthSync } from "../../src/util/auth";
 
 const EditInformations = () => {
@@ -11,11 +10,7 @@ const EditInformations = () => {
     <LayoutMandataire>
       <BoxWrapper mt={6} px="1">
         <Heading1>Editer mes informations</Heading1>
-        <UserInformations
-          Component={props => {
-            return <MandataireEditInformations {...props} mt="3" />;
-          }}
-        />
+        <MandataireEditInformations mt="3" />
       </BoxWrapper>
     </LayoutMandataire>
   );
