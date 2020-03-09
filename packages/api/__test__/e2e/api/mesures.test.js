c28234d8484a1c41dae763b6958603a05c7bfa8e
kwiss <christophe@hooraylabs.com>
2020-03-09 16:46:32 +0100

    ci(api-test): add timeout config

--- packages/api/__test__/e2e/api/mesures.test.js
+++ packages/api/__test__/e2e/api/mesures.test.js
@@ -9,7 +9,7 @@ process.env.APP_URL = "https://emjpm.gouv.fr";
 
 const { knex } = global;
 jest.setMock("@emjpm/api/src/db/knex", knex);
-jest.setTimeout(30000);
+
 const server = require("@emjpm/api/src/app");
 
 beforeAll(async () => {
--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   collectCoverageFrom: ["auth/**/*.js", "email/**/*.js", "routes/**/*.js"],
   coverageDirectory: process.env.JEST_COVERAGE_DIRECTORY,
   roots: ["<rootDir>/__test__/"],
+  testTimeout: 30000,
   testEnvironment: "jest-environment-knex",
   testEnvironmentOptions: require("@emjpm/knex/knexfile.js").test
 };
