c266a38affce22681a7c8c548d7b2961416e53d6
kwiss <christophe@hooraylabs.com>
2020-03-09 11:19:38 +0100

    refactor(user-informations): remove userInformations

--- packages/app/src/components/UserInformations/UserInformations.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-
-import { UserInformations } from "./UserInformations";
-
-it("renders correctly", () => {
-  const tree = renderer.create(<UserInformations />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
