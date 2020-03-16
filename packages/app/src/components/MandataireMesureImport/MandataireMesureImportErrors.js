ed422c09107ba4867482be46ddefb852c1fc0a12
Paul Launay <launay.p@gmail.com>
2020-03-16 13:30:35 +0100

    fix(mesure-import): fix mandataire mesure import errors

--- packages/app/src/components/MandataireMesureImport/MandataireMesureImportErrors.js
+++ packages/app/src/components/MandataireMesureImport/MandataireMesureImportErrors.js
@@ -1,6 +1,6 @@
-import { Card, Heading3, Heading5 } from "@socialgouv/emjpm-ui-core";
-import React, { Fragment } from "react";
-import { Box, Text } from "rebass";
+import { Card, Heading3 } from "@socialgouv/emjpm-ui-core";
+import React from "react";
+import { Flex, Text } from "rebass";
 
 import { importErrorsWrapperStyle } from "./style";
 
@@ -10,27 +10,13 @@ const MandataireMesureImportErrors = props => {
   return (
     <Card sx={importErrorsWrapperStyle} overflow="hidden">
       <Heading3 mb={4}>Erreurs par ligne</Heading3>
-      {errors.map(({ line, messages }, index) => (
-        <Box key={`error-${index}`} mb={3}>
-          {line ? (
-            <Fragment>
-              <Heading5 mb={1}>Ligne {line}</Heading5>
-              {messages.map(message => (
-                <Text width={1} color="error" key={message}>
-                  - {message}
-                </Text>
-              ))}
-            </Fragment>
-          ) : (
-            <Fragment>
-              {messages.map(message => (
-                <Text width={1} color="error" key={message}>
-                  - {message}
-                </Text>
-              ))}
-            </Fragment>
-          )}
-        </Box>
+      {errors.map(({ line = 0, message }) => (
+        <Flex key={`${line}-${message}`} mb={1}>
+          <Text color="warning" mr={1}>
+            LIGNE {line}
+          </Text>
+          <Text>{message}</Text>
+        </Flex>
       ))}
     </Card>
   );
