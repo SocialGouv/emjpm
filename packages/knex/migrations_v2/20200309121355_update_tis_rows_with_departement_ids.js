b0bd9128823dee5685c18314835f40880dc610cd
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-03-17 15:42:49 +0100

    fix(migration): remove pkg import and duplicate util function (#1599)

--- packages/knex/migrations_v2/20200309121355_update_tis_rows_with_departement_ids.js
+++ packages/knex/migrations_v2/20200309121355_update_tis_rows_with_departement_ids.js
@@ -1,4 +1,17 @@
-const getRegionCode = require("@emjpm/api/src/utils/getRegionCode");
+function getRegionCode(zipcode) {
+  if (!zipcode || zipcode.length !== 5) {
+    throw "zip code is invalid";
+  }
+  if (zipcode.startsWith("20")) {
+    // Corse
+    return parseInt(zipcode) < 20200 ? "2A" : "2B";
+  } else if (zipcode.startsWith("97") || zipcode.startsWith("98")) {
+    // Dom-Tom
+    return zipcode.substring(0, 3);
+  } else {
+    return zipcode.substring(0, 2);
+  }
+}
 
 exports.up = async knex => {
   const departements = await knex("departements").select("id", "code");
