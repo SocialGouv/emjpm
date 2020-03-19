758c504b1f926ebb7b8971a2df49342d4448c502
Thomas Glatt <tglatt@gmail.com>
2020-03-19 08:43:52 +0100

    ci(anonymise): update anonymisation script
    ci(anonymise): update anonymisation script
    
    ci(anonymise): update anonymisation script

--- .k8s/postgres/backup/configmap/anonymize.sql
+++ .k8s/postgres/backup/configmap/anonymize.sql
@@ -4,11 +4,13 @@ update mandataires set etablissement = ''
 	, telephone = '0140506070'
 	, adresse = '37 Quai de Grenelle'
 	, telephone_portable = '0660203010'
-	, competences = null;
+	, competences = null
+  , siret = null;
 update service_antenne set contact_email = null, contact_firstname = null, contact_lastname = null, contact_phone = null;
 update services set email = concat('service-', id, '@justice.fr'), nom = null, prenom = null, telephone = '0140506070', competences = null;
 delete from sessions;
 update tis set telephone = '0140506070';
+update mesures set numero_dossier = null, annee = 1968;
 update users set username = concat("type",'-',id, '@justice.fr'), nom = "type", prenom = 'Paula', email = concat("type",'-',id, '@justice.fr');
 update users set password = '$2a$10$AAuEwAMCl3AUuOE3bXhzS.IWOx6Y1SPlScWcVxhW.nyNMXVL.Q1lO';
 
