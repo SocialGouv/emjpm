78710327eb8872b62c37c28b491b1aead96ba048
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-03-22 14:25:54 +0100

    feat(import): handle INSEE_API_TRIBUNAL (#1625)

---
+++ packages/api/src/routes/insee-api.js
@@ -0,0 +1,37 @@
+const sentry = require("../utils/sentry");
+const siret = require("../utils/siret");
+const logger = require("../utils/logger");
+
+const fetchTribunalDatas = async tribunal_siret => {
+  const { error, data } = await siret.find(tribunal_siret);
+
+  if (error) {
+    logger.error(error);
+    sentry.captureException(new Error(error));
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  const { adresseEtablissement, periodesEtablissement } = data;
+  const {
+    numeroVoieEtablissement,
+    typeVoieEtablissement,
+    libelleVoieEtablissement,
+    codePostalEtablissement,
+    libelleCommuneEtablissement
+  } = adresseEtablissement;
+  const periode = periodesEtablissement.find(p => !p.dateFin);
+  const { enseigne1Etablissement } = periode || {};
+
+  return {
+    address: `${numeroVoieEtablissement} ${typeVoieEtablissement} ${libelleVoieEtablissement}`,
+    code_postal: codePostalEtablissement,
+    etablissement: enseigne1Etablissement,
+    ville: libelleCommuneEtablissement,
+    siret: tribunal_siret
+  };
+};
+
+module.exports = { fetchTribunalDatas };
