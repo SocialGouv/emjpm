acd113b0072398379ad271ef5936ff9160384fda
Rémi Roycourt <remiroycourt@gmail.com>
2020-03-31 19:38:57 +0200

    feat(app): new validation messages

--- packages/app/src/lib/validationSchemas/yup.js
+++ packages/app/src/lib/validationSchemas/yup.js
@@ -2,10 +2,11 @@ import * as yup from "yup";
 
 yup.setLocale({
   mixed: {
-    required: "Champs obligatoire"
+    required: "Veuillez renseigner ce champ."
   },
   string: {
-    email: "Le format de l'adresse email n'est pas correct"
+    email: "Le format de l'adresse email n'est pas correct.",
+    length: "Le champ doit comporter ${length} caractères."
   }
 });
 
