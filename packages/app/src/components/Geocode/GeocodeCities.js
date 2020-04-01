f1383dfd67e7d84012ae4c55578381bc38bc4125
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-04-01 10:36:15 +0200

    feat(app): create select field to show all cities by postcode (#1641)
    * feat(app): create GeocodeCities component
    
    * feat(hook): move useAsyncMemo to the proper folder

---
+++ packages/app/src/components/Geocode/GeocodeCities.js
@@ -0,0 +1,34 @@
+import { Select } from "@emjpm/ui";
+import React from "react";
+
+import { useAsyncMemo } from "../../lib/hooks/useAsyncMemo";
+import { debouncedGeocode } from "../../util/geocode";
+
+export const GeocodeCities = props => {
+  const { onChange, placeholder, zipcode, value } = props;
+
+  const options = useAsyncMemo(
+    async () => {
+      const results = await debouncedGeocode({
+        q: zipcode,
+        postcode: zipcode,
+        type: "municipality"
+      });
+      return results;
+    },
+    [zipcode],
+    []
+  );
+
+  return (
+    <Select
+      isClearable={false}
+      value={{ label: value, value }}
+      placeholder={placeholder}
+      onChange={({ city }) => onChange(city)}
+      options={options}
+    />
+  );
+};
+
+export default GeocodeCities;
