e5110b84d86f67ce366b47d58f1194171faa0707
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-04-02 13:09:20 +0200

    feat(app): add warning message in mandataire reservation page (#1647)

--- packages/app/src/components/MagistratMesureServiceTitle/queries.js
+++ packages/app/src/components/MagistratMesureServiceTitle/queries.js
@@ -4,6 +4,9 @@ export const SERVICE = gql`
   query service($id: Int!) {
     services(limit: 1, where: { id: { _eq: $id } }) {
       etablissement
+      dispo_max
+      mesures_awaiting
+      mesures_in_progress
     }
   }
 `;
