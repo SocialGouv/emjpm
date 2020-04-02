1f0885947480ff7318d2a4e583f257e422467d75
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-04-02 13:09:20 +0200

    feat(app): add warning message in mandataire reservation page (#1647)

--- packages/app/src/components/MagistratMesureServiceTitle/MagistratMesureServiceTitle.js
+++ packages/app/src/components/MagistratMesureServiceTitle/MagistratMesureServiceTitle.js
@@ -1,7 +1,8 @@
 import { useQuery } from "@apollo/react-hooks";
-import { Heading2 } from "@emjpm/ui";
+import { Heading2, Heading4 } from "@emjpm/ui";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { Fragment } from "react";
+import { Box } from "rebass";
 
 import { SERVICE } from "./queries";
 
@@ -14,12 +15,21 @@ const MagistratMesureServiceTitle = props => {
   });
 
   if (loading) {
-    return "Loading...";
+    return <Box p={1}>{"Chargement..."}</Box>;
   }
 
   const [service] = data.services;
-  const { etablissement } = service;
-  return <Heading2 mb="1">{`Réserver une mesure auprès de ${etablissement}`}</Heading2>;
+  const { etablissement, dispo_max, mesures_awaiting, mesures_in_progress } = service;
+  const limitReached = mesures_in_progress + mesures_awaiting >= dispo_max;
+
+  return (
+    <Fragment>
+      <Heading2 mb="1">{`Réserver une mesure auprès de ${etablissement}`}</Heading2>
+      {limitReached && (
+        <Heading4 color="error">{`Pour votre information, le mandataire a atteint le nombre de mesures souhaitées`}</Heading4>
+      )}
+    </Fragment>
+  );
 };
 
 MagistratMesureServiceTitle.propTypes = {
