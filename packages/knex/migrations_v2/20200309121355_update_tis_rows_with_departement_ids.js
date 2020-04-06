d8da3a4e2bf0d5c144691be5b4c0f94c5a0beefa
Rémi Roycourt <remiroycourt@gmail.com>
2020-04-06 15:34:56 +0200

    feat(knex): new migration to update tis rows

--- packages/knex/migrations_v2/20200309121355_update_tis_rows_with_departement_ids.js
+++ packages/knex/migrations_v2/20200309121355_update_tis_rows_with_departement_ids.js
@@ -1,17 +1,4 @@
-function getRegionCode(zipcode) {
-  if (!zipcode || zipcode.length !== 5) {
-    throw "zip code is invalid";
-  }
-  if (zipcode.startsWith("20")) {
-    // Corse
-    return parseInt(zipcode) < 20200 ? "2A" : "2B";
-  } else if (zipcode.startsWith("97") || zipcode.startsWith("98")) {
-    // Dom-Tom
-    return zipcode.substring(0, 3);
-  } else {
-    return zipcode.substring(0, 2);
-  }
-}
+const getRegionCode = require("../util/getRegionCode");
 
 exports.up = async knex => {
   const departements = await knex("departements").select("id", "code");
