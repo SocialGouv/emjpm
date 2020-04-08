d9fa174e240cf5050a66cb0e82d307ba596ed78c
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-04-08 11:41:49 +0200

    ci(ui): handle pipeline for ui (#1639)
    * ci(ui): handle pipeline for ui
    
    * ci(gitlab): allow link on install
    
    * ci(gitlab): remove node_modules cache
    
    * chore(lock): lock update
    
    * ci(gitlab): install ui dependencies when installing app
    
    * build(ui): use rollup
    
    * fix(ui): use import instead of export
    
    * fix(ui): use get emotion-normalize from src
    
    * fix(app): restore polish
    
    * udpate license
    
    * udpate dockerfile
    
    * gooooo bro
    
    * more shm size
    
    * missing public folder
    
    * deploy app on port 3000
    
    * deploy app on port 80
    
    * ci(gitlab): add next cache when building app
    
    * build(app): add next.config for env
    
    * ci(gitlab): make all deply stage use same extend
    
    * ci(gitlab): remove extra quotes
    
    * Update package.json
    
    * Update Dockerfile
    
    Co-authored-by: Douglas Duteil <douglasduteil@gmail.com>

--- packages/ui/babel.config.js
+++ packages/ui/babel.config.js
@@ -4,12 +4,10 @@ module.exports = (api) => {
   return {
     env: {
       test: {
-        plugins: ['require-context-hook']
-      }
+        plugins: ['require-context-hook'],
+      },
     },
-    plugins: [
-      '@babel/plugin-proposal-object-rest-spread',
-    ],
-    presets: ['@babel/env', '@babel/react']
+    plugins: ['@babel/plugin-proposal-object-rest-spread', 'babel-plugin-emotion'],
+    presets: ['@babel/env', '@babel/react'],
   };
 };
