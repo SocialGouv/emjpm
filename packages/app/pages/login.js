bc1dc0c4f53b011138612fe212db994e81ed5feb
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-04-09 14:15:06 +0200

    feat(app): create exclude browser banner (#1657)
    * feat(app): create exclude browser banner
    
    * feat(browser-banner): change text

--- packages/app/pages/login.js
+++ packages/app/pages/login.js
@@ -1,34 +1,46 @@
 import { BoxWrapper, FlexWrapper, Heading1 } from "@emjpm/ui";
-import React from "react";
+import dynamic from "next/dynamic";
+import React, { Fragment } from "react";
 import { Box, Image } from "rebass";
 
 import { LayoutPublic } from "../src/components/Layout";
 import { Login, LoginCreateAccount } from "../src/components/Login";
 import { withAuthSync } from "../src/util/auth";
 
+const ExcludeBrowserBanner = dynamic(
+  async () => {
+    const { ExcludeBrowserBanner } = await import("../src/components/ExcludeBrowserBanner");
+    return ExcludeBrowserBanner;
+  },
+  {
+    ssr: false
+  }
+);
+
 const LoginPage = () => {
   return (
-    <LayoutPublic>
-      <BoxWrapper>
-        <Heading1 mt={"80px"} textAlign="center">
-          Trouvez le bon professionnel pour les majeurs à protéger
-        </Heading1>
-      </BoxWrapper>
-      <FlexWrapper my="50px">
-        <Box
-          sx={{
-            flexBasis: ["100%", "50%"],
-            p: "3"
-          }}
-        >
-          <Image
-            src="/static/images/login.png"
+    <Fragment>
+      <LayoutPublic>
+        <BoxWrapper>
+          <Heading1 mt={"80px"} textAlign="center">
+            Trouvez le bon professionnel pour les majeurs à protéger
+          </Heading1>
+        </BoxWrapper>
+        <FlexWrapper my="50px">
+          <Box
             sx={{
-              p: "3",
-              width: ["100%"]
+              flexBasis: ["100%", "50%"],
+              p: "3"
             }}
-          />
-          {/* <Box sx={{ p: "6" }}>
+          >
+            <Image
+              src="/static/images/login.png"
+              sx={{
+                p: "3",
+                width: ["100%"]
+              }}
+            />
+            {/* <Box sx={{ p: "6" }}>
             <Heading4 mb="3">
               Une connaissance en temps réel de l’activité des travailleurs sociaux
             </Heading4>
@@ -37,18 +49,20 @@ const LoginPage = () => {
             </Heading4>
             <Heading4>Une simplification des démarches pour les personnes sous protection</Heading4>
           </Box> */}
-        </Box>
-        <Box
-          sx={{
-            flexBasis: ["100%", "50%"],
-            p: "3"
-          }}
-        >
-          <Login />
-          <LoginCreateAccount />
-        </Box>
-      </FlexWrapper>
-    </LayoutPublic>
+          </Box>
+          <Box
+            sx={{
+              flexBasis: ["100%", "50%"],
+              p: "3"
+            }}
+          >
+            <ExcludeBrowserBanner />
+            <Login />
+            <LoginCreateAccount />
+          </Box>
+        </FlexWrapper>
+      </LayoutPublic>
+    </Fragment>
   );
 };
 
---
+++ packages/app/src/components/ExcludeBrowserBanner/ExcludeBrowserBanner.js
@@ -0,0 +1,42 @@
+import { Card, Text } from "@emjpm/ui";
+import { detect as detectBrowser } from "detect-browser";
+import PropTypes from "prop-types";
+import React from "react";
+
+import { isUnsupportedBrowser } from "../../util/browser";
+
+const excludedBrowsers = [
+  { name: "chrome", version: "44.0.2403" },
+  { name: "ie", version: "10" },
+  { name: "firefox", version: "68.0" }
+];
+const currentBrowser = detectBrowser();
+
+export const ExcludeBrowserBanner = () => {
+  if (isUnsupportedBrowser(currentBrowser, excludedBrowsers)) {
+    return (
+      <Card p={3}>
+        <Text lineHeight={2} fontWeight="bold" color="error">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.google.com/intl/fr_fr/chrome"
+          >
+            {`La plateforme e-MJPM n'est pas compatible avec votre navigateur (${currentBrowser.name} ${currentBrowser.version}). Nous vous conseillons d'utiliser une version récente de Chrome pour profiter pleinement des fonctionnalités d'eMJPM. Veuillez cliquer ici pour installer ou mettre à jour ce navigateur.`}
+          </a>
+        </Text>
+      </Card>
+    );
+  }
+  return null;
+};
+
+ExcludeBrowserBanner.propTypes = {
+  excludedBrowsers: PropTypes.arrayOf(PropTypes.string)
+};
+
+ExcludeBrowserBanner.defaultProps = {
+  excludedBrowsers: []
+};
+
+export default ExcludeBrowserBanner;
