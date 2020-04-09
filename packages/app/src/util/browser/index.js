8c05a2b1d9428e6cbff32d4edaf797563e6a6968
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-04-09 14:15:06 +0200

    feat(app): create exclude browser banner (#1657)
    * feat(app): create exclude browser banner
    
    * feat(browser-banner): change text

--- packages/app/src/util/browser/index.js
+++ packages/app/src/util/browser/index.js
@@ -1,1 +1,15 @@
-export * from "./BrowserUtil";
+import compareVersions from "compare-versions";
+
+export function isUnsupportedBrowser(currentBrowser, excludedBrowsers) {
+  return excludedBrowsers.some(excludedBrowser => {
+    const isRestrictedBrowser = excludedBrowser.name.toLowerCase() === currentBrowser.name;
+    const isUnsupportedVersion =
+      !excludedBrowser.version ||
+      compareVersions(currentBrowser.version, excludedBrowser.version) <= 0;
+    return isRestrictedBrowser && isUnsupportedVersion;
+  });
+}
+
+export function isBrowser() {
+  return typeof window !== "undefined";
+}
---
+++ packages/app/src/util/browser/index.test.js
@@ -0,0 +1,44 @@
+import { isUnsupportedBrowser } from ".";
+
+const testBrowserObject = {
+  name: "safari",
+  version: "11.0.3"
+};
+
+test("Does not exclude browsers not declared in exclusion list", () => {
+  const testExclusionObject = [{ name: "chrome" }];
+  expect(isUnsupportedBrowser(testBrowserObject, testExclusionObject)).toBe(false);
+});
+
+test("Excludes browser when specifying just name", () => {
+  const testExclusionObject = [{ name: "safari" }];
+  expect(isUnsupportedBrowser(testBrowserObject, testExclusionObject)).toBe(true);
+});
+
+test("Excludes browser when current version is lower than required", () => {
+  const testExclusionObject = [{ name: "safari", version: "12" }];
+  expect(isUnsupportedBrowser(testBrowserObject, testExclusionObject)).toBe(true);
+});
+
+test("Does not exclude browser when current version is higher than required", () => {
+  const testExclusionObject = [{ name: "safari", version: "9" }];
+  expect(isUnsupportedBrowser(testBrowserObject, testExclusionObject)).toBe(false);
+});
+
+test("Does not exclude browser when current version is equal to required", () => {
+  const testExclusionObject = [{ name: "safari", version: "11" }];
+  expect(isUnsupportedBrowser(testBrowserObject, testExclusionObject)).toBe(false);
+});
+
+test("Handles granular version arguments", () => {
+  const testExclusionObject = [{ name: "safari", version: "11.0.4" }];
+  expect(isUnsupportedBrowser(testBrowserObject, testExclusionObject)).toBe(true);
+});
+
+test("Handles multiple browsers", () => {
+  const testExclusionObject = [
+    { name: "chrome", version: "30" },
+    { name: "safari", version: "12" }
+  ];
+  expect(isUnsupportedBrowser(testBrowserObject, testExclusionObject)).toBe(true);
+});
