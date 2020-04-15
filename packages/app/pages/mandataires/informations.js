9b915d68cc4a7974e82bea69b40a7b9172ff7abe
Thomas Glatt <tglatt@gmail.com>
2020-04-15 11:10:56 +0200

    feat(enquete-form): hide enquete forms for individuel and service

--- packages/app/pages/mandataires/informations.js
+++ packages/app/pages/mandataires/informations.js
@@ -1,36 +1,16 @@
-import { BoxWrapper, Tab, TabList, TabPanel, Tabs } from "@emjpm/ui";
+import { BoxWrapper, Card } from "@emjpm/ui";
 import React from "react";
 
-import { IndividuelInformationAgrement } from "../../src/components/IndividuelInformationAgrement";
-import { IndividuelInformationExercice } from "../../src/components/IndividuelInformationExercice";
-import { IndividuelInformationFormation } from "../../src/components/IndividuelInformationFormation";
 import { LayoutMandataire } from "../../src/components/Layout";
 import { MandataireInformations } from "../../src/components/MandataireInformations";
 import { withAuthSync } from "../../src/util/auth";
 
 const Informations = () => (
   <LayoutMandataire>
-    <BoxWrapper mt={4}>
-      <Tabs p={0}>
-        <TabList>
-          <Tab>{`Informations générales`}</Tab>
-          <Tab>{`Agrément`}</Tab>
-          <Tab>{`Modalité d'exercice`}</Tab>
-          <Tab>{`Formation`}</Tab>
-        </TabList>
-        <TabPanel>
-          <MandataireInformations />
-        </TabPanel>
-        <TabPanel>
-          <IndividuelInformationAgrement />
-        </TabPanel>
-        <TabPanel>
-          <IndividuelInformationExercice />
-        </TabPanel>
-        <TabPanel>
-          <IndividuelInformationFormation />
-        </TabPanel>
-      </Tabs>
+    <BoxWrapper mt={6} px="1">
+      <Card p="5">
+        <MandataireInformations />
+      </Card>
     </BoxWrapper>
   </LayoutMandataire>
 );
