59c10be2bc019dd5932d81d1635d238e8bf1e368
Thomas Glatt <tglatt@gmail.com>
2020-04-16 08:47:39 +0200

    feat(tribunaux): affiche uniquement les tribunaux immutables

--- packages/app/src/components/TribunalAutoComplete/queries.js
+++ packages/app/src/components/TribunalAutoComplete/queries.js
@@ -2,7 +2,7 @@ import gql from "graphql-tag";
 
 export const TRIBUNAL = gql`
   query Trinbunal($name: String!) {
-    tis(where: { etablissement: { _ilike: $name } }) {
+    tis(where: { immutable: { _eq: true }, etablissement: { _ilike: $name } }) {
       id
       etablissement
     }
