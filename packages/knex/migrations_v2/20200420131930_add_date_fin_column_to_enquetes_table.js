a02506ad6c0bb90054e6db176f3a243c6cd4657a
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-04-20 15:26:01 +0200

    fix(direction): create enquetes form (#1688)
    * fix(enquetes): direction form
    
    * feat(knex): add enquetes migration
    
    * feat(enquetes): new form fields
    
    * fix(knex): update snapshot

---
+++ packages/knex/migrations_v2/20200420131930_add_date_fin_column_to_enquetes_table.js
@@ -0,0 +1,11 @@
+exports.up = async function(knex) {
+  return knex.schema.alterTable("enquetes", table => {
+    table.timestamp("date_fin");
+  });
+};
+
+exports.down = async function(knex) {
+  return knex.schema.alterTable("enquetes", function(table) {
+    table.dropColumn("date_fin");
+  });
+};
