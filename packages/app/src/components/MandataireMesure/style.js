8900d52d1b8399544b8c63db43f87932c4f0edcf
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-04-23 13:34:05 +0200

    feat(mesure): visualize mesure (#1698)

--- packages/app/src/components/MandataireMesure/style.js
+++ packages/app/src/components/MandataireMesure/style.js
@@ -1,24 +1,18 @@
-const TYPES = {
-  "Mesure éteinte": "#7EA3AA",
-  "Mesure en attente": "#F4AF00",
-  "Mesure en cours": "#2DA0FB"
-};
-
-const titleStyle = {
+export const titleStyle = {
   color: "mediumGray",
   fontSize: "2",
   fontStyle: "italic"
 };
 
-const subtitle = {
-  color: "mediumGray",
+export const subtitle = {
+  color: "#C1C1C1",
   fontWeight: "500",
   fontSize: "12px",
   lineHeight: "1.4"
 };
 
-const content = {
-  color: "black",
+export const content = {
+  color: "#404040",
   fontSize: 1,
   fontWeight: "600",
   lineHeight: "1.4",
@@ -26,21 +20,12 @@ const content = {
   mb: 1
 };
 
-const boxStyle = {
-  width: "50%"
+export const statusBox = {
+  padding: "4px 10px",
+  margin: "auto",
+  display: "inline-block",
+  borderRadius: "2px",
+  color: "#ffffff",
+  fontWeight: "bold",
+  backgroundColor: "#2DA0FB"
 };
-
-const flexStyle = {
-  flexWrap: "wrap",
-  mt: 5
-};
-
-const statusStyle = status => {
-  return {
-    color: TYPES[status],
-    display: "inline-block",
-    fontSize: 1
-  };
-};
-
-export { titleStyle, content, subtitle, boxStyle, flexStyle, statusStyle };
