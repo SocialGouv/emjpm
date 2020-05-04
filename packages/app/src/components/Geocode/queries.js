710ec5e4ca9f8b9ee9afe50d517808862c7cf3c3
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-05-04 13:05:45 +0200

    fix(geocode): search on geolocalisation_code_postal table (#1713)

---
+++ packages/app/src/components/Geocode/queries.js
@@ -0,0 +1,9 @@
+import gql from "graphql-tag";
+
+export const CODE_POSTAL = gql`
+  query code_postal($zipcode: String!) {
+    geolocalisation_code_postal(where: { code_postal: { _eq: $zipcode } }, distinct_on: cities) {
+      cities
+    }
+  }
+`;
