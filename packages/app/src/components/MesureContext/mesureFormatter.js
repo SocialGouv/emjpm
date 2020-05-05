f8205c3481897c3afb116c62785d9a1bad145add
Nicolas Toublanc <n.toublanc@gmail.com>
2020-05-05 10:28:19 +0300

    feat(pays): affichage pays service/individuel (#1721)

---
+++ packages/app/src/components/MesureContext/mesureFormatter.js
@@ -0,0 +1,20 @@
+import { COUNTRIES } from "../../constants/mesures";
+
+export const mesureFormatter = {
+  formatMajeurProtege,
+  formatPays
+};
+
+function formatMajeurProtege(civilite, realAge) {
+  if (!civilite) {
+    return `${realAge} ans`;
+  }
+  return `${civilite === "F" ? "Femme de" : "Homme de"} ${realAge} ans`;
+}
+
+function formatPays(codePays) {
+  if (COUNTRIES[codePays]) {
+    return COUNTRIES[codePays];
+  }
+  return `pays non renseigné`;
+}
