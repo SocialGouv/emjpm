49f63f0fa5eca2e198acfbef8dda1dea98b4110f
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-05-06 16:01:28 +0200

    fix(magistrat): decrement mesures counter

--- packages/app/src/lib/validationSchemas/magistratMesureEditSchema.js
+++ packages/app/src/lib/validationSchemas/magistratMesureEditSchema.js
@@ -2,7 +2,7 @@ import yup from "./yup";
 
 const magistratMesureEditSchema = yup.object().shape({
   annee: yup.string().required(),
-  cabinet: yup.string(),
+  cabinet: yup.string().nullable(),
   civilite: yup.string().required(),
   numero_rg: yup.string().required(),
   type: yup.string().required()
