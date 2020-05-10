81c7c2caf6291c17447f5d16821e3b456ac1692b
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-05-10 14:29:35 +0200

    ci(restore-data): fix restore data (#1737)

--- .k8s/postgres/backup/configmap/backup.sh
+++ .k8s/postgres/backup/configmap/backup.sh
@@ -7,3 +7,7 @@ pg_dump --version
 pg_dump --verbose -Fc ${PGDATABASE} > ${DUMP_DIR}/backup_emjpm_${TIMESTAMP}.dump
 
 echo "backup_emjpm_${TIMESTAMP}.dump" > ${DUMP_DIR}/LATEST
+
+cp ${DUMP_DIR}/backup_emjpm_${TIMESTAMP}.dump ${DUMP_DEV_DIR}/backup_emjpm_${TIMESTAMP}.dump
+
+echo "backup_emjpm_${TIMESTAMP}.dump" > ${DUMP_DEV_DIR}/LATEST
--- .k8s/postgres/backup/configmap/wait_for_status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-while [ ! -f ${CRONJOB_STATUS_DIR}/${1} ]
-do
-  echo "Waiting for ${1} status"
-  sleep 2
-done
