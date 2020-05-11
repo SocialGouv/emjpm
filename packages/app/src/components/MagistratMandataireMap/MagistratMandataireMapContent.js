cdcace4247854d41adbbef9a4c4b9683feff4673
Nicolas Toublanc <n.toublanc@gmail.com>
2020-05-11 11:35:12 +0300

    feat(magistrats/map): affichage du cluster de mesures sur la carte #1739 (#1740)

--- packages/app/src/components/MagistratMandataireMap/MagistratMandataireMapContent.js
+++ packages/app/src/components/MagistratMandataireMap/MagistratMandataireMapContent.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import { MapContainer, MapLayer } from "../Map";
+import { MapCluster, MapContainer, MapLayer } from "../Map";
 import { mapImages } from "../Map/utils";
 
 const MagistratMandataireMapContent = props => {
   const { latitude, longitude, id, discriminator, mesures } = props;
   return (
     <MapContainer latitude={latitude} longitude={longitude}>
-      <MapLayer image={mapImages["MESURE"]} items={mesures} type={"MESURE"} />
+      <MapCluster items={mesures} type="MESURE" />
       <MapLayer
         image={mapImages[discriminator]}
         items={[{ id, latitude, longitude }]}
