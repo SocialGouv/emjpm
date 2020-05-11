7b3e7e5b5d69f9dd4d398f80fd9d0498808beb96
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-05-11 10:32:34 +0200

    fix(mesure): refresh mesures count (#1735)
    * fix(mesure): count
    
    * Revert "fix(magistrat): decrement mesures counter"
    
    This reverts commit 46bc96c9473c4c0d1bb2b1691a49068681fcbc5f.

--- packages/graphql-server/src/config/config.ts
+++ packages/graphql-server/src/config/config.ts
@@ -1,5 +1,6 @@
 export const getConfiguration = (env: typeof process.env) => {
   return {
+    HASURA_GRAPHQL_ADMIN_SECRET: env.HASURA_GRAPHQL_ADMIN_SECRET,
     HASURA_GRAPHQL_URI:
       env.HASURA_GRAPHQL_URI || "http://localhost:5000/v1/graphql",
     PORT: env.PORT || 4001
