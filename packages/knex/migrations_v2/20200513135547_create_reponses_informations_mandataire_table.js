2516b50d1dfdf7abfe819e4df054492a25b08c22
Rémi Roycourt <remiroycourt@gmail.com>
2020-05-13 15:45:09 +0200

    feat(enquete): reorganize enquete tables

---
+++ packages/knex/migrations_v2/20200513135547_create_reponses_informations_mandataire_table.js
@@ -0,0 +1,23 @@
+exports.up = async knex => {
+  await knex.schema.createTable(
+    "enquete_reponses_informations_mandataire",
+    table => {
+      table.increments();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.timestamp("last_update").defaultTo(knex.fn.now());
+
+      table.boolean("benevole").nullable();
+      table.string("forme_juridique").nullable();
+      table.string("sexe").nullable();
+      table.string("ancienete").nullable();
+
+      table.string("estimation_etp").nullable();
+      table.string("secretaire_specialise_etp").nullable();
+      table.boolean("local_professionnel").nullable();
+    }
+  );
+};
+
+exports.down = async knex => {
+  await knex.raw("DROP TABLE enquete_reponses_informations_mandataire");
+};
---
+++ packages/knex/migrations_v2/20200513145403_create_enquete_reponses_agrements_formations_table.js
@@ -0,0 +1,31 @@
+exports.up = async knex => {
+  await knex.schema.createTable(
+    "enquete_reponses_agrements_formations",
+    table => {
+      table.increments();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.timestamp("last_update").defaultTo(knex.fn.now());
+
+      table.integer("annee_agrement".nullable());
+      table.integer("annee_debut_activite").nullable();
+      table.integer("cnc_maj_heure_formation").nullable();
+      table.integer("cnc_mjpm_heure_formation").nullable();
+      table.integer("cnc_maj_annee_obtention").nullable();
+      table.integer("cnc_dpf_annee_obtention").nullable();
+      table.integer("cnc_dpf_heure_formation").nullable();
+      table.integer("cnc_mjpm_annee_obtention").nullable();
+      table.boolean("cumul_delegue_service").nullable();
+      table.string("cumul_delegue_service_etp").nullable();
+      table.boolean("cumul_prepose").nullable();
+      table.string("cumul_prepose_etp").nullable();
+      table.boolean("debut_activite_avant_2009").nullable();
+      table.integer("niveau_qualification").nullable();
+      table.integer("niveau_qualification_secretaire_spe").nullable();
+      table.boolean("secretaire_specialise").nullable();
+    }
+  );
+};
+
+exports.down = async knex => {
+  await knex.raw("DROP TABLE enquete_reponses_agrements_formations");
+};
