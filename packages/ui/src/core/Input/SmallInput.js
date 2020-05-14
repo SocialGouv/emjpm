f926dee46d98bcd4bc065f2be1c9ab6356ba8317
Rémi Roycourt <remiroycourt@gmail.com>
2020-05-14 09:38:29 +0200

    fix(ui): small input

--- packages/ui/src/core/Input/SmallInput.js
+++ packages/ui/src/core/Input/SmallInput.js
@@ -1,38 +1,41 @@
-import { Input } from '@rebass/forms';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { Input } from './Input';
+
 function getBorderProps({ hasError, hasWarning }) {
-  if(hasWarning) {
-    return { border: "3px solid #1D2649" };
-  }if(hasError) {
-    return { border: "3px solid #FF6862" };
+  if (hasWarning) {
+    return { border: '3px solid #1D2649' };
+  }
+  if (hasError) {
+    return { border: '3px solid #FF6862' };
   }
   return {};
 }
 
 export const SmallInput = (props) => {
   return (
-  <Input
-    {...props}
-    sx={{
-      ...getBorderProps(props),
-      borderRadius: "8px",
-      color: "#000000",
-      fontSize: "14px",
-      padding: "15px 5px",
-      textAlign: "center",
-      width: '60px'
-    }}
-  />
-)};
+    <Input
+      {...props}
+      sx={{
+        ...getBorderProps(props),
+        borderRadius: '8px',
+        color: '#000000',
+        fontSize: '14px',
+        padding: '15px 5px',
+        textAlign: 'center',
+        width: '60px',
+      }}
+    />
+  );
+};
 
 SmallInput.defaultProps = {
-  hasError: false
-}
+  hasError: false,
+};
 
 SmallInput.propTypes = {
-  hasError: PropTypes.bool
-}
+  hasError: PropTypes.bool,
+};
 
 export default SmallInput;
