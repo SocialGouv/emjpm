b306a628d49d5735d0fd278a0fc779fcf4459236
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-05-14 15:46:27 +0200

    fix(excel-parser): read cvs as text

--- packages/api/src/utils/file/excelParser.js
+++ packages/api/src/utils/file/excelParser.js
@@ -2,13 +2,17 @@ var XLSX = require("xlsx");
 
 const toJson = ({ base64str, type }) => {
   const workbook = XLSX.read(base64str, {
-    type: "base64"
+    cellDates: true,
+    dateNF: "dd/mm/yyyy",
+    locale: "fr-FR",
+    type: "base64",
+    raw: type === "csv" ? true : false
   });
 
   const [worksheetName] = workbook.SheetNames;
   const worksheet = workbook.Sheets[worksheetName];
 
-  return XLSX.utils.sheet_to_json(worksheet, { raw: true }).map(row => {
+  return XLSX.utils.sheet_to_json(worksheet, { raw: false }).map(row => {
     return Object.keys(row).reduce((acc, key) => {
       acc[key] = row[key].toString().trim();
 
