493c359250e485c69025860164a47a9e40b570e8
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-05-19 16:30:27 +0200

    feat(enquete): validation indicators (#1807)
    * feat(api): hasura trigger & actions refactoring
    
    * feat(api): remove jwt auth and trust hasura for actions hooks
    
    * fix(tribunal-query): display correct tis as default options (#1799)
    
    * feat(api): fix tests
    
    * feat(api): create hasura action to initialize enquete reponse (#1803)
    
    * feat(api): create hasura action to initialize enquete reponse
    
    * feat(enquete): ssr enquete validation status
    
    * fix(enquete): ssr validations
    
    * refactor(api): enquete
    
    Co-authored-by: Nicolas Toublanc <n.toublanc@gmail.com>
    Co-authored-by: Thomas Glatt <47273014+tglatt@users.noreply.github.com>

--- packages/app/src/components/Enquete/EnqueteIndividuel/EnqueteIndividuel.js
+++ packages/app/src/components/Enquete/EnqueteIndividuel/EnqueteIndividuel.js
@@ -1,3 +1,4 @@
+import { Heading1 } from "@emjpm/ui";
 import React from "react";
 import { useQuery } from "react-apollo";
 import { Box } from "rebass";
@@ -20,102 +21,131 @@ import {
 import { EnqueteIndividuelPrestationsSociales } from "../EnqueteIndividuelPrestationsSociales";
 import { EnquetePopulationsCuratelle, EnquetePopulationsTutelle } from "../EnquetePopulations";
 import { EnqueteIndividuelWelcome } from "./EnqueteIndividuelWelcome";
-import { ENQUETE_INDIVIDUEL_RESPONSE } from "./queries";
+import { ENQUETE_MANDATAIRE_INDIVIDUEL } from "./queries";
 
-const MENU_SECTIONS = [
-  {
-    steps: [
-      {
-        label: "Bienvenue",
-        component: EnqueteIndividuelWelcome
-      }
-    ]
-  },
-  {
-    label: "Vos informations",
-    steps: [
-      {
-        label: "Informations générales",
-        component: EnqueteIndividuelInformations
-      },
-      {
-        label: "Agréments",
-        component: EnqueteIndividuelInformationsAgrements
-      },
-      {
-        label: "Formation",
-        component: EnqueteIndividuelInformationsFormation
-      }
-    ]
-  },
-  {
-    label: "Votre activité",
-    steps: [
-      {
-        label: "Curatelle renforcée",
-        component: EnqueteActiviteCuratelleRenforcee
-      },
-      {
-        label: "Curatelle simple",
-        component: EnqueteActiviteCuratelleSimple
-      },
-      {
-        label: "Tutelle",
-        component: EnqueteActiviteTutelle
-      },
-      {
-        label: "Mesure d'accompagnement judiciaire",
-        component: EnqueteActiviteAccompagnementJudiciaire
-      },
-      {
-        label: "Tutelle ou curatelle aux biens",
-        component: EnqueteActiviteCuratelleBiens
-      },
-      {
-        label: "Tutelle ou curatelle à la personne",
-        component: EnqueteActiviteCuratellePersonne
-      },
-      {
-        label: "Révision de mesures",
-        component: EnqueteActiviteRevisionMesures
-      }
-    ]
-  },
-  {
-    label: "Populations",
-    steps: [
-      { label: "Curatelle", component: EnquetePopulationsCuratelle },
-      { label: "Tutelle", component: EnquetePopulationsTutelle },
-      { label: "Mesure d'accompagnement de justice", component: EnquetePopulationsTutelle },
-      { label: "Sauvegarde de justice", component: EnquetePopulationsTutelle },
-      { label: "Autre", component: EnquetePopulationsTutelle }
-    ]
-  },
-  {
-    label: "Prestations sociales",
-    steps: [{ label: "Prestations sociales", component: EnqueteIndividuelPrestationsSociales }]
-  },
-  {
-    steps: [{ label: "Envoi de vos réponses", component: null }]
+function transformStatusToIsValidProperty(status) {
+  if (status === 0) {
+    return null;
   }
-];
+
+  return status === 2 ? true : false;
+}
 
 export const EnqueteIndividuel = props => {
   const { enqueteId, mandataireId } = props;
 
-  const { data, loading } = useQuery(ENQUETE_INDIVIDUEL_RESPONSE, {
+  const { data, loading, error } = useQuery(ENQUETE_MANDATAIRE_INDIVIDUEL, {
     variables: { enqueteId, mandataireId }
   });
 
   if (loading) {
-    return <Box>Chargement...</Box>;
+    return <Box mt={4}>Chargement...</Box>;
   }
 
-  const { enqueteIndividuelReponse } = data;
+  if (error) {
+    return (
+      <Box mt={4}>
+        <Heading1 mb={4}>Oups</Heading1>
+        <Box>Une erreur est survenue. Merci de réessayer ultérieurement.</Box>
+      </Box>
+    );
+  }
+
+  const { enquete_individuel } = data;
+
+  const MENU_SECTIONS = [
+    {
+      steps: [
+        {
+          label: "Bienvenue",
+          component: EnqueteIndividuelWelcome
+        }
+      ]
+    },
+    {
+      label: "Vos informations",
+      steps: [
+        {
+          label: "Informations générales",
+          component: EnqueteIndividuelInformations,
+          isValid: transformStatusToIsValidProperty(
+            enquete_individuel.enquete_reponses_informations_mandataire_generales_status
+          )
+        },
+        {
+          label: "Agréments",
+          component: EnqueteIndividuelInformationsAgrements,
+          isValid: transformStatusToIsValidProperty(
+            enquete_individuel.enquete_reponses_informations_mandataire_agrements_status
+          )
+        },
+        {
+          label: "Formation",
+          component: EnqueteIndividuelInformationsFormation,
+          isValid: transformStatusToIsValidProperty(
+            enquete_individuel.enquete_reponses_informations_mandataire_formation_status
+          )
+        }
+      ]
+    },
+    {
+      label: "Votre activité",
+      steps: [
+        {
+          label: "Curatelle renforcée",
+          component: EnqueteActiviteCuratelleRenforcee,
+          isValid: false
+        },
+        {
+          label: "Curatelle simple",
+          component: EnqueteActiviteCuratelleSimple,
+          isValid: false
+        },
+        {
+          label: "Tutelle",
+          component: EnqueteActiviteTutelle,
+          isValid: false
+        },
+        {
+          label: "Mesure d'accompagnement judiciaire",
+          component: EnqueteActiviteAccompagnementJudiciaire
+        },
+        {
+          label: "Tutelle ou curatelle aux biens",
+          component: EnqueteActiviteCuratelleBiens
+        },
+        {
+          label: "Tutelle ou curatelle à la personne",
+          component: EnqueteActiviteCuratellePersonne
+        },
+        {
+          label: "Révision de mesures",
+          component: EnqueteActiviteRevisionMesures
+        }
+      ]
+    },
+    {
+      label: "Populations",
+      steps: [
+        { label: "Curatelle", component: EnquetePopulationsCuratelle, isValid: false },
+        { label: "Tutelle", component: EnquetePopulationsTutelle },
+        { label: "Mesure d'accompagnement de justice", component: EnquetePopulationsTutelle },
+        { label: "Sauvegarde de justice", component: EnquetePopulationsTutelle },
+        { label: "Autre", component: EnquetePopulationsTutelle }
+      ]
+    },
+    {
+      label: "Prestations sociales",
+      steps: [{ label: "Prestations sociales", component: EnqueteIndividuelPrestationsSociales }]
+    },
+    {
+      steps: [{ label: "Envoi de vos réponses", component: null }]
+    }
+  ];
 
   return (
     <MenuStepper
-      enqueteReponse={enqueteIndividuelReponse}
+      enqueteReponse={enquete_individuel}
       mandataireId={mandataireId}
       sections={MENU_SECTIONS}
     />
