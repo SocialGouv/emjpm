a14fd640c03da067fc463c1c570624b8baa2ef97
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-05-19 09:27:05 +0200

    fix(tribunal-query): display correct tis as default options (#1799)

--- packages/app/src/components/ServiceMesureEdit/queries.js
+++ packages/app/src/components/ServiceMesureEdit/queries.js
@@ -12,7 +12,7 @@ export const DEPARTEMENTS = gql`
 
 export const SERVICE_TRIBUNAL = gql`
   query ServiceTribunal($serviceId: Int!) {
-    service_tis(where: { service_id: { _eq: $serviceId } }) {
+    service_tis(order_by: { ti: { ville: asc } }, where: { service_id: { _eq: $serviceId } }) {
       id
       ti {
         etablissement
