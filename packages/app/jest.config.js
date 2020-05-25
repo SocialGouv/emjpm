abd139380bc3a7a9f455fc13cbae9c5d06e8eea9
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-05-25 10:49:36 +0200

    feat(enquete): create components unit tests (#1820)

--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   collectCoverageFrom: ["src/**/*.js"],
   coverageDirectory: process.env.JEST_COVERAGE_DIRECTORY,
   setupFiles: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  testEnvironment: "jest-environment-jsdom",
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/", "<rootDir>/cypress"]
 };
