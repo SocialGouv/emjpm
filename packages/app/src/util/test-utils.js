496483ce0b934f36640e8d856b914fb21b878190
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-05-25 10:49:36 +0200

    feat(enquete): create components unit tests (#1820)

---
+++ packages/app/src/util/test-utils.js
@@ -0,0 +1,19 @@
+import { presetEmjpm } from "@emjpm/ui";
+import { render as rtlRender } from "@testing-library/react";
+import PropTypes from "prop-types";
+import React from "react";
+import { ThemeProvider } from "theme-ui";
+
+function render(ui, { ...options } = {}) {
+  function Wrapper({ children }) {
+    return <ThemeProvider theme={presetEmjpm}>{children}</ThemeProvider>;
+  }
+  Wrapper.propTypes = {
+    children: PropTypes.node
+  };
+
+  return rtlRender(ui, { wrapper: Wrapper, ...options });
+}
+
+export * from "@testing-library/react";
+export { render };
