1ef4921d8c726dcac6dc8cc05f004fa87c9b713f
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-05-26 11:10:14 +0200

    feat(admin-enquete): update style (#1834)

--- packages/app/pages/direction/enquetes/create.js
+++ packages/app/pages/direction/enquetes/create.js
@@ -1,4 +1,4 @@
-import { BoxWrapper, Heading2 } from "@emjpm/ui";
+import { BoxWrapper, Card, Heading2 } from "@emjpm/ui";
 import React from "react";
 
 import { EnqueteCreate } from "../../../src/components/EnqueteCreate";
@@ -9,8 +9,10 @@ const CreateEnquete = () => {
   return (
     <LayoutDirection>
       <BoxWrapper mt={6} px="1">
-        <Heading2 mb={3}>Créer une enquête</Heading2>
-        <EnqueteCreate />
+        <Card p={5}>
+          <Heading2 mb={3}>Créer une enquête</Heading2>
+          <EnqueteCreate />
+        </Card>
       </BoxWrapper>
     </LayoutDirection>
   );
