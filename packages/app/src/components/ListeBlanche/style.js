0b110e3bd794725e00254d6b23091bcb12d36684
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-05-28 17:23:41 +0200

    feat(liste-blanche): add liste blanche view for admin (#1856)
    * feat(liste-blanche): display liste blanche
    
    * feat(liste-blanche): add pagination
    
    * feat(liste-blanche): add type to lb_user
    
    * feat(liste-blanche): update snapshot

---
+++ packages/app/src/components/ListeBlanche/style.js
@@ -0,0 +1,30 @@
+const cardStyle = userId => {
+  return {
+    mb: "1",
+    overflow: "hidden",
+    pl: "16px",
+    position: "relative",
+    width: "100%",
+    borderLeft: userId ? "2px solid green" : "2px solid red"
+  };
+};
+
+const labelStyle = {
+  color: "mediumGray",
+  fontFamily: "body",
+  fontSize: "11px",
+  fontWeight: "600",
+  mb: "5px",
+  mt: "7px"
+};
+
+const descriptionStyle = {
+  fontFamily: "heading",
+  fontSize: "13px",
+  fontWeight: "600",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  whiteSpace: "nowrap"
+};
+
+export { cardStyle, labelStyle, descriptionStyle };
