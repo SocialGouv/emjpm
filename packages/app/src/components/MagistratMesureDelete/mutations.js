f1d13ed080fecdbb4c0b700cbe43a0beab58a4e9
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-05-29 09:01:32 +0200

    fix(mesure-cancel): remove unseless gql mutation

--- packages/app/src/components/MagistratMesureDelete/mutations.js
+++ packages/app/src/components/MagistratMesureDelete/mutations.js
@@ -5,13 +5,6 @@ export const DELETE_MANDATAIRE_MESURE = gql`
     delete_mesures(where: { id: { _eq: $id } }) {
       affected_rows
     }
-    update_mandataires(where: { id: { _eq: $mandataire_id } }) {
-      affected_rows
-      returning {
-        id
-        mesures_en_attente
-      }
-    }
   }
 `;
 
@@ -37,13 +30,6 @@ export const DELETE_SERVICE_MESURE = gql`
   mutation deleteMesure($id: Int!, $service_id: Int!) {
     delete_mesures(where: { id: { _eq: $id } }) {
       affected_rows
-    }
-    update_services(where: { id: { _eq: $service_id } }) {
-      affected_rows
-      returning {
-        id
-        mesures_awaiting
-      }
     }
   }
 `;
