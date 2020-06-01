61a28e9e069a0136c5f545fa07a7f9edc2ef2357
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-06-01 16:08:42 +0200

    feat(liste-blanche): add summary (#1876)
    * feat(hasura): update metadata for liste blanche
    
    * feat(liste-blanche): add summary

---
+++ packages/app/src/components/ListeBlancheSummary/queries.js
@@ -0,0 +1,58 @@
+import gql from "graphql-tag";
+
+export const LB_SUMMARY = gql`
+  query listeBlancheSummary($departementId: Int) {
+    individuel: lb_users_aggregate(
+      where: {
+        type: { _eq: "individuel" }
+        lb_departements: {
+          departement_id: { _eq: $departementId }
+          departement_financeur: { _eq: true }
+        }
+      }
+    ) {
+      aggregate {
+        count
+      }
+    }
+    prepose: lb_users_aggregate(
+      where: {
+        type: { _eq: "prepose" }
+        lb_departements: {
+          departement_id: { _eq: $departementId }
+          departement_financeur: { _eq: true }
+        }
+      }
+    ) {
+      aggregate {
+        count
+      }
+    }
+    service: lb_users_aggregate(
+      where: {
+        type: { _eq: "service" }
+        lb_departements: {
+          departement_id: { _eq: $departementId }
+          departement_financeur: { _eq: true }
+        }
+      }
+    ) {
+      aggregate {
+        count
+      }
+    }
+    ti: lb_users_aggregate(
+      where: {
+        type: { _eq: "ti" }
+        lb_departements: {
+          departement_id: { _eq: $departementId }
+          departement_financeur: { _eq: true }
+        }
+      }
+    ) {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
