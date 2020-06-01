9079b0301d853d192fae77786dab734231c7b56f
Rémi Roycourt <remiroycourt@gmail.com>
2020-06-01 09:06:19 +0200

    refactor(api): reorganize action folders

--- packages/api/src/routes/hasura-actions/enquete-mandataire-prepose/import/index.js
+++ packages/api/src/routes/hasura-actions/enquete-mandataire-prepose/import/index.js
@@ -1,11 +1,11 @@
-import { parse } from "./parser";
+const parser = require("./parser");
 
 async function importEnqueteMandatairePreposeFile({
   file: { content }
   // importContext
 }) {
   // const { enqueteId, userId, service, mandataire } = importContext;
-  await parse({ content });
+  await parser.parse({ content });
 }
 
 module.exports = { importEnqueteMandatairePreposeFile };
