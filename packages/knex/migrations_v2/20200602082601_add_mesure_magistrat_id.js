ee81c54d87f423b298281174896070c50fad330a
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-06-02 09:25:14 +0200

    feat(mesure): add magistrat

---
+++ packages/knex/migrations_v2/20200602082601_add_mesure_magistrat_id.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  return knex.schema.alterTable("mesures", function(table) {
+    table
+      .integer("magistrat_id")
+      .references("id")
+      .inTable("magistrat")
+      .nullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.alterTable("mesures", function(table) {
+    table.dropColumn("magistrat_id");
+  });
+};
