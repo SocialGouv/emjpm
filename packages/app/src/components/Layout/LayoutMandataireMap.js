5c683ac14f7e81ef877599cec830ad0f9c41ede1
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-06-02 11:59:03 +0200

    feat(enquete): add link in LayoutMap

--- packages/app/src/components/Layout/LayoutMandataireMap.js
+++ packages/app/src/components/Layout/LayoutMandataireMap.js
@@ -1,9 +1,10 @@
 import { BoxWrapper } from "@emjpm/ui";
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import { Box } from "rebass";
 
 import { Header } from "../Header";
 import { Navigation } from "../Navigation";
+import { UserContext } from "../UserContext";
 
 const navigationLinks = [
   {
@@ -22,12 +23,23 @@ const navigationLinks = [
 
 const LayoutMandataireMap = props => {
   const { children } = props;
+
+  const user = useContext(UserContext);
+
+  const links = user.enquete
+    ? navigationLinks.concat({
+        title: `Enquête ${user.enquete.annee}`,
+        url: "/mandataires/enquetes/[enquete_id]",
+        as: `/mandataires/enquetes/${user.enquete.id}`
+      })
+    : navigationLinks;
+
   return (
     <Fragment>
       <Box sx={{ position: "relative", "z-index": "1000" }} bg="cardPrimary">
         <Header dropDownLinks={[{ title: "Vos informations", url: "/mandataires/informations" }]} />
         <BoxWrapper>
-          <Navigation links={navigationLinks} />
+          <Navigation links={links} />
         </BoxWrapper>
       </Box>
       {children}
