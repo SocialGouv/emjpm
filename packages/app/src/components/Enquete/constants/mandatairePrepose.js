0252f8d06c6604d58b57a1b4912c23e40a7004ba
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-06-03 16:36:44 +0200

    feat(enquete): enquete for prepose (modalite exercice) (#1888)

---
+++ packages/app/src/components/Enquete/constants/mandatairePrepose.js
@@ -0,0 +1,14 @@
+import { enqueteKeysBuilder } from "./enqueteKeysBuilder.service";
+
+export const PERSONNALITE_JURIDIQUE = enqueteKeysBuilder.buildKeys({
+  etat_ou_collectivite: "Etat ou collectivité locale",
+  etablissement_public_hospitalisation: "Etablissement public d'hospitalisation",
+  ccas: "CCAS",
+  groupement_public: "Groupement public (SIH, GCS, GCSMS, GIP, GIE)",
+  etablissement_public_social: "Etablissement public social ou médico-social",
+  etablissement_public_autre: "Autre type d'établissement public",
+  etablissement_public_industriel_commercial:
+    "Etablissement public à caractère industriel et commercial",
+  organisme_prive_non_lucratif: "Organisme privé à but non lucratif (dont CG privés)",
+  organisme_prive_commercial: "Organisme privé à caractère commercial"
+});
