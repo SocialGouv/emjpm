f5019fb86909331140188bb1d1649e06cc6a2043
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-06-04 12:04:45 +0200

    fix(mesures-import): update sheet range (#1891)

--- packages/api/src/utils/file/excelParser.js
+++ packages/api/src/utils/file/excelParser.js
@@ -10,12 +10,29 @@ const parseSheets = ({ content, parseOptions }) => {
   }, {});
 };
 
+// https://github.com/SheetJS/sheetjs/issues/764
+function update_sheet_range(ws) {
+  var range = { s: { r: 20000000, c: 20000000 }, e: { r: 0, c: 0 } };
+  Object.keys(ws)
+    .filter(function(x) {
+      return x.charAt(0) != "!";
+    })
+    .map(XLSX.utils.decode_cell)
+    .forEach(function(x) {
+      range.s.c = Math.min(range.s.c, x.c);
+      range.s.r = Math.min(range.s.r, x.r);
+      range.e.c = Math.max(range.e.c, x.c);
+      range.e.r = Math.max(range.e.r, x.r);
+    });
+  ws["!ref"] = XLSX.utils.encode_range(range);
+}
+
 const parseSheetByIndex = ({ content, parseOptions, sheetIndex }) => {
   const workbook = XLSX.read(content, parseOptions);
-
   const worksheetNames = workbook.SheetNames;
   if (sheetIndex < worksheetNames.length) {
     const worksheet = workbook.Sheets[worksheetNames[sheetIndex]];
+    update_sheet_range(worksheet);
     return _parseSheetByName(worksheet);
   }
   throw new Error(
