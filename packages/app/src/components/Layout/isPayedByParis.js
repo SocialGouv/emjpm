b8825f8b6aa117416b1a264c9aa30562dc7d93e9
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-06-17 16:51:05 +0200

    fix(enquete-visibility): update isPayedByParis

--- packages/app/src/components/Layout/isPayedByParis.js
+++ packages/app/src/components/Layout/isPayedByParis.js
@@ -1,12 +1,14 @@
 export default ({ mandataire }) => {
   const lbUser = mandataire.lb_user;
-  if (!lbUser) {
+  if (!lbUser || !lbUser.lb_departements) {
     return false;
   }
-  return lbUser.lb_departements.filter((elm) => {
-    if (!elm.departement_financeur) {
-      return false;
-    }
-    return elm.departement.code === "75";
-  });
+  return (
+    lbUser.lb_departements.filter((elm) => {
+      if (!elm.departement_financeur) {
+        return false;
+      }
+      return elm.departement.code === "75";
+    }).length > 0
+  );
 };
