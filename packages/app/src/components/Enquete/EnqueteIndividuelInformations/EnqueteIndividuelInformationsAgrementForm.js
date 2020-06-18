b556b40f22ce3011abc152c0f45472ae95e9e9c6
Nicolas Toublanc <n.toublanc@gmail.com>
2020-06-18 08:46:28 +0300

    feat(enquete): lecture seule - individuels #1911 (#1948)

--- packages/app/src/components/Enquete/EnqueteIndividuelInformations/EnqueteIndividuelInformationsAgrementForm.js
+++ packages/app/src/components/Enquete/EnqueteIndividuelInformations/EnqueteIndividuelInformationsAgrementForm.js
@@ -1,13 +1,15 @@
-import { Field, Heading1, Heading3, InlineError, Input, Select } from "@emjpm/ui";
-import { Label } from "@rebass/forms";
+import { Heading1, Heading3 } from "@emjpm/ui";
 import React from "react";
 import { Box } from "rebass";
 
-import { YesNoComboBox } from "../../../components/Commons";
 import yup from "../../../lib/validationSchemas/yup";
 import { formatFormBoolean, formatFormInput } from "../../../util";
-import { findOption } from "../../../util/option/OptionUtil";
 import { ENQ_REP_AGREMENTS_FORMATIONS } from "../constants";
+import {
+  EnqueteFormInputField,
+  EnqueteFormSelectField,
+  EnqueteFormYesNoField,
+} from "../EnqueteForm";
 import { EnqueteStepperButtons } from "../EnqueteStepperButtons";
 import { useEnqueteForm } from "../useEnqueteForm.hook";
 
@@ -40,15 +42,7 @@ export const EnqueteIndividuelInformationsAgrementForm = (props) => {
     dispatchEnqueteContextEvent,
   } = props;
 
-  const {
-    submitForm,
-    handleChange,
-    values,
-    errors,
-    setFieldValue,
-    showError,
-    submit,
-  } = useEnqueteForm({
+  const enqueteForm = useEnqueteForm({
     onSubmit,
     enqueteContext,
     dispatchEnqueteContextEvent,
@@ -59,6 +53,8 @@ export const EnqueteIndividuelInformationsAgrementForm = (props) => {
     loading,
   });
 
+  const { submitForm, submit } = enqueteForm;
+
   return (
     <form onSubmit={submitForm}>
       <Heading1 textAlign="center" mb={"80px"}>
@@ -67,112 +63,43 @@ export const EnqueteIndividuelInformationsAgrementForm = (props) => {
       <Heading3>{"Agrément"}</Heading3>
 
       <Box mt={4}>
-        <Field>
-          <Label mb={1} htmlFor="debut_activite_avant_2009">
-            {"Exerciez vous l'activité de mandataire avant le 01/01/2009 ?"}
-          </Label>
-          <YesNoComboBox
-            value={values.debut_activite_avant_2009}
-            name="debut_activite_avant_2009"
-            onChange={(value) => {
-              setFieldValue("debut_activite_avant_2009", value);
-              if (!value) {
-                setFieldValue("debut_activite_avant_2009", "");
-              }
-            }}
-          />
-          <InlineError
-            showError={showError}
-            message={errors.debut_activite_avant_2009}
-            fieldId="debut_activite_avant_2009"
-          />
-        </Field>
-        <Field>
-          <Label mb={1} htmlFor={"annee_agrement"}>
-            {
-              "Indiquez l'année de votre agrément selon les nouvelles modalités prévues par la loi du 5 mars 2007"
-            }
-          </Label>
-          <Input
-            type="number"
-            placeholder=""
-            value={values.annee_agrement}
-            id="annee_agrement"
-            name="annee_agrement"
-            hasError={showError && !!errors.annee_agrement}
-            onChange={handleChange}
-            min={2009}
-          />
-          <InlineError
-            showError={showError}
-            message={errors.annee_agrement}
-            fieldId="annee_agrement"
-          />
-        </Field>
-
-        <Field>
-          <Label mb={1} htmlFor="nb_departements">
-            {"Nombre de départements dans lesquels vous disposez d'un agrément"}
-          </Label>
-          <Select
-            placeholder=""
-            id="nb_departements"
-            instanceId={"nb_departements"}
-            name="nb_departements"
-            value={findOption(
-              ENQ_REP_AGREMENTS_FORMATIONS.NB_DEPARTEMENTS.byKey,
-              values.nb_departements
-            )}
-            hasError={showError && !!errors.nb_departements}
-            onChange={(option) => setFieldValue("nb_departements", option.value)}
-            options={ENQ_REP_AGREMENTS_FORMATIONS.NB_DEPARTEMENTS.byKey}
-          />
-          <InlineError
-            showError={showError}
-            message={errors.nb_departements}
-            fieldId="nb_departements"
-          />
-        </Field>
-        <Field>
-          <Label mb={1} htmlFor={"nb_mesures_dep_finance"}>
-            {
-              "Nombre de mesures au 31/12 exercées dans le département dans lequel vous avez obtenu votre premier agrément et qui vous finance"
-            }
-          </Label>
-          <Input
-            type="number"
-            placeholder=""
-            value={values.nb_mesures_dep_finance}
-            id="nb_mesures_dep_finance"
-            name="nb_mesures_dep_finance"
-            hasError={showError && !!errors.nb_mesures_dep_finance}
-            onChange={handleChange}
-          />
-          <InlineError
-            showError={showError}
-            message={errors.nb_mesures_dep_finance}
-            fieldId="nb_mesures_dep_finance"
-          />
-        </Field>
-        <Field>
-          <Label mb={1} htmlFor={"nb_mesures_dep_autres"}>
-            {"Le cas échéant, nombre de mesures exerçées dans les autres départements"}
-          </Label>
-          <Input
-            type="number"
-            placeholder=""
-            value={values.nb_mesures_dep_autres}
-            id="nb_mesures_dep_autres"
-            name="nb_mesures_dep_autres"
-            hasError={showError && !!errors.nb_mesures_dep_autres}
-            onChange={handleChange}
-          />
-          <InlineError
-            showError={showError}
-            message={errors.nb_mesures_dep_autres}
-            fieldId="nb_mesures_dep_autres"
-          />
-        </Field>
+        <EnqueteFormYesNoField
+          id={`debut_activite_avant_2009`}
+          label="Exerciez vous l'activité de mandataire avant le 01/01/2009 ?"
+          enqueteContext={enqueteContext}
+          enqueteForm={enqueteForm}
+        />
+        <EnqueteFormInputField
+          id="annee_agrement"
+          label="Indiquez l'année de votre agrément selon les nouvelles modalités prévues par la loi du 5 mars 2007"
+          enqueteContext={enqueteContext}
+          enqueteForm={enqueteForm}
+          type="number"
+          min={2009}
+        />
+        <EnqueteFormSelectField
+          id="nb_departements"
+          label="Nombre de départements dans lesquels vous disposez d'un agrément"
+          options={ENQ_REP_AGREMENTS_FORMATIONS.byKey}
+          enqueteContext={enqueteContext}
+          enqueteForm={enqueteForm}
+        />
+        <EnqueteFormInputField
+          id="nb_mesures_dep_finance"
+          label="Nombre de mesures au 31/12 exercées dans le département dans lequel vous avez obtenu votre premier agrément et qui vous finance"
+          enqueteContext={enqueteContext}
+          enqueteForm={enqueteForm}
+          type="number"
+          min={0}
+        />
+        <EnqueteFormInputField
+          id="nb_mesures_dep_autres"
+          label="Le cas échéant, nombre de mesures exerçées dans les autres départements"
+          enqueteContext={enqueteContext}
+          enqueteForm={enqueteForm}
+          type="number"
+          min={0}
+        />
         <EnqueteStepperButtons submit={submit} disabled={loading} />
       </Box>
     </form>
