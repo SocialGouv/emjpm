5640ce5465cdf5375551f3f2c0c82cce0dda9ee3
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-06-22 13:25:08 +0200

    fix(service-edition): use correct formik errors property for email

--- packages/app/src/lib/validationSchemas/serviceSchema.js
+++ packages/app/src/lib/validationSchemas/serviceSchema.js
@@ -4,7 +4,6 @@ const serviceSchema = yup.object().shape({
   competences: yup.string().max(255, "Maximum 255 caractères"),
   dispo_max: yup.number().required(),
   email: yup.string().required(),
-  etablissement: yup.string().required(),
   geocode: yup.object().nullable().required(),
   nom: yup.string().required(),
   prenom: yup.string().required(),
