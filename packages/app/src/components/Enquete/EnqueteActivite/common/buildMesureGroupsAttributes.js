b9fa8cf76b1a9b3ab161ed58af37e46318d1d0a4
Nicolas Toublanc <n.toublanc@gmail.com>
2020-06-22 11:47:30 +0300

    fix(enquete): fix form validation behavior (#1963)

--- packages/app/src/components/Enquete/EnqueteActivite/common/buildMesureGroupsAttributes.js
+++ packages/app/src/components/Enquete/EnqueteActivite/common/buildMesureGroupsAttributes.js
@@ -1,4 +1,5 @@
 import yup from "../../../../lib/validationSchemas/yup";
+
 // fonction identique à activiteSchemaUtil
 export function buildMesureGroupsAttributes(mesureGroups) {
   const attributes = mesureGroups.reduce((acc, mesureGroup) => {
@@ -8,29 +9,42 @@ export function buildMesureGroupsAttributes(mesureGroups) {
     const sortieMesures = mesureGroup ? `${mesureGroup}SortieMesures` : "sortieMesures";
     const somme = mesureGroup ? `${mesureGroup}Somme` : "somme";
 
-    acc[debutAnnee] = yup.number().min(0).integer();
+    acc[debutAnnee] = yup.number().min(0).integer().required();
+    acc[mesuresNouvelles] = yup.number().min(0).integer().required();
+    acc[sortieMesures] = yup.number().min(0).integer().required();
+    acc[finAnnee] = yup.number().min(0).integer().required();
 
-    acc[mesuresNouvelles] = yup.number().min(0).integer();
-    acc[sortieMesures] = yup.number().min(0).integer();
-    acc[finAnnee] = yup.number().min(0).integer();
+    acc[somme] = yup.mixed().test(
+      // "diff-match",
+      // "La valeur de fin d'année n'est pas cohérente avec les autres données.",
+      function () {
+        const nbDebutAnnee = this.parent[debutAnnee];
+        const nbSortieMesures = this.parent[sortieMesures];
+        const nbMesuresNouvelles = this.parent[mesuresNouvelles];
+        const nbFinAnnee = this.parent[finAnnee];
 
-    acc[somme] = yup
-      .number()
-      .min(0)
-      .integer()
-      .test(
-        "diff-match",
-        "La valeur de fin d'année n'est pas cohérente avec les autres données.",
-        function () {
-          const nbDebutAnnee = this.parent[debutAnnee] | 0;
-          const nbSortieMesures = this.parent[sortieMesures] | 0;
-          const nbMesuresNouvelles = this.parent[mesuresNouvelles] | 0;
-          const nbFinAnnee = this.parent[finAnnee] | 0;
+        if (
+          nbDebutAnnee !== undefined &&
+          nbDebutAnnee !== null &&
+          nbSortieMesures !== undefined &&
+          nbSortieMesures !== null &&
+          nbMesuresNouvelles !== undefined &&
+          nbMesuresNouvelles !== null &&
+          nbFinAnnee !== undefined &&
+          nbFinAnnee !== null
+        ) {
           const expectedFinAnnee = nbDebutAnnee + nbMesuresNouvelles - nbSortieMesures;
 
-          return expectedFinAnnee === nbFinAnnee;
+          if (expectedFinAnnee !== nbFinAnnee) {
+            return this.createError({
+              message: `La valeur de fin d'année n'est pas cohérente avec les autres données: ${nbDebutAnnee} + ${nbMesuresNouvelles} - ${nbSortieMesures} = ${expectedFinAnnee}`,
+              path: somme,
+            });
+          }
         }
-      );
+        return true;
+      }
+    );
 
     return acc;
   }, {});
