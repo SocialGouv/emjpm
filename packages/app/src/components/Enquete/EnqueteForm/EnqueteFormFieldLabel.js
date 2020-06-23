21bc79338b5ae056789bd9a6c203fce62c63ed9b
Nicolas Toublanc <n.toublanc@gmail.com>
2020-06-23 12:16:23 +0300

    fix(enquete): fix form bugs (#1981)

--- packages/app/src/components/Enquete/EnqueteForm/EnqueteFormFieldLabel.js
+++ packages/app/src/components/Enquete/EnqueteForm/EnqueteFormFieldLabel.js
@@ -1,6 +1,6 @@
 import { Label } from "@rebass/forms";
 import React, { Fragment, useMemo } from "react";
-import { Text } from "rebass";
+import { Box, Text } from "rebass";
 
 const FieldLabel = ({ isRequired, readOnly }) =>
   isRequired && !readOnly
@@ -12,8 +12,7 @@ const FieldLabel = ({ isRequired, readOnly }) =>
         },
       }
     : {};
-
-export const EnqueteFormFieldLabel = ({ id, label, text, enqueteForm, required }) => {
+export const EnqueteFormFieldLabel = ({ id, label, text, enqueteForm, required, children }) => {
   const { validationSchema, readOnly } = enqueteForm;
 
   const isRequired = useMemo(() => {
@@ -31,7 +30,16 @@ export const EnqueteFormFieldLabel = ({ id, label, text, enqueteForm, required }
           {label}
         </Label>
       )}
-      {text && <Text mb={"5px"}>{text}</Text>}
+      {text && (
+        <Text sx={FieldLabel({ isRequired: isRequired && !label, readOnly })} mb={"5px"}>
+          {text}
+        </Text>
+      )}
+      {children ? (
+        <Box sx={FieldLabel({ isRequired: isRequired && !label && !text, readOnly })}>
+          {children}
+        </Box>
+      ) : null}
     </Fragment>
   );
 };
