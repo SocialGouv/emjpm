7d665a81b84c3cf40a91ec294a556fd3cd65d979
Nicolas Toublanc <n.toublanc@gmail.com>
2020-06-23 10:06:56 +0300

    fix(enquete): activite champs facultatifs (#1977)

--- packages/api/src/routes/hasura-actions/enquete/common/enquete-status/activite/activiteSchemaUtil.js
+++ packages/api/src/routes/hasura-actions/enquete/common/enquete-status/activite/activiteSchemaUtil.js
@@ -7,28 +7,10 @@ function buildMesureGroupsAttributes(mesureGroups) {
     const mesuresNouvelles = `${mesureGroup}_mesures_nouvelles`;
     const sortieMesures = `${mesureGroup}_sortie_mesures`;
 
-    acc[debutAnnee] = yup.number().min(0).integer().required();
-    acc[mesuresNouvelles] = yup.number().min(0).integer().required();
-    acc[sortieMesures] = yup.number().min(0).integer().required();
-
-    acc[finAnnee] = yup
-      .number()
-      .min(0)
-      .integer()
-      .required()
-      .test(
-        "diff-match",
-        "La valeur de fin d'année n'est pas cohérente avec les autres données.",
-        function (value) {
-          const nbDebutAnnee = this.parent[debutAnnee] | 0;
-          const nbSortieMesures = this.parent[sortieMesures] | 0;
-          const nbMesuresNouvelles = this.parent[mesuresNouvelles] | 0;
-          const expectedFinAnnee =
-            nbDebutAnnee + nbMesuresNouvelles - nbSortieMesures;
-
-          return expectedFinAnnee === (value | 0);
-        }
-      );
+    acc[debutAnnee] = yup.number().min(0).integer().nullable();
+    acc[mesuresNouvelles] = yup.number().min(0).integer().nullable();
+    acc[sortieMesures] = yup.number().min(0).integer().nullable();
+    acc[finAnnee] = yup.number().min(0).integer().nullable();
 
     return acc;
   }, {});
