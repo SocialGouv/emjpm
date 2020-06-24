7e827a5e838c2ad594443ecdf2e92872d6c283a1
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-06-24 12:11:04 +0200

    fix(enquete): it fixes #2000 (#2004)

--- packages/app/src/components/Enquete/EnquetePopulations/mutations.js
+++ packages/app/src/components/Enquete/EnquetePopulations/mutations.js
@@ -20,14 +20,14 @@ function buildEnquetePopulationMutation(prefix) {
       $anciennete_3_5_ans: Int
       $anciennete_5_10_ans: Int
       $anciennete_sup_10_ans: Int
-      $type_etablissement_personne_handicapee: Int
-      $type_service_personne_handicapee: Int
-      $type_ehpad: Int
-      $type_autre_etablissement_personne_agee: Int
-      $type_chrs: Int
-      $type_service_hospitalier_soins_longue_duree: Int
-      $type_service_psychiatrique: Int
-      $type_autre_service: Int
+      $etablissement_personne_handicapee: Int
+      $service_personne_handicapee: Int
+      $ehpad: Int
+      $autre_etablissement_personne_agee: Int
+      $chrs: Int
+      $service_hospitalier_soins_longue_duree: Int
+      $service_psychiatrique: Int
+      $autre_service: Int
     ) {
       update_enquete_reponses_populations_by_pk(
         pk_columns: { id: $id }
@@ -47,14 +47,14 @@ function buildEnquetePopulationMutation(prefix) {
           ${prefix}_anciennete_3_5_ans: $anciennete_3_5_ans
           ${prefix}_anciennete_5_10_ans: $anciennete_5_10_ans
           ${prefix}_anciennete_sup_10_ans: $anciennete_sup_10_ans
-          ${prefix}_etablissement_personne_handicapee: $type_etablissement_personne_handicapee
-          ${prefix}_service_personne_handicapee: $type_service_personne_handicapee
-          ${prefix}_ehpad: $type_ehpad
-          ${prefix}_autre_etablissement_personne_agee: $type_autre_etablissement_personne_agee
-          ${prefix}_chrs: $type_chrs
-          ${prefix}_service_hospitalier_soins_longue_duree: $type_service_hospitalier_soins_longue_duree
-          ${prefix}_service_psychiatrique: $type_service_psychiatrique
-          ${prefix}_autre_service: $type_autre_service
+          ${prefix}_etablissement_personne_handicapee: $etablissement_personne_handicapee
+          ${prefix}_service_personne_handicapee: $service_personne_handicapee
+          ${prefix}_ehpad: $ehpad
+          ${prefix}_autre_etablissement_personne_agee: $autre_etablissement_personne_agee
+          ${prefix}_chrs: $chrs
+          ${prefix}_service_hospitalier_soins_longue_duree: $service_hospitalier_soins_longue_duree
+          ${prefix}_service_psychiatrique: $service_psychiatrique
+          ${prefix}_autre_service: $autre_service
         }
       ) {
         id
