e7eb118a4003f581041393414ff393c2c5812fc9
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-07-02 11:17:11 +0200

    feat(admin-service): display service members

--- packages/app/src/components/AdminServices/AdminServiceInformations.js
+++ packages/app/src/components/AdminServices/AdminServiceInformations.js
@@ -22,7 +22,7 @@ const AdminServiceInformations = (props) => {
   }
 
   const [service] = data.services;
-  const { id, etablissement, code_postal, ville, service_tis } = service;
+  const { id, etablissement, code_postal, ville, service_tis, service_members } = service;
 
   return (
     <Card>
@@ -56,6 +56,21 @@ const AdminServiceInformations = (props) => {
         </Box>
         <Box width={2 / 3} px={4} py={2}>
           <Text>{ville}</Text>
+        </Box>
+      </Flex>
+      <Flex mb={4}>
+        <Box width={1 / 3} p={2} bg="cardSecondary">
+          Membres du service
+        </Box>
+        <Box width={2 / 3} px={4} py={2}>
+          {service_members &&
+            service_members.map(({ user }) => {
+              return (
+                <Flex mb={10} alignItems="center" justifyContent="space-between" key={user.id}>
+                  <Text>{`${user.nom} ${user.prenom} > #${user.id} > ${user.email}`}</Text>
+                </Flex>
+              );
+            })}
         </Box>
       </Flex>
       <Flex mb={4}>
