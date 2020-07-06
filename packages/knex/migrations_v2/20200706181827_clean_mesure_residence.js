0e462faaa899dc054c196e344cb0997b34140320
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-07-06 18:20:51 +0200

    feat(schema): add migration to clean residence

---
+++ packages/knex/migrations_v2/20200706181827_clean_mesure_residence.js
@@ -0,0 +1,13 @@
+exports.up = async function (knex) {
+  return knex.raw(`
+  update mesures set residence = 'en établissement' where residence = 'ehpad';
+  update mesures set residence = null where residence = 'non renseigné';
+  update mesures set residence = 'en établissement' where residence = 'en t�ablissement';
+  update mesures set residence = null where residence = 'autres';
+  update mesures set residence = 'en établissement' where residence = 'en étbablissement';
+  update mesures set residence = 'en établissement' where residence = 'en �tablissemn';
+  update mesures set residence = 'en établissement' where residence = 'epsm';
+    `);
+};
+
+exports.down = async function () {};
