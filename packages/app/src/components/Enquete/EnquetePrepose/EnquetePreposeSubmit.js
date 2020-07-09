3a908a80aa66242e80c6fdcc433956870a3b986e
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-07-09 13:31:40 +0200

    fix(enquete): reload enquete after submission (#2068)

--- packages/app/src/components/Enquete/EnquetePrepose/EnquetePreposeSubmit.js
+++ packages/app/src/components/Enquete/EnquetePrepose/EnquetePreposeSubmit.js
@@ -17,6 +17,12 @@ export const EnquetePreposeSubmit = (props) => {
         variables: { enqueteId, userId },
       },
     ],
+    onCompleted: () => {
+      // TODO: it must be changed, but actually SUBMIT_ENQUETE_REPONSE
+      // use useSubscription method with graphql query 🤷
+      // The problem is refetchQueries doesn't works with useSubscription.
+      document.location.reload(true);
+    },
   });
 
   return (
