ab9862fd1b696203b91aad326bcc605d4fca0b74
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-07-13 15:48:30 +0200

    feat(ui): delete ununsed files

--- packages/ui/src/components/MesureList/context/index.js
+++
@@ -1,5 +0,0 @@
-export {
-  Context as MesureContext,
-  Provider as MesureContextProvider,
-  Consumer as MesureContextConsumer,
-} from './MesureContext';
--- packages/ui/src/components/MesureList/index.js
+++
@@ -1,5 +0,0 @@
-import { MESURE_TYPE, PANEL_TYPE } from './constants/type';
-import { MesureContext, MesureContextConsumer, MesureContextProvider } from './context';
-import { Mesure } from './Mesure';
-
-export { Mesure, MesureContext, MesureContextConsumer, MesureContextProvider, MESURE_TYPE, PANEL_TYPE };
