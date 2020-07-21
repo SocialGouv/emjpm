c2dc6a02276a7e27c4fde86ea23ebf5f7664d20a
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-07-21 14:08:03 +0200

    fix(liste-blanche): allow admin edit liste blanche (#2085)

---
+++ packages/core/src/constants/DIRECTION.const.js
@@ -0,0 +1,9 @@
+import { buildKeys } from './keysBuilder.service';
+
+export const DIRECTION = {
+  DIRECTION_TYPE: buildKeys({
+    departemental: 'Direction départementale',
+    national: 'Direction nationale',
+    regional: 'Direction régionale',
+  }),
+};
