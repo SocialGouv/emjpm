f02a83f0164d13e2e5b20e433f8d51e1a9650647
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-07-22 15:24:47 +0200

    fix(mesure-map): display only active mesures

--- packages/app/src/components/ServiceMap/queries.js
+++ packages/app/src/components/ServiceMap/queries.js
@@ -2,7 +2,7 @@ import gql from "graphql-tag";
 
 export const MESURES_SERVICE = gql`
   query MesureGestionnaire {
-    mesures {
+    mesures(where: { status: { _eq: "en_cours" } }) {
       id
       code_postal
       longitude
