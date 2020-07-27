8ac5ff1a123d39e13625322594c91281d51146e3
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-07-27 12:24:04 +0200

    feat(admin-user): display direction type (#2097)

--- packages/core/src/services/users.service.js
+++ packages/core/src/services/users.service.js
@@ -2,6 +2,18 @@ export function isAdmin(user) {
   return user.type === 'admin';
 }
 
+export function isIndividuel(user) {
+  return user.type === 'individuel';
+}
+
+export function isPrepose(user) {
+  return user.type === 'prepose';
+}
+
+export function isDirection(user) {
+  return user.type === 'direction';
+}
+
 export function isDirectionNationale(user) {
   return user.user_roles.map((ur) => ur.role.name).includes('direction_nationale');
 }
