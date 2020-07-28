5b97b0320547f609255b2e38c45b713fc7e9a046
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-07-28 09:53:19 +0200

    feat(direction): display administration info in profil page

---
+++ packages/core/src/formatters/direction.formatter.js
@@ -0,0 +1,16 @@
+import { isDirectionTypeDepartemental, isDirectionTypeNational, isDirectionTypeRegional } from '../services';
+
+export const directionFormatter = {
+  formatAdministrationInfo(direction) {
+    if (isDirectionTypeNational(direction)) {
+      return 'DGCS';
+    }
+    if (isDirectionTypeRegional(direction)) {
+      return `Direction régionale (${direction.region ? direction.region.nom : ''})`;
+    }
+    if (isDirectionTypeDepartemental(direction)) {
+      return `Direction départementale (${direction.departement ? direction.departement.nom : ''})`;
+    }
+    return '-';
+  },
+};
--- packages/core/src/formatters/index.js
+++ packages/core/src/formatters/index.js
@@ -1,1 +1,2 @@
 export * from './mesure.formatter.js';
+export * from './direction.formatter.js';
