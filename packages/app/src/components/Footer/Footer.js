0f505b20d8288b588bd65c1d3e4984ffb3fa1dde
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-07-30 09:47:49 +0200

    feat(politique-confidentialite): add politique confidentialite page (#2105)

--- packages/app/src/components/Footer/Footer.js
+++ packages/app/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 import { Box, Flex } from "rebass";
 
 import { Link, List, ListItem, ListTitle } from "../Commons";
-import { FooterFlexStyle, FooterItemStyle, FooterWrapperStyle } from "./style";
+import { FooterItemStyle, FooterWrapperStyle } from "./style";
 
 const { publicRuntimeConfig } = getConfig();
 
@@ -13,15 +13,24 @@ function Footer(props) {
   return (
     <Box mt={hasMarginTop ? 7 : 0} sx={FooterWrapperStyle}>
       <BoxWrapper>
-        <Flex sx={FooterFlexStyle}>
+        <Flex
+          flexWrap="wrap"
+          justifyContent="space-between"
+          flexDirection={["column", "column", "row"]}
+        >
           <Box mb={[3, 0]} flexBasis={["100%", "13%"]}>
             <Logo hasTitle={false} />
           </Box>
           <List sx={FooterItemStyle}>
-            <ListTitle>A propos de nous</ListTitle>
+            <ListTitle>eMJPM</ListTitle>
             <ListItem>
               <Link target="_blank" href="/mentions-legales">
                 Mentions légales
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link target="_blank" href="/politique-confidentialite">
+                {`Politique de confidentialité`}
               </Link>
             </ListItem>
             <ListItem>
@@ -29,25 +38,49 @@ function Footer(props) {
                 {`Conditions d'utilisation`}
               </Link>
             </ListItem>
-          </List>
-          <List sx={FooterItemStyle}>
-            <ListTitle>Contactez-nous</ListTitle>
-            <ListItem>
-              <Link target="_blank" href="mailto:support.emjpm@fabrique.social.gouv.fr">
-                Par email
-              </Link>
-            </ListItem>
-          </List>
-          <List sx={FooterItemStyle}>
-            <ListTitle>Aide et ressources</ListTitle>
-            <ListItem>
-              <Link target="_blank" href="https://github.com/SocialGouv/emjpm">
-                Version {publicRuntimeConfig.PACKAGE_VERSION}
-              </Link>
-            </ListItem>
             <ListItem>
               <Link target="_blank" href="/stats">
                 Statistique de connexion
+              </Link>
+            </ListItem>
+          </List>
+          <List sx={FooterItemStyle}>
+            <ListTitle>Aidez-nous à améliorer cet outil</ListTitle>
+            <ListItem>
+              <Link target="_blank" href="mailto:support.emjpm@fabrique.social.gouv.fr">
+                Contact
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link
+                target="_blank"
+                href={`https://github.com/SocialGouv/emjpm/tree/${publicRuntimeConfig.PACKAGE_VERSION}`}
+              >
+                Contribuer sur GitHub
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link
+                target="_blank"
+                href={`https://github.com/SocialGouv/emjpm/releases/tag/${publicRuntimeConfig.PACKAGE_VERSION}`}
+              >
+                Journal des modifications
+              </Link>
+            </ListItem>
+          </List>
+          <List sx={FooterItemStyle}>
+            <ListTitle>En collaboration avec</ListTitle>
+            <ListItem>
+              <Link
+                target="_blank"
+                href="https://solidarites-sante.gouv.fr/ministere/organisation/organisation-des-directions-et-services/article/organisation-de-la-direction-generale-de-la-cohesion-sociale-dgcs"
+              >
+                {`La Direction Générale de la Cohésion Sociale`}
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Link target="_blank" href="https://www.fabrique.social.gouv.fr/">
+                {`La fabrique numérique des ministères sociaux`}
               </Link>
             </ListItem>
           </List>
