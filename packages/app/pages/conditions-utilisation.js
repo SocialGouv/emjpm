42d962e7f304fc4ccca0d63b9456d27a04f526c4
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-07-30 09:47:49 +0200

    feat(politique-confidentialite): add politique confidentialite page (#2105)

--- packages/app/pages/conditions-utilisation.js
+++ packages/app/pages/conditions-utilisation.js
@@ -5,20 +5,10 @@ import { Card } from "rebass";
 
 import { ConditionsUtilisation } from "../src/components/ConditionsUtilisation";
 import { LayoutPublic } from "../src/components/Layout";
-import { A, H1, H2, H3, List, ListItem, Paragraph } from "../src/components/MdxComponents";
-
-const components = {
-  a: A,
-  h1: H1,
-  h2: H2,
-  h3: H3,
-  li: ListItem,
-  p: Paragraph,
-  ul: List,
-};
+import { mdxComponents } from "../src/components/MdxComponents";
 
 const ConditionsUtilistionPage = () => (
-  <MDXProvider components={components}>
+  <MDXProvider components={mdxComponents}>
     <LayoutPublic>
       <FlexWrapper p="4" my="50px">
         <Card p={3}>
