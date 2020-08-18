39334d3d961a94a69589f675a9ae83cf956cdd19
Rémi Roycourt <remiroycourt@gmail.com>
2020-08-18 11:14:40 +0200

    feat(api): add checks for mesure creation

--- packages/api/src/routes/api/editors.js
+++ packages/api/src/routes/api/editors.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { param, body, check } = require("express-validator");
 const { MESURE_PROTECTION } = require("@emjpm/core");
+const isBefore = require("date-fns/isBefore");
+const parseISO = require("date-fns/parseISO");
 
 const {
   mesures,
@@ -23,7 +25,55 @@ router.post(
     body("numero_rg").not().isEmpty().trim().escape(),
     body("annee_naissance").not().isEmpty().trim().escape(),
     body("civilite").isIn(MESURE_PROTECTION.CIVILITE.keys),
-    body("date_nomination").isDate().toDate(),
+    body("date_nomination")
+      .custom((value, { req }) => {
+        if (value) {
+          if (
+            req.body.date_premier_mesure &&
+            isBefore(
+              parseISO(`${value}T00:00:00.000Z`),
+              parseISO(`${req.body.date_premier_mesure}T00:00:00.000Z`)
+            )
+          ) {
+            throw new Error(
+              "date_nomination must be after or equivalent to date_premier_mesure"
+            );
+          }
+
+          if (
+            req.body.date_protection_en_cours &&
+            isBefore(
+              parseISO(`${req.body.date_protection_en_cours}T00:00:00.000Z`),
+              parseISO(`${value}T00:00:00.000Z`)
+            )
+          ) {
+            throw new Error(
+              "date_nomination must be before or equivalent to date_protection_en_cours"
+            );
+          }
+        }
+
+        if (req.body.etats && req.body.etats.length) {
+          const lastEtat = req.body.etats[req.body.etats.length - 1];
+          if (lastEtat) {
+            if (
+              lastEtat.date_changement_etat &&
+              isBefore(
+                parseISO(`${value}T00:00:00.000Z`),
+                parseISO(`${lastEtat.date_changement_etat}T00:00:00.000Z`)
+              )
+            ) {
+              throw new Error(
+                "date_nomination must be after or equivalent to date_changement_etat"
+              );
+            }
+          }
+        }
+
+        return value;
+      })
+      .isDate()
+      .toDate(),
     body("date_fin_mesure").optional().isDate().toDate(),
     body("date_premier_mesure").optional().isDate().toDate(),
     body("date_protection_en_cours").optional().isDate().toDate(),
