4428a2259359e0a2b5670cb851f9f93105e5ce74
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2020-09-07 17:59:52 +0200

    feat(api): mesure dates (#2179)
    * feat(api): mesure dates
    
    * fix(direction): 'useEffect' is defined but never used

--- packages/api/src/controllers/editor/mesure.js
+++ packages/api/src/controllers/editor/mesure.js
@@ -1,4 +1,5 @@
 const { Mesure } = require("../../models/Mesure");
+const { sanitizeMesureProperties } = require("../../utils/mesure");
 
 const mesure = async (req, res) => {
   const {
@@ -14,7 +15,8 @@ const mesure = async (req, res) => {
     if (!mesure) {
       return res.status(404).end();
     }
-    return res.status(200).json(mesure);
+
+    return res.status(200).json(sanitizeMesureProperties(mesure));
   } catch (err) {
     return res.status(400).json({
       errors: [{ msg: "oups, something goes wrong. please contact support." }],
