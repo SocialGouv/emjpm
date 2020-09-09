7c117473202e26a3f7c1b86ea8168146b2d3ba0e
Rémi Roycourt <remiroycourt@gmail.com>
2020-09-09 15:48:08 +0200

    feat(api): apply type_etablissement modifications

--- packages/core/src/constants/MESURE_PROTECTION.const.js
+++ packages/core/src/constants/MESURE_PROTECTION.const.js
@@ -8,25 +8,21 @@ export const MESURE_PROTECTION = {
     dessaisissement_famille: 'dessaisissement en faveur de la famille',
     mainlevee: 'mainlevée',
   }),
-
   CHAMP_MESURE: buildKeys({
     protection_bien: 'aux biens',
     protection_bien_personne: 'aux biens et à la personne',
     protection_personne: 'à la personne',
   }),
-
   CIVILITE: buildKeys({
     madame: 'Madame',
     monsieur: 'Monsieur',
   }),
-
   LIEU_VIE_MAJEUR: buildKeys({
     domicile: 'domicile',
     etablissement: 'en établissement',
     etablissement_conservation_domicile: 'en établissement avec conservation du domicile',
     sdf: 'SDF',
   }),
-
   NATURE_MESURE: buildKeys({
     curatelle_renforcee: 'curatelle renforcée',
     curatelle_simple: 'curatelle simple',
@@ -38,7 +34,6 @@ export const MESURE_PROTECTION = {
     subroge_tuteur: 'subrogé tuteur',
     tutelle: 'tutelle',
   }),
-
   RESULTAT_REVISION: buildKeys({
     aggravation: 'aggravation',
     allegement: 'allègement',
@@ -55,5 +50,13 @@ export const MESURE_PROTECTION = {
     en_attente: 'Mesure en attente',
     en_cours: 'Mesure en cours',
     eteinte: 'Mesure éteinte',
+  }),
+  TYPE_ETABLISSEMENT: buildKeys({
+    autre_etablissement: 'Autre établissement',
+    autre_etablissement_s_ms: 'Autre établissement social ou médico-social',
+    etablissement_handicapes: 'Etablissement ou service pour handicapés',
+    etablissement_hospitalier: 'Etablissement hospitalier',
+    etablissement_personne_agee: 'Etablissement pour personnes agées',
+    etablissement_psychiatrique: 'Etablissement psychiatrique',
   }),
 };
