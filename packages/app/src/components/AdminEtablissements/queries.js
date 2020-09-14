9e62811557abb5a7cdc1fa12faaeee63914b339b
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-09-14 16:16:37 +0200

    chore(lint): format code

--- packages/app/src/components/AdminEtablissements/queries.js
+++ packages/app/src/components/AdminEtablissements/queries.js
@@ -1,7 +1,12 @@
 import gql from "graphql-tag";
 
 export const ETABLISSEMENTS = gql`
-  query etablissements($search: String, $departementCode: String, $limit: Int = 100, $offset: Int = 0) {
+  query etablissements(
+    $search: String
+    $departementCode: String
+    $limit: Int = 100
+    $offset: Int = 0
+  ) {
     etablissements_aggregate {
       aggregate {
         count
