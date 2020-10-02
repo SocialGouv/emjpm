84f5015c9a5ef1f74f283ddefc972e25024eab92
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-10-02 11:14:56 +0200

    chore(lint): make lint happy

--- packages/app/src/components/Enquete/constants/ENQ_REP_AGREMENTS_FORMATIONS.const.js
+++ packages/app/src/components/Enquete/constants/ENQ_REP_AGREMENTS_FORMATIONS.const.js
@@ -3,10 +3,10 @@ import { enqueteKeysBuilder } from "./enqueteKeysBuilder.service";
 // constantes synchronisées avec le fichier api/ENQ_REP_AGREMENTS_FORMATIONS.const.js
 export const ENQ_REP_AGREMENTS_FORMATIONS = {
   NB_DEPARTEMENTS: enqueteKeysBuilder.buildKeys({
-    "1": "1",
-    "2": "2",
-    "3": "3",
-    "4": "4",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
     "5+": "Plus de 5",
   }),
 };
