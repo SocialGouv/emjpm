195c4a11e78ee2fedd526acf54fbbbf2f76cda62
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-10-02 11:14:56 +0200

    chore(lint): make lint happy

--- packages/api/src/routes/hasura-actions/enquete/common/constants/ENQ_REP_AGREMENTS_FORMATIONS.const.js
+++ packages/api/src/routes/hasura-actions/enquete/common/constants/ENQ_REP_AGREMENTS_FORMATIONS.const.js
@@ -3,10 +3,10 @@ const enqueteKeysBuilder = require("./enqueteKeysBuilder.service");
 // constantes synchronisées avec le fichier app/ENQ_REP_AGREMENTS_FORMATIONS.const.js
 const ENQ_REP_AGREMENTS_FORMATIONS = {
   NB_DEPARTEMENTS: enqueteKeysBuilder.buildKeys({
-    "1": "1",
-    "2": "2",
-    "3": "3",
-    "4": "4",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
     "5+": "Plus de 5",
   }),
 };
