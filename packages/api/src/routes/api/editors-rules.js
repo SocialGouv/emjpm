9c5c6cef94d315a20d463c5d182454cb2bd1748d
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-10-05 16:00:51 +0200

    feat(api): implement batch creation properly (#2238)
    * feat(api): refactor
    
    * feat(api): implement properly batch creation

--- packages/api/src/routes/api/editors-rules.js
+++ packages/api/src/routes/api/editors-rules.js
@@ -8,55 +8,7 @@ const rules = [
   body("annee_naissance").not().isEmpty().trim().escape(),
   body("civilite").isIn(MESURE_PROTECTION.CIVILITE.keys),
 
-  body("date_nomination")
-    .custom((value, { req }) => {
-      if (value) {
-        if (
-          req.body.date_premier_mesure &&
-          isBefore(
-            parseISO(`${value}T00:00:00.000Z`),
-            parseISO(`${req.body.date_premier_mesure}T00:00:00.000Z`)
-          )
-        ) {
-          throw new Error(
-            "date_nomination must be after or equivalent to date_premier_mesure"
-          );
-        }
-
-        if (
-          req.body.date_protection_en_cours &&
-          isBefore(
-            parseISO(`${req.body.date_protection_en_cours}T00:00:00.000Z`),
-            parseISO(`${value}T00:00:00.000Z`)
-          )
-        ) {
-          throw new Error(
-            "date_nomination must be before or equivalent to date_protection_en_cours"
-          );
-        }
-      }
-
-      if (req.body.etats && req.body.etats.length) {
-        const lastEtat = req.body.etats[req.body.etats.length - 1];
-        if (lastEtat) {
-          if (
-            lastEtat.date_changement_etat &&
-            isBefore(
-              parseISO(`${lastEtat.date_changement_etat}T00:00:00.000Z`),
-              parseISO(`${value}T00:00:00.000Z`)
-            )
-          ) {
-            throw new Error(
-              "date_nomination must be before or equivalent to date_changement_etat"
-            );
-          }
-        }
-      }
-
-      return value;
-    })
-    .isDate()
-    .toDate(),
+  body("date_nomination").custom(checkDateNomination).isDate().toDate(),
   body("date_fin_mesure").optional().isDate().toDate(),
   body("date_premier_mesure").optional().isDate().toDate(),
   body("date_protection_en_cours").optional().isDate().toDate(),
@@ -70,49 +22,131 @@ const rules = [
   check("etats.*.date_changement_etat").isDate().toDate(),
   check("etats.*.nature_mesure").isIn(MESURE_PROTECTION.NATURE_MESURE.keys),
   check("etats.*.lieu_vie").isIn(MESURE_PROTECTION.LIEU_VIE_MAJEUR.keys),
+  body("etats.*").custom(checkTypeEtablissement),
+  body("etats.*.champ_mesure").custom(checkChampMesure),
+];
 
-  body("etats.*").custom((value) => {
-    if (
-      value.lieu_vie === "etablissement" ||
-      value.lieu_vie === "etablissement_conservation_domicile"
-    ) {
-      if (!value.type_etablissement) {
-        throw new Error("type_etablissement is required");
-      } else if (
-        !MESURE_PROTECTION.TYPE_ETABLISSEMENT.keys.includes(
-          value.type_etablissement
-        )
-      ) {
-        throw new Error("type_etablissement is not valid");
-      }
-    }
-    return true;
-  }),
+const batchRules = [
+  body("mesures.*.numero_rg").not().isEmpty().trim().escape(),
+  body("mesures.*.annee_naissance").not().isEmpty().trim().escape(),
+  body("mesures.*.civilite").isIn(MESURE_PROTECTION.CIVILITE.keys),
 
-  body("etats.*.champ_mesure").custom((value, { req }) => {
-    if (req.body.etats && req.body.etats.length) {
-      const { nature_mesure } = req.body.etats[req.body.etats.length - 1];
-      if (
-        (nature_mesure === "curatelle_simple" ||
-          nature_mesure === "curatelle_renforcee" ||
-          nature_mesure === "curatelle_renforcee") &&
-        !value
-      ) {
-        throw new Error("champ_mesure is required");
-      }
-
-      if (value && !MESURE_PROTECTION.CHAMP_MESURE.keys.includes(value)) {
-        throw new Error(
-          `champ_mesure must be equal to ${MESURE_PROTECTION.CHAMP_MESURE.keys.join(
-            " or "
-          )}`
-        );
-      }
-    }
-    return true;
-  }),
+  body("mesures.*.date_nomination")
+    .custom(checkDateNomination)
+    .isDate()
+    .toDate(),
+  body("mesures.*.date_fin_mesure").optional().isDate().toDate(),
+  body("mesures.*.date_premier_mesure").optional().isDate().toDate(),
+  body("mesures.*.date_protection_en_cours").optional().isDate().toDate(),
+  body("mesures.*.tribunal_siret").not().isEmpty().trim().escape(),
+  body("mesures.*.antenne_id").optional().toInt(10),
+  body("mesures.*.cause_sortie")
+    .optional()
+    .isIn(MESURE_PROTECTION.CAUSE_SORTIE.keys),
+  body("mesures.*.resultat_revision")
+    .optional()
+    .isIn(MESURE_PROTECTION.RESULTAT_REVISION.keys),
+  check("mesures.*.etats.*.pays").isISO31661Alpha2(),
+  check("mesures.*.etats.*.date_changement_etat").isDate().toDate(),
+  check("mesures.*.etats.*.nature_mesure").isIn(
+    MESURE_PROTECTION.NATURE_MESURE.keys
+  ),
+  check("mesures.*.etats.*.lieu_vie").isIn(
+    MESURE_PROTECTION.LIEU_VIE_MAJEUR.keys
+  ),
+  body("mesures.*.etats.*").custom(checkTypeEtablissement),
+  body("mesures.*.etats.*.champ_mesure").custom(checkChampMesure),
 ];
 
 module.exports = {
   rules,
+  batchRules,
 };
+
+function checkChampMesure(value, { req }) {
+  if (req.body.etats && req.body.etats.length) {
+    const { nature_mesure } = req.body.etats[req.body.etats.length - 1];
+    if (
+      (nature_mesure === "curatelle_simple" ||
+        nature_mesure === "curatelle_renforcee" ||
+        nature_mesure === "curatelle_renforcee") &&
+      !value
+    ) {
+      throw new Error("champ_mesure is required");
+    }
+
+    if (value && !MESURE_PROTECTION.CHAMP_MESURE.keys.includes(value)) {
+      throw new Error(
+        `champ_mesure must be equal to ${MESURE_PROTECTION.CHAMP_MESURE.keys.join(
+          " or "
+        )}`
+      );
+    }
+  }
+  return true;
+}
+
+function checkTypeEtablissement(value) {
+  if (
+    value.lieu_vie === "etablissement" ||
+    value.lieu_vie === "etablissement_conservation_domicile"
+  ) {
+    if (!value.type_etablissement) {
+      throw new Error("type_etablissement is required");
+    } else if (
+      !MESURE_PROTECTION.TYPE_ETABLISSEMENT.keys.includes(
+        value.type_etablissement
+      )
+    ) {
+      throw new Error("type_etablissement is not valid");
+    }
+  }
+  return true;
+}
+
+function checkDateNomination(value, { req }) {
+  if (value) {
+    if (
+      req.body.date_premier_mesure &&
+      isBefore(
+        parseISO(`${value}T00:00:00.000Z`),
+        parseISO(`${req.body.date_premier_mesure}T00:00:00.000Z`)
+      )
+    ) {
+      throw new Error(
+        "date_nomination must be after or equivalent to date_premier_mesure"
+      );
+    }
+
+    if (
+      req.body.date_protection_en_cours &&
+      isBefore(
+        parseISO(`${req.body.date_protection_en_cours}T00:00:00.000Z`),
+        parseISO(`${value}T00:00:00.000Z`)
+      )
+    ) {
+      throw new Error(
+        "date_nomination must be before or equivalent to date_protection_en_cours"
+      );
+    }
+  }
+
+  if (req.body.etats && req.body.etats.length) {
+    const lastEtat = req.body.etats[req.body.etats.length - 1];
+    if (lastEtat) {
+      if (
+        lastEtat.date_changement_etat &&
+        isBefore(
+          parseISO(`${lastEtat.date_changement_etat}T00:00:00.000Z`),
+          parseISO(`${value}T00:00:00.000Z`)
+        )
+      ) {
+        throw new Error(
+          "date_nomination must be before or equivalent to date_changement_etat"
+        );
+      }
+    }
+  }
+
+  return value;
+}
