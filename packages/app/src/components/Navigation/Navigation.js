ac5d0916eef4e3817c199b6102328ee5fb9d10df
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-10-19 21:02:25 +0200

    fix(navigation): test user type

--- packages/app/src/components/Navigation/Navigation.js
+++ packages/app/src/components/Navigation/Navigation.js
@@ -1,14 +1,33 @@
 import React, { useContext } from "react";
 import { Box, Flex } from "rebass";
 
+import { isMandataire } from "../../util";
 import { UserContext } from "../UserContext";
 import Link from "./Link";
+
+const MandataireBadge = ({ mandataire }) => {
+  const { mesures_en_cours, dispo_max } = mandataire;
+  return (
+    <Box
+      sx={{
+        color: "white",
+        bg: mesures_en_cours > dispo_max ? "error" : "primary",
+        px: 1,
+        mb: 2,
+        ml: 3,
+        borderRadius: 9999,
+      }}
+      variant="badge"
+    >
+      {mesures_en_cours} / {dispo_max} mesures
+    </Box>
+  );
+};
 
 export const Navigation = (props) => {
   const { links } = props;
   const user = useContext(UserContext);
-  const { mandataire } = user;
-  const { dispo_max, mesures_en_cours } = mandataire;
+  const { type } = user;
   return (
     <Box mt="2">
       <Flex alignItems="center" flexWrap="wrap">
@@ -19,19 +38,7 @@ export const Navigation = (props) => {
             </Link>
           </Box>
         ))}
-        <Box
-          sx={{
-            color: "white",
-            bg: mesures_en_cours > dispo_max ? "error" : "primary",
-            px: 1,
-            mb: 2,
-            ml: 3,
-            borderRadius: 9999,
-          }}
-          variant="badge"
-        >
-          {mesures_en_cours} / {dispo_max} mesures
-        </Box>
+        {isMandataire(type) && <MandataireBadge mandataire={user.mandataire} />}
       </Flex>
     </Box>
   );
