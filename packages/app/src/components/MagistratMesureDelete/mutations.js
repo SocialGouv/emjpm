52a929bd2e5613706db03e33783fa1a87dadf604
pom421 <mauguet.po@gmail.com>
2020-10-23 16:46:28 +0200

    feat(delete-mesure): remove email-cancel-reservation event in action (#2261)
    Remove email-cancel-reservation event in Hasura action delete_mesure
    Rebase with origin/master

--- packages/app/src/components/MagistratMesureDelete/mutations.js
+++ packages/app/src/components/MagistratMesureDelete/mutations.js
@@ -1,10 +1,8 @@
 import gql from "graphql-tag";
 
-export const DELETE_MANDATAIRE_MESURE = gql`
-  mutation deleteMesure($id: Int!, $mandataire_id: Int) {
-    delete_mesures(where: { id: { _eq: $id } }) {
-      affected_rows
-    }
+export const DELETE_MESURE = gql`
+  mutation delete_mesure_action($mesure_id: Int!) {
+    delete_mesure_action(mesure_id: $mesure_id)
   }
 `;
 
@@ -13,14 +11,6 @@ export const CALCULATE_MESURES = gql`
     calculate_mesures(mandataireId: $mandataireId, serviceId: $serviceId) {
       success
       updatedRows
-    }
-  }
-`;
-
-export const DELETE_SERVICE_MESURE = gql`
-  mutation deleteMesure($id: Int!, $service_id: Int!) {
-    delete_mesures(where: { id: { _eq: $id } }) {
-      affected_rows
     }
   }
 `;
