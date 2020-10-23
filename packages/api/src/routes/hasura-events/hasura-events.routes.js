9582dff1cf3c66ea4ca26175c27352f74a70c947
pom421 <mauguet.po@gmail.com>
2020-10-23 16:19:34 +0200

    feat(delete-mesure): remove email-cancel-reservation event in action
    Remove email-cancel-reservation event in Hasura action delete_mesure
    Rebase with origin/master

--- packages/api/src/routes/hasura-events/hasura-events.routes.js
+++ packages/api/src/routes/hasura-events/hasura-events.routes.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const uid = require("rand-token").uid;
-const { isEnAttente } = require("@emjpm/core");
 const { Service } = require("../../models/Service");
 const {
   ServiceMemberInvitation,
 } = require("../../models/ServiceMemberInvitation");
-const { Tis } = require("../../models/Tis");
-const { reservationEmail } = require("../../email/reservation-email");
-const {
-  cancelReservationEmail,
-} = require("../../email/cancel-reservation-email");
 const { validationEmail } = require("../../email/validation-email");
 const {
   serviceMemberInvitationMail,
 } = require("../../email/service-member-invitation-mail");
-const { getEmailUserDatas } = require("../../email/email-user-data");
 const {
   editorConfirmationEmail,
 } = require("../../email/editor-confirmation-email");
 const {
   editorConfirmationAdminEmail,
 } = require("../../email/editor-confirmation-admin-email");
+
 // ----------------------------------
 // -------EMAIL ACCOUNT VALIDATION---
 // ----------------------------------
@@ -47,52 +41,6 @@ router.post("/email-account-validation", function (req, res) {
   const oldUser = req.body.event.data.old;
   if (newUser.active && !oldUser.active) {
     validationEmail(newUser.email, `${process.env.APP_URL}`);
-  }
-  res.json({ success: true });
-});
-
-// ----------------------------------
-// ------------- EMAIL RESERVATION---
-// ----------------------------------
-
-router.post("/email-reservation", async function (req, res) {
-  const mesure = req.body.event.data.new;
-  const { ti_id, service_id, mandataire_id, status } = mesure;
-
-  if (isEnAttente({ status })) {
-    const ti = await Tis.query().findById(ti_id);
-    const users = await getEmailUserDatas(mandataire_id, service_id);
-    const emails = users.map((user) => reservationEmail(ti, mesure, user));
-
-    await Promise.all(emails);
-  }
-
-  res.json({ success: true });
-});
-
-// -----------------------------------------
-// ------------- EMAIL CANCEL RESERVATION---
-// -----------------------------------------
-
-router.post("/email-cancel-reservation", async function (req, res) {
-  const sessionVars = req.body.event.session_variables;
-  if (sessionVars) {
-    const role = sessionVars["x-hasura-role"];
-    const userId = sessionVars["x-hasura-user-id"];
-
-    const isTi = role && role === "ti";
-
-    if (isTi && userId) {
-      const oldMesure = req.body.event.data.old;
-      const { ti_id, service_id, mandataire_id, status } = oldMesure;
-      if (isEnAttente({ status })) {
-        const [currentTi] = await Tis.query().where("id", ti_id);
-        const users = await getEmailUserDatas(mandataire_id, service_id);
-        for (const user of users) {
-          cancelReservationEmail(currentTi, oldMesure, user);
-        }
-      }
-    }
   }
   res.json({ success: true });
 });
