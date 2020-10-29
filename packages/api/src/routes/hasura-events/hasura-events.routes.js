b9857dd811aab17f8bc647ac18527fc22f3b3652
pom421 <mauguet.po@gmail.com>
2020-10-29 20:20:53 +0100

    feat(email-service-member-invitation): transform event in Hasura action (#2266)
    * feat(email-service-member-invitation): transform event in Hasura action
    feat: new
    
    * chore(event): remove unnecessary event anymore

--- packages/api/src/routes/hasura-events/hasura-events.routes.js
+++ packages/api/src/routes/hasura-events/hasura-events.routes.js
@@ -1,12 +1,4 @@
 const express = require("express");
-const uid = require("rand-token").uid;
-const { Service } = require("../../models/Service");
-const {
-  ServiceMemberInvitation,
-} = require("../../models/ServiceMemberInvitation");
-const {
-  serviceMemberInvitationMail,
-} = require("../../email/service-member-invitation-mail");
 const {
   editorConfirmationEmail,
 } = require("../../email/editor-confirmation-email");
@@ -32,26 +24,6 @@ router.post("/token-request", function (req, res) {
   } = req;
   editorConfirmationEmail(email);
   editorConfirmationAdminEmail(email, name);
-  res.json({ success: true });
-});
-
-router.post("/email-service-member-invitation", async function (req, res) {
-  const invitation = req.body.event.data.new;
-
-  const serviceMemberInvitation = await ServiceMemberInvitation.query().patchAndFetchById(
-    invitation.id,
-    {
-      sent_at: new Date(),
-      token: uid(32),
-    }
-  );
-
-  const service = await Service.query().findById(
-    serviceMemberInvitation.service_id
-  );
-
-  serviceMemberInvitationMail(serviceMemberInvitation, service);
-
   res.json({ success: true });
 });
 
