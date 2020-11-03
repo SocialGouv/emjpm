ebd705d1a1c4a480e6213277c839d5f496237fae
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-11-03 09:35:53 +0100

    chore(test): remove old tests (#2286)

--- packages/app/src/components/DirectionInformations/DirectionInformations.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-
-import { DirectionInformations } from "./DirectionInformations";
-
-it("renders correctly", () => {
-  const tree = renderer.create(<DirectionInformations />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
