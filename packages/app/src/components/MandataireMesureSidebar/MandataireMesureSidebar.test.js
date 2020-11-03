9d686434b25f4cc40b4b54d7f583da659e48d048
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-11-03 09:35:53 +0100

    chore(test): remove old tests (#2286)

--- packages/app/src/components/MandataireMesureSidebar/MandataireMesureSidebar.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-
-import { MandataireMesureSidebar } from "./MandataireMesureSidebar";
-
-it("renders correctly", () => {
-  const tree = renderer.create(<MandataireMesureSidebar />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
--- packages/app/src/components/ServiceMesureSidebar/ServiceMesureSidebar.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-
-import { ServiceMesureSidebar } from "./ServiceMesureSidebar";
-
-it("renders correctly", () => {
-  const tree = renderer.create(<ServiceMesureSidebar />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
