9c8e5ee32d4575fe44ca8abb38efce6b20eb76b6
Douglas Duteil <douglasduteil@gmail.com>
2020-11-04 10:26:56 +0100

    chore(core): remove dev dep

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,12 +1,12 @@
+import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import babel from "rollup-plugin-babel";
 
 import pkg from "./package.json";
 
 export default [
   {
-    external: [...Object.keys(pkg.peerDependencies || {})],
+    external: [...Object.keys(pkg.peerDependencies || {}), /@babel\/runtime/],
     input: "src/index.js",
     output: [
       { file: pkg.main, format: "cjs", sourcemap: true },
@@ -15,7 +15,10 @@ export default [
     plugins: [
       resolve({ mainFields: ["module", "js:next", "main"] }),
       commonjs({ exclude: "src/**" }),
-      babel({ exclude: "node_modules/**" }),
+      babel({
+        babelHelpers: "runtime",
+        exclude: "node_modules/**",
+      }),
     ],
     watch: {
       clearScreen: false,
