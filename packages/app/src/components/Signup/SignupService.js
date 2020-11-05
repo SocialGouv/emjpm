b76378e3df5c4481bb8e0101a254d73ff6115da6
pom421 <mauguet.po@gmail.com>
2020-11-05 17:22:35 +0100

    refactor(signup): use of FormGroupSelect & FormGroupInput

--- packages/app/src/components/Signup/SignupService.js
+++ packages/app/src/components/Signup/SignupService.js
@@ -1,15 +1,6 @@
 import { useQuery } from "@apollo/react-hooks";
 import { getDepartementCode } from "@emjpm/core";
-import {
-  Button,
-  Card,
-  Field,
-  Heading1,
-  Heading4,
-  InlineError,
-  Select,
-  Text,
-} from "@emjpm/ui";
+import { Button, Card, Heading1, Heading4, Text } from "@emjpm/ui";
 import { useFormik } from "formik";
 import Link from "next/link";
 import Router from "next/router";
@@ -17,6 +8,8 @@ import React, { Fragment, useContext } from "react";
 import { Box, Flex } from "rebass";
 
 import { signupServiceSchema } from "../../lib/validationSchemas";
+import { toOptions } from "../../util";
+import { FormGroupSelect } from "../AppForm";
 import { SignupContext } from "./context";
 import { DEPARTMENTS } from "./queries";
 import signup from "./signup";
@@ -25,25 +18,24 @@ import { SignupGeneralError } from "./SignupGeneralError";
 import { cardStyle, grayBox } from "./style";
 
 function getServiceOptions(services, departments, selectedValue) {
-  const department = departments.find((data) => data.id === selectedValue);
+  const department = departments.find(
+    (department) => department.id === selectedValue
+  );
   if (!department) {
     return [];
   }
 
   const { code } = department;
-  return services
-    .filter(({ code_postal }) => {
-      // Permet de gérer les code postaux invalides en base de données
-      try {
-        return getDepartementCode(code_postal) === code;
-      } catch (err) {
-        return false;
-      }
-    })
-    .map(({ etablissement, id }) => ({
-      label: etablissement,
-      value: id,
-    }));
+  const compatibleServices = services.filter(({ code_postal }) => {
+    // Permet de gérer les code postaux invalides en base de données
+    try {
+      return getDepartementCode(code_postal) === code;
+    } catch (err) {
+      return false;
+    }
+  });
+
+  return toOptions(compatibleServices, "id", "etablissement");
 }
 
 const SignupServiceForm = ({ serviceDatas }) => {
@@ -54,13 +46,13 @@ const SignupServiceForm = ({ serviceDatas }) => {
 
   const formik = useFormik({
     initialValues: {
-      departement: service ? service.departement : null,
-      service: service ? service.service : null,
+      departement: service?.departement || null,
+      service: service?.service || null,
     },
     onSubmit: (values, { setSubmitting, setErrors }) => {
       const body = {
         service: {
-          service_id: values.service.value,
+          service_id: values.service,
         },
         user: {
           username: user.email,
@@ -82,18 +74,14 @@ const SignupServiceForm = ({ serviceDatas }) => {
   }
 
   const { departements: departments = [] } = data;
-  const selectedValue = formik.values.departement
-    ? formik.values.departement.value
-    : undefined;
+
   const serviceOptions = getServiceOptions(
     serviceDatas,
     departments,
-    selectedValue
+    formik.values.departement
   );
-  const departmentsOptions = departments.map(({ nom, id }) => ({
-    label: nom,
-    value: id,
-  }));
+
+  const departmentsOptions = toOptions(departments, "id", "nom");
 
   return (
     <Fragment>
@@ -111,44 +99,19 @@ const SignupServiceForm = ({ serviceDatas }) => {
           <Box p="5" pb={0} mb="4" width={[1, 3 / 5]}>
             <form onSubmit={formik.handleSubmit}>
               <SignupGeneralError errors={formik.errors} />
-              <Field>
-                <Select
-                  id="departement"
-                  name="departement"
-                  placeholder="Département de votre service"
-                  value={formik.values.departement}
-                  hasError={
-                    formik.errors.departement && formik.touched.departement
-                  }
-                  onChange={(option) =>
-                    formik.setFieldValue("departement", option)
-                  }
-                  options={departmentsOptions}
-                />
-                {formik.touched.departement && (
-                  <InlineError
-                    message={formik.errors.departement}
-                    fieldId="departement"
-                  />
-                )}
-              </Field>
-              <Field>
-                <Select
-                  id="service"
-                  name="service"
-                  placeholder="Votre service"
-                  value={formik.values.service}
-                  hasError={formik.errors.service && formik.touched.service}
-                  onChange={(option) => formik.setFieldValue("service", option)}
-                  options={serviceOptions}
-                />
-                {formik.touched.service && (
-                  <InlineError
-                    message={formik.errors.service}
-                    fieldId="service"
-                  />
-                )}
-              </Field>
+              <FormGroupSelect
+                id="departement"
+                formik={formik}
+                placeholder="Département de votre service"
+                options={departmentsOptions}
+              />
+              <FormGroupSelect
+                id="service"
+                formik={formik}
+                placeholder="Votre service"
+                options={serviceOptions}
+                isClearable={true}
+              />
               <Flex justifyContent="flex-end">
                 <Box>
                   <Link href="/">
@@ -162,7 +125,7 @@ const SignupServiceForm = ({ serviceDatas }) => {
                     mr="2"
                     variant="outline"
                     onClick={() => {
-                      setService(formik.values);
+                      setService(null);
                       validateStepOne(false);
                     }}
                   >
