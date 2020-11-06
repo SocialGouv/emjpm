0588f5e96328a159e9e02f538a467c347a18defb
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-11-06 15:20:21 +0100

    refactor(signup): format form

--- packages/app/src/lib/validationSchemas/signupDirectionSchema.js
+++ packages/app/src/lib/validationSchemas/signupDirectionSchema.js
@@ -1,7 +1,15 @@
 import yup from "./yup";
 
 const signupDirectionSchema = yup.object().shape({
+  departement: yup.number().nullable().when("directionType", {
+    is: "departemental",
+    then: yup.number().required(),
+  }),
   directionType: yup.string().required(),
+  region: yup.number().nullable().when("directionType", {
+    is: "regional",
+    then: yup.number().required(),
+  }),
 });
 
 export { signupDirectionSchema };
