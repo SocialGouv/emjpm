99af551e16eb8cc33aedb382d16a781e5b0e9f6d
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-11-07 10:03:26 +0100

    chore(test): update test database

--- .k8s/postgres/restore/configmap/anonymize.sql
+++ .k8s/postgres/restore/configmap/anonymize.sql
@@ -11,5 +11,9 @@ update tis set telephone = '0140506070';
 update mesures set numero_dossier = null, annee_naissance = 1968;
 update users set username = concat("type",'-',id, '@justice.fr'), nom = "type", prenom = 'Paula', email = concat("type",'-',id, '@justice.fr');
 update users set password = '$2a$10$AAuEwAMCl3AUuOE3bXhzS.IWOx6Y1SPlScWcVxhW.nyNMXVL.Q1lO';
-update lb_users set nom = concat("type",'-',id), prenom = 'Paula', email = concat("type",'-',id, '@justice.fr');
+update lb_users set nom = concat("type",'-',id)
+  , prenom = 'Paula'
+  , email = concat("type",'-',id, '@justice.fr')
+  , adresse1 = '37 Quai de Grenelle'
+  , adresse2 = null;
 
