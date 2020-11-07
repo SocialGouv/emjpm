d088f996a3d464439ff80af0c2acd7f9014e6ef9
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-11-07 12:02:25 +0100

    chore(clean): delete unused code

--- packages/app/src/components/MesureCreateOrEdit/queries.js
+++ packages/app/src/components/MesureCreateOrEdit/queries.js
@@ -1,15 +1,5 @@
 import gql from "graphql-tag";
 
-export const DEPARTEMENTS = gql`
-  {
-    departements {
-      id
-      code
-      nom
-    }
-  }
-`;
-
 export const SERVICE_TRIBUNAL = gql`
   query ServiceTribunal {
     tribunaux: service_tis(order_by: { ti: { ville: asc } }) {
