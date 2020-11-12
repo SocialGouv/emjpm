3165a88fffbc873fc74f275da0125cd3165dacab
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-11-12 09:27:31 +0100

    feat(etablissement): search by name and city

--- packages/app/src/components/ListeBlanche/ListeBlanchePrepose/queries.js
+++ packages/app/src/components/ListeBlanche/ListeBlanchePrepose/queries.js
@@ -2,7 +2,15 @@ import gql from "graphql-tag";
 
 export const ETABLISSEMENTS = gql`
   query etablissements($input: String!) {
-    etablissements(where: { rslongue: { _ilike: $input } }, limit: 50) {
+    etablissements(
+      where: {
+        _or: [
+          { rslongue: { _ilike: $input } }
+          { ligneacheminement: { _ilike: $input } }
+        ]
+      }
+      limit: 50
+    ) {
       id
       ligneacheminement
       rslongue
