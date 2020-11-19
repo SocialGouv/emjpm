c4a66920821191791ef4a4b96ec9fabc5cfa4d0d
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-11-19 14:59:04 +0100

    fix(email-unicity): add check email unicity action

---
+++ packages/api/src/routes/hasura-actions/user/hasura-actions.users.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const { User } = require("../../../models/User");
+
+const router = express.Router();
+
+router.post("/check-email-exists", async (req, res) => {
+  const { email } = req.body.input;
+
+  const users = await User.query().where({ email });
+
+  // success
+  return res.json({
+    exist: users.length > 0,
+  });
+});
+
+module.exports = router;
--- packages/app/src/query-service/EmailQueryService.js
+++ packages/app/src/query-service/EmailQueryService.js
@@ -1,22 +1,20 @@
 import gql from "graphql-tag";
 
 const CHECK_EMAIL_UNICITY = gql`
-  {
-    users {
-      email
+  query checkEmailExists($email: String!) {
+    check_email_exists(email: $email) {
+      exist
     }
   }
 `;
 
 export const isEmailExists = async (client, email) => {
   const checkEmail = await client.query({
-    context: {
-      headers: {
-        "X-Hasura-Email": email,
-      },
-    },
     fetchPolicy: "network-only",
     query: CHECK_EMAIL_UNICITY,
+    variables: {
+      email,
+    },
   });
-  return checkEmail.data.users.length > 0;
+  return checkEmail.data.check_email_exists.exist;
 };
