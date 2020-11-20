d330decc075fb1430c6a50ea38814ac85141d126
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-11-20 12:59:04 +0100

    fix(geocode): add custom geocode validation (#2309)

--- packages/app/src/lib/validationSchemas/adminTribunalSchema.js
+++ packages/app/src/lib/validationSchemas/adminTribunalSchema.js
@@ -1,9 +1,14 @@
-import yup from "./yup";
+import { validateGeocode } from "./fieldValidators";
+import yup, { FORM_REQUIRED_MESSAGE } from "./yup";
 
 const adminTribunalSchema = yup.object().shape({
   email: yup.string().email(),
   etablissement: yup.string().required(),
-  geocode: yup.object().nullable().required(),
+  geocode: yup
+    .object()
+    .required()
+    .nullable()
+    .test("geocode-check", FORM_REQUIRED_MESSAGE, validateGeocode),
   siret: yup.string().required(),
   telephone: yup.string(),
 });
