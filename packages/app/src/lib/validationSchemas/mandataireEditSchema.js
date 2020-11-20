845b42e6ccfba8061b955127fc578f4ecc2c5349
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-11-20 12:59:04 +0100

    fix(geocode): add custom geocode validation (#2309)

--- packages/app/src/lib/validationSchemas/mandataireEditSchema.js
+++ packages/app/src/lib/validationSchemas/mandataireEditSchema.js
@@ -1,11 +1,15 @@
-import yup from "./yup";
+import { validateGeocode } from "./fieldValidators";
+import yup, { FORM_REQUIRED_MESSAGE } from "./yup";
 
 const mandataireEditSchema = yup.object().shape({
   competences: yup.string(),
   dispo_max: yup.number().required(),
   email: yup.string().email().required(),
   genre: yup.string().required(),
-  geocode: yup.object().nullable().required(),
+  geocode: yup
+    .object()
+    .required()
+    .test("geocode-check", FORM_REQUIRED_MESSAGE, validateGeocode),
   nom: yup.string().required(),
   prenom: yup.string().required(),
   telephone: yup.string().required(),
