aa07e03fdda55fadfc342a82cc9a6625ca8070ae
Thomas Glatt <47273014+tglatt@users.noreply.github.com>
2020-11-29 08:31:57 +0100

    feat(mandataire): add board page (#2315)

---
+++ packages/app/pages/mandataires/mesures.js
@@ -0,0 +1,49 @@
+import { BoxWrapper, Heading1 } from "@emjpm/ui";
+import React from "react";
+import { Flex } from "rebass";
+
+import { LayoutMandataire } from "../../src/components/Layout";
+import { MesureList } from "../../src/components/MesureList";
+import { MesureListButtonBar } from "../../src/components/MesureListButtonBar";
+import { MesureListFilters } from "../../src/components/MesureListFilters";
+import { FiltersContextProvider } from "../../src/components/MesureListFilters/context";
+import {
+  DEFAULT_MESURE_NATURE,
+  MESURE_STATUS_LABEL_VALUE,
+} from "../../src/constants/mesures";
+import { withAuthSync } from "../../src/util/auth";
+
+const MesuresListView = () => {
+  return (
+    <FiltersContextProvider
+      initialValues={{
+        mesureStatus: MESURE_STATUS_LABEL_VALUE[0],
+        natureMesure: DEFAULT_MESURE_NATURE,
+      }}
+    >
+      <LayoutMandataire>
+        <BoxWrapper mt={6} px="1">
+          <Flex flexDirection="row" justifyContent="space-between">
+            <Heading1>Toutes vos mesures</Heading1>
+            <MesureListButtonBar />
+          </Flex>
+          <MesureListFilters />
+          <Flex
+            sx={{
+              flexWrap: "wrap",
+              mt: "2",
+            }}
+          >
+            <MesureList />
+          </Flex>
+        </BoxWrapper>
+      </LayoutMandataire>
+    </FiltersContextProvider>
+  );
+};
+
+const MandataireMesuresList = () => {
+  return <MesuresListView />;
+};
+
+export default withAuthSync(MandataireMesuresList);
---
+++ packages/app/src/components/MandataireBoard/MandataireBoard.js
@@ -0,0 +1,97 @@
+import { Heading4, Text } from "@emjpm/ui";
+import React, { useContext } from "react";
+import { Box, Card, Flex } from "rebass";
+
+import { Link } from "../Commons";
+import { UserContext } from "../UserContext";
+
+const MandataireBoard = () => {
+  const { mandataire } = useContext(UserContext);
+  const {
+    mesures_en_cours = 0,
+    mesures_en_attente = 0,
+    dispo_max = 0,
+    mandataire_tis: mandataireTis,
+    lb_user: lbUser = {},
+  } = mandataire;
+  return (
+    <Box>
+      <Flex p={1} flexDirection="column" width="300px">
+        <Box bg="cardSecondary">
+          <Box p={1}>
+            <Heading4>Vos indicateurs</Heading4>
+          </Box>
+        </Box>
+        <Card>
+          <Box>
+            <Flex px={1} mb={1}>
+              <Text fontWeight="bold">{dispo_max}</Text>
+              <Link href="/mandataires/edit-informations">
+                <Text ml={1}>{`mesures souhaitées`}</Text>
+              </Link>
+            </Flex>
+            <Flex px={1} mb={1}>
+              <Text fontWeight="bold">{mesures_en_cours}</Text>
+              <Link href="/mandataires/mesures">
+                <Text ml={1}>{`mesures en cours`}</Text>
+              </Link>
+            </Flex>
+
+            <Flex px={1} mb={1}>
+              <Text fontWeight="bold">{mesures_en_attente}</Text>
+              <Link href="/mandataires/mesures">
+                <Text ml={1}>{`mesures en attente`}</Text>
+              </Link>
+            </Flex>
+          </Box>
+        </Card>
+      </Flex>
+
+      <Flex p={1} flexDirection="column" width="300px">
+        <Box bg="cardSecondary">
+          <Box p={1}>
+            <Heading4>Vos agréments</Heading4>
+          </Box>
+        </Box>
+        <Card>
+          {lbUser.lb_departements.map(
+            ({ departement, departement_financeur }) => {
+              return (
+                <Flex px={1} mb={1} key={departement.id} alignItems="center">
+                  <Text>{departement.nom} </Text>
+                  {departement_financeur && (
+                    <Text ml={1} fontSize="12px" fontWeight="bold">
+                      {"département financeur"}
+                    </Text>
+                  )}
+                </Flex>
+              );
+            }
+          )}
+        </Card>
+      </Flex>
+
+      <Flex p={1} flexDirection="column" width="300px">
+        <Box bg="cardSecondary">
+          <Box p={1}>
+            <Heading4>Vos tribunaux</Heading4>
+          </Box>
+        </Box>
+        <Card>
+          {mandataireTis.map(({ ti }) => {
+            return (
+              <Text px={1} mb={1} key={ti.id}>
+                {ti.etablissement}
+              </Text>
+            );
+          })}
+          <Flex justifyContent="flex-end">
+            <Link href="/mandataires/edit-informations">{`modifier`}</Link>
+          </Flex>
+        </Card>
+      </Flex>
+    </Box>
+  );
+};
+
+export { MandataireBoard };
