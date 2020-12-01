90994a554dbeb4908a008b354de264f1e5ed426c
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-12-01 12:01:29 +0100

    fix(service): edit informations

--- packages/app/pages/services/edit-informations.js
+++ packages/app/pages/services/edit-informations.js
@@ -1,17 +1,22 @@
 import { BoxWrapper, Heading1 } from "@emjpm/ui";
-import React from "react";
+import React, { useContext } from "react";
 
 import { LayoutServices } from "../../src/components/Layout";
 import { ServiceEditInformations } from "../../src/components/ServiceEditInformations";
+import { UserContext } from "../../src/components/UserContext";
 import { withAuthSync } from "../../src/util/auth";
 
 const EditInformations = () => {
+  const {
+    service: { id: serviceId },
+  } = useContext(UserContext);
   return (
     <LayoutServices>
       <BoxWrapper mt={6}>
         <Heading1 mx="1">Editer les informations de votre service</Heading1>
         <ServiceEditInformations
           mt="3"
+          serviceId={serviceId}
           cancelLink="/services/informations"
           successLink="/services/informations"
         />
