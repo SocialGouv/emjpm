6fd202d0e8847ddecde8bf242e0f3544b4966883
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-12-08 16:59:45 +0100

    feat(oauth): post authorization form automatically

--- packages/app/src/components/AccessToken/queries.js
+++ packages/app/src/components/AccessToken/queries.js
@@ -4,11 +4,13 @@ export const USER_TOKEN = gql`
   query AccessToken($userId: Int) {
     access_tokens(where: { user_id: { _eq: $userId } }) {
       access_token
+      refresh_token
       editor_id
       editor_url
       id
       user_id
       editors {
+        id
         name
       }
     }
