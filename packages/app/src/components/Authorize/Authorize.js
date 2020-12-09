18aff73d569c49e89574433a9bb619cd64493dbd
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-12-09 15:16:50 +0100

    fix(oauth): test query result

--- packages/app/src/components/Authorize/Authorize.js
+++ packages/app/src/components/Authorize/Authorize.js
@@ -21,7 +21,7 @@ const Authorize = (props) => {
   });
 
   useEffect(() => {
-    const { accessTokens = [] } = data;
+    const { accessTokens = [] } = data || {};
     if (accessTokens?.length > 0) {
       const form = document.forms[0];
       form.submit();
@@ -39,32 +39,47 @@ const Authorize = (props) => {
 
   return (
     <Card mt="5" p="0">
-      <Box bg="cardSecondary" borderRadius="5px 0 0 5px" p="5">
-        <Box>
-          <Heading4 mb="1">{`Autoriser ${editor.name} à accéder à votre compte eMJPM.`}</Heading4>
-          <Text lineHeight="1.5" color="textSecondary">
-            {`Vos informations eMJPM seront accessibles par ce dernier pour faciliter l'échange et la fluidité des services.`}
-          </Text>
-        </Box>
-      </Box>
-      <Box p="5">
-        <form method="post" action={url}>
-          <input type="hidden" name="response_type" value="code" />
-          <input type="hidden" name="client_id" value={editorId} />
-          <input type="hidden" name="redirect_uri" value={redirectUrl} />
-          <input type="hidden" name="access_token" value={token} />
-          <input type="hidden" name="state" value={state} />
-          <Button type="submit">{`Autoriser`}</Button>
-          <Button
-            onClick={() => {
-              document.location.href = `${redirectUrl}?error_reason=user_denied&error=access_denied&error_description=Permissions+error`;
-            }}
-            type="button"
-            ml="2"
-            variant="outline"
-          >{`refuser`}</Button>
-        </form>
-      </Box>
+      {editor && (
+        <>
+          <Box bg="cardSecondary" borderRadius="5px 0 0 5px" p="5">
+            <Box>
+              <Heading4 mb="1">{`Autoriser ${editor.name} à accéder à votre compte eMJPM.`}</Heading4>
+              <Text lineHeight="1.5" color="textSecondary">
+                {`Vos informations eMJPM seront accessibles par ce dernier pour faciliter l'échange et la fluidité des services.`}
+              </Text>
+            </Box>
+          </Box>
+          <Box p="5">
+            <form method="post" action={url}>
+              <input type="hidden" name="response_type" value="code" />
+              <input type="hidden" name="client_id" value={editorId} />
+              <input type="hidden" name="redirect_uri" value={redirectUrl} />
+              <input type="hidden" name="access_token" value={token} />
+              <input type="hidden" name="state" value={state} />
+              <Button type="submit">{`Autoriser`}</Button>
+              <Button
+                onClick={() => {
+                  document.location.href = `${redirectUrl}?error_reason=user_denied&error=access_denied&error_description=Permissions+error`;
+                }}
+                type="button"
+                ml="2"
+                variant="outline"
+              >{`refuser`}</Button>
+            </form>
+          </Box>
+        </>
+      )}
+      {!editor && (
+        <>
+          <Box bg="cardSecondary" borderRadius="5px 0 0 5px" p="5">
+            <Box>
+              <Text lineHeight="1.5" color="textSecondary">
+                {`Aucun client ne correspond aux informations transmises.`}
+              </Text>
+            </Box>
+          </Box>
+        </>
+      )}
     </Card>
   );
 };
