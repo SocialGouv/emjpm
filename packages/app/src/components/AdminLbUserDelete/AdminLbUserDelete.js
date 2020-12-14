2efa167c6a3f0844ba8c3c6fdda9e72621bce670
devthejo <jo@surikat.pro>
2020-12-14 19:21:37 +0100

    feat(admin-lb-delete): admin list-blanche delete mandataire
    fix #2320

---
+++ packages/app/src/components/AdminLbUserDelete/AdminLbUserDelete.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Flex } from "rebass";
+
+import { AdminLbUserDeleteForm } from "./AdminLbUserDeleteForm";
+import { AdminLbUserDeleteRemoveStyle } from "./style";
+
+export const AdminLbUserDelete = (props) => {
+  const { lbUserId } = props;
+
+  return (
+    <Flex sx={AdminLbUserDeleteRemoveStyle}>
+      <AdminLbUserDeleteForm lbUserId={lbUserId} />
+    </Flex>
+  );
+};
