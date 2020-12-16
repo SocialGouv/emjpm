0eff8f8c842cf5b3c53a905274cd26e83c6e8935
Jo <jo@surikat.pro>
2020-12-16 20:00:36 +0100

    chore(clean): remove 3rdpart deps (#2380)
    * chore(clean): remove 3rdpart deps #2366
    
    * chore(clean): bringback tarteaucitron without hotjar

--- packages/app/public/static/tarteaucitron/initTarteaucitron.js
+++ packages/app/public/static/tarteaucitron/initTarteaucitron.js
@@ -30,8 +30,5 @@ tarteaucitron.init({
 
 tarteaucitron.user.matomoId = 13;
 tarteaucitron.user.matomoHost = "https://matomo.fabrique.social.gouv.fr/";
-tarteaucitron.user.hotjarId = 1871650;
-tarteaucitron.user.HotjarSv = 6;
 
 (tarteaucitron.job = tarteaucitron.job || []).push("matomo");
-(tarteaucitron.job = tarteaucitron.job || []).push("hotjar");
