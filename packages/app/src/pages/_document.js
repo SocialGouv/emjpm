c5f319bb02ddc47f0ff5dd4b9fb95b07ecafa800
devthejo <jo@surikat.pro>
2020-12-16 19:00:44 +0100

    chore(clean): remove 3rdpart deps #2366

--- packages/app/src/pages/_document.js
+++ packages/app/src/pages/_document.js
@@ -8,11 +8,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* TODO : Remove me in favor of self hosted */}
-          <link
-            href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Quicksand:400,700&display=swap"
-            rel="stylesheet"
-          />
+          <link rel="stylesheet" href="/static/css/font-open-sans.css" />
+          <link rel="stylesheet" href="/static/css/font-quicksand.css" />
           <meta name="description" content="" />
           <meta name="author" content="" />
           <style>{`
@@ -31,17 +28,9 @@ export default class MyDocument extends Document {
             href="https://api.tiles.mapbox.com/mapbox-gl-js/v1.4.1/mapbox-gl.css"
             rel="stylesheet"
           />
-          <link rel="stylesheet" href="/static/css/font.css" />
+          <link rel="stylesheet" href="/static/css/font-source-sans-pro.css" />
           <link rel="stylesheet" href="/static/css/pagination.css" />
           {this.props.styleTags}
-          <script
-            type="text/javascript"
-            src="/static/tarteaucitron/tarteaucitron.js"
-          />
-          <script
-            type="text/javascript"
-            src="/static/tarteaucitron/initTarteaucitron.js"
-          />
         </Head>
         <body>
           {/* @socialgouv global style */}
