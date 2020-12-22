6b31580b75009389017473c3d0e50eda03867dbc
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2020-12-22 13:17:29 +0100

    fix(jwt-invalid): logout and clear apollo store

--- packages/app/src/components/UserContext/UserContext.js
+++ packages/app/src/components/UserContext/UserContext.js
@@ -1,8 +1,9 @@
-import { useQuery } from "@apollo/react-hooks";
+import { useApolloClient, useQuery } from "@apollo/react-hooks";
 import { isService } from "@emjpm/biz";
 import React, { createContext, Fragment } from "react";
 
-import { setUser } from "~/util/sentry";
+import { captureException, setUser } from "~/util/sentry";
+import { logout } from "~/util/auth";
 
 import {
   ADMIN_USERS,
@@ -24,12 +25,18 @@ const QUERY_TYPE = {
 };
 
 const UserProvider = (props) => {
+  const apolloClient = useApolloClient();
   const { type, userId, agrements, children } = props;
   const { data } = useQuery(QUERY_TYPE[type], {
     onCompleted: ({ users_by_pk: user }) => {
       if (user) {
         setUser({ id: user.id, role: type });
       }
+    },
+    onError: (error) => {
+      captureException(error);
+      apolloClient.clearStore();
+      logout();
     },
     ssr: false,
     variables: {
