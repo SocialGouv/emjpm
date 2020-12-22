8997b2955201ee0ccdbf6247f3edfdda75fc5a4e
devthejo <jo@surikat.pro>
2020-12-22 04:28:10 +0100

    refactor(gitlab-ci): fix

--- .gitlab-ci/env.sh
+++ .gitlab-ci/env.sh
@@ -58,10 +58,10 @@ else
 fi
 
 export API_HOST="api-${DOMAIN}";
-export API_SRV_HOST="api-api";
+export API_SRV_HOST="api";
 export FRONTEND_HOST="${DOMAIN}";
 export HASURA_HOST="hasura-${DOMAIN}";
-export HASURA_SRV_HOST="hasura-hasura";
+export HASURA_SRV_HOST="hasura";
 
 
 export API_URL="https://${API_HOST}"
