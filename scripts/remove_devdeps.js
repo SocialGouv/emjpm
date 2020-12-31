9a8750763a7f604f9a97890faa636371ae76f0bb
devthejo <jo@surikat.pro>
2020-12-31 13:45:20 +0100

    refactor(ci): improve dev/prod isomorphism

--- scripts/remove_devdeps.js
+++
@@ -1,6 +0,0 @@
-const {resolve} = require('path');
-const [, ,file] = process.argv;
-const pkgFile = resolve(file);
-const pkg = require(pkgFile);
-delete pkg.devDependencies;
-require("fs").writeFileSync(pkgFile, JSON.stringify(pkg, null, 2));
