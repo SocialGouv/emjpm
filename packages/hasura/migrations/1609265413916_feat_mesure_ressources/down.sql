2b466fa1e77c47d1c6bf33e3b1eb590207969bbe
Jo <jo@surikat.pro>
2021-01-02 00:22:52 +0100

    improvement(ci): faster multistage build + psql helper script (#2422)
    * improvement(ci): faster multistage build
    
    * fix(yaml): wip
    
    * improvement(ci): auto bootstrap on dev
    
    * fix(ci): pull
    
    * fix(ci): collapsed multi-line command
    
    * fix(ci): wip
    
    * fix(ci): wip
    
    * improvement(ci): decouple build and register stages
    
    * fix(ci): shm_size
    
    * fix(ci): shm_size
    
    * fix(ci): stage
    
    * fix(ci): wip
    
    * fix(ci): wip
    
    * fix(hasura-metadata): seed to migration
    
    * fix(hasura-metadata): add unique constraint
    
    * feat(scripts): socialgouv-psql

--- packages/hasura/migrations/1609265413916_feat_mesure_ressources/down.sql
+++ packages/hasura/migrations/1609265413916_feat_mesure_ressources/down.sql
@@ -1,3 +1,4 @@
+ALTER TABLE "public"."mesure_ressources_prestations_sociales" DROP CONSTRAINT "mesure_ressources_prestations_sociales_mesure_ressources_id_prestations_sociales_key";
 
 ALTER TABLE "public"."mesure_ressources" ALTER COLUMN "niveau_ressource" TYPE numeric;
 
--- packages/hasura/seeds/1609180902066_enum_prestation_sociales.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO public.prestations_sociales (value) VALUES ('AAH');
-INSERT INTO public.prestations_sociales (value) VALUES ('PCH');
-INSERT INTO public.prestations_sociales (value) VALUES ('ASI');
-INSERT INTO public.prestations_sociales (value) VALUES ('RSA');
-INSERT INTO public.prestations_sociales (value) VALUES ('ALS');
-INSERT INTO public.prestations_sociales (value) VALUES ('APL');
-INSERT INTO public.prestations_sociales (value) VALUES ('ASPA');
-INSERT INTO public.prestations_sociales (value) VALUES ('APA');
