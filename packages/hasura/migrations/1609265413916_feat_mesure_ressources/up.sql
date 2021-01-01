a53a833e3dbf7017a81ce41f30d6b20cdec71bf6
devthejo <jo@surikat.pro>
2021-01-01 13:09:38 +0100

    fix(hasura-metadata): add unique constraint

--- packages/hasura/migrations/1609265413916_feat_mesure_ressources/up.sql
+++ packages/hasura/migrations/1609265413916_feat_mesure_ressources/up.sql
@@ -1,10 +1,12 @@
 
 
+
 CREATE TABLE "public"."prestations_sociales"("value" text NOT NULL, PRIMARY KEY ("value") , UNIQUE ("value"));
 
 ALTER TABLE "public"."mesure_ressources" DROP COLUMN "prestations_sociales" CASCADE;
 
 CREATE TABLE "public"."mesure_ressources_prestations_sociales"("id" serial NOT NULL, "mesure_ressources_id" integer NOT NULL, "prestations_sociales" text NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("mesure_ressources_id") REFERENCES "public"."mesure_ressources"("id") ON UPDATE cascade ON DELETE cascade, FOREIGN KEY ("prestations_sociales") REFERENCES "public"."prestations_sociales"("value") ON UPDATE restrict ON DELETE restrict, UNIQUE ("mesure_ressources_id", "prestations_sociales"));
+ALTER TABLE "public"."mesure_ressources_prestations_sociales" ADD CONSTRAINT "mesure_ressources_prestations_sociales_mesure_ressources_id_prestations_sociales_key" UNIQUE ("mesure_ressources_id", "prestations_sociales");
 
 ALTER TABLE "public"."mesure_ressources" ALTER COLUMN "niveau_ressource" TYPE numeric;
 
@@ -16,3 +18,4 @@ INSERT INTO public.prestations_sociales (value) VALUES ('ALS');
 INSERT INTO public.prestations_sociales (value) VALUES ('APL');
 INSERT INTO public.prestations_sociales (value) VALUES ('ASPA');
 INSERT INTO public.prestations_sociales (value) VALUES ('APA');
+
