98da509660a28205020419a7941a429bcc43a827
devthejo <jo@surikat.pro>
2021-01-09 03:35:08 +0100

    fix(prod-config): env

--- packages/app/src/config/config.js
+++ packages/app/src/config/config.js
@@ -1,10 +1,10 @@
 const config = {
-  API_URL: process.env.API_URL || "http://127.0.0.1:4000",
+  API_URL: process.env.REACT_APP_API_URL || "http://127.0.0.1:4000",
   GRAPHQL_SERVER_URI:
-    process.env.GRAPHQL_SERVER_URI || "http://localhost:5000/v1/graphql",
+    process.env.REACT_APP_GRAPHQL_SERVER_URI ||
+    "http://localhost:5000/v1/graphql",
   NODE_ENV: process.env.NODE_ENV,
-  PACKAGE_VERSION: process.env.VERSION || require("../../package.json").version,
-  SENTRY_PUBLIC_DSN: process.env.SENTRY_PUBLIC_DSN,
+  PACKAGE_VERSION: require("../../package.json").version,
+  SENTRY_PUBLIC_DSN: process.env.REACT_APP_SENTRY_PUBLIC_DSN,
 };
-
 export default config;
