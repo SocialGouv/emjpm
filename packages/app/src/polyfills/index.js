e803a4bb2a5a2e599dfa441878f6348e06af786e
devthejo <jo@surikat.pro>
2021-01-12 00:41:39 +0100

    fix(ci): tests

--- packages/app/src/polyfills/index.js
+++ packages/app/src/polyfills/index.js
@@ -1,2 +1,1 @@
-import "./polyfills";
 import "./readAsBinaryString";
--- packages/app/src/polyfills/polyfills.js
+++
@@ -1,6 +0,0 @@
-import "core-js/features/array/includes";
-import "core-js/features/array/find";
-import "core-js/features/object/values";
-import "core-js/features/set";
-import "core-js/features/map";
-import "core-js/features/promise";
