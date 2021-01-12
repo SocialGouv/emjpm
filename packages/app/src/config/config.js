1486221c685479b1ecb2ed6cff3d7dcd909664e2
devthejo <jo@surikat.pro>
2021-01-12 22:16:43 +0100

    feat(one-build): url inference

--- packages/app/src/config/config.js
+++ packages/app/src/config/config.js
@@ -1,10 +1,22 @@
 import version from "./version";
 
+const { location } = window;
+const { hostname } = location;
+
+let GRAPHQL_SERVER_URI;
+let API_URL;
+if (hostname == "localhost") {
+  (GRAPHQL_SERVER_URI = "http://localhost:5000/v1/graphql"),
+    (API_URL = "http://127.0.0.1:4000");
+} else {
+  const { protocol } = location;
+  GRAPHQL_SERVER_URI = protocol + "//hasura-" + hostname + "/v1/graphql";
+  API_URL = protocol + "//api-" + hostname;
+}
+
 const config = {
-  API_URL: process.env.REACT_APP_API_URL || "http://127.0.0.1:4000",
-  GRAPHQL_SERVER_URI:
-    process.env.REACT_APP_GRAPHQL_SERVER_URI ||
-    "http://localhost:5000/v1/graphql",
+  API_URL,
+  GRAPHQL_SERVER_URI,
   NODE_ENV: process.env.NODE_ENV,
   PACKAGE_VERSION: version,
   SENTRY_PUBLIC_DSN: process.env.REACT_APP_SENTRY_PUBLIC_DSN,
