ad735248f00fe9a5eea810bf1a4d9859d9711b10
devthejo <jo@surikat.pro>
2021-01-14 03:30:18 +0100

    fix(ci): wip

--- packages/app/src/config/version.js
+++ packages/app/src/config/version.js
@@ -1,6 +1,6 @@
 let version;
 version = require("../../package.json").version;
 if (version === "0.0.0") {
-  version = require("~/config/lerna.ci.json").version;
+  version = require("~/lerna.ci.json").version;
 }
 export default version;
