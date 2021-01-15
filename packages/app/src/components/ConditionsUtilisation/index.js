0e594dc90f25eba5fcdf86f3cffee2a2326ad0b4
devthejo <jo@surikat.pro>
2021-01-15 11:29:06 +0100

    fix(public-docs): markdown

--- packages/app/src/components/ConditionsUtilisation/index.js
+++ packages/app/src/components/ConditionsUtilisation/index.js
@@ -1,3 +1,13 @@
-import ConditionsUtilisation from "./ConditionsUtilisation.md";
+import { lazy, Suspense } from "react";
+import { importMDX } from "mdx.macro";
 
-export { ConditionsUtilisation };
+const Content = lazy(() => importMDX("./ConditionsUtilisation.md"));
+export function ConditionsUtilisation(props) {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Content />
+      </Suspense>
+    </div>
+  );
+}
