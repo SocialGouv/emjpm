3f59249b7325fc7462ba3d7bc127637660b1ec9f
Jo <jo@surikat.pro>
2021-01-29 11:28:05 +0100

    ci(fast): k8s jobs (#2458)
    * ci(k8s-job): wip
    
    * ci(k8s-job): hourra !!!
    
    * ci(k8s-job): webhook chart wip
    
    * ci(k8s-job): working build !
    
    * ci(k8s-job): fix token from env
    
    * ci(k8s-job): remove server from secret
    
    * ci(k8s-job): remove NOTES.txt
    
    * ci(refacto): k8s charts
    
    * ci(k8s-job): wip
    
    * ci(k8s-job): build-args + deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): use harbor registry
    
    * ci(k8s-job): deploy wip
    
    * ci(k8s-job): decouple final and cache registry
    
    * ci(k8s-job): harbor is now working
    
    * ci(k8s-job): fix push path
    
    * ci(k8s-job): wip
    
    * ci(k_s-job): contextList
    
    * ci(k8s-job): fix
    
    * ci(k8s-job): fix
    
    * ci(k8s-job): fix
    
    * ci(k8s-job): fix
    
    * ci(k8s-job): fix
    
    * ci(k8s-job): fix
    
    * ci(k8s-job): fix
    
    * ci(k8s-job): create db
    
    * ci(k8s-job): clean
    
    * remove k8s-ci (now decoupled from emjpm)
    
    * ci(k8s-job): wip
    
    * ci(k8s-job): wip
    
    * ci(k8s-job): wip
    
    * ci(k8s-job): wip
    
    * ci(k8s-job): wip
    
    * wip
    
    * ci(k8s-job): wip
    
    * ci(k8s-job): wip
    
    * ci(k8s-job): wip
    
    * ci(k8s-job): wip
    
    * ci(k8s-job): wip
    
    * ci(k8s-job): wip
    
    * ci(k8s-job): wip
    
    * ci(k8s-job): wip

---
+++ .k8s/k8s-ci-hook/pre-deploy-script.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+### this file will be loaded by git clone $repo from deploy-job
+### it's path is specified in CI_PRE_DEPLOY_SCRIPT var in env.hook.sh
+
+if [ -n "$CI_PRODUCTION" ]; then
+  export RANCHER_CLUSTER_ID="c-lfcxv"
+  export RANCHER_PROJECT_ID="p-ttzld"
+else
+  export RANCHER_CLUSTER_ID="c-bd7z2"
+  export RANCHER_PROJECT_ID="p-57mxc"
+fi
+
+# create namespace if doesn't exists
+if [ -z "$(kubectl --server $K8S_SERVER --token $K8S_TOKEN get namespace $K8S_NS 2>/dev/null)" ]; then
+
+  kubectl \
+    --server $K8S_SERVER \
+    --token $K8S_TOKEN \
+    create -f - <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  annotations:
+    field.cattle.io/projectId: $RANCHER_CLUSTER_ID:$RANCHER_PROJECT_ID
+  labels:
+    cert: wildcard
+    field.cattle.io/projectId: $RANCHER_PROJECT_ID
+  name: $K8S_NS
+EOF
+
+  # copy secrets to new namespace
+  SECRET_NS="${PROJECT}-secret"
+  SECRET_NAME="${PROJECT}-secret"
+  kubectl --server $K8S_SERVER --token $K8S_TOKEN \
+    -n $SECRET_NS get secret $SECRET_NAME -o json \
+    | jq 'del(.metadata["namespace","creationTimestamp","resourceVersion","selfLink","uid"])' \
+    | kubectl --server $K8S_SERVER --token $K8S_TOKEN -n $K8S_NS apply -f -
+
+  # create managed db if doesn't exists
+  DB_SECRET_NS="${PROJECT}-secret"
+  DB_SECRET_NAME="azure-pg-admin-user"
+  export PGHOST=$(kubectl --server $K8S_SERVER --token $K8S_TOKEN \
+    -n $DB_SECRET_NS get secret $DB_SECRET_NAME -ojsonpath='{.data.PGHOST}' \
+    | base64 --decode)
+  export PGUSER=$(kubectl --server $K8S_SERVER --token $K8S_TOKEN \
+    -n $DB_SECRET_NS get secret $DB_SECRET_NAME -ojsonpath='{.data.PGUSER}' \
+    | base64 --decode)
+  export PGPASSWORD=$(kubectl --server $K8S_SERVER --token $K8S_TOKEN \
+    -n $DB_SECRET_NS get secret $DB_SECRET_NAME -ojsonpath='{.data.PGPASSWORD}' \
+    | base64 --decode)
+  DEPLOYMENT_PGUSER=$PROJECT
+  if ! psql -lqt | cut -d \| -f 1 | grep -qw "$DB_NAME"; then
+    psql -v ON_ERROR_STOP=1 postgres <<EOF
+      CREATE DATABASE "${DB_NAME}";
+      \c "${DB_NAME}"
+
+      CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+      GRANT CONNECT ON DATABASE "${DB_NAME}" TO ${DEPLOYMENT_PGUSER};
+      GRANT ALL PRIVILEGES ON DATABASE "${DB_NAME}" TO ${DEPLOYMENT_PGUSER};
+EOF
+
+  fi
+fi
