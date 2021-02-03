538d35ffdb0baf77640cb6409ae2d21e9afa9cf4
devthejo <jo@surikat.pro>
2021-02-03 18:28:02 +0100

    refactor(departement): options from js

--- packages/app/src/util/geodata/index.js
+++ packages/app/src/util/geodata/index.js
@@ -3,6 +3,22 @@ import departements from "./departements.json";
 
 const codePostalDB = import("./code_postal.json");
 
+export const departementList = Object.entries(departements).map(
+  ([code, { name, region }]) => {
+    return {
+      code,
+      name,
+      region,
+    };
+  }
+);
+export const regionList = Object.entries(regions).map(([code, name]) => {
+  return {
+    code,
+    name,
+  };
+});
+
 export const getRegionName = (code) => {
   return regions[code];
 };
@@ -30,3 +46,40 @@ export const getCommunesByCodePostal = async (cp) => {
   const codePostal = await codePostalDB;
   return codePostal[cp]?.communes;
 };
+
+export const createRegionOptions = (rows, options = { all: true }) => {
+  const opts = [];
+  if (options.all) {
+    opts.push({
+      label: "Toutes les régions",
+      value: null,
+    });
+  }
+  opts.push(
+    ...Object.entries(rows).map(([code, { name }]) => ({
+      value: parseInt(code),
+      label: name,
+    }))
+  );
+  return opts;
+};
+
+export const createDepartementOptions = (rows, options = { all: true }) => {
+  const opts = [];
+  if (options.all) {
+    opts.push({
+      label: "Tous les départements",
+      value: null,
+    });
+  }
+  opts.push(
+    ...Object.entries(rows).map(([code, { name }]) => ({
+      value: parseInt(code),
+      label: name,
+    }))
+  );
+  return opts;
+};
+
+export const regionOptions = createRegionOptions(regionList);
+export const departementOptions = createDepartementOptions(departementList);
