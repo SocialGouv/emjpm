3ee372428763edb88de037e76c3c49bae7c9016b
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2021-02-04 08:47:09 +0100

    refactor(departement): try fixing test

--- packages/api/knexfile.js
+++ packages/api/knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
     client: "pg",
     connection: process.env.DATABASE_URL || {
       host: "localhost",
-      user: "emjpm",
+      user: "postgres",
       password: "test",
       port: "5434",
       database: "emjpm",
