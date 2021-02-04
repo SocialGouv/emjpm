fe73a20cfc00be1c020a8e08658af3e0a62662da
Thomas Glatt <thomas.glatt@fabrique.social.gouv.fr>
2021-02-04 08:23:10 +0100

    refactor(departement): try fixing test

--- packages/api/src/services/getDepartement.js
+++ packages/api/src/services/getDepartement.js
@@ -4,7 +4,7 @@ const { getDepartementCode } = require("@emjpm/biz");
 module.exports = async (code_postal) => {
   const departementCode = getDepartementCode(code_postal);
   const departement = await Departement.query()
-    .where({ code: departementCode })
+    .where({ id: departementCode })
     .first();
   return departement;
 };
