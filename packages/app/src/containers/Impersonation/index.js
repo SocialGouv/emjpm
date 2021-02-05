2e254fc5a05d3c15f2349ee2e55e0ec70ba25584
devthejo <jo@surikat.pro>
2021-02-05 20:12:34 +0100

    fix(impersonation): remove on logout

--- packages/app/src/containers/Impersonation/index.js
+++ packages/app/src/containers/Impersonation/index.js
@@ -1,24 +1,13 @@
 import { useRef, useState, useCallback, useEffect } from "react";
 
 import { UserSecret } from "@styled-icons/fa-solid/UserSecret";
-import { impersonateLogout } from "~/user/Auth";
+import { useAuth, impersonateLogout } from "~/user/Auth";
 
-const isImpersonated = localStorage.getItem("impersonate");
+import style from "./style";
+
 export default function Impersonation({}) {
-  const style = {
-    position: "absolute",
-    top: 10,
-    left: "35%",
-    padding: "1em",
-    zIndex: 1050,
-    backgroundColor: "white",
-    borderColor: "#ff0000",
-    borderWidth: 1,
-    borderStyle: "solid",
-    borderRadius: 5,
-    textAlign: "center",
-  };
-  if (!isImpersonated) return null;
+  const { authStore } = useAuth();
+  if (!authStore.isImpersonated) return null;
   return (
     <div style={style}>
       <button onClick={impersonateLogout}>
