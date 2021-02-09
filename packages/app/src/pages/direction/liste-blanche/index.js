b39d04e9c0cbc5db114164e1eefd980f49634986
devthejo <jo@surikat.pro>
2021-02-09 22:58:45 +0100

    feat(filters): default direction departement lb

--- packages/app/src/pages/direction/liste-blanche/index.js
+++ packages/app/src/pages/direction/liste-blanche/index.js
@@ -1,16 +1,32 @@
+import { useContext } from "react";
 import { Box, Flex } from "rebass";
+
+import { BoxWrapper } from "~/components/Grid";
 
 import { FiltersContextSerializableProvider } from "~/containers/FiltersContextSerializable";
 import { LayoutDirection } from "~/containers/Layout";
 import { ListeBlanche } from "~/containers/ListeBlanche";
 import { ListeBlancheFilter } from "~/containers/ListeBlancheFilter";
 import { ListeBlancheSummary } from "~/containers/ListeBlancheSummary";
-import { BoxWrapper } from "~/components/Grid";
+import { UserContext } from "~/containers/UserContext";
 
 function ListBlanchePage() {
+  const user = useContext(UserContext);
+  const [direction] = user.directions;
+  const initialFilters = {};
+
+  if (direction.type === "departemental") {
+    initialFilters.departement = direction.departement.id;
+  } else if (direction.type === "regional") {
+    initialFilters.region = direction.region.id;
+  }
+
   return (
     <LayoutDirection>
-      <FiltersContextSerializableProvider useLocalStorage={true}>
+      <FiltersContextSerializableProvider
+        useLocalStorage={true}
+        initialFilters={initialFilters}
+      >
         <BoxWrapper mt={4} px={1}>
           <Flex flexDirection="column">
             <Box mb="2">
