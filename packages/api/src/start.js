f896977315b92199401370dcfa4bf17819b32184
devthejo <jo@surikat.pro>
2021-02-13 14:42:35 +0100

    feat(api): implement lightship

--- packages/api/src/start.js
+++ packages/api/src/start.js
@@ -1,10 +1,22 @@
 const app = require("./server.js");
 const logger = require("./utils/logger");
+const { createLightship } = require("lightship");
 
 const env = process.env.NODE_ENV || "development";
 const host = "0.0.0.0";
 const port = process.env.PORT || 4000;
 
-app.listen(port, host, () => {
-  logger.info(`Starting ${env} server at http://${host}:${port}`);
+const lightship = createLightship();
+
+const server = app
+  .listen(port, host, () => {
+    lightship.signalReady();
+    logger.info(`Starting ${env} server at http://${host}:${port}`);
+  })
+  .on("error", () => {
+    lightship.shutdown();
+  });
+
+lightship.registerShutdownHandler(() => {
+  server.close();
 });
