37fe04a4f020f8654f1e826260172848ac319ad9
devthejo <jo@surikat.pro>
2021-02-20 08:33:29 +0100

    fix(agrements): parse

--- packages/app/src/user/formatUserFromToken.js
+++ packages/app/src/user/formatUserFromToken.js
@@ -2,11 +2,10 @@ export function formatUserFromToken(currentUser) {
   if (currentUser) {
     const hasuraClaims = currentUser["https://hasura.io/jwt/claims"];
     const agrements = hasuraClaims["x-hasura-agrements"];
-
     return {
       __typename: "CurrentUser",
       agrements: agrements
-        ? JSON.parse(agrements.replace("{", "[").replace("}", "]"))
+        ? agrements.slice(1, agrements.length - 1).split(",")
         : [],
       id: hasuraClaims["x-hasura-user-id"],
       role: hasuraClaims["x-hasura-default-role"],
