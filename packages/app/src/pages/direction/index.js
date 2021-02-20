a558051d45c11d496ed4d4213453b9336c5e8b67
devthejo <jo@surikat.pro>
2021-02-20 08:49:54 +0100

    fix(direction): empty d list (national)

--- packages/app/src/pages/direction/index.js
+++ packages/app/src/pages/direction/index.js
@@ -28,9 +28,9 @@ export default function DirectionPage() {
   const [direction] = user.directions;
   const initialFilters = {};
 
-  if (direction.type === "departemental") {
+  if (direction?.type === "departemental") {
     initialFilters.departement = direction.departement.id;
-  } else if (direction.type === "regional") {
+  } else if (direction?.type === "regional") {
     initialFilters.region = direction.region.id;
   }
 
