be03ef425e8eb4cfdc5fce3fbd0603084c9b46d7
devthejo <jo@surikat.pro>
2021-03-12 10:41:01 +0100

    fix(niveau_ressource): zero by default

---
+++ packages/hasura/migrations/1615542043850_alter_table_public_mesure_ressources_alter_column_niveau_ressource/down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE ONLY "public"."mesure_ressources" ALTER COLUMN "niveau_ressource" DROP DEFAULT;
---
+++ packages/hasura/migrations/1615542043850_alter_table_public_mesure_ressources_alter_column_niveau_ressource/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE ONLY "public"."mesure_ressources" ALTER COLUMN "niveau_ressource" SET DEFAULT 0;
