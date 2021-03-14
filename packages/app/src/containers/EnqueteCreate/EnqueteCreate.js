39ea8ab60364c150e774c8ece1f2ed6f2e18ed3c
devthejo <jo@surikat.pro>
2021-03-14 16:41:30 +0100

    fix(perms): enquete create

--- packages/app/src/containers/EnqueteCreate/EnqueteCreate.js
+++ packages/app/src/containers/EnqueteCreate/EnqueteCreate.js
@@ -5,6 +5,7 @@ import { useHistory } from "react-router-dom";
 
 import yup from "~/validation-schemas/yup";
 import { Button, Field, InlineError, InputDate, InputYear } from "~/components";
+import useQueryReady from "~/hooks/useQueryReady";
 
 import { CREATE_ENQUETE } from "./mutations";
 import { ENQUETES } from "./queries";
@@ -12,9 +13,11 @@ import { ENQUETES } from "./queries";
 export function EnqueteCreate() {
   const client = useApolloClient();
   const history = useHistory();
-  const [createEnquete] = useMutation(CREATE_ENQUETE, {
+  const [createEnquete, { error, loading }] = useMutation(CREATE_ENQUETE, {
     refetchQueries: [{ query: ENQUETES }],
+    context: { headers: { "x-hasura-role": "direction_nationale" } },
   });
+  useQueryReady(loading, error);
   const formik = useFormik({
     initialValues: {
       endedAt: "",
@@ -23,15 +26,16 @@ export function EnqueteCreate() {
     onSubmit: async (values, formikHelpers) => {
       const { data } = await client.query({ query: ENQUETES });
       if (
-        data &&
-        data.enquetes.some((e) => Number(e.annee) === Number(values.year))
+        !(
+          data &&
+          data.enquetes.some((e) => Number(e.annee) === Number(values.year))
+        )
       ) {
-        history.push("/direction/enquetes");
+        await createEnquete({
+          variables: { endedAt: `${values.endedAt}`, year: `${values.year}` },
+        });
       }
 
-      await createEnquete({
-        variables: { endedAt: `${values.endedAt}`, year: `${values.year}` },
-      });
       formikHelpers.setSubmitting(false);
       history.push("/direction/enquetes");
     },
