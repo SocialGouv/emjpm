b9a54e985c15c2e498e85248a37713c0d28ba0c7
devthejo <jo@surikat.pro>
2021-03-29 17:10:40 +0200

    feat(ocmi-import): manual trigger button

--- packages/app/src/containers/MandataireOcmiMesureImport/MandataireOcmiMesureImportSwitcher.js
+++ packages/app/src/containers/MandataireOcmiMesureImport/MandataireOcmiMesureImportSwitcher.js
@@ -9,10 +9,11 @@ import { CheckCircleOutline } from "@styled-icons/material/CheckCircleOutline";
 import styled from "styled-components";
 
 import { MESURES_QUERY } from "~/containers/MesureList/queries";
-import { Text, Switch } from "~/components";
+import { Text, Switch, Button } from "~/components";
 
 import { SYNC_OCMI_ENABLE, IMPORT_OCMI_MESURES } from "./mutations";
 import { SYNC_OCMI_ENABLED } from "./queries";
+import { toast } from "react-toastify";
 
 const StyledCheckCircleOutline = styled(CheckCircleOutline)`
   margin-right: 10px;
@@ -31,7 +32,7 @@ function MandataireOcmiMesureImportSwitcher({
 
   const [importOcmiMesures] = useMutation(IMPORT_OCMI_MESURES);
 
-  const importMesure = useCallback(async () => {
+  const importMesures = useCallback(async () => {
     try {
       await importOcmiMesures({
         awaitRefetchQueries: true,
@@ -58,7 +59,16 @@ function MandataireOcmiMesureImportSwitcher({
   const [syncOCMIEnable] = useMutation(SYNC_OCMI_ENABLE, {
     onCompleted: async () => {
       if (syncEnabled) {
-        await importMesure();
+        try {
+          await importMesures();
+          toast.success(
+            "Synchronisation activée et import réalisé avec succès"
+          );
+        } catch (e) {
+          toast.error("Une erreur s'est produite");
+        }
+      } else {
+        toast.success("Synchronisation désactivée");
       }
     },
     refetchQueries: [
@@ -88,39 +98,68 @@ function MandataireOcmiMesureImportSwitcher({
         mandataireId,
       },
     });
-  }, [importMesure, mandataireId, syncEnabled, syncOCMIEnable]);
+  }, [importMesures, mandataireId, syncEnabled, syncOCMIEnable]);
+
+  const importMesuresNow = async () => {
+    try {
+      await importMesures();
+      toast.success("Import réalisé avec succès");
+    } catch (e) {
+      toast.error("Une erreur s'est produite");
+    }
+  };
 
   return (
     <Flex flexDirection="column">
-      <Flex justifyContent="left">
-        <Box mb={2} p={1}>
-          <Switch
-            name="sync-ocmi"
-            onChange={onChangeSyncEnabled}
-            isChecked={syncEnabled}
-            label={"Synchroniser mes mesures avec OCMI"}
-          />
-        </Box>
-      </Flex>
-      {syncEnabled || (
-        <Box mb={2} p={1}>
+      <Box m={2}>
+        <Box mb={50}>
           <Text mb="1" lineHeight="2">
-            {`Pour importer vos mesures de votre compte OCMI dans votre compte eMJPM, activer le bouton ci-dessus.`}
+            {`Pour importer maintenant vos mesures de votre compte OCMI dans votre compte eMJPM, activer le bouton ci-dessous.`}
+          </Text>
+          <Text mb="1" lineHeight="2">
+            {`Vous pourrez ensuite modifier vos mesures dans eMJPM.`}
           </Text>
           <Text mb="1" lineHeight="2">
             <StyledWarning size={18} />
             {`Toutes les mesures de votre compte eMJPM seront définitivement supprimées et remplacées par les mesures de votre compte OCMI.`}
           </Text>
+          <Flex justifyContent="center">
+            <Button onClick={importMesuresNow}>Importer maintenant</Button>
+          </Flex>
         </Box>
-      )}
-      {syncEnabled && (
-        <Box mb={2} p={1}>
+
+        <Flex justifyContent="left">
+          <Box mb={2}>
+            <Switch
+              name="sync-ocmi"
+              onChange={onChangeSyncEnabled}
+              isChecked={syncEnabled}
+              label={"Synchroniser automatiquement mes mesures avec OCMI"}
+            />
+          </Box>
+        </Flex>
+        {syncEnabled || (
+          <Box>
+            <Text mb="1" lineHeight="2">
+              {`Pour activer la synchronisation automatique et importer vos mesures de votre compte OCMI dans votre compte eMJPM, activer le bouton ci-dessus.`}
+            </Text>
+          </Box>
+        )}
+        {syncEnabled && (
+          <Box mb={2}>
+            <Text mb="1" lineHeight="2">
+              <StyledCheckCircleOutline size={18} />
+              {`Votre compte eMJPM est synchronisé avec votre compte OCMI, une mise à jour des données d'eMJPM s'effectuera chaque nuit.`}
+            </Text>
+          </Box>
+        )}
+        <Box mb={2}>
           <Text mb="1" lineHeight="2">
-            <StyledCheckCircleOutline size={18} />
-            {`Votre compte eMJPM est synchronisé avec votre compte OCMI, une mise à jour des données d'eMJPM s'effectuera chaque nuit.`}
+            <StyledWarning size={18} />
+            {`Toutes les mesures de votre compte eMJPM seront définitivement supprimées et remplacées par les mesures de votre compte OCMI lors de chaque synchronisation. N'activez pas cette option si vous comptez modifier vous même vos mesures, vos modifications seraient perdues lors de chaque synchronisation.`}
           </Text>
         </Box>
-      )}
+      </Box>
     </Flex>
   );
 }
