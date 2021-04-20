9bad099f68f29769fce382975a9f7228418f97af
devthejo <jo@surikat.pro>
2021-04-20 17:36:00 +0200

    fix(stats): departement filter right target

---
+++ packages/hasura/migrations/1618932886826_fix_view_departement_indicateur_inscrit/up.sql
@@ -0,0 +1,67 @@
+CREATE OR REPLACE VIEW "public"."view_departement_indicateur_inscrit" AS 
+SELECT d.id,
+    d.nom,
+    u.type,
+    count(DISTINCT m.id) AS count
+   FROM ((((mandataires m
+     JOIN users u ON ((((u.type)::text = 'individuel'::text) AND (m.user_id = u.id))))
+     JOIN lb_users lbu ON ((m.lb_user_id = lbu.id)))
+     JOIN lb_departements lbd ON ((m.lb_user_id = lbd.lb_user_id)))
+     JOIN departements d ON (((lbd.departement_code)::text = (d.id)::text)))
+  WHERE (u.active = true)
+  GROUP BY d.id, u.type
+UNION
+ SELECT d.id,
+    d.nom,
+    u.type,
+    count(DISTINCT m.id) AS count
+   FROM (((((mandataires m
+     JOIN users u ON (u.type = 'prepose' AND (m.user_id = u.id))))
+     JOIN lb_users lbu ON ((m.lb_user_id = lbu.id)))
+     JOIN lb_user_etablissements lbue ON ((m.lb_user_id = lbue.lb_user_id)))
+     JOIN etablissements etbl ON ((lbue.etablissement_id = etbl.id))
+     JOIN departements d ON (((etbl.departement_code)::text = (d.id)::text)))
+  WHERE (u.active = true)
+  GROUP BY d.id, u.type
+UNION
+ SELECT d.id,
+    d.nom,
+    u.type,
+    count(DISTINCT sm.service_id) AS count
+   FROM (((users u
+     JOIN service_members sm ON ((u.id = sm.user_id)))
+     JOIN services s ON ((sm.service_id = s.id)))
+     JOIN departements d ON (((s.departement_code)::text = (d.id)::text)))
+  WHERE (u.active = true)
+  GROUP BY d.id, u.type
+UNION
+ SELECT d.id,
+    d.nom,
+    u.type,
+    count(DISTINCT m.id) AS count
+   FROM (((tis t
+     JOIN departements d ON (((d.id)::text = (t.departement_code)::text)))
+     JOIN magistrat m ON ((m.ti_id = t.id)))
+     JOIN users u ON ((u.id = m.user_id)))
+  GROUP BY d.id, d.nom, u.type
+UNION
+ SELECT d.id,
+    d.nom,
+    u.type,
+    count(DISTINCT di.id) AS count
+   FROM ((direction di
+     JOIN departements d ON (((d.id)::text = (di.departement_code)::text)))
+     JOIN users u ON ((di.user_id = u.id)))
+  WHERE (((di.type)::text = 'departemental'::text) AND (u.active = true))
+  GROUP BY d.id, d.nom, u.type
+UNION
+ SELECT d.id,
+    d.nom,
+    u.type,
+    count(DISTINCT di.id) AS count
+   FROM (((direction di
+     JOIN regions r ON ((r.id = di.region_id)))
+     JOIN departements d ON ((d.id_region = r.id)))
+     JOIN users u ON ((di.user_id = u.id)))
+  WHERE (((di.type)::text = 'regional'::text) AND (u.active = true))
+  GROUP BY d.id, d.nom, u.type;
