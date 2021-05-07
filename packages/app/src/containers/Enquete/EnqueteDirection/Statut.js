aaa4ca6aab260293c0a9a05d57ad6d32e539f27b
devthejo <jo@surikat.pro>
2021-05-07 16:16:45 +0200

    feat(enquete): direction validate on statut page

--- packages/app/src/containers/Enquete/EnqueteDirection/Statut.js
+++ packages/app/src/containers/Enquete/EnqueteDirection/Statut.js
@@ -1,5 +1,11 @@
 import { Box, Button, Text } from "rebass";
 import { HeadingTitle } from "~/containers/HeadingTitle";
+import { VALIDATE_ENQUETE_REPONSE } from "./mutations";
+import { useMutation } from "@apollo/client";
+
+import { ENQUETE_WITH_REPONSE_STATUS } from "../queries";
+import useQueryReady from "~/hooks/useQueryReady";
+import useUser from "~/hooks/useUser";
 
 const typeLabels = {
   individuel: "Mandataire individuel",
@@ -18,6 +24,30 @@ export default function EnqueteDirectionStatut({
   enqueteReponse,
   goToFirstPage,
 }) {
+  const { id: enqueteId } = enquete;
+  const { id: reponseId } = enqueteReponse;
+  const { id: userId } = useUser();
+  const [validateEnqueteReponseMutation, { loading, error }] = useMutation(
+    VALIDATE_ENQUETE_REPONSE,
+    {
+      variables: {
+        reponseId,
+      },
+      refetchQueries: [
+        {
+          query: ENQUETE_WITH_REPONSE_STATUS,
+          variables: { enqueteId, userId, reponseId },
+        },
+      ],
+    }
+  );
+
+  useQueryReady(loading, error);
+
+  const validateEnqueteReponse = async () => {
+    await validateEnqueteReponseMutation();
+  };
+
   return (
     <Box py={"50px"}>
       <HeadingTitle textAlign="center">
@@ -32,13 +62,30 @@ export default function EnqueteDirectionStatut({
       </Box>
       <Box>
         <Box sx={{ lineHeight: "30px", marginTop: 4, textAlign: "center" }}>
-          <Text>
-            Pour visualiser ou modifier les réponses, cliquez sur le bouton
-            suivant:
-          </Text>
-          <Button onClick={() => goToFirstPage()} mx="auto" mt={30}>
-            Réviser
-          </Button>
+          {enqueteReponse.status === "submitted" && (
+            <Text>
+              Vous pouvez visualiser, modifier ou valider les réponses.
+            </Text>
+          )}
+          {enqueteReponse.status === "validated" && (
+            <Text>Vous pouvez visualiser ou modifier réponses.</Text>
+          )}
+          <Box>
+            <Button onClick={() => goToFirstPage()} mx="auto" mt={30}>
+              Accéder aux réponses
+            </Button>
+          </Box>
+          {enqueteReponse.status === "submitted" && (
+            <Box>
+              <Button
+                onClick={() => validateEnqueteReponse()}
+                mx="auto"
+                mt={30}
+              >
+                Valider
+              </Button>
+            </Box>
+          )}
         </Box>
       </Box>
     </Box>
