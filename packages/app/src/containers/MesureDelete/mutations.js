f272865e6b2fdc7f27d766ddab14106697b9070c
devthejo <jo@surikat.pro>
2021-05-24 22:21:38 +0200

    fix(refacto): mesures-update

--- packages/app/src/containers/MesureDelete/mutations.js
+++ packages/app/src/containers/MesureDelete/mutations.js
@@ -1,11 +1,7 @@
 import gql from "graphql-tag";
 
 export const DELETE_MESURE = gql`
-  mutation deleteMesure(
-    $id: Int!
-    mandataireId: $mandataireId
-    serviceId: $serviceId
-  ) {
+  mutation deleteMesure($id: Int!, $mandataireId: Int, $serviceId: Int) {
     delete_mesures(where: { id: { _eq: $id } }) {
       affected_rows
       returning {
