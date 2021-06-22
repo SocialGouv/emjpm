9e794e32010d2377e34b4be8237ea96052d82516
devthejo <jo@surikat.pro>
2021-06-22 10:01:18 +0200

    fix(mesures-count): nullable columns

--- packages/hasura/migrations/default/1624348114153_nullable_musures_counts/up.sql
+++ packages/hasura/migrations/default/1624348114153_nullable_musures_counts/up.sql
@@ -1,4 +1,4 @@
 alter table "public"."mandataires" alter column "mesures_en_cours" drop not null;
-alter table "public"."mandataires" alter column "mesures_last_update" drop not null;
+alter table "public"."mandataires" alter column "mesures_en_attente" drop not null;
 alter table "public"."service_antenne" alter column "mesures_in_progress" drop not null;
 alter table "public"."service_antenne" alter column "mesures_awaiting" drop not null;
