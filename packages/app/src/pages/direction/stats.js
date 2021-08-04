a666e734e3f2e29903d3cf3ec8124b7d8fd8d379
devthejo <jo@surikat.pro>
2021-08-04 20:31:49 +0200

    fix(dd): refine dashboard priorities

---
+++ packages/app/src/pages/direction/stats.js
@@ -0,0 +1,55 @@
+import { Box } from "rebass";
+
+import { AvailabilityMap } from "~/containers/DirectionAvailabilityMap";
+import { DirectionFilters } from "~/containers/DirectionFilters";
+import { DirectionStatsKPI } from "~/containers/DirectionIndicators";
+import { MandatairesActivity } from "~/containers/DirectionMandatairesActivity";
+import { MandatairesDisponibility } from "~/containers/DirectionMandatairesDisponibility";
+import { MandatairesSubNavigation } from "~/containers/DirectionMandatairesSubNavigation";
+import { FiltersContextSerializableProvider } from "~/containers/FiltersContextSerializable";
+import { LayoutDirection } from "~/containers/Layout";
+import useUser from "~/hooks/useUser";
+import { BoxWrapper, FlexWrapper, twoColumnStyle } from "~/components/Grid";
+
+export default function DirectionPage() {
+  const user = useUser();
+  const [direction] = user.directions;
+  const initialFilters = {};
+
+  if (direction?.type === "departemental") {
+    initialFilters.departement = direction.departement.id;
+  } else if (direction?.type === "regional") {
+    initialFilters.region = direction.region.id.toString();
+  }
+
+  return (
+    <FiltersContextSerializableProvider
+      useLocalStorage={true}
+      initialFilters={initialFilters}
+    >
+      <LayoutDirection>
+        <BoxWrapper mt={5} px="1">
+          <DirectionFilters />
+        </BoxWrapper>
+        <DirectionStatsKPI />
+        <FlexWrapper flexWrap={"wrap"} mt={5}>
+          <MandatairesSubNavigation />
+        </FlexWrapper>
+        <FlexWrapper flexWrap={"wrap"} mt={5}>
+          <Box sx={twoColumnStyle}>
+            <MandatairesDisponibility />
+          </Box>
+
+          <Box sx={twoColumnStyle}>
+            <MandatairesActivity />
+          </Box>
+        </FlexWrapper>
+        <FlexWrapper flexWrap={"wrap"} mt={5}>
+          <Box sx={twoColumnStyle}>
+            <AvailabilityMap />
+          </Box>
+        </FlexWrapper>
+      </LayoutDirection>
+    </FiltersContextSerializableProvider>
+  );
+}
