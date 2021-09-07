df1ce0fb78e9f05e3e7ede185e9288cd9f386f03
devthejo <jo@surikat.pro>
2021-09-07 12:21:39 +0200

    fix(migration): create or replace function

--- packages/hasura/migrations/default/1630086481013_mesure_email_magistrat/up.sql
+++ packages/hasura/migrations/default/1630086481013_mesure_email_magistrat/up.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION mesure_email_magistrat(mesure_row mesures)
+CREATE OR REPLACE FUNCTION mesure_email_magistrat(mesure_row mesures)
 RETURNS TEXT AS $$
   SELECT users.email FROM users, magistrat WHERE magistrat.id = mesure_row.magistrat_id AND users.id = magistrat.user_id
 $$ LANGUAGE sql STABLE;
