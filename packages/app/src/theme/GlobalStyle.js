9882ddd11f3b848e59cc896848caba70f7abe041
devthejo <jo@surikat.pro>
2021-09-24 17:29:55 +0200

    fix(champ-coupe): fix #2652

--- packages/app/src/theme/GlobalStyle.js
+++ packages/app/src/theme/GlobalStyle.js
@@ -82,6 +82,12 @@ const styles = css`
     position: relative;
     text-decoration: none;
   }
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active{
+      -webkit-box-shadow: 0 0 0 30px white inset !important;
+  }
 
 `;
 
