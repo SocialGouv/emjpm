1b6f24a92fcb23c388d52a0d8e718c6c54cfb80b
devthejo <jo@surikat.pro>
2021-10-01 18:05:00 +0200

    fix(dedup): rg + mesure ti-siret or ti-id

--- packages/api/src/utils/dedup-mesures.js
+++ packages/api/src/utils/dedup-mesures.js
@@ -3,14 +3,15 @@ const { normalizeNumeroRG } = require("~/utils/numero-rg");
 module.exports = function dedupMesures(rows) {
   return Array.from(
     new Set(
-      rows.map(({ numero_rg, tribunal_siret }) =>
-        [normalizeNumeroRG(numero_rg), tribunal_siret].join(".")
+      rows.map(({ numero_rg, tribunal_siret, ti_id }) =>
+        [normalizeNumeroRG(numero_rg), tribunal_siret || ti_id].join(".")
       )
     )
   ).map((uniqHash) => {
     return rows.find(
-      ({ numero_rg, tribunal_siret }) =>
-        [normalizeNumeroRG(numero_rg), tribunal_siret].join(".") === uniqHash
+      ({ numero_rg, tribunal_siret, ti_id }) =>
+        [normalizeNumeroRG(numero_rg), tribunal_siret || ti_id].join(".") ===
+        uniqHash
     );
   });
 };
