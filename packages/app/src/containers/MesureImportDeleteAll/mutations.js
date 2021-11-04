4643e20fe6679dad40c108635efececff766f29d
devthejo <jo@surikat.pro>
2021-11-04 16:49:22 +0100

    fix(delete-all): fix import delete all for service

--- packages/app/src/containers/MesureImportDeleteAll/mutations.js
+++ packages/app/src/containers/MesureImportDeleteAll/mutations.js
@@ -4,7 +4,20 @@ export const DELETE_ALL_MESURES = gql`
   mutation deleteAllMesures($mandataireId: Int, $serviceId: Int) {
     delete_mesures(
       where: {
-        mandataire_id: { _eq: $mandataireId }
+        _or: [
+          {
+            _and: [
+              { mandataire_id: { _eq: $mandataireId } }
+              { mandataire_id: { _is_null: false } }
+            ]
+          }
+          {
+            _and: [
+              { service_id: { _eq: $serviceId } }
+              { service_id: { _is_null: false } }
+            ]
+          }
+        ]
         status: { _neq: en_attente }
       }
     ) {
