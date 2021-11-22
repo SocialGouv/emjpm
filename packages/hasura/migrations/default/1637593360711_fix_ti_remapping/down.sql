0792157d018c07bb6438ffd88da7c7f8c6ddd547
devthejo <jo@surikat.pro>
2021-11-22 16:03:06 +0100

    fix(ti-remapping): using pgsql query

---
+++ packages/hasura/migrations/default/1637593360711_fix_ti_remapping/down.sql
@@ -0,0 +1,12 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- UPDATE mesures m SET ti_id = (SELECT COALESCE(t.actual_tribunal_id, t.id) FROM tis t WHERE t.id=m.ti_id)
+-- WHERE NOT EXISTS (
+--    SELECT 1 FROM mesures
+--    JOIN tis t ON t.id = ti_id
+--    WHERE
+--     mandataire_id = m.mandataire_id
+--     AND service_id = m.service_id
+--     AND numero_rg = m.numero_rg
+--     AND ti_id = COALESCE(t.actual_tribunal_id, t.id)
+-- );
