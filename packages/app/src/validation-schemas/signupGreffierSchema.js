e36ab742eefa048e99ede10c75ea14b6c1674e16
devthejo <jo@surikat.pro>
2021-11-23 17:33:26 +0100

    fix(nullable): signup greffier et magistrat

--- packages/app/src/validation-schemas/signupGreffierSchema.js
+++ packages/app/src/validation-schemas/signupGreffierSchema.js
@@ -2,7 +2,7 @@ import yup from "./yup";
 
 const signupGreffierSchema = yup.object().shape({
   cabinet: yup.string(),
-  ti: yup.string().required(),
+  ti: yup.string().nullable().required(),
 });
 
 export { signupGreffierSchema };
