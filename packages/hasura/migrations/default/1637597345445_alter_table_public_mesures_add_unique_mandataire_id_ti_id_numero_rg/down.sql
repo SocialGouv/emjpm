d796b648defe0e94f6e4e9c5125da9963ff0eeb2
devthejo <jo@surikat.pro>
2021-12-07 11:48:02 +0100

    fix(migration): dedup double pass

--- packages/hasura/migrations/default/1637597345445_alter_table_public_mesures_add_unique_mandataire_id_ti_id_numero_rg/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."mesures" drop constraint "mesures_mandataire_id_ti_id_numero_rg_key";
--- packages/hasura/migrations/default/1637597345445_alter_table_public_mesures_add_unique_mandataire_id_ti_id_numero_rg/up.sql
+++ packages/hasura/migrations/default/1637597345445_alter_table_public_mesures_add_unique_mandataire_id_ti_id_numero_rg/up.sql
@@ -1,1 +1,37 @@
+WITH ct AS
+(
+  SELECT
+    id,
+    ROW_NUMBER() OVER (PARTITION BY mandataire_id, numero_rg ORDER BY id) rn
+  FROM
+    mesures
+)
+UPDATE
+  mesures
+SET
+  numero_rg = numero_rg || CASE WHEN ct.rn = 1 THEN '' ELSE ('-' || (ct.rn-1)::text) END
+FROM
+  ct
+WHERE
+  ct.id = mesures.id
+  AND mandataire_id IS NOT NULL;
+WITH ct AS
+(
+  SELECT
+    id,
+    ROW_NUMBER() OVER (PARTITION BY mandataire_id, numero_rg ORDER BY id) rn
+  FROM
+    mesures
+)
+UPDATE
+  mesures
+SET
+  numero_rg = numero_rg || CASE WHEN ct.rn = 1 THEN '' ELSE ('-' || (ct.rn-1)::text) END
+FROM
+  ct
+WHERE
+  ct.id = mesures.id
+  AND mandataire_id IS NOT NULL;
+
+alter table "public"."mesures" drop constraint if exists "mesures_mandataire_id_ti_id_numero_rg_key";
 alter table "public"."mesures" add constraint "mesures_mandataire_id_ti_id_numero_rg_key" unique ("mandataire_id", "ti_id", "numero_rg");
