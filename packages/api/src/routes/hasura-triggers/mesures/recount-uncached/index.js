6e508fb8fcb9c173e3c773423c9eaaf8c2bacd2a
devthejo <jo@surikat.pro>
2021-12-14 17:40:23 +0100

    fix(trx): move recount in transaction

--- packages/api/src/routes/hasura-triggers/mesures/recount-uncached/index.js
+++ packages/api/src/routes/hasura-triggers/mesures/recount-uncached/index.js
@@ -3,36 +3,41 @@ const logger = require("~/utils/logger");
 
 module.exports = async (_req, res) => {
   logger.info(`[TRIGGER_MESURES_RECOUNT_UNCACHED] start`);
-  const mandatairesCountMesures = knex.raw(`
-UPDATE
-    mandataires
-SET
-    mesures_en_cours_cached = (SELECT COUNT(*) FROM mesures m WHERE m.mandataire_id = mandataires.id AND m.status = 'en_cours'),
-    mesures_en_attente_cached = (SELECT COUNT(*) FROM mesures m WHERE m.mandataire_id = mandataires.id AND m.status = 'en_attente')
-WHERE mandataires.mesures_en_cours_cached IS NULL OR mandataires.mesures_en_attente_cached IS NULL;
-`);
-  const servicesCountMesures = knex.raw(`
-UPDATE
-    services
-SET
-    mesures_in_progress_cached = (SELECT COUNT(*) FROM mesures m WHERE m.service_id = services.id AND m.status = 'en_cours'),
-    mesures_awaiting_cached = (SELECT COUNT(*) FROM mesures m WHERE m.service_id = services.id AND m.status = 'en_attente')
-WHERE services.mesures_in_progress_cached IS NULL OR services.mesures_awaiting_cached IS NULL;
-`);
-  const serviceAntenneCountMesures = knex.raw(`
-UPDATE
-    service_antenne
-SET
-    mesures_in_progress_cached = (SELECT COUNT(*) FROM mesures m WHERE m.antenne_id = service_antenne.id AND m.status = 'en_cours'),
-    mesures_awaiting_cached = (SELECT COUNT(*) FROM mesures m WHERE m.antenne_id = service_antenne.id AND m.status = 'en_attente'),
-    dispo_cached = service_antenne.mesures_max-((SELECT COUNT(*) FROM mesures m WHERE m.antenne_id = service_antenne.id AND m.status = 'en_attente')+(SELECT COUNT(*) FROM mesures m WHERE m.antenne_id = service_antenne.id AND m.status = 'en_cours'))
-WHERE service_antenne.mesures_in_progress_cached IS NULL OR service_antenne.mesures_awaiting_cached IS NULL;
-`);
-  await Promise.all([
-    mandatairesCountMesures,
-    servicesCountMesures,
-    serviceAntenneCountMesures,
-  ]);
+  await knex.transaction(async (trx) => {
+    const promises = [];
+    promises.push(
+      trx.raw(`
+      UPDATE
+          mandataires
+      SET
+          mesures_en_cours_cached = (SELECT COUNT(*) FROM mesures m WHERE m.mandataire_id = mandataires.id AND m.status = 'en_cours'),
+          mesures_en_attente_cached = (SELECT COUNT(*) FROM mesures m WHERE m.mandataire_id = mandataires.id AND m.status = 'en_attente')
+      WHERE mandataires.mesures_en_cours_cached IS NULL OR mandataires.mesures_en_attente_cached IS NULL;
+    `)
+    );
+    promises.push(
+      trx.raw(`
+      UPDATE
+          services
+      SET
+          mesures_in_progress_cached = (SELECT COUNT(*) FROM mesures m WHERE m.service_id = services.id AND m.status = 'en_cours'),
+          mesures_awaiting_cached = (SELECT COUNT(*) FROM mesures m WHERE m.service_id = services.id AND m.status = 'en_attente')
+      WHERE services.mesures_in_progress_cached IS NULL OR services.mesures_awaiting_cached IS NULL;
+    `)
+    );
+    promises.push(
+      trx.raw(`
+      UPDATE
+          service_antenne
+      SET
+          mesures_in_progress_cached = (SELECT COUNT(*) FROM mesures m WHERE m.antenne_id = service_antenne.id AND m.status = 'en_cours'),
+          mesures_awaiting_cached = (SELECT COUNT(*) FROM mesures m WHERE m.antenne_id = service_antenne.id AND m.status = 'en_attente'),
+          dispo_cached = service_antenne.mesures_max-((SELECT COUNT(*) FROM mesures m WHERE m.antenne_id = service_antenne.id AND m.status = 'en_attente')+(SELECT COUNT(*) FROM mesures m WHERE m.antenne_id = service_antenne.id AND m.status = 'en_cours'))
+      WHERE service_antenne.mesures_in_progress_cached IS NULL OR service_antenne.mesures_awaiting_cached IS NULL;
+    `)
+    );
+    await Promise.all(promises);
+  });
 
   logger.info(`[TRIGGER_MESURES_RECOUNT_UNCACHED] end`);
 
