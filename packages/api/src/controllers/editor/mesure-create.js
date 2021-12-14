c37db5093e7751e53576c61deeb90170ef3121a2
devthejo <jo@surikat.pro>
2021-12-14 17:31:41 +0100

    fix(trx): temp disable buggy transactions

--- packages/api/src/controllers/editor/mesure-create.js
+++ packages/api/src/controllers/editor/mesure-create.js
@@ -1,6 +1,6 @@
 const { validationResult } = require("express-validator");
 
-const knex = require("~/db/knex");
+// const knex = require("~/db/knex");
 
 const { sanitizeMesureProperties } = require("~/utils/mesure");
 const { validateNumeroRG } = require("~/utils/numero-rg");
@@ -37,24 +37,27 @@ const mesureCreate = async (req, res) => {
   }
 
   try {
-    let mesureQueryResult;
-    await knex.transaction(async function (trx) {
-      try {
-        mesureQueryResult = await saveMesure(
-          {
-            antenneId,
-            datas: body,
-            editorId,
-            serviceOrMandataire,
-            ti,
-            type,
-          },
-          trx
-        );
-        await trx.commit();
-      } catch (e) {
-        await trx.rollback(e);
-      }
+    // let mesureQueryResult;
+    // await knex.transaction(async function (trx) {
+    //   mesureQueryResult = await saveMesure(
+    //     {
+    //       antenneId,
+    //       datas: body,
+    //       editorId,
+    //       serviceOrMandataire,
+    //       ti,
+    //       type,
+    //     },
+    //     trx
+    //   );
+    // });
+    const mesureQueryResult = await saveMesure({
+      antenneId,
+      datas: body,
+      editorId,
+      serviceOrMandataire,
+      ti,
+      type,
     });
 
     return res.status(201).json(sanitizeMesureProperties(mesureQueryResult));
