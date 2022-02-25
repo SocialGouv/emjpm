454b09bd9ebb622d55880f7cc5d52b98f8ea9296
devthejo <jo@surikat.pro>
2022-02-25 19:11:58 +0100

    fix(mesure): date-protection delete rule fix #2812

--- packages/api/src/routes/hasura-actions/mesure-etat/actions/updateCurrentDataOfMesure.js
+++ packages/api/src/routes/hasura-actions/mesure-etat/actions/updateCurrentDataOfMesure.js
@@ -4,38 +4,42 @@ const getGeoDatas = require("~/services/getGeoDatas");
 const getDepartement = require("~/services/getDepartement");
 
 module.exports = async function updateCurrentDataOfMesure(mesureId) {
-  const {
-    nature_mesure,
-    champ_mesure,
-    lieu_vie,
-    ville,
-    code_postal,
-    pays,
-    date_changement_etat,
-  } = await MesureEtat.query()
-    .findOne({ mesure_id: mesureId })
-    .orderBy("date_changement_etat", "desc");
+  const mesureEtats = await MesureEtat.query()
+    .where({ mesure_id: mesureId })
+    .orderBy("date_changement_etat", "asc");
+
+  let date_changement_etat;
+  let natureLast;
+  let champLast;
+  for (const mesureEtat of mesureEtats) {
+    if (
+      natureLast !== mesureEtat.nature_mesure ||
+      champLast !== mesureEtat.champ_mesure
+    ) {
+      natureLast = mesureEtat.nature_mesure;
+      champLast = mesureEtat.champ_mesure;
+      date_changement_etat = mesureEtat.date_changement_etat;
+    }
+  }
+
+  const { nature_mesure, champ_mesure, lieu_vie, ville, code_postal, pays } =
+    mesureEtats[mesureEtats.length - 1];
 
   const geodata = (await getGeoDatas(code_postal, ville)) || {};
   const departement = code_postal ? await getDepartement(code_postal) : {};
 
-  const { nature_mesure: natureOrigin, champ_mesure: champOrigin } =
-    await Mesure.query().findOne({ id: mesureId });
-
-  const updateFields = {
-    champ_mesure,
-    code_postal,
-    departement_code: departement.id,
-    latitude: geodata.latitude,
-    lieu_vie,
-    longitude: geodata.longitude,
-    nature_mesure,
-    pays,
-    ville,
-  };
-  if (natureOrigin !== nature_mesure || champOrigin !== champ_mesure) {
-    updateFields.date_protection_en_cours = date_changement_etat;
-  }
-
-  await Mesure.query().update(updateFields).where({ id: mesureId });
+  await Mesure.query()
+    .update({
+      champ_mesure,
+      code_postal,
+      date_protection_en_cours: date_changement_etat,
+      departement_code: departement.id,
+      latitude: geodata.latitude,
+      lieu_vie,
+      longitude: geodata.longitude,
+      nature_mesure,
+      pays,
+      ville,
+    })
+    .where({ id: mesureId });
 };
