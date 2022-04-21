671d78a31ac677e6990ca056569beb5e22f14a90
devthejo <jo@surikat.pro>
2022-04-21 11:10:05 +0200

    fix: hasura + seed

---
+++ packages/hasura/migrations/default/1650531969007_cleanfix_hasura/up.sql
@@ -0,0 +1,17 @@
+DROP FUNCTION IF EXISTS hdb_catalog."notify_hasura_lb_update_lb_user_departement_DELETE"() CASCADE;
+
+DROP FUNCTION IF EXISTS hdb_catalog."notify_hasura_lb_update_lb_user_departement_INSERT"() CASCADE;
+
+DROP FUNCTION IF EXISTS hdb_catalog."notify_hasura_lb_update_lb_user_departement_UPDATE"() CASCADE;
+
+DROP FUNCTION IF EXISTS hdb_catalog."notify_hasura_lb_update_lb_user_etablissement_DELETE"() CASCADE;
+
+DROP FUNCTION IF EXISTS hdb_catalog."notify_hasura_lb_update_lb_user_etablissement_INSERT"() CASCADE;
+
+DROP FUNCTION IF EXISTS hdb_catalog."notify_hasura_lb_update_lb_user_etablissement_UPDATE"() CASCADE;
+
+DROP FUNCTION IF EXISTS hdb_catalog."notify_hasura_lb_update_service_departements_DELETE"() CASCADE;
+
+DROP FUNCTION IF EXISTS hdb_catalog."notify_hasura_lb_update_service_departements_INSERT"() CASCADE;
+
+DROP FUNCTION IF EXISTS hdb_catalog."notify_hasura_lb_update_service_departements_UPDATE"() CASCADE;
