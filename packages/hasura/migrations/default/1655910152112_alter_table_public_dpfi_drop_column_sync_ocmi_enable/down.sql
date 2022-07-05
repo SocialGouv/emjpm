9b2296615769fa7a074e861f7eab8deaacf4faa1
unicscode <lounas.koceila@gmail.com>
2022-07-05 17:47:37 +0200

    feat(profil-dpfi): apply migrations

--- packages/hasura/migrations/default/1655910152112_alter_table_public_dpfi_drop_column_sync_ocmi_enable/down.sql
+++
@@ -1,3 +0,0 @@
-alter table "public"."dpfi" alter column "sync_ocmi_enable" set default false;
-alter table "public"."dpfi" alter column "sync_ocmi_enable" drop not null;
-alter table "public"."dpfi" add column "sync_ocmi_enable" bool;
