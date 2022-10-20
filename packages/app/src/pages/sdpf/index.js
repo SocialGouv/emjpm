ad9582ccb687fa1b9eda125d8eb8d05093292c4f
Koceila LOUNAS <lounas.koceila@gmail.com>
2022-10-20 17:09:49 +0200

    fix(app): display message to sdpf profil (#2880)

--- packages/app/src/pages/sdpf/index.js
+++ packages/app/src/pages/sdpf/index.js
@@ -1,4 +1,4 @@
-import { Flex } from "rebass";
+import { Flex, Box, Text, Link, Card } from "rebass";
 
 import { LayoutSdpf } from "~/containers/Layout";
 
@@ -11,7 +11,37 @@ function ServiceBoardView() {
       <SkipToContent skipTo="vos_indicateurs" />
       <LayoutSdpf>
         <BoxWrapper mt={3} px="1">
-          <Flex></Flex>
+          <Flex>
+            <Card mb="1" role="alert" mr="2">
+              <Text
+                as="p"
+                sx={{
+                  fontSize: "1.1rem",
+                  fontWeight: "normal",
+                  lineHeight: "1.5",
+                }}
+              >
+                Bonjour;
+                <br />
+                En tant que Service DPF, aucune fonctionnalité concernant votre
+                profil n’est disponible sur eMJPM. L’inscription sur eMJPM vous
+                permet uniquement d’être authentifiés sur eFSM. Vous pouvez y
+                accéder directement en suivant{" "}
+                <Link
+                  title="accéder à eFSM"
+                  ariaLabel="accéder à eFSM"
+                  href="https://efsm.pjm.social.gouv.fr/"
+                  target="_blank"
+                  sx={{
+                    textDecoration: "underline",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {"ce lien vers eFSM."}
+                </Link>
+              </Text>
+            </Card>
+          </Flex>
         </BoxWrapper>
       </LayoutSdpf>
     </>
